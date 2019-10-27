4cae26bea8fe93e2b93d60abc815149508e2db0f
Barak Amar <barak.amar@treeverse.io>
2019-10-27 08:48:55 +0200

    initial journal

---
+++ index/journal/journal.go
@@ -0,0 +1,42 @@
+package journal
+
+import (
+	"github.com/apple/foundationdb/bindings/go/src/fdb/tuple"
+	"versio-index/db"
+	"versio-index/ident"
+)
+
+type OpCode int
+
+type Event struct {
+	Time        int64             // logical timestamp derived from the fdb transaction ID
+	RepoId      string            // requesting repo
+	ClientId    string            // requesting Client
+	Operation   OpCode            // what did we perform?
+	Path        string            // file system path
+	Metadata    map[string]string // as exist in the object or tree entry
+	LogicalId   string            // say, object ID
+	PhysicalIds []string          // actual blocks on the backing block store
+}
+
+func (e Event) Identity() []byte {
+	return []byte(ident.MultiHash(e.ClientId, e.RepoId, e.Path))
+}
+
+type Journal interface {
+	Log(Event) error
+}
+
+type KVJournal struct {
+	store         db.Store
+	TopicName  string
+	Partitions int
+}
+
+func (kv *KVJournal) Log(e Event) error {
+	_, err := kv.store.Transact([]tuple.TupleElement, func(q db.Query) ( interface{}, error) {
+
+		return nil, nil
+	})
+	return err
+}
