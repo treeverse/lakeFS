0fa13080ca56911c114144a0d66f3f2a47ca3490
Barak Amar <barak.amar@treeverse.io>
2020-01-06 17:23:32 +0200

    Update local.go

--- block/local.go
+++ block/local.go
@@ -5,8 +5,6 @@ import (
 	"io"
 	"os"
 	"path"
-
-	"golang.org/x/sys/unix"
 )
 
 type LocalFSAdapter struct {
@@ -21,7 +19,9 @@ func NewLocalFSAdapter(path string) (Adapter, error) {
 	if !stt.IsDir() {
 		return nil, fmt.Errorf("path provided is not a valid directory")
 	}
-	if unix.Access(path, unix.W_OK) != nil {
+
+	//if unix.Access(path, unix.W_OK) != nil {
+	if ! isDirectoryWritable(path) {
 		return nil, fmt.Errorf("path provided is not writable")
 	}
 	return &LocalFSAdapter{path: path}, nil
@@ -48,3 +48,21 @@ func (l *LocalFSAdapter) Get(identifier string) (reader ReadAtCloser, err error)
 	}
 	return f, nil
 }
+
+func isDirectoryWritable(path string) bool {
+	// test ability to write to directory.
+	// as there is no simple way to test this in windows, I prefer the "brute force" method
+	// of creating s dummy file. will work in any OS.
+	// speed is not an issue, as this will be activated very few times during startup
+
+	fileName := path + "dummy.tmp"
+	os.Remove(fileName)
+	file,err := os.Create(fileName)
+	if err == nil {
+		file.Close()
+		os.Remove(fileName)
+		return true
+	} else {
+		return false}
+}
+
