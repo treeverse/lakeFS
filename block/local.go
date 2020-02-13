592f4629cd49a5572eebd7898772179675d00277
Barak Amar <barak.amar@treeverse.io>
2020-02-13 16:28:18 +0200

    fixed directory readablitiy test

--- block/local.go
+++ block/local.go
@@ -62,13 +62,14 @@ func (l *LocalFSAdapter) GetRange(_ string, identifier string, start int64, end 
 	}
 	return f, nil
 }
-func isDirectoryWritable(path string) bool {
+
+func isDirectoryWritable(pth string) bool {
 	// test ability to write to directory.
 	// as there is no simple way to test this in windows, I prefer the "brute force" method
 	// of creating s dummy file. will work in any OS.
 	// speed is not an issue, as this will be activated very few times during startup
 
-	fileName := path + "dummy.tmp"
+	fileName := path.Join(pth, "dummy.tmp")
 	os.Remove(fileName)
 	file, err := os.Create(fileName)
 	if err == nil {
