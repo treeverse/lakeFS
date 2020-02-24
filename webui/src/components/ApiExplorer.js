c2fc88e7ed4f42155c9025434e7981dceeee824c
Oz Katz <ozkatz100@gmail.com>
2020-02-24 21:52:06 +0200

    basic repo and branch CURD operations

--- webui/src/components/ApiExplorer.js
+++ webui/src/components/ApiExplorer.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
-import RepoList from "./RepositoryList";
+import {Route, Switch, useRouteMatch} from "react-router-dom";
+
+import RepositoryList from "./RepositoryList";
+import RepositoryCreate from "./RepositoryCreate";
+import BranchList from "./BranchList";
+import BranchCreate  from "./BranchCreate";
 
 
 export const ApiExplorer = () => {
+    let { path } = useRouteMatch();
+
     return (
-        <RepoList/>
+        <Switch>
+            <Route exact path={path}>
+                <RepositoryList/>
+            </Route>
+            <Route path={`${path}/create`}>
+                <RepositoryCreate/>
+            </Route>
+            <Route path={`${path}/:repoId/branches/create`}>
+                <BranchCreate/>
+            </Route>
+            <Route path={`${path}/:repoId`}>
+                <BranchList/>
+            </Route>
+        </Switch>
     );
-}
+};
