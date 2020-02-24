b46d737b42451775182a2305b5d8578167a29740
Barak Amar <barak.amar@treeverse.io>
2020-02-24 13:10:25 +0200

    removed unused code

--- webui/src/store/index.js
+++ webui/src/store/index.js
@@ -4,8 +4,6 @@ import thunkMiddleware from 'redux-thunk'
 import auth from './auth';
 import repositories from './repositories';
 
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
 const reducer = combineReducers({ auth, repositories });
 
 const store = createStore(reducer, compose(applyMiddleware(thunkMiddleware)));
