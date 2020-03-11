d41a9bfcbee5ceab94386a37b21c3d6d81347d4a
Oz Katz <ozkatz100@gmail.com>
2020-03-11 18:52:22 +0200

    pagination for results in tree
    Former-commit-id: d18f5263b4fb691bc2c94d22914dddc07b0be67f

--- webui/src/store/objects.js
+++ webui/src/store/objects.js
@@ -1,5 +1,5 @@
 import * as async from "./async";
-import {OBJECTS_DELETE, OBJECTS_LIST_TREE, OBJECTS_UPLOAD} from "../actions/objects";
+import {OBJECTS_DELETE, OBJECTS_LIST_TREE, OBJECTS_LIST_TREE_PAGINATE, OBJECTS_UPLOAD} from "../actions/objects";
 
 const initialState = {
     list: async.initialState,
@@ -14,6 +14,27 @@ export default (state = initialState, action) => {
         upload: async.actionReduce(OBJECTS_UPLOAD, state.upload, action),
         delete: async.actionReduce(OBJECTS_DELETE, state.delete, action),
     };
+
+    state.list  = async.reduce(
+        OBJECTS_LIST_TREE_PAGINATE, state.list, action,
+        (listState) => {
+            return {
+                ...listState,
+                payload: listState.payload, // retain original payload
+            };
+        },
+        (listState, action) => {
+            return {
+                loading: false,
+                payload: {
+                    // add results to current results
+                    results: [...listState.payload.results, ...action.payload.results],
+                    pagination: action.payload.pagination,
+                },
+                error: null,
+            };
+        }
+    );
 
     switch (action.type) {
         default:
