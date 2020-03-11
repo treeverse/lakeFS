7c0f0d91cbc597d590577f2d2269e6697c835ebf
Oz Katz <ozkatz100@gmail.com>
2020-03-11 18:52:22 +0200

    pagination for results in tree
    Former-commit-id: 2b5b938cc7549b683b3752807cafeb51e305ea66

--- webui/src/actions/objects.js
+++ webui/src/actions/objects.js
@@ -2,16 +2,25 @@ import * as api from './api';
 import {AsyncActionType} from "./request";
 
 export const
+    PAGINATION_AMOUNT = 1000,
     OBJECTS_LIST_TREE = new AsyncActionType('OBJECTS_GET_TREE'),
+    OBJECTS_LIST_TREE_PAGINATE = new AsyncActionType('OBJECTS_GET_TREE_PAGINATE'),
     OBJECTS_UPLOAD = new AsyncActionType('OBJECTS_UPLOAD'),
     OBJECTS_DELETE = new AsyncActionType('OBJECTS_DELETE');
 
 
-export const listTree = (repoId, branchId, tree, after = "", amount = 1000) => {
+export const listTree = (repoId, branchId, tree, amount = PAGINATION_AMOUNT) => {
     return OBJECTS_LIST_TREE.execute(async () => {
+        return await api.objects.list(repoId, branchId, tree, "", amount);
+    });
+};
+
+export const listTreePaginate = (repoId, branchId, tree, after, amount = PAGINATION_AMOUNT) => {
+    return OBJECTS_LIST_TREE_PAGINATE.execute(async () => {
         return await api.objects.list(repoId, branchId, tree, after, amount);
     });
 };
+
 
 export const upload = (repoId, branchId, path, fileObject) => {
     return OBJECTS_UPLOAD.execute(async () => {
