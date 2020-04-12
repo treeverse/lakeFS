2d6988e6da2cb47810dda0f2358de5a9920af444
tzahij <tzahij@hotmail.com>
2020-04-12 14:20:18 +0300

    commit after innitial tests
    Former-commit-id: 34a8e15ba722599bdc5cf05e604b7cd53fa36199

--- upload/objectWriter.go
+++ upload/objectWriter.go
@@ -1,7 +1,9 @@
 package upload
 
 import (
+	"crypto/md5"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/treeverse/lakefs/block"
@@ -22,51 +24,66 @@ type Blob struct {
 	Size     int64
 }
 
-type sha256Reader struct {
+type HashingReader struct {
 	sha256         hash.Hash
+	md5            hash.Hash
 	originalReader io.Reader
 	copiedSize     int64
 }
 
-func (s *sha256Reader) Read(p []byte) (int, error) {
+func (s *HashingReader) Read(p []byte) (int, error) {
 	len, err := s.originalReader.Read(p)
 	if len > 0 {
 		s.sha256.Write(p[0:len])
+		s.md5.Write(p[0:len])
 		s.copiedSize += int64(len)
 	}
 	return len, err
 }
 
-func newsha256Reader(body io.Reader) (s *sha256Reader) {
-	s = new(sha256Reader)
+func newHashingReaderReader(body io.Reader) (s *HashingReader) {
+	s = new(HashingReader)
 	s.sha256 = sha256.New()
+	s.md5 = md5.New()
 	s.originalReader = body
 
 	return
 }
 
-func (s *sha256Reader) Seek(offset int64, whence int) (int64, error) {
+func (s *HashingReader) Seek(offset int64, whence int) (int64, error) {
 	panic("Seek was called while reading in upload\n")
 }
 
 func WriteBlob(index index.Index, bucketName string, body io.Reader, adapter block.Adapter) (*Blob, error) {
 	// handle the upload itself
-
-	shaReader := newsha256Reader(body)
+	hashReader := newHashingReaderReader(body)
 	objName := uuidAsHex()
-	err := adapter.Put(bucketName, objName, shaReader)
+	err := adapter.Put(bucketName, objName, hashReader)
 	if err != nil {
 		panic("could not copy object to destination\n")
 	}
-	block := new(model.Block)
-	dedupId := shaReader.sha256.Sum(nil)
-	objName = index.CreateDedupEntryIfNone(bucketName, dedupId, objName)
-	block.Address = objName
-	block.Size = shaReader.copiedSize
+	Block := new(model.Block)
+	Block.Size = hashReader.copiedSize
+	dedupId := hex.EncodeToString(hashReader.sha256.Sum(nil))
+	checksum := hex.EncodeToString(hashReader.md5.Sum(nil))
+	existingName, err := index.CreateDedupEntryIfNone(bucketName, dedupId, objName)
+	if existingName != objName { // object already exist
+		adapter.Remove(bucketName, objName)
+		objName = existingName
+	}
+	Block.Address = objName
+	/*obj,err := index.ReadObjectByKey(bucketName,dedupId)
+	if err == nil { // Object already exist under different name
+		adapter.Remove(bucketName,objName)
+		objName = obj.Checksum
+	} else if !xerrors.Is(err, db.ErrNotFound) {
+		panic("error reading object ")
+		return nil,err
+	}*/
 	blob := new(Blob)
-	blob.Blocks = append(blob.Blocks, block)
-	blob.Checksum = objName
-	blob.Size = block.Size
+	blob.Blocks = append(blob.Blocks, Block)
+	blob.Checksum = checksum
+	blob.Size = Block.Size
 	return blob, nil
 
 }
