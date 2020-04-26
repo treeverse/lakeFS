c9562e8acf5a6ec59c5cf628b3d4023264f08a0d
Oz Katz <ozkatz100@gmail.com>
2020-04-26 19:48:53 +0300

    simplified collector and tests

--- stats/collector.go
+++ stats/collector.go
@@ -4,124 +4,175 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"github.com/treeverse/lakefs/logging"
 )
 
-const CollectorEventBufferSize = 1024 * 1024
-
-type primaryKey struct {
-	class string
-	name  string
-}
-
-func (p primaryKey) String() string {
-	return fmt.Sprintf("%s/%s", p.class, p.name)
-}
-
-type keyIndex map[primaryKey]uint64
-
-type flushRequest struct {
-	flushed chan keyIndex
-}
+const (
+	DefaultCollectorEventBufferSize = 1024 * 1024
+	DefaultFlushInterval            = time.Second * 600
+)
 
 type Collector interface {
 	Collect(class, action string)
 }
 
-type CallHomeCollector struct {
-	ctx           context.Context
-	stop          context.CancelFunc
-	cache         keyIndex
-	writes        chan primaryKey
-	flushes       chan flushRequest
-	flushInterval time.Duration
-	drained       chan bool
+type Metric struct {
+	Class string `json:"class"`
+	Name  string `json:"name"`
+	Value uint64 `json:"value"`
 }
 
-type CollectorOpt func(c *CallHomeCollector)
+type InputEvent struct {
+	Email     string   `json:"email"`
+	ProcessId string   `json:"process_id"`
+	Time      string   `json:"time"`
+	Metrics   []Metric `json:"metrics"`
+}
 
-func WriteBufferSize(bufSize int) CollectorOpt {
-	return func(c *CallHomeCollector) {
-		c.writes = make(chan primaryKey, bufSize)
+type primaryKey struct {
+	class  string
+	action string
+}
+
+func (p primaryKey) String() string {
+	return fmt.Sprintf("%s/%s", p.class, p.action)
+}
+
+type keyIndex map[primaryKey]uint64
+
+type FlushTicker interface {
+	Stop()
+	Tick() <-chan time.Time
+}
+
+type TimeTicker struct {
+	ticker *time.Ticker
+}
+
+func (t *TimeTicker) Stop() {
+	t.ticker.Stop()
+}
+
+func (t *TimeTicker) Tick() <-chan time.Time {
+	return t.ticker.C
+}
+
+type BufferedCollector struct {
+	cache       keyIndex
+	writes      chan primaryKey
+	sender      Sender
+	flushTicker FlushTicker
+	done        chan bool
+}
+
+type BufferedCollectorOpts func(s *BufferedCollector)
+
+func WithWriteBufferSize(bufferSize int) BufferedCollectorOpts {
+	return func(s *BufferedCollector) {
+		s.writes = make(chan primaryKey, bufferSize)
 	}
 }
 
-func NewCallHomeCollector(opts ...CollectorOpt) *CallHomeCollector {
-	ctx, stop := context.WithCancel(context.Background())
-	collector := &CallHomeCollector{
-		ctx:     ctx,
-		stop:    stop,
-		cache:   make(keyIndex),
-		writes:  make(chan primaryKey, CollectorEventBufferSize),
-		flushes: make(chan flushRequest),
-		drained: make(chan bool),
+func WithSender(sender Sender) BufferedCollectorOpts {
+	return func(s *BufferedCollector) {
+		s.sender = sender
 	}
+}
+
+func WithTicker(t FlushTicker) BufferedCollectorOpts {
+	return func(s *BufferedCollector) {
+		s.flushTicker = t
+	}
+}
+
+func NewBufferedCollector(opts ...BufferedCollectorOpts) *BufferedCollector {
+	s := &BufferedCollector{
+		cache:       make(keyIndex),
+		writes:      make(chan primaryKey, DefaultCollectorEventBufferSize),
+		done:        make(chan bool),
+		sender:      NewDummySender(),
+		flushTicker: &TimeTicker{ticker: time.NewTicker(DefaultFlushInterval)},
+	}
+
 	for _, opt := range opts {
-		opt(collector)
+		opt(s)
 	}
-	return collector
+
+	return s
 }
 
-func (c *CallHomeCollector) Collect(class, action string) {
-	pk := primaryKey{
-		class: class,
-		name:  action,
-	}
-	c.writes <- pk
-}
-
-func (c *CallHomeCollector) incr(pk primaryKey) {
-	if currentValue, exists := c.cache[pk]; exists {
-		c.cache[pk] = currentValue + 1
+func (s *BufferedCollector) incr(k primaryKey) {
+	if current, exists := s.cache[k]; !exists {
+		s.cache[k] = 1
 	} else {
-		c.cache[pk] = 1
+		s.cache[k] = current + 1
 	}
 }
 
-func (c *CallHomeCollector) flushCounters(r flushRequest) {
-	flushed := make(keyIndex)   // create a new map to copy values into
-	for k, v := range c.cache { // copy all current cache values
-		flushed[k] = v
-	}
-	r.flushed <- flushed     // write new map back to the request channel
-	c.cache = make(keyIndex) // replace cache with a new empty map
-}
-
-func (c *CallHomeCollector) Flush() keyIndex {
-	r := flushRequest{make(chan keyIndex)}
-	c.flushes <- r     // ask loop to flushes counters
-	return <-r.flushed // read and return flushed counters
-}
-
-func (c *CallHomeCollector) Drain() keyIndex {
-	c.stop()       // trigger the run loop to stop
-	<-c.drained    // wait until the drained signal is received
-	return c.cache // return remaining counts if any
-}
-
-// read everything buffered in the writes channel, blocking until empty
-func (c *CallHomeCollector) drainWrites() {
+func (s *BufferedCollector) drain() {
 	for {
 		select {
-		case pk := <-c.writes:
-			c.incr(pk)
+		case w := <-s.writes:
+			s.incr(w)
 		default:
 			return
 		}
 	}
 }
 
-// Run waits for write and flush requests on the appropriate channels
-func (c *CallHomeCollector) Run() {
+func (s *BufferedCollector) send() {
+	metrics := makeMetrics(s.cache)
+	if len(metrics) == 0 {
+		return
+	}
+	err := s.sender.Send(metrics)
+	if err != nil {
+		logging.Default().
+			WithError(err).
+			WithField("service", "stats_collector").
+			Debug("could not send stats")
+	}
+	s.cache = make(keyIndex)
+}
+
+func (s *BufferedCollector) Collect(class, action string) {
+	s.writes <- primaryKey{
+		class:  class,
+		action: action,
+	}
+}
+
+func (s *BufferedCollector) Done() <-chan bool {
+	return s.done
+}
+
+func (s *BufferedCollector) Run(ctx context.Context) {
 	for {
 		select {
-		case <-c.ctx.Done(): // got a signal telling us to stop
-			c.drainWrites()   // drain all remaining writes on the writes channel
-			c.drained <- true // signal that we're drained and will no longer poll for writes
+		case w := <-s.writes: // collect events
+			s.incr(w)
+		case <-s.flushTicker.Tick(): // every N seconds, send the collected events
+			s.send()
+		case <-ctx.Done(): // we're done
+			s.drain()
+			s.send()
+			s.done <- true
 			return
-		case fr := <-c.flushes:
-			c.flushCounters(fr)
-		case pk := <-c.writes:
-			c.incr(pk)
 		}
 	}
+}
+
+func makeMetrics(counters keyIndex) []Metric {
+	metrics := make([]Metric, len(counters))
+	i := 0
+	for k, v := range counters {
+		metrics[i] = Metric{
+			Class: k.class,
+			Name:  k.action,
+			Value: v,
+		}
+		i++
+	}
+	return metrics
 }
