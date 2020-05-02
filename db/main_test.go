23309b7c77c8db66b72f9666c4bb01c9eef9c2cf
Barak Amar <barak.amar@treeverse.io>
2020-05-02 12:53:06 +0300

    test ConnectDB

---
+++ db/main_test.go
@@ -0,0 +1,76 @@
+package db_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/ory/dockertest/v3"
+)
+
+const dbContainerTimeoutSeconds = 10 * 60 // 10 min
+
+var (
+	pool        *dockertest.Pool
+	databaseURI string
+)
+
+func runDBInstance(pool *dockertest.Pool) (string, func()) {
+	resource, err := pool.Run("postgres", "11", []string{
+		"POSTGRES_USER=lakefs",
+		"POSTGRES_PASSWORD=lakefs",
+		"POSTGRES_DB=lakefs_db",
+	})
+	if err != nil {
+		log.Fatalf("Could not start postgresql: %s", err)
+	}
+
+	// set cleanup
+	closer := func() {
+		err := pool.Purge(resource)
+		if err != nil {
+			log.Fatalf("could not kill postgres container")
+		}
+	}
+
+	// expire, just to make sure
+	err = resource.Expire(dbContainerTimeoutSeconds)
+	if err != nil {
+		log.Fatalf("could not expire postgres container")
+	}
+
+	// create connection
+	var conn *sqlx.DB
+	uri := fmt.Sprintf("postgres://lakefs:lakefs@localhost:%s/lakefs_db?sslmode=disable", resource.GetPort("5432/tcp"))
+	err = pool.Retry(func() error {
+		var err error
+		conn, err = sqlx.Connect("pgx", uri)
+		if err != nil {
+			return err
+		}
+		return conn.Ping()
+	})
+	if err != nil {
+		log.Fatalf("could not connect to postgres: %s", err)
+	}
+	_ = conn.Close()
+
+	// return DB URI
+	return uri, closer
+}
+
+func TestMain(m *testing.M) {
+	var err error
+	pool, err = dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %s", err)
+	}
+	var cleanup func()
+	databaseURI, cleanup = runDBInstance(pool)
+	code := m.Run()
+	cleanup()
+	os.Exit(code)
+}
