d0728f1c8f496cdb35088cc73cea2fee441f48f0
Barak Amar <barak.amar@treeverse.io>
2020-05-03 17:21:26 +0300

    remove unsed action reset setup lakefs

--- webui/src/actions/setup.js
+++ webui/src/actions/setup.js
@@ -4,6 +4,3 @@ import {AsyncActionType} from "./request";
 export const SETUP_LAKEFS = new AsyncActionType('SETUP_LAKEFS');
 
 export const doSetupLakeFS = (email, fullName) => SETUP_LAKEFS.execute(async () => api.setup.lakeFS(email, fullName));
-
-export const resetSetupLakeFS = () => SETUP_LAKEFS.resetAction();
-
