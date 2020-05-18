71a83e5dc67775b4be1941d6d11425ffd4115caa
Barak Amar <barak.amar@treeverse.io>
2020-05-18 19:38:49 +0300

    prevent setup call while one is in process (#134)
    Former-commit-id: c44b9cf3f881c61ca2dccbd266ffd100fdb60de7

--- webui/src/store/setup.js
+++ webui/src/store/setup.js
@@ -2,12 +2,12 @@ import * as async from "./async";
 import { SETUP_LAKEFS } from "../actions/setup";
 
 const initialState = {
-    setupLakeFS: async.initialState,
+    setupLakeFS: async.actionInitialState,
 };
 
 export default (state = initialState, action) => {
     return {
         ...state,
-        setupLakeFS: async.reduce(SETUP_LAKEFS, state.setupLakeFS, action),
+        setupLakeFS: async.actionReduce(SETUP_LAKEFS, state.setupLakeFS, action),
     };
 };
