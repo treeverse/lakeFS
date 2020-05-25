a57133d1982d2109299beea6c4fdd8bd8d0c91b6
Barak Amar <barak.amar@treeverse.io>
2020-05-25 10:41:51 +0300

    basic migrate support (#168)
    Former-commit-id: 530958366aeb54760a417af17e9d5e68ba71bb56

---
+++ db/database.go
@@ -0,0 +1,83 @@
+package db
+
+import (
+	"database/sql"
+	"io"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type TxFunc func(tx Tx) (interface{}, error)
+
+type Database interface {
+	io.Closer
+	Transact(fn TxFunc, opts ...TxOpt) (interface{}, error)
+}
+
+type SqlxDatabase struct {
+	db *sqlx.DB
+}
+
+func NewSqlxDatabase(db *sqlx.DB) *SqlxDatabase {
+	return &SqlxDatabase{db: db}
+}
+func (d *SqlxDatabase) Close() error {
+	return d.db.Close()
+}
+
+func (d *SqlxDatabase) Transact(fn TxFunc, opts ...TxOpt) (interface{}, error) {
+	options := DefaultTxOptions()
+	for _, opt := range opts {
+		opt(options)
+	}
+	var attempt int
+	var ret interface{}
+	for attempt < SerializationRetryMaxAttempts {
+		if attempt > 0 {
+			duration := time.Duration(int(SerializationRetryStartInterval) * attempt)
+			options.logger.
+				WithField("attempt", attempt).
+				WithField("sleep_interval", duration).
+				Warn("retrying transaction due to serialization error")
+			time.Sleep(duration)
+		}
+
+		tx, err := d.db.BeginTxx(options.ctx, &sql.TxOptions{
+			Isolation: options.isolationLevel,
+			ReadOnly:  options.readOnly,
+		})
+		if err != nil {
+			return nil, err
+		}
+		ret, err = fn(&dbTx{tx: tx, logger: options.logger})
+		if err != nil {
+			rollbackErr := tx.Rollback()
+			if rollbackErr != nil {
+				return nil, rollbackErr
+			}
+			// retry on serialization error
+			if isSerializationError(err) {
+				// retry
+				attempt++
+				continue
+			}
+			return nil, err
+		} else {
+			err = tx.Commit()
+			if err != nil {
+				// retry on serialization error
+				if isSerializationError(err) {
+					attempt++
+					continue
+				}
+				// other commit error
+				return nil, err
+			}
+			// committed successfully, we're done
+			return ret, nil
+		}
+	}
+
+	return nil, ErrSerialization
+}
