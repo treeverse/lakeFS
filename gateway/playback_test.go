59088556e8ed2703cbf5a2f13fdc34ee0442086e
Barak Amar <barak.amar@treeverse.io>
2020-05-26 18:02:00 +0300

    get simulator recordings from s3
    Former-commit-id: a08be718a15122b9b2d338867799acbedc01d5c6

--- gateway/playback_test.go
+++ gateway/playback_test.go
@@ -16,39 +16,76 @@ import (
 
 	"github.com/treeverse/lakefs/block"
 	"github.com/treeverse/lakefs/gateway"
-	"github.com/treeverse/lakefs/gateway/utils"
+	"github.com/treeverse/lakefs/gateway/simulator"
 	"github.com/treeverse/lakefs/index"
 	"github.com/treeverse/lakefs/testutil"
 )
 
 type dependencies struct {
 	blocks block.Adapter
-	auth   utils.GatewayAuthService
+	auth   simulator.GatewayAuthService
 	meta   index.Index
 }
 
-const RecordingsDir = "testdata/recordings"
+const (
+	RecordingsDir = "testdata/recordings"
+	BucketName    = "lakefs-recordings"
+)
 
 func TestGatewayRecording(t *testing.T) {
-	dirList, err := ioutil.ReadDir(RecordingsDir)
-	if err != nil {
-		t.Fatalf("Failed reading recording directories: %v", err)
+
+	testData := []struct {
+		Name     string
+		Key      string
+		Filename string
+		Enabled  bool
+	}{
+		{
+			"presto",
+			"presto.zip",
+			"presto.zip",
+			true,
+		},
+		{
+			"aws",
+			"aws.zip",
+			"aws.zip",
+			true,
+		},
+		{
+			"spark emr",
+			"emr-spark.zip",
+			"emr-spark.zip",
+			true,
+		},
 	}
-	for _, dir := range dirList {
-		zipName := dir.Name()
-		if filepath.Ext(dir.Name()) != ".zip" {
+
+	downloader := simulator.NewExternalRecordDownloader("us-east-1")
+
+	for _, recording := range testData {
+
+		if !recording.Enabled {
 			continue
 		}
-		dirName := zipName[:len(zipName)-4]
-		t.Run(dirName+" recording", func(t *testing.T) {
+		filename := filepath.Join(RecordingsDir, recording.Filename)
+		t.Run(recording.Name, func(t *testing.T) {
+			// download record
+			err := downloader.DownloadRecording(BucketName, recording.Key, filename)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-			setGlobalPlaybackParams(dirName)
-			os.RemoveAll(utils.PlaybackParams.RecordingDir)
-			os.MkdirAll(utils.PlaybackParams.RecordingDir, 0755)
-			archive := filepath.Join(RecordingsDir, zipName)
-			deCompressRecordings(archive, utils.PlaybackParams.RecordingDir)
+			zipName := recording.Filename
+			if filepath.Ext(recording.Filename) != ".zip" {
+				return
+			}
+			setGlobalPlaybackParams(recording.Filename)
+			os.RemoveAll(simulator.PlaybackParams.RecordingDir)
+			os.MkdirAll(simulator.PlaybackParams.RecordingDir, 0755)
+			deCompressRecordings(filename, simulator.PlaybackParams.RecordingDir)
 			handler, _ := getBasicHandler(t, zipName)
 			DoTestRun(handler, false, 1.0, t)
+
 		})
 	}
 }
@@ -90,7 +127,7 @@ func getBasicHandler(t *testing.T, testDir string) (http.Handler, *dependencies)
 
 	blockAdapter := testutil.GetBlockAdapter(t, IdTranslator)
 
-	authService := newGatewayAuth(t, utils.PlaybackParams.RecordingDir)
+	authService := newGatewayAuth(t, simulator.PlaybackParams.RecordingDir)
 
 	testutil.Must(t, meta.CreateRepo("example", "example-tzahi", "master"))
 	server := gateway.NewServer(authService.Region,
@@ -106,9 +143,9 @@ func getBasicHandler(t *testing.T, testDir string) (http.Handler, *dependencies)
 	}
 }
 
-func newGatewayAuth(t *testing.T, directory string) *utils.PlayBackMockConf {
-	m := new(utils.PlayBackMockConf)
-	fName := filepath.Join(directory, utils.SimulationConfig)
+func newGatewayAuth(t *testing.T, directory string) *simulator.PlayBackMockConf {
+	m := new(simulator.PlayBackMockConf)
+	fName := filepath.Join(directory, simulator.SimulationConfig)
 	confStr, err := ioutil.ReadFile(fName)
 	if err != nil {
 		t.Fatal(fName + " not found\n")
@@ -149,7 +186,7 @@ func decompressRecordingsFile(f *zip.File) {
 	defer func() {
 		_ = compressedFile.Close()
 	}()
-	fileName := filepath.Join(utils.PlaybackParams.RecordingDir, filepath.Base(f.Name))
+	fileName := filepath.Join(simulator.PlaybackParams.RecordingDir, filepath.Base(f.Name))
 	decompressedFile, err := os.Create(fileName)
 	if err != nil {
 		logging.Default().WithError(err).Fatal("failed creating file " + f.Name)
--- gateway/simulator/simulation_utils.go
+++ gateway/simulator/simulation_utils.go
@@ -1,4 +1,4 @@
-package utils
+package simulator
 
 import (
 	"encoding/json"
