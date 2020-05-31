df2c756c303c7739f7568b636d373e88cf608a24
tzahij <tzahij@hotmail.com>
2020-05-31 18:51:04 +0300

    initial write entry

---
+++ catalog/cataloger_write_entry_test.go
@@ -0,0 +1,28 @@
+package catalog
+
+import (
+	"context"
+	"testing"
+
+	"github.com/treeverse/lakefs/testutil"
+)
+
+func Must(err error, t *testing.T, message string) {
+	if err != nil {
+		t.Fatal(message)
+	}
+}
+func TestCataloger_WriteEntry(t *testing.T) {
+	ctx := context.Background()
+	cdb, _ := testutil.GetDB(t, databaseURI, "lakefs_catalog")
+	c := NewCataloger(cdb)
+
+	t.Run("simple write", func(t *testing.T) {
+		Must(c.CreateRepo(ctx, "example", "example-tzahi", "master"), t, "error creating repository")
+		//meta := make(map[string]string)
+		Must(c.WriteEntry(ctx, "example", "master", "aaa/bbb/ccc", "1234", "5678", 100, false, nil), t, "error creating repository")
+		Must(c.WriteEntry(ctx, "example", "master", "aaa/bbb/ccc", "6789", "5678", 100, false, nil), t, "error creating repository")
+
+	})
+
+}
