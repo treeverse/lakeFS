18dac80065c40b636dc5229463c059c84bff9530
tzahij <tzahij@hotmail.com>
2020-06-02 18:47:58 +0300

    progress on diff

--- catalog/cataloger_diff.go
+++ catalog/cataloger_diff.go
@@ -67,14 +67,31 @@ func doDiff(tx db.Tx, leftId, rightId int, log logging.Logger) (Differences, err
 }
 
 func FromFatherDiff(tx db.Tx, leftId, rightId int, log logging.Logger) (Differences, error) {
-	// just need to check there are no conflicts
-
+	// get the last son commit number of the last father merge
+	// if there is none - then it is  the first merge
+	// the condition on merge_source_branch is redundent. a given branch may have only one father.
+	// it is there in the hope it makes the idea less confusing
+	x := `select coalesc(max(commit_number),0) from commits 
+			where branch_id = $1 and merge_type = 'fromFather' and merge_source_branch = $2`
+	x = `select * from entries where branch_id = $1`
 	// check if father lineage was modified since last diff. if not we can skip the view
-	//x := `select * from lineage l join lineage r on
-	//      l.branch_id=$1 and r.branch_id =$2 and
-	//	  l.ancestor_branch=r.ancestor_branch
-	//      where
-	//      l.effective_commit > r.effective_commit`
+	x = `select s.effective_commit as min_effective_commit,e.effective_commit as min_effective_commit from lineage s join lineage e on
+	     s.branch_id=$1 and e.branch_id =$2 and
+		  s.ancestor_branch=e.ancestor_branch
+	     `
+	x = `select * from entries_lineage_v e join lineage_dif l 
+		 on e.source_branch = l.ancestore_branch
+		where
+		e.branch_id = $1 and
+		e.min_branch > l.min_effective_commit`
+	x = `select * from father f  left join  son s on 
+			s.path  = f.path 
+			where
+			s.path is null 
+			or not  -- if they have the same object, than it is not deleted in one and exist in the other
+			(f.physical_address = s.physical_address and --  the same object 
+              ( f.max_commit != 2147483647 or f.max_commit != s.max_commit ))
+			or`
 	// from father - select comitted objects that were modified after the lineage was created
 	x := ` with father as ( select * from entries_lineage_v e join lineage l on
 			e.displayed_branch = $1 and l.branch_id = $2 and
@@ -91,9 +108,9 @@ func FromFatherDiff(tx db.Tx, leftId, rightId int, log logging.Logger) (Differen
 }
 
 func FromSonDiff(tx db.Tx, leftId, rightId int, log logging.Logger) (Differences, error) {
-	x := `select max(merge_source_commit) from commits where branch_id = $1 and merge_type = 'fromSon' `
+	x := `select coalesc(max(merge_source_commit),0) from commits where branch_id = $1 and merge_type = 'fromSon' `
 	// relevant entries from son
-	x = ` select * from entries where min_commit > merge_commit `
+	x = ` select * from entries where min_commit > merge_commit and branch_id = $1 `
 	// relevant entries from father
 	x := `select l.ancestor_branch, l.effective_commit as start_commit,r.effective_commit as end_commit 
 			from lineage l join lineage_v r 
