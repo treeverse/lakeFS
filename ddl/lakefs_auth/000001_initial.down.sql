21ab49b42cc8d3e2fef361b9f5e4c2ebcedcca55
Barak Amar <barak.amar@treeverse.io>
2020-06-04 18:35:27 +0300

    Remove role as attachment for policies
    Former-commit-id: 5775876e66e1192ab13ccc1292b743fe2859d1a4

--- ddl/lakefs_auth/000001_initial.down.sql
+++ ddl/lakefs_auth/000001_initial.down.sql
@@ -1,9 +1,7 @@
 DROP TABLE IF EXISTS credentials;
-DROP TABLE IF EXISTS role_policies;
-DROP TABLE IF EXISTS policies;
-DROP TABLE IF EXISTS group_roles;
-DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS group_policies;
+DROP TABLE IF EXISTS user_policies;
 DROP TABLE IF EXISTS user_groups;
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS policies;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS users;
