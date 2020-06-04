3e89f7eeea942c479d4966270af107b9e2931949
Oz Katz <ozkatz100@gmail.com>
2020-06-04 18:35:27 +0300

    Remove role as attachment for policies
    Former-commit-id: 5775876e66e1192ab13ccc1292b743fe2859d1a4

--- ddl/lakefs_auth/000001_initial.up.sql
+++ ddl/lakefs_auth/000001_initial.up.sql
@@ -17,15 +17,6 @@ CREATE TABLE IF NOT EXISTS groups (
 );
 
 
-CREATE TABLE IF NOT EXISTS roles (
-    id serial NOT NULL PRIMARY KEY,
-    created_at timestamptz NOT NULL,
-    display_name text NOT NULL,
-
-    CONSTRAINT roles_unique_display_name UNIQUE (display_name)
-);
-
-
 CREATE TABLE IF NOT EXISTS policies (
     id serial NOT NULL PRIMARY KEY,
     created_at timestamptz NOT NULL,
@@ -48,31 +39,22 @@ CREATE INDEX idx_user_groups_user_id ON user_groups (user_id); -- list groups by
 CREATE INDEX idx_user_groups_group_id ON user_groups (group_id); -- list users by group
 
 
-CREATE TABLE IF NOT EXISTS user_roles (
+CREATE TABLE IF NOT EXISTS user_policies (
     user_id integer REFERENCES users (id) ON DELETE CASCADE NOT NULL,
-    role_id integer REFERENCES roles (id) ON DELETE CASCADE NOT NULL,
+    policy_id integer REFERENCES policies (id) ON DELETE CASCADE NOT NULL,
 
-    PRIMARY KEY (user_id, role_id)
+    PRIMARY KEY (user_id, policy_id)
 );
-CREATE INDEX idx_user_role_user_id ON user_roles (user_id); -- list roles by user
+CREATE INDEX idx_user_policies_user_id ON user_policies (user_id); -- list policies by user
 
 
-CREATE TABLE IF NOT EXISTS group_roles (
+CREATE TABLE IF NOT EXISTS group_policies (
     group_id integer REFERENCES groups (id) ON DELETE CASCADE NOT NULL,
-    role_id integer REFERENCES roles (id) ON DELETE CASCADE NOT NULL,
+    policy_id integer REFERENCES policies (id) ON DELETE CASCADE NOT NULL,
 
-    PRIMARY KEY (group_id, role_id)
+    PRIMARY KEY (group_id, policy_id)
 );
-CREATE INDEX idx_group_roles_group_id ON group_roles (group_id); -- list roles by group
-
-
-CREATE TABLE IF NOT EXISTS role_policies (
-    role_id integer REFERENCES roles (id) ON DELETE CASCADE NOT NULL,
-    policy_id integer NOT NULL REFERENCES policies (id) ON DELETE CASCADE,
-
-    PRIMARY KEY (role_id, policy_id)
-);
-CREATE INDEX idx_role_policies_role_id ON role_policies (role_id); -- list policies by role
+CREATE INDEX idx_group_policies_group_id ON group_policies (group_id); -- list policies by group
 
 
 CREATE TABLE IF NOT EXISTS credentials (
