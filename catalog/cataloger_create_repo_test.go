034d28ce37cab4d40d6905c9503b84b492818328
Barak Amar <barak.amar@treeverse.io>
2020-06-10 11:19:03 +0300

    test cover for repo create by get and match data

--- catalog/cataloger_create_repo_test.go
+++ catalog/cataloger_create_repo_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"testing"
+
+	"github.com/treeverse/lakefs/testutil"
 )
 
 func TestCataloger_CreateRepo(t *testing.T) {
@@ -34,8 +36,20 @@ func TestCataloger_CreateRepo(t *testing.T) {
 			asErr:   ErrInvalidValue,
 		},
 		{
-			name:    "missing branch",
+			name:    "unknown branch",
 			args:    args{name: "repo3", bucket: "bucket3", branch: ""},
+			wantErr: true,
+			asErr:   ErrInvalidValue,
+		},
+		{
+			name:    "missing repo",
+			args:    args{name: "", bucket: "bucket1", branch: "master"},
+			wantErr: true,
+			asErr:   ErrInvalidValue,
+		},
+		{
+			name:    "missing bucket",
+			args:    args{name: "repo1", bucket: "", branch: "master"},
 			wantErr: true,
 			asErr:   ErrInvalidValue,
 		},
@@ -45,14 +59,26 @@ func TestCataloger_CreateRepo(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			err := c.CreateRepository(ctx, tt.args.name, tt.args.bucket, tt.args.branch)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("CreateRepository() error = %v, wantErr %v", err, tt.wantErr)
-				return
+				t.Fatalf("CreateRepository() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if err != nil && tt.asErr != nil && !errors.As(err, &tt.asErr) {
-				t.Errorf("CreateRepository() error = %v, expected as %v", err, tt.asErr)
+				t.Fatalf("CreateRepository() error = %v, expected as %v", err, tt.asErr)
+			}
+			if err != nil {
 				return
 			}
+			// get repository information and verify we got what we created
+			rep, err := c.GetRepository(ctx, tt.args.name)
+			testutil.MustDo(t, "Get repository for create repository verification", err)
+			if rep.Name != tt.args.name {
+				t.Errorf("Create repository got name = %s, expected = %s", rep.Name, tt.args.name)
+			}
+			if rep.DefaultBranch != tt.args.branch {
+				t.Errorf("Create repository got branch = %s, expected = %s", rep.DefaultBranch, tt.args.branch)
+			}
+			if rep.StorageNamespace != tt.args.bucket {
+				t.Errorf("Create repository got branch = %s, expected = %s", rep.StorageNamespace, tt.args.bucket)
+			}
 		})
-		// TODO(barak): list repository to verification
 	}
 }
