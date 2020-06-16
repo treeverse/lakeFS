3dee1e0a2715d833351a1d2a88e8207a5ae7cc47
Barak Amar <barak.amar@treeverse.io>
2020-06-16 14:33:19 +0300

    reuse merge common code

--- catalog/cataloger_merge.go
+++ catalog/cataloger_merge.go
@@ -59,54 +59,23 @@ func formatMergeMessage(leftBranch string, rightBranch string) string {
 }
 
 func (c *cataloger) doMergeByRelation(tx db.Tx, relation RelationType, leftID int, rightID int, committer string, msg string, metadata Metadata) (CommitID, error) {
-	switch relation {
-	case RelationTypeFromFather:
-		return c.mergeFromFather(tx, leftID, rightID, committer, msg, metadata)
-	case RelationTypeFromSon:
-		return c.mergeFromSun(tx, leftID, rightID, committer, msg, metadata)
-	case RelationTypeNotDirect:
-		return c.mergeNonDirect(tx, leftID, rightID, committer, msg, metadata)
-	default:
-		return 0, nil
-	}
-}
-
-func (c *cataloger) mergeFromFather(tx db.Tx, leftID int, rightID int, committer string, msg string, metadata Metadata) (CommitID, error) {
 	// get commit id on destination
 	commitID, err := getCommitID(tx, rightID)
 	if err != nil {
 		return 0, err
 	}
 
-	// set current lineages max commit to current one
-	if _, err := tx.Exec(`UPDATE lineage SET max_commit=($2 - 1) WHERE branch_id=$1 AND max_commit=$3`, rightID, commitID, MaxCommitID); err != nil {
-		return 0, err
+	// do the merge based on the relation
+	switch relation {
+	case RelationTypeFromFather:
+		err = c.mergeFromFather(tx, commitID, leftID, rightID)
+	case RelationTypeFromSon:
+		err = c.mergeFromSun(tx, commitID, leftID, rightID)
+	case RelationTypeNotDirect:
+		err = c.mergeNonDirect(tx, commitID, leftID, rightID)
+	default:
+		return 0, ErrUnsupportedRelation
 	}
-
-	// copy source lineages with an effective commit
-	_, err = tx.Exec(`INSERT INTO lineage (branch_id, precedence, ancestor_branch, effective_commit, min_commit)
-			SELECT $1, precedence + 1, ancestor_branch, effective_commit, $3
-			FROM lineage_v
-			WHERE branch_id = $2 AND active_lineage`, rightID, leftID, commitID)
-	if err != nil {
-		return 0, err
-	}
-
-	// DifferenceTypeRemoved and DifferenceTypeChanged - set max_commit the our commit for committed entries
-	_, err = tx.Exec(`UPDATE entries SET max_commit = ($2 - 1)
-			WHERE branch_id = $1 AND max_commit = $3
-				AND path in (SELECT path FROM `+diffResultsTableName+` WHERE diff_type IN ($4,$5))`,
-		rightID, commitID, MaxCommitID, DifferenceTypeRemoved, DifferenceTypeChanged)
-	if err != nil {
-		return 0, err
-	}
-
-	// DifferenceTypeChanged - create entries into this commit based on father branch
-	_, err = tx.Exec(`INSERT INTO entries (branch_id,path,physical_address,creation_date,size,checksum,metadata,min_commit)
-				SELECT $1,path,physical_address,creation_date,size,checksum,metadata,$2 AS min_commit
-				FROM entries e
-				WHERE e.ctid IN (SELECT entry_ctid FROM `+diffResultsTableName+` WHERE diff_type=$3)`,
-		rightID, commitID, DifferenceTypeChanged)
 	if err != nil {
 		return 0, err
 	}
@@ -120,14 +89,47 @@ func (c *cataloger) mergeFromFather(tx db.Tx, leftID int, rightID int, committer
 	if err := commitInsertCommitRecord(tx, rightID, commitID, committer, msg, metadata, c.Clock.Now()); err != nil {
 		return 0, err
 	}
-
 	return commitID, nil
 }
 
-func (c *cataloger) mergeFromSun(tx db.Tx, leftID int, rightID int, committer string, msg string, metadata Metadata) (CommitID, error) {
+func (c *cataloger) mergeFromFather(tx db.Tx, commitID CommitID, leftID int, rightID int) error {
+	// set current lineages max commit to current one
+	if _, err := tx.Exec(`UPDATE lineage SET max_commit=($2 - 1) WHERE branch_id=$1 AND max_commit=$3`,
+		rightID, commitID, MaxCommitID); err != nil {
+		return err
+	}
+
+	// copy source lineages with an effective commit
+	_, err := tx.Exec(`INSERT INTO lineage (branch_id, precedence, ancestor_branch, effective_commit, min_commit)
+			SELECT $1, precedence + 1, ancestor_branch, effective_commit, $3
+			FROM lineage_v
+			WHERE branch_id = $2 AND active_lineage`, rightID, leftID, commitID)
+	if err != nil {
+		return err
+	}
+
+	// DifferenceTypeRemoved and DifferenceTypeChanged - set max_commit the our commit for committed entries
+	_, err = tx.Exec(`UPDATE entries SET max_commit = ($2 - 1)
+			WHERE branch_id = $1 AND max_commit = $3
+				AND path in (SELECT path FROM `+diffResultsTableName+` WHERE diff_type IN ($4,$5))`,
+		rightID, commitID, MaxCommitID, DifferenceTypeRemoved, DifferenceTypeChanged)
+	if err != nil {
+		return err
+	}
+
+	// DifferenceTypeChanged - create entries into this commit based on father branch
+	_, err = tx.Exec(`INSERT INTO entries (branch_id,path,physical_address,creation_date,size,checksum,metadata,min_commit)
+				SELECT $1,path,physical_address,creation_date,size,checksum,metadata,$2 AS min_commit
+				FROM entries e
+				WHERE e.ctid IN (SELECT entry_ctid FROM `+diffResultsTableName+` WHERE diff_type=$3)`,
+		rightID, commitID, DifferenceTypeChanged)
+	return err
+}
+
+func (c *cataloger) mergeFromSun(tx db.Tx, commitID CommitID, leftID int, rightID int) error {
 	panic("implement")
 }
 
-func (c *cataloger) mergeNonDirect(tx db.Tx, leftID int, rightID int, committer string, msg string, metadata Metadata) (CommitID, error) {
+func (c *cataloger) mergeNonDirect(tx db.Tx, commitID CommitID, leftID int, rightID int) error {
 	panic("implement")
 }
