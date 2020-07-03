611b4edc8c6321e82110e3fa9a2586312ffc889d
Barak Amar <barak.amar@treeverse.io>
2020-07-03 08:38:22 +0300

    missing delete cascade

--- ddl/lakefs_catalog/000001_mvcc.up.sql
+++ ddl/lakefs_catalog/000001_mvcc.up.sql
@@ -419,7 +419,7 @@ ALTER TABLE ONLY commits
     ADD CONSTRAINT commits_branches_repository_id_fk FOREIGN KEY (branch_id) REFERENCES branches(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY entries
-    ADD CONSTRAINT entries_branches_fk FOREIGN KEY (branch_id) REFERENCES branches(id) NOT VALID;
+    ADD CONSTRAINT entries_branches_fk FOREIGN KEY (branch_id) REFERENCES branches(id) ON DELETE CASCADE NOT VALID;
 
 -- ALTER TABLE ONLY lineage
 --     ADD CONSTRAINT lineage_branches_repository_id_fk FOREIGN KEY (branch_id) REFERENCES branches(id) ON DELETE CASCADE;
