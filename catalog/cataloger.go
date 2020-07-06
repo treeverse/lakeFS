5354ed67cbc30e5e6fd2ba9d8743111d48edde32
tzahij <tzahij@hotmail.com>
2020-07-06 12:12:15 +0300

    directory listing is working - before  testing all  posibilities
    Former-commit-id: ba5e947de902d3b2221bc05843ef0cae38581a3c

--- catalog/cataloger.go
+++ catalog/cataloger.go
@@ -34,6 +34,7 @@ type EntryCataloger interface {
 	ListEntries(ctx context.Context, repository, reference string, prefix, after string, limit int) ([]*Entry, bool, error)
 	ResetEntry(ctx context.Context, repository, branch string, path string) error
 	ResetEntries(ctx context.Context, repository, branch string, prefix string) error
+	ListEntriesByLevel(ctx context.Context, prefix, delimiter, repository, branchName string, maxLines int, requestedCommit CommitID) (interface{}, error)
 }
 
 type MultipartUpdateCataloger interface {
