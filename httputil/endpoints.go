ef55dbcba52531e5a6db587af27ef18dd639250b
Barak Amar <barak.amar@treeverse.io>
2020-07-15 15:01:31 +0300

    single healthcheck, docker will call run by default (#275)
    Former-commit-id: 133d02c294a3ee62d6dbc8585ea1ae4cf51df3b4

---
+++ httputil/endpoints.go
@@ -0,0 +1,36 @@
+package httputil
+
+import (
+	"net/http"
+	"net/http/pprof"
+	"strings"
+)
+
+func ServeHealth() http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		_, _ = writer.Write([]byte("alive!"))
+	})
+}
+
+func ServePPROF(endpoint string) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		endpoint = strings.TrimPrefix(request.URL.Path, endpoint)
+		switch endpoint {
+		case "":
+			http.HandlerFunc(pprof.Index).ServeHTTP(writer, request)
+		case "cmdline":
+			http.HandlerFunc(pprof.Cmdline).ServeHTTP(writer, request)
+		case "profile":
+			http.HandlerFunc(pprof.Profile).ServeHTTP(writer, request)
+		case "symbol":
+			http.HandlerFunc(pprof.Symbol).ServeHTTP(writer, request)
+		case "trace":
+			http.HandlerFunc(pprof.Trace).ServeHTTP(writer, request)
+		case "block", "goroutine", "heap", "threadcreate":
+			pprof.Handler(endpoint).ServeHTTP(writer, request)
+		default:
+			writer.WriteHeader(http.StatusNotFound)
+		}
+	})
+}
