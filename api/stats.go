38705a6ed40575a7a3a263c3bc91beda05615ef3
Barak Amar <barak.amar@treeverse.io>
2020-07-22 14:10:07 +0300

    dedip wait for remove object (#288)

--- api/stats.go
+++ api/stats.go
@@ -12,3 +12,17 @@ var requestCounter = promauto.NewCounterVec(
 	},
 	[]string{"code", "method"},
 )
+
+var dedupRemoveObjectDroppedCounter = promauto.NewCounter(
+	prometheus.CounterOpts{
+		Name: "dedup_remove_object_dropped",
+		Help: "A counter for dedup remove object that we dropped.",
+	},
+)
+
+var dedupRemoveObjectFailedCounter = promauto.NewCounter(
+	prometheus.CounterOpts{
+		Name: "dedup_remove_object_failed",
+		Help: "A counter for failed dedup remove objects.",
+	},
+)
