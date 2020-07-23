c75991ff4b670bdcaab7fc8671c2de514433cb4e
tzahij <tzahij@hotmail.com>
2020-07-23 17:21:39 +0300

    fixes to regression in mertge

--- catalog/cataloger_list_entries_by_level.go
+++ catalog/cataloger_list_entries_by_level.go
@@ -56,7 +56,8 @@ type resultRow struct {
 	MaxCommit CommitID
 }
 
-func loopByLevel(tx db.Tx, prefix, after, delimiter string, limit, batchSize int, branchID int64, requestedCommit CommitID, lineage []lineageCommit) ([]string, error) {
+func loopByLevel(tx db.Tx, prefix, after, delimiter string, limit, branchBatchSize int, branchID int64, requestedCommit CommitID, lineage []lineageCommit) ([]string, error) {
+	// translate logical (umcommitted and commited )commit id to actual minCommit,maxCommit numbers in Rows
 	lowestCommitID := CommitID(1)
 	topCommitID := requestedCommit
 	if requestedCommit == 0 {
@@ -65,33 +66,34 @@ func loopByLevel(tx db.Tx, prefix, after, delimiter string, limit, batchSize int
 	if requestedCommit <= 0 {
 		topCommitID = MaxCommitID
 	}
+	// list of branches ordered form son to ancestors
 	sortedBranches := make([]int64, len(lineage)+1)
 	sortedBranches[0] = branchID
 	for i, l := range lineage {
 		sortedBranches[i+1] = l.BranchID
 	}
-	limit += 1
-	unionQueryParts := buildBaseLevelQuery(branchID, lineage, batchSize, lowestCommitID, topCommitID)
+	limit += 1 // increase limit to get indication of more rows to come
+	unionQueryParts := buildBaseLevelQuery(branchID, lineage, branchBatchSize, lowestCommitID, topCommitID, len(prefix))
 	endOfPrefixRange := prefix + DirectoryTermination
-	//nextPath := make([]string, len(lineage)+1)
-	//listAfter := prefix + strings.TrimPrefix(after, prefix)
-	listAfter := make([]string, len(lineage)+1)
-	for i := 0; i < len(listAfter); i++ {
-		listAfter[i] = prefix + strings.TrimPrefix(after, prefix)
-	}
+	listAfter := prefix + strings.TrimPrefix(after, prefix)
 	var markerList []string
 	pathCond := "path > ? and path < '" + endOfPrefixRange + "'"
-	unionSelect := unionQueryParts[0].Where(pathCond, listAfter[0]).Prefix("(").Suffix(")")
 	var responseRows []resultRow
 	for i := 0; i < limit; i++ {
+		unionSelect := unionQueryParts[0].Where(pathCond, listAfter).Prefix("(").Suffix(")")
 		for j := 1; j < len(lineage)+1; j++ {
+			// add the path condition to each union part
 			unionSelect = unionSelect.SuffixExpr(sq.ConcatExpr("\n UNION ALL \n", "(",
-				unionQueryParts[j].Where(pathCond, listAfter[j]), ")"))
+				unionQueryParts[j].Where(pathCond, listAfter), ")"))
 		}
 		fullQuery := sq.Select("*").FromSelect(unionSelect, "u")
 		deb := sq.DebugSqlizer(fullQuery)
 		_ = deb
 		unionSQL, args, err := fullQuery.PlaceholderFormat(sq.Dollar).ToSql()
+		if err != nil {
+			return nil, err
+		}
+		responseRows = responseRows[:0]
 		err = tx.Select(&responseRows, unionSQL, args...)
 		if errors.As(err, &db.ErrNotFound) {
 			return markerList, nil
@@ -99,149 +101,86 @@ func loopByLevel(tx db.Tx, prefix, after, delimiter string, limit, batchSize int
 		if err != nil {
 			return nil, err
 		}
-		trimmedResponseRows := make([]resultRow, len(responseRows))
-		for i, r := range responseRows {
-			trimmedResponseRows[i] = r
-			trimmedResponseRows[i].Path = r.Path[len(prefix):]
+		pathSuffix := findCommonPrefix(responseRows, lineage, delimiter, sortedBranches)
+		if pathSuffix == nil {
+			return markerList, nil
 		}
-		findCommonPrefix(trimmedResponseRows, lineage, delimiter, sortedBranches)
-		/*nextPart := nextPath[len(prefix):]
-		pos := strings.Index(nextPart, delimiter)
-		if pos != -1 {
-			nextPath = nextPath[:len(prefix)+pos+1]
-			listAfter = nextPath + DirectoryTermination
-		} else {
-			listAfter = nextPath
-		}*/
-		//markerList = append(markerList, nextPath)
+		markerList = append(markerList, *pathSuffix)
+		if strings.HasSuffix(*pathSuffix, delimiter) {
+			*pathSuffix += DirectoryTermination
+		}
+		listAfter = prefix + *pathSuffix
 	}
 	return markerList, nil
 }
 
-// heap implementation, using "container/heap"
-type responseRowHeapType []resultRow
-
-func (h responseRowHeapType) Len() int { return len(h) }
-func (h responseRowHeapType) Less(i, j int) bool {
-	if (h[i]).Path == h[j].Path {
-		return h[i].BranchID > h[j].BranchID
-	} else {
-		return h[i].Path < h[j].Path
-	}
-}
-func (h responseRowHeapType) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-func (h *responseRowHeapType) Push(x interface{}) {
-	*h = append(*h, x.(resultRow))
-}
-func (h *responseRowHeapType) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
 func findCommonPrefix(response []resultRow, lineage []lineageCommit, delimiter string, branches []int64) *string {
-	rh := new(responseRowHeapType)
-	heap.Init(rh)
-	branchRanges := make(map[int64][]resultRow, len(lineage)+1)
+	// split results by branch
+	branchRanges := make(map[int64][]resultRow, len(branches))
 	for len(response) > 0 {
-		branch := response[0].BranchID
+		b := response[0].BranchID
 		i := 1
-		for i < len(response) && response[i].BranchID == branch {
+		for i < len(response) && response[i].BranchID == b {
 			i++
 		}
-		branchRanges[branch] = response[:i]
+		branchRanges[b] = response[:i]
 		response = response[i:]
 	}
 	// init heap
-	for _, branch := range branches {
-		heap.Push(rh, branchRanges[branch][0])
+	rh := new(responseRowHeapType)
+	heap.Init(rh)
+	for _, b := range branches {
+		heap.Push(rh, branchRanges[b][0]) // bug - not
 	}
-	resRow := heap.Pop(rh).(resultRow)
+	lowestResult := heap.Pop(rh).(resultRow)
 	for true { // exit loop by break
-		b := resRow.BranchID
+		p := lowestResult.Path
+		b := lowestResult.BranchID
 		var i int
 		numOfRes := len(branchRanges[b])
-		// find range of rows with this name - only case it is not one is uncommitted tombstone
-		for i < numOfRes {
-			if branchRanges[b][i].Path == resRow.Path {
-				i++
-			} else {
-				break
-			}
+		// find range of rows with this name - only cases it is not one are uncommitted or tombstone
+		for i = 0; (i < numOfRes) && (branchRanges[b][i].Path) == p; i++ {
 		}
 		if i == numOfRes {
-			panic("got to end of list")
+			panic("got to end of list - will be implemented later")
 		}
 		pathResults := branchRanges[b][:i] // take all results with this path
 		if checkPath(pathResults) {        // minimal path was found
-			return &resRow.Path
+			pos := strings.Index(p, delimiter)
+			if pos > -1 {
+				p = p[:pos+1]
+			}
+			return &p
 		}
 		branchRanges[b] = branchRanges[b][i:] // remove  rows of this path
 		heap.Push(rh, branchRanges[b][0])
-		deletedRow := resRow
-		resRow = heap.Pop(rh).(resultRow)
-		for resRow.Path == deletedRow.Path {
-			b = resRow.BranchID
-			branchRanges[b] = branchRanges[b][1:]
+		rejectedResult := lowestResult
+		lowestResult = heap.Pop(rh).(resultRow)        // todo : deleted should not be filtered ???
+		for lowestResult.Path == rejectedResult.Path { // clear results with the same path as the one that was rejected
+			b = lowestResult.BranchID
+			branchRanges[b] = branchRanges[b][1:] // to do - handle empty result
 			heap.Push(rh, branchRanges[b][0])
-			resRow = heap.Pop(rh).(resultRow)
+			lowestResult = heap.Pop(rh).(resultRow)
 		}
-
 	}
-
-	//tombstones := make(map[string]int64)
-	//for _, branch := range branches {
-	//	branchResponse, found := branchRanges[branch]
-	//	if !found {
-	//		continue
-	//	}
-	//	for i, e := range branchResponse {
-	//		_, found = tombstones[e.Path]
-	//		if found {
-	//			continue
-	//		}
-	//		if e.MaxCommit == MaxCommitID {
-	//
-	//		}
-	//
-	//	}
-	//
-	//}
-
 	return nil
 }
 
 func checkPath(pathResults []resultRow) bool {
-	if pathResults[0].MaxCommit == MaxCommitID { // top path not deleted
-		for _, r := range pathResults[1:] {
-			if r.MinCommit == 0 && r.MaxCommit == 0 { // uncommitted tombstone - has precedence
-				return false
-			}
-			return true
+	if pathResults[0].MaxCommit != MaxCommitID { // top is deleted
+		return false
+	} // top path not deleted, but may have uncommitted tombstone
+	for _, r := range pathResults[1:] {
+		if r.MinCommit == 0 && r.MaxCommit == 0 { // uncommitted tombstone - has precedence
+			return false
 		}
-	} // top is deleted
-	return false
+	}
+	return true
 }
 
-//func doOneStrech(tx db.Tx, prefix string, delimiter string, BranchID int64, limit int, numOfBranches int) (string, error) {
-//	var resultBuf []resultRow
-//	var nextPath string
-//	pathCandidates := make([]string, numOfBranches)
-//	readPrefixes := make([]string, numOfBranches)
-//	for i := 0; i < numOfBranches; i++ {
-//		readPrefixes[i] = prefix
-//	}
-//
-//	foundPath := false
-//	for !foundPath {
-//
-//	}
-
-//}
-func buildBaseLevelQuery(baseBranchID int64, lineage []lineageCommit, brancEntryLimit int, lowestCommitId, topCommitID CommitID) []sq.SelectBuilder {
-	rowSelect := sq.Select("branch_id as BranchID", "path", "min_commit as minCommit").
+func buildBaseLevelQuery(baseBranchID int64, lineage []lineageCommit, brancEntryLimit int, lowestCommitId, topCommitID CommitID, prefixLen int) []sq.SelectBuilder {
+	rowSelect := sq.Select("branch_id as BranchID", "min_commit as minCommit").
+		Column("substr(path,?) as path", prefixLen+1).
 		From("entries").
 		OrderBy("branch_id", "path", "min_commit desc").
 		Limit(uint64(brancEntryLimit))
@@ -252,7 +191,7 @@ func buildBaseLevelQuery(baseBranchID int64, lineage []lineageCommit, brancEntry
 
 	for i, l := range lineage {
 		unionParts[i+1] = rowSelect.
-			Column("CASE WHEN max_commit >= ? THEN max_commit_id() ELSE max_commit END AS maxCommit").
+			Column("CASE WHEN max_commit >= ? THEN max_commit_id() ELSE max_commit END AS maxCommit", l.CommitID).
 			Where("branch_id = ?", l.BranchID).
 			Where("min_commit between 1 and  ? and (max_commit > ? or max_commit = 0)", l.CommitID, l.CommitID)
 
@@ -271,8 +210,6 @@ func loadEntriesIntoMarkerList(markerList []string, tx db.Tx, branchID int64, co
 	var run entryRun
 	entries := make([]LevelEntry, len(markerList))
 	for i, p := range markerList {
-		// remove termination character, if present
-		p = strings.TrimSuffix(p, DirectoryTermination)
 		entries[i].Path = prefix + p
 		if strings.HasSuffix(p, delimiter) { // terminating by '/'(slash) character is an indication of a directory
 			entries[i].CommonLevel = true
