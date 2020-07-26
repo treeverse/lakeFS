a6c7b645b3c96fb25a0784fcc553f4b093c13681
johnnyaug <johnnyaug@gmail.com>
2020-07-26 20:11:41 +0300

    loadtests: handle storage namespace (#301)
    Co-authored-by: yoni <yoni.augarten@treeverse.io>
    Former-commit-id: e80f8f995fc9be0c3a6289ba5f0edd28b8a1586a

--- cmd/lakefs-loadtest/cmd/run.go
+++ cmd/lakefs-loadtest/cmd/run.go
@@ -14,11 +14,12 @@ import (
 )
 
 const (
-	DurationFlag   = "duration"
-	FrequencyFlag  = "freq"
-	RepoNameFlag   = "repo"
-	KeepFlag       = "keep"
-	MaxWorkersFlag = "max-workers"
+	DurationFlag         = "duration"
+	FrequencyFlag        = "freq"
+	RepoNameFlag         = "repo"
+	StorageNamespaceFlag = "storage-namespace"
+	KeepFlag             = "keep"
+	MaxWorkersFlag       = "max-workers"
 )
 
 // runCmd represents the run command
@@ -32,16 +33,28 @@ var runCmd = &cobra.Command{
 			fmt.Println(err)
 			os.Exit(1)
 		}
+		storageNamespace, err := cmd.Flags().GetString(StorageNamespaceFlag)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if (repoName == "" && storageNamespace == "") || (repoName != "" && storageNamespace != "") {
+			fmt.Println(fmt.Sprintf("Exactly one of the following flags should be specified: "+
+				"%s (to test on a new repo) or %s (to test on an existing repo)",
+				StorageNamespaceFlag, RepoNameFlag))
+			os.Exit(1)
+		}
 		duration, _ := cmd.Flags().GetDuration(DurationFlag)
 		maxWorkers, _ := cmd.Flags().GetUint64(MaxWorkersFlag)
 		requestsPerSeq, _ := cmd.Flags().GetInt(FrequencyFlag)
 		isKeep, _ := cmd.Flags().GetBool(KeepFlag)
 		testConfig := loadtest.Config{
-			FreqPerSecond: requestsPerSeq,
-			Duration:      duration,
-			MaxWorkers:    maxWorkers,
-			RepoName:      repoName,
-			KeepRepo:      isKeep,
+			FreqPerSecond:    requestsPerSeq,
+			Duration:         duration,
+			MaxWorkers:       maxWorkers,
+			RepoName:         repoName,
+			StorageNamespace: storageNamespace,
+			KeepRepo:         isKeep,
 			Credentials: model.Credential{
 				AccessKeyId:     viper.GetString(ConfigAccessKeyId),
 				AccessSecretKey: viper.GetString(ConfigSecretAccessKey),
@@ -60,6 +73,7 @@ var runCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(runCmd)
 	runCmd.Flags().StringP(RepoNameFlag, "r", "", "Existing lakeFS repo name to use. Leave empty to create a dedicated repo")
+	runCmd.Flags().StringP(StorageNamespaceFlag, "s", "", "Storage namespace where data for the new repository will be stored (e.g.: s3://example-bucket/)")
 	runCmd.Flags().Bool(KeepFlag, false, "Do not delete repo at the end of the test")
 	runCmd.Flags().IntP(FrequencyFlag, "f", 5, "Number of requests to send per second")
 	runCmd.Flags().DurationP(DurationFlag, "d", 30*time.Second, "Duration of test")
