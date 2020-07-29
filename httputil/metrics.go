8bad17cf910fe2d0ea239931afc6770bdba7d700
yoni <yoni.augarten@treeverse.io>
2020-07-29 11:07:26 +0300

    typo

--- httputil/metrics.go
+++ httputil/metrics.go
@@ -11,7 +11,7 @@ func NewMetricResponseWriter(w http.ResponseWriter) *MetricResponseWriter {
 	return &MetricResponseWriter{w, http.StatusOK}
 }
 
-func (lrw *MetricResponseWriter) WriteHeader(code int) {
-	lrw.StatusCode = code
-	lrw.ResponseWriter.WriteHeader(code)
+func (mrw *MetricResponseWriter) WriteHeader(code int) {
+	mrw.StatusCode = code
+	mrw.ResponseWriter.WriteHeader(code)
 }
