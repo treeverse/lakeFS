67267d5f9b9394076db6ee7e0d88877e5757db80
johnnyaug <johnnyaug@gmail.com>
2020-07-30 16:57:03 +0300

    revert test file (#353)
    Former-commit-id: 12f98f312cfa2e14aec7952c94b92afadac29cf8

--- db/connect.go
+++ db/connect.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/dlmiddlecote/sqlstats"
 	"github.com/jmoiron/sqlx"
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/treeverse/lakefs/logging"
 )
 
@@ -27,14 +25,5 @@ func ConnectDB(driver string, uri string) (Database, error) {
 	conn.SetConnMaxLifetime(5 * time.Minute)
 
 	log.Info("initialized DB connection")
-	registerPrometheusCollector(conn)
 	return NewSqlxDatabase(conn), nil
-}
-
-func registerPrometheusCollector(conn *sqlx.DB) {
-	collector := sqlstats.NewStatsCollector("lakefs", conn)
-	err := prometheus.Register(collector)
-	if err != nil {
-		logging.Default().WithError(err).Error("failed to register db stats collector")
-	}
 }
