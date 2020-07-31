eb48ead6af5b393fe754178827806b657eaaabfa
guyhardonag <guy.hardonag@treeverse.io>
2020-07-31 17:13:08 +0300

    change order and add missing drops
    Former-commit-id: df166f5fb140b09b45ad94480e6a2fb1526ed08a

--- ddl/000001_initial.down.sql
+++ ddl/000001_initial.down.sql
@@ -1,22 +1,26 @@
-DROP TABLE IF EXISTS catalog_auth_credentials;
-DROP TABLE IF EXISTS catalog_auth_group_policies;
-DROP TABLE IF EXISTS catalog_auth_user_policies;
-DROP TABLE IF EXISTS catalog_auth_user_groups;
-DROP TABLE IF EXISTS catalog_auth_policies;
-DROP TABLE IF EXISTS catalog_auth_groups;
-DROP TABLE IF EXISTS catalog_auth_users;
-DROP TABLE IF EXISTS catalog_auth_account_metadata;
+DROP TABLE IF EXISTS auth_credentials;
+DROP TABLE IF EXISTS auth_group_policies;
+DROP TABLE IF EXISTS auth_user_policies;
+DROP TABLE IF EXISTS auth_user_groups;
+DROP TABLE IF EXISTS auth_policies;
+DROP TABLE IF EXISTS auth_groups;
+DROP TABLE IF EXISTS auth_users;
+DROP TABLE IF EXISTS auth_installation_metadata;
 
-SET CONSTRAINTS catalog_repositories_branches_id_fk DEFERRED;
-DROP TABLE IF EXISTS catalog_repositories;
+ALTER TABLE IF EXISTS catalog_repositories
+    DROP CONSTRAINT IF EXISTS  catalog_repositories_branches_id_fk;
+
+DROP TABLE IF EXISTS catalog_repositories_config;
 DROP TABLE IF EXISTS catalog_object_dedup;
 DROP TABLE IF EXISTS catalog_multipart_uploads;
-DROP TABLE IF EXISTS catalog_lineage;
+DROP VIEW IF EXISTS catalog_entries_v;
 DROP TABLE IF EXISTS catalog_entries;
 DROP TABLE IF EXISTS catalog_commits;
 DROP TABLE IF EXISTS catalog_branches;
 DROP TYPE IF EXISTS catalog_merge_type;
 DROP TYPE IF EXISTS catalog_commit_status;
-DROP VIEW IF EXISTS catalog_entries_v;
+DROP TABLE IF EXISTS catalog_repositories;
 DROP SEQUENCE IF EXISTS catalog_repositories_id_seq;
 DROP SEQUENCE IF EXISTS catalog_branches_id_seq;
+DROP SEQUENCE IF EXISTS catalog_commit_id_seq;
+DROP FUNCTION IF EXISTS catalog_max_commit_id;
