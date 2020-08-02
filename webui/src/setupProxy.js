420d4d9efd098015b49fef84ff0c019718592adf
Barak Amar <barak.amar@treeverse.io>
2020-08-02 15:33:03 +0300

    dev proxy all routes (#380)
    Former-commit-id: 0a8ba9a6f537546ed8e11e6d049b82a32df13aed

--- webui/src/setupProxy.js
+++ webui/src/setupProxy.js
@@ -1,11 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:8000',
-            changeOrigin: true,
-        })
-    );
+    app.use('/', createProxyMiddleware({
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+    }))
 };
