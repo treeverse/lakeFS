dfa4d228cf6ef2f986849afa38cf265b516ef5c8
Itai Admi <itaiad200@gmail.com>
2020-08-02 17:31:33 +0300

    Add repo name with trailing slash test

--- gateway/handler_test.go
+++ gateway/handler_test.go
@@ -1,80 +1,39 @@
 package gateway_test
 
 import (
-	"reflect"
+	"github.com/stretchr/testify/assert"
+	"github.com/treeverse/lakefs/gateway/simulator"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
-
-	"github.com/treeverse/lakefs/gateway"
 )
 
-func TestSplitFirst(t *testing.T) {
-	cases := []struct {
-		Name          string
-		InputPath     string
-		InputParts    int
-		ResultSuccess bool
-		ResultSlice   []string
-	}{
-		{
-			Name:          "repo_only",
-			InputPath:     "/foo",
-			InputParts:    1,
-			ResultSuccess: true,
-			ResultSlice:   []string{"foo"},
-		},
-		{
-			Name:          "repo_branch_1",
-			InputPath:     "/foo/bar",
-			InputParts:    1,
-			ResultSuccess: true,
-			ResultSlice:   []string{"foo/bar"},
-		},
-		{
-			Name:          "repo_branch_split",
-			InputPath:     "/foo/bar",
-			InputParts:    2,
-			ResultSuccess: true,
-			ResultSlice:   []string{"foo", "bar"},
-		},
-		{
-			Name:          "repo_branch_path",
-			InputPath:     "/foo/bar/a/b/c",
-			InputParts:    3,
-			ResultSuccess: true,
-			ResultSlice:   []string{"foo", "bar", "a/b/c"},
-		},
-		{
-			Name:          "all_path",
-			InputPath:     "/foo/bar/a/b/c",
-			InputParts:    1,
-			ResultSuccess: true,
-			ResultSlice:   []string{"foo/bar/a/b/c"},
-		},
-		{
-			Name:          "empty match",
-			InputPath:     "/",
-			InputParts:    1,
-			ResultSuccess: false,
-			ResultSlice:   []string{""},
-		},
-		{
-			Name:          "not enough parts",
-			InputPath:     "/foo/bar",
-			InputParts:    3,
-			ResultSuccess: false,
-			ResultSlice:   []string{""},
-		},
-	}
+func setupTest(t *testing.T, method, target string, body io.Reader) *http.Response {
+	h, _ := getBasicHandler(t, &simulator.PlayBackMockConf{
+		BareDomain:      "example.com",
+		AccessKeyID:     "AKIAJLQLVNL2XZ7EH6RQ",
+		AccessSecretKey: "MockAccessSecretKey",
+		UserID:          1,
+		Region:          "MockRegion",
+	})
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(method, target, body)
+	req.Header["Content-Type"] = []string{"text/tab - separated - values"}
+	req.Header["X-Amz-Content-Sha256"] = []string{"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"}
+	req.Header["X-Amz-Date"] = []string{"20200517T093907Z"}
+	req.Header["Host"] = []string{"host.domain.com"}
+	req.Header["Authorization"] = []string{"AWS4-HMAC-SHA256 Credential=AKIAJLQLVNL2XZ7EH6RQ/20200517/us-east-1/s3/aws4_request, SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date, Signature=cdb193f2140d1d0c093adc7aba9a62bc3c75f84b117100888553115900f39223"}
+	h.ServeHTTP(rr, req)
+	return rr.Result()
+}
 
-	for _, cas := range cases {
-		t.Run(cas.Name, func(t *testing.T) {
-			gotSlice, gotSuccess := gateway.SplitFirst(cas.InputPath, cas.InputParts)
-			if gotSuccess != cas.ResultSuccess {
-				t.Fatalf("expected success = %v for split '%s', got %v", cas.ResultSuccess, cas.InputPath, gotSuccess)
-			}
-			if cas.ResultSuccess && !reflect.DeepEqual(gotSlice, cas.ResultSlice) {
-				t.Fatalf("expected parts = %+v for split '%s', got %+v", cas.ResultSlice, cas.InputPath, gotSlice)
-			}
-		})
-	}
+func TestPathWithTrailingSlash(t *testing.T) {
+	result := setupTest(t, http.MethodHead, "/example/", nil)
+	assert.Equal(t, 200, result.StatusCode)
+	bytes, err := ioutil.ReadAll(result.Body)
+	assert.NoError(t, err)
+	assert.Len(t, bytes, 0)
+	assert.Contains(t, result.Header, "X-Amz-Request-Id")
 }
