343b9e45fadba96029f47be0b5b18b420e227f90
tzahij <tzahij@hotmail.com>
2020-08-09 02:46:03 +0300

    list commits shows changes made by merge from son

--- catalog/cataloger_list_commits_test.go
+++ catalog/cataloger_list_commits_test.go
@@ -209,7 +209,7 @@ func setupListCommitsByBranchData(t *testing.T, ctx context.Context, c Cataloger
 		}, CreateEntryParams{}); err != nil {
 			t.Fatal("Write entry for list repository commits failed", err)
 		}
-		message := "commit" + strconv.Itoa(i+1)
+		message := "commit" + strconv.Itoa(i+1) + " on branch " + branch
 		commitLog, err := c.Commit(ctx, repository, branch, message, "tester", nil)
 		if err != nil {
 			t.Fatalf("Commit for list repository commits failed '%s': %s", message, err)
@@ -273,5 +273,116 @@ func TestCataloger_ListCommits_Lineage(t *testing.T) {
 	testutil.MustDo(t, "list br_1 commits", err)
 	if diff := deep.Equal(masterCommits[0], got[1]); diff != nil {
 		t.Error("br_1 did not inherit commits correctly", diff)
+	}
+}
+
+func TestCataloger_ListCommits_Lineage_from_son(t *testing.T) {
+	ctx := context.Background()
+	c := testCataloger(t)
+
+	repository := testCatalogerRepo(t, ctx, c, "repository", "master")
+	_ = setupListCommitsByBranchData(t, ctx, c, repository, "master")
+
+	testCatalogerBranch(t, ctx, c, repository, "br_1_1", "master")
+	testCatalogerBranch(t, ctx, c, repository, "br_1_2", "br_1_1")
+	masterCommits, _, err := c.ListCommits(ctx, repository, "master", "", 100)
+	testutil.MustDo(t, "list master commits", err)
+
+	br1Commits, _, err := c.ListCommits(ctx, repository, "br_1_1", "", 100)
+	testutil.MustDo(t, "list br_1_1 commits", err)
+
+	// get all commits without the first one
+	if diff := deep.Equal(masterCommits, br1Commits[1:]); diff != nil {
+		t.Error("br_1_1 did not inherit commits correctly", diff)
+	}
+
+	b2Commits, _, err := c.ListCommits(ctx, repository, "br_1_2", "", 100)
+	testutil.MustDo(t, "list br_1_2 commits", err)
+
+	if diff := deep.Equal(br1Commits, b2Commits[1:]); diff != nil {
+		t.Error("br_1_2 did not inherit commits correctly", diff)
+	}
+
+	if err := c.CreateEntry(ctx, repository, "master", Entry{
+		Path:            "master-file",
+		Checksum:        "ssss",
+		PhysicalAddress: "xxxxxxx",
+		Size:            10000,
+	}, CreateEntryParams{}); err != nil {
+		t.Fatal("Write entry for list repository commits failed", err)
+	}
+	_, err = c.Commit(ctx, repository, "master", "commit master-file on master", "tester", nil)
+	if err != nil {
+		t.Fatalf("Commit for list repository commits failed '%s': %s", "master commit failed", err)
+	}
+	_, err = c.Merge(ctx, repository, "master", "br_1_1", "tester", "merge master to br_1_1", nil)
+	testutil.MustDo(t, "merge master  into br_1_1", err)
+
+	got, _, err := c.ListCommits(ctx, repository, "br_1_2", "", 100)
+	testutil.MustDo(t, "list br_1_2 commits", err)
+	if diff := deep.Equal(got, b2Commits); diff != nil {
+		t.Error("br_1_2 changed although not merged", diff)
+	}
+	masterCommits, _, err = c.ListCommits(ctx, repository, "master", "", 100)
+	testutil.MustDo(t, "list master commits", err)
+
+	br_1_1_base_list, _, err := c.ListCommits(ctx, repository, "br_1_1", "", 100)
+	testutil.MustDo(t, "list br_1_1 commits", err)
+	if diff := deep.Equal(masterCommits[0], br_1_1_base_list[1]); diff != nil {
+		t.Error("br_1_1 did not inherit commits correctly", diff)
+	}
+
+	testCatalogerBranch(t, ctx, c, repository, "br_2_1", "master")
+	testCatalogerBranch(t, ctx, c, repository, "br_2_2", "br_2_1")
+	if err := c.CreateEntry(ctx, repository, "br_2_2", Entry{
+		Path:            "master-file",
+		Checksum:        "zzzzz",
+		PhysicalAddress: "yyyyy",
+		Size:            20000,
+	}, CreateEntryParams{}); err != nil {
+		t.Fatal("Write entry to br_2_2 failed", err)
+	}
+	_, err = c.Commit(ctx, repository, "br_2_2", "commit master-file to br_2_2", "tester", nil)
+	if err != nil {
+		t.Fatalf("Commit for list repository commits failed '%s': %s", "br_2_2  commit failed", err)
+	}
+	br_2_2_list, _, err := c.ListCommits(ctx, repository, "br_2_2", "", 100)
+	testutil.MustDo(t, "list br_2_2  commits", err)
+	_ = br_2_2_list
+	_, err = c.Merge(ctx, repository, "br_2_2", "br_2_1", "tester", "merge br_2_2 to br_2_1", nil)
+	testutil.MustDo(t, "merge br_2_2  into br_2_1", err)
+	br_2_1_list, _, err := c.ListCommits(ctx, repository, "br_2_1", "", 100)
+	testutil.MustDo(t, "list br_2_1  commits", err)
+	_ = br_2_1_list
+	masterList, _, err := c.ListCommits(ctx, repository, "master", "", 100)
+	testutil.MustDo(t, "list master commits", err)
+	if diff := deep.Equal(masterCommits, masterList); diff != nil {
+		t.Error("master commits changed befor merge", diff)
+	}
+	merge_2, err := c.Merge(ctx, repository, "br_2_1", "master", "tester", "merge br_2_1 to master", nil)
+	testutil.MustDo(t, "merge br_2_1  into master", err)
+	if merge_2.Differences[0].Type != DifferenceTypeChanged || merge_2.Differences[0].Path != "master-file" {
+		t.Error("merge br_2_1 into master with unexpected results", merge_2.Differences[0])
+	}
+	masterList, _, err = c.ListCommits(ctx, repository, "master", "", 100)
+	testutil.MustDo(t, "list master commits", err)
+	if diff := deep.Equal(br_2_1_list, masterList[1:]); diff != nil {
+		t.Error("master commits list mismatch with br_2_1_list", diff)
+	}
+
+	br_1_1_list, _, err := c.ListCommits(ctx, repository, "br_1_1", "", 100)
+	testutil.MustDo(t, "list br_1_1 commits", err)
+	if diff := deep.Equal(br_1_1_base_list, br_1_1_list); diff != nil {
+		t.Error("br_1_1 commits changed before merge", diff)
+	}
+	_, err = c.Merge(ctx, repository, "master", "br_1_1", "tester", "merge master to br_1_1", nil)
+	testutil.MustDo(t, "merge master  into br_1_1", err)
+	br_1_1_list, _, err = c.ListCommits(ctx, repository, "br_1_1", "", 100)
+	testutil.MustDo(t, "list br_1_1 commits", err)
+	if diff := deep.Equal(masterList[:5], br_1_1_list[1:6]); diff != nil {
+		t.Error("master 5 first different from br_1_1 [1:6]", diff)
+	}
+	if diff := deep.Equal(masterList[6:], br_1_1_list[9:]); diff != nil {
+		t.Error("master 5 first different from br_1_1 [1:6]", diff)
 	}
 }
