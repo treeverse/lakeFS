dcdc78781da7a384095ef5f7145337b70a912b97
tzahij <tzahij@hotmail.com>
2020-08-16 21:10:10 +0300

    improving read performance by batching read requests with minimal delay

--- catalog/db_batch_entry_read.go
+++ catalog/db_batch_entry_read.go
@@ -18,7 +18,6 @@ const (
 	waitTimeout          = time.Microsecond * 1000
 	MaxEnteriesInRequest = 64
 	ReadersNum           = 6
-	ReadsPerConnection   = 100
 )
 
 type pathRequest struct {
@@ -114,64 +113,11 @@ func readOrchestrator(c *cataloger) {
 		for k, v := range bufferingMap {
 			if len(v.pathList) == MaxEnteriesInRequest || time.Now().Sub(v.startTime) > waitTimeout {
 				batchChan <- batchReadMessage{k, v.pathList}
-				//readEntriesBatch(c, k, v.pathList)
 				delete(bufferingMap, k)
 			}
 		}
 	}
 }
-
-//func readEntriesBatch(c *cataloger, bufKey bufferingKey, pathReqList []pathRequest) {
-//	//ctx := context.Background()
-//	branchID, err := c.getBranchIDCache(c.db, bufKey.repository, bufKey.ref.Branch)
-//	if err != nil {
-//		fmt.Printf("in readEntriesBatch- getBranchIDcache: %w", err)
-//		return
-//	}
-//
-//	lineage, err := getLineage(c.db, branchID, bufKey.ref.CommitID)
-//	if err != nil {
-//		fmt.Printf("in readEntriesBatch- getLineage: %w", err)
-//		return
-//	}
-//
-//	//var lineage = []lineageCommit{{3, 14}, {2, 6}, {1, 5}}
-//
-//	p := make([]string, len(pathReqList))
-//	for i, s := range pathReqList {
-//		p[i] = s.path
-//	}
-//	pathInExper := "('" + strings.Join(p, "','") + "')"
-//	inExper := sq.Select("path", "physical_address", "creation_date", "size", "checksum", "metadata", "is_expired").
-//		FromSelect(sqEntriesLineage(branchID, bufKey.ref.CommitID, lineage), "entries").
-//		Where("path in " + pathInExper + " and not is_deleted")
-//	deb := sq.DebugSqlizer(inExper)
-//	_ = deb
-//	sql, args, err := inExper.PlaceholderFormat(sq.Dollar).ToSql()
-//	if err != nil {
-//		fmt.Printf("in readEntriesBatch- ToSql: %w", err)
-//		return
-//	}
-//
-//	var entList []*Entry
-//	if err := c.db.Select(&entList, sql, args...); err != nil {
-//		fmt.Printf("in readEntriesBatch- Select: %w", err)
-//		return
-//	}
-//	entMap := make(map[string]*Entry)
-//	for _, ent := range entList {
-//		entMap[ent.Path] = ent
-//	}
-//	for _, pathReq := range pathReqList {
-//		e, exists := entMap[pathReq.path]
-//		if exists {
-//			pathReq.replyChan <- readResponse{e, nil}
-//		} else {
-//			pathReq.replyChan <- readResponse{nil, db.ErrNotFound}
-//		}
-//
-//	}
-//}
 
 func readEntriesBatch(c *cataloger, batchRead chan batchReadMessage) {
 	ctx := context.Background()
@@ -190,8 +136,6 @@ func readEntriesBatch(c *cataloger, batchRead chan batchReadMessage) {
 			if err != nil {
 				return nil, fmt.Errorf("get lineage: %w", err)
 			}
-
-			//var lineage = []lineageCommit{{3, 14}, {2, 6}, {1, 5}}
 
 			p := make([]string, len(pathReqList))
 			for i, s := range pathReqList {
@@ -229,58 +173,5 @@ func readEntriesBatch(c *cataloger, batchRead chan batchReadMessage) {
 			//}
 			return nil, nil
 		}, c.txOpts(ctx, db.ReadOnly(), db.WithIsolationLevel(sql.LevelReadCommitted))...)
-		//fmt.Print("finished 500 reads in loop\n")
 	}
 }
-
-//func readEntriesBatch(c *cataloger, bufKey bufferingKey, pathReqList []pathRequest) {
-//	ctx := context.Background()
-//	_, _ = c.db.Transact(func(tx db.Tx) (interface{}, error) {
-//
-//		branchID, err := c.getBranchIDCache(tx, bufKey.repository, bufKey.ref.Branch)
-//		if err != nil {
-//			return nil, err
-//		}
-//
-//		lineage, err := getLineage(tx, branchID, bufKey.ref.CommitID)
-//		if err != nil {
-//			return nil, fmt.Errorf("get lineage: %w", err)
-//		}
-//
-//		//var lineage = []lineageCommit{{3, 14}, {2, 6}, {1, 5}}
-//
-//		p := make([]string, len(pathReqList))
-//		for i, s := range pathReqList {
-//			p[i] = s.path
-//		}
-//		pathInExper := "('" + strings.Join(p, "','") + "')"
-//		inExper := sq.Select("path", "physical_address", "creation_date", "size", "checksum", "metadata", "is_expired").
-//			FromSelect(sqEntriesLineage(branchID, bufKey.ref.CommitID, lineage), "entries").
-//			Where("path in " + pathInExper + " and not is_deleted")
-//		deb := sq.DebugSqlizer(inExper)
-//		_ = deb
-//		sql, args, err := inExper.PlaceholderFormat(sq.Dollar).ToSql()
-//		if err != nil {
-//			return nil, fmt.Errorf("build sql: %w", err)
-//		}
-//
-//		var entList []*Entry
-//		if err := tx.Select(&entList, sql, args...); err != nil {
-//			return nil, err
-//		}
-//		entMap := make(map[string]*Entry)
-//		for _, ent := range entList {
-//			entMap[ent.Path] = ent
-//		}
-//		for _, pathReq := range pathReqList {
-//			e, exists := entMap[pathReq.path]
-//			if exists {
-//				pathReq.replyChan <- readResponse{e, nil}
-//			} else {
-//				pathReq.replyChan <- readResponse{nil, db.ErrNotFound}
-//			}
-//
-//		}
-//		return nil, nil
-//	}, c.txOpts(ctx, db.ReadOnly(), db.WithIsolationLevel(sql.LevelReadCommitted))...)
-//}
