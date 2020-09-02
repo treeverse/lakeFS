f20626016e3a51c0ced91f9e01a136e64933c45c
Barak Amar <barak.amar@treeverse.io>
2020-09-02 12:41:02 +0300

    remove old scripts from repo (#553)

--- scripts/get-repository.sql
+++
@@ -1,3 +0,0 @@
-SELECT r.name, r.storage_namespace, b.name AS default_branch, r.creation_date
-		 	FROM repositories r, branches b
-		 	WHERE r.id = b.repository_id AND r.default_branch = b.id AND r.name = 'repo1'
