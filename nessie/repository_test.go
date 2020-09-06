5d4572987351371127ed08525a76deb2ec24aa68
Barak Amar <barak.amar@treeverse.io>
2020-09-06 13:07:02 +0300

    sanity test using Nessie (#559)

---
+++ nessie/repository_test.go
@@ -0,0 +1,34 @@
+package nessie
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/treeverse/lakefs/api/gen/client/repositories"
+)
+
+func TestRepositoryBasicOps(t *testing.T) {
+	ctx := context.Background()
+	const numOfRepos = 5
+
+	// create repositories
+	repos := make([]string, numOfRepos)
+	for i := range repos {
+		repos[i] = createRepositoryUnique(ctx, t)
+	}
+
+	// list repositories - make sure we have the ones we created
+	listedRepos := listRepositoriesIDs(t, ctx)
+	for _, repo := range repos {
+		require.Contains(t, listedRepos, repo, "repository missing in listing")
+	}
+
+	// delete repositories
+	for _, repo := range repos {
+		_, err := client.Repositories.DeleteRepository(
+			repositories.NewDeleteRepositoryParamsWithContext(ctx).
+				WithRepository(repo), nil)
+		require.NoErrorf(t, err, "failed to delete repository %s, storage %s", repo)
+	}
+}
