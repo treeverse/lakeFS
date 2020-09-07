dbf83b7e0004e1ddc28ead609fe6fe0474960405
tzahij <tzahij@hotmail.com>
2020-09-07 16:03:09 +0300

    remove reference to public

--- ddl/000005_index.up.sql
+++ ddl/000005_index.up.sql
@@ -2,11 +2,11 @@ alter table catalog_entries drop constraint if exists catalog_entries_pk;
 drop index if exists catalog_fki_entries_branches_fk;
 alter table catalog_entries drop constraint if exists catalog_entries_branches_fk;
 CREATE UNIQUE INDEX catalog_entries_uidx
-    ON public.catalog_entries USING btree
+    ON catalog_entries USING btree
         (branch_id ASC NULLS LAST, path COLLATE pg_catalog."C" ASC NULLS LAST, min_commit DESC NULLS FIRST)
     INCLUDE(max_commit);
 CREATE INDEX commits_lineage_idx
-    ON public.catalog_commits USING btree
+    ON catalog_commits USING btree
         (merge_type ASC NULLS LAST, branch_id ASC NULLS LAST, commit_id DESC NULLS LAST)
     INCLUDE(lineage_commits, merge_source_branch, merge_source_commit)
     WHERE merge_type = ANY (ARRAY['from_parent'::catalog_merge_type, 'from_child'::catalog_merge_type]);
