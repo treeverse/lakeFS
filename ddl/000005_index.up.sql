d11822e6cd76af51b9f46c318eb39451ec7ad52f
Barak Amar <barak.amar@treeverse.io>
2020-09-07 15:33:57 +0300

    modify main unique indexe on entities remove branch index and constraint add index on commits

--- ddl/000005_index.up.sql
+++ ddl/000005_index.up.sql
@@ -1,3 +1,12 @@
-alter table catalog_entries drop constraint catalog_entries_pk if exists
-
-drop index catalog_fki_entries_branches_fk if exists
+alter table catalog_entries drop constraint if exists catalog_entries_pk;
+drop index if exists catalog_fki_entries_branches_fk;
+alter table catalog_entries drop constraint if exists catalog_entries_branches_fk;
+CREATE UNIQUE INDEX catalog_entries_uidx
+    ON public.catalog_entries USING btree
+        (branch_id ASC NULLS LAST, path COLLATE pg_catalog."C" ASC NULLS LAST, min_commit DESC NULLS FIRST)
+    INCLUDE(max_commit);
+CREATE INDEX commits_lineage_idx
+    ON public.catalog_commits USING btree
+        (merge_type ASC NULLS LAST, branch_id ASC NULLS LAST, commit_id DESC NULLS LAST)
+    INCLUDE(lineage_commits, merge_source_branch, merge_source_commit)
+    WHERE merge_type = ANY (ARRAY['from_parent'::catalog_merge_type, 'from_child'::catalog_merge_type]);
