11d24a2f8b0dd7eb3a210d42f81bb7ee72eba003
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2020-09-08 11:32:42 +0300

    Replace task_dependencies with just a task count
    *Much* faster: 50-80 seconds for a 50_000 task test with 5 concurrent
    connections.  If scaling persists then we can do 1_000_000 tasks in
    1000-1600 seconds, i.e. <30 minutes.
    
    This is a fairly major change.  It makes debugging tasks that never
    got started a bit harder (there is still a to_signal column, but it is
    hard to determine which tasks must *precede* a given task).

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -15,12 +15,21 @@ CREATE TABLE IF NOT EXISTS tasks (
     id VARCHAR(64) NOT NULL PRIMARY KEY, -- nanoid
 
     action VARCHAR(128) NOT NULL, -- name (type) of action to perform
-    body JSONB,			-- data used by action
+    body TEXT,			-- data used by action
     status TEXT,		-- status text defined by action, visible to action
 
     status_code TASK_STATUS_CODE_VALUE NOT NULL DEFAULT 'pending', -- internal status code, used by parade to issue tasks
     num_tries INTEGER NOT NULL DEFAULT 0,
     max_tries INTEGER,
+
+    total_dependencies INTEGER,	-- number of tasks that must signal this task
+    num_signals INTEGER NOT NULL DEFAULT 0, -- number of tasks that have already signalled this task
+
+    -- BUG(ariels): add REFERENCES dependency to each of the to_signal
+    --     tasks.  Or at least add triggers that perform ON DELETE
+    --     CASCADE.
+    to_signal VARCHAR(64) ARRAY, -- IDs to signal after performing this task
+
     actor_id VARCHAR(64),    -- ID of performing actor if in-progress
     action_deadline TIMESTAMPTZ, -- offer this task to other actors once action_deadline has elapsed
     performance_token UUID,
@@ -29,24 +38,13 @@ CREATE TABLE IF NOT EXISTS tasks (
 --     task, and accept task completion only when lock token is unchanged.
 );
 
-CREATE TABLE IF NOT EXISTS task_dependencies (
-    after VARCHAR(64) REFERENCES tasks(id) -- after this task ID is done
-        ON DELETE CASCADE ON UPDATE CASCADE,
-    run VARCHAR(64) REFERENCES tasks(id)	-- run this task ID
-        ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE INDEX IF NOT EXISTS task_dependencies_after ON task_dependencies(after);
-CREATE INDEX IF NOT EXISTS task_dependencies_run ON task_dependencies(run);
-ALTER TABLE task_dependencies ALTER run SET STATISTICS 1000;
-
 -- Returns true if task with this id, code and deadline can
 -- be allocated.
-CREATE OR REPLACE FUNCTION can_allocate_task(id VARCHAR(64), code TASK_STATUS_CODE_VALUE, deadline TIMESTAMPTZ)
+CREATE OR REPLACE FUNCTION can_allocate_task(id VARCHAR(64), code TASK_STATUS_CODE_VALUE, deadline TIMESTAMPTZ, num_signals INTEGER, total_dependencies INTEGER)
 RETURNS BOOLEAN
 LANGUAGE sql IMMUTABLE AS $$
     SELECT (code = 'pending' OR (code = 'in-progress' AND deadline < NOW())) AND
-        id NOT IN (SELECT DISTINCT run AS id FROM task_dependencies)
+        (total_dependencies IS NULL OR num_signals = total_dependencies)
 $$;
 
 -- Marks up to `max_tasks' on one of `actions' as in-progress and
@@ -55,7 +53,7 @@ $$;
 CREATE OR REPLACE FUNCTION own_tasks(
     max_tasks INTEGER, actions VARCHAR(128) ARRAY, owner_id VARCHAR(64), max_duration INTERVAL
 )
-RETURNS TABLE(task_id VARCHAR(64), token UUID, body jsonb)
+RETURNS TABLE(task_id VARCHAR(64), token UUID, body TEXT)
 LANGUAGE sql VOLATILE AS $$
     UPDATE tasks
     SET actor_id = owner_id,
@@ -66,8 +64,8 @@ LANGUAGE sql VOLATILE AS $$
     WHERE id IN (
         SELECT id
 	FROM tasks
-        WHERE can_allocate_task(id, status_code, action_deadline) AND
-	    ARRAY[action] <@ actions AND
+        WHERE can_allocate_task(id, status_code, action_deadline, num_signals, total_dependencies) AND
+	    action = ANY(actions) AND
 	    (max_tries IS NULL OR num_tries < max_tries)
 	-- maybe: AND not_before <= NOW()
 	-- maybe: ORDER BY priority (eventually)
@@ -86,22 +84,25 @@ LANGUAGE plpgsql AS $$
 DECLARE
     num_updated INTEGER;
     channel VARCHAR(64);
+    tasks_to_signal VARCHAR(64) ARRAY;
 BEGIN
-    UPDATE tasks INTO channel
+    UPDATE tasks INTO channel, tasks_to_signal
     SET status = result_status,
     	status_code = result_status_code,
 	actor_id = NULL,
 	performance_token = NULL
     WHERE id = task_id AND performance_token = token
-    RETURNING finish_channel;
+    RETURNING finish_channel, to_signal;
 
     GET DIAGNOSTICS num_updated := ROW_COUNT;
-
-    DELETE FROM task_dependencies WHERE after=task_id;
 
     IF channel IS NOT NULL THEN
         SELECT pg_notify(channel, NULL);
     END IF;
+
+    UPDATE tasks
+    SET num_signals = num_signals+1
+    WHERE id = ANY(tasks_to_signal);
 
     RETURN num_updated;
 END;
