06edd99b7391d9a71c68b3e27fa7f40caca2568e
tzahij <tzahij@hotmail.com>
2020-09-08 10:33:02 +0300

    modify indexes

--- ddl/000005_index.up.sql
+++ ddl/000005_index.up.sql
@@ -1,6 +1,8 @@
-alter table catalog_entries drop constraint if exists catalog_entries_pk;
-drop index if exists catalog_fki_entries_branches_fk;
-alter table catalog_entries drop constraint if exists catalog_entries_branches_fk;
+
+BEGIN;
+ALTER TABLE catalog_entries DROP CONSTRAINT IF EXISTS catalog_entries_pk;
+DROP INDEX IF EXISTS catalog_fki_entries_branches_fk;
+ALTER TABLE catalog_entries DROP CONSTRAINT IF EXISTS catalog_entries_branches_fk;
 CREATE UNIQUE INDEX catalog_entries_uidx
     ON catalog_entries USING btree
         (branch_id ASC NULLS LAST, path COLLATE pg_catalog."C" ASC NULLS LAST, min_commit DESC NULLS FIRST)
@@ -10,3 +12,4 @@ CREATE INDEX commits_lineage_idx
         (merge_type ASC NULLS LAST, branch_id ASC NULLS LAST, commit_id DESC NULLS LAST)
     INCLUDE(lineage_commits, merge_source_branch, merge_source_commit)
     WHERE merge_type = ANY (ARRAY['from_parent'::catalog_merge_type, 'from_child'::catalog_merge_type]);
+COMMIT;
