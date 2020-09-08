d89b204a9b009db9ade0a6d0f054f231b59a33f9
Barak Amar <barak.amar@treeverse.io>
2020-09-08 10:33:02 +0300

    modify indexes

--- ddl/000005_index.down.sql
+++ ddl/000005_index.down.sql
@@ -1,7 +1,9 @@
-drop index catalog_entries_uidx;
-drop index commits_lineage_idx;
+BEGIN;
+DROP INDEX catalog_entries_uidx;
+DROP INDEX commits_lineage_idx;
 ALTER TABLE ONLY catalog_entries
     ADD CONSTRAINT catalog_entries_branches_fk FOREIGN KEY (branch_id) REFERENCES catalog_branches(id) ON DELETE CASCADE NOT VALID;
 CREATE INDEX catalog_fki_entries_branches_fk ON catalog_entries USING btree (branch_id);
 ALTER TABLE ONLY catalog_entries
     ADD CONSTRAINT catalog_entries_pk PRIMARY KEY (path, branch_id, min_commit) INCLUDE (max_commit);
+COMMIT;
