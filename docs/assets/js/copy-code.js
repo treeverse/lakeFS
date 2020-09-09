ce9e4c214f49edbafeaa7e0cfbb872d38a7dd9dc
johnnyaug <yoni.augarten@treeverse.io>
2020-09-09 14:38:32 +0300

    copy button - docs

--- docs/assets/js/copy-code.js
+++ docs/assets/js/copy-code.js
@@ -1,14 +1,19 @@
-$("div.highlighter-rouge").prepend("<div class=\"copy-code-container\"><button class=\"copy-code-button\" aria-label=\"Copy code block to your clipboard\"></button></div>")
-
-$("div.highlighter-rouge .copy-code-button").click((e)=> {
-    const tempTextArea = document.createElement('textarea');
-    tempTextArea.textContent = $(e.target).parent().parent().find("code").text()
-    document.body.appendChild(tempTextArea);
-    const selection = document.getSelection();
-    selection.removeAllRanges();
-    tempTextArea.select();
-    document.execCommand('copy');
-    selection.removeAllRanges();
-    document.body.removeChild(tempTextArea);
-
+$(() => {
+    let copyCodeContainer = $("<div class=\"copy-code-container\">" +
+        "<button class=\"copy-code-button\" aria-label=\"Copy code block to your clipboard\">" +
+        "<i class=\"far fa-copy\"></i>" +
+        "</button></div>");
+    $("div.highlighter-rouge").prepend(copyCodeContainer);
+    $("div.highlighter-rouge .copy-code-button").click(function() {
+        const tempTextArea = document.createElement('textarea');
+        console.log($(this))
+        tempTextArea.textContent = $(this).parent().parent().find("code").text()
+        document.body.appendChild(tempTextArea);
+        const selection = document.getSelection();
+        selection.removeAllRanges();
+        tempTextArea.select();
+        document.execCommand('copy');
+        selection.removeAllRanges();
+        document.body.removeChild(tempTextArea);
+    });
 });
