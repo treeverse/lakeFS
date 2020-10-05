af8678910caf30a039c766859965ba28d55273b1
Barak Amar <barak.amar@treeverse.io>
2020-10-05 11:31:32 +0300

    branch iterator without initialBuf

--- catalog/db_branch_reader.go
+++ catalog/db_branch_reader.go
@@ -8,24 +8,25 @@ import (
 )
 
 type DBBranchReader struct {
-	tx         db.Tx
-	branchID   int64
-	initialBuf []*DBReaderEntry
-	buf        []*DBReaderEntry
-	bufSize    int
-	EOF        bool
-	after      string
-	commitID   CommitID
+	tx       db.Tx
+	branchID int64
+	buf      []*DBReaderEntry
+	bufSize  int
+	idx      int
+	EOF      bool
+	after    string
+	commitID CommitID
 }
 
 func NewDBBranchReader(tx db.Tx, branchID int64, commitID CommitID, bufSize int, after string) *DBBranchReader {
 	return &DBBranchReader{
-		tx:         tx,
-		branchID:   branchID,
-		bufSize:    bufSize,
-		initialBuf: make([]*DBReaderEntry, 0, bufSize),
-		after:      after,
-		commitID:   commitID,
+		tx:       tx,
+		branchID: branchID,
+		buf:      make([]*DBReaderEntry, 0, bufSize),
+		bufSize:  bufSize,
+		after:    after,
+		idx:      0,
+		commitID: commitID,
 	}
 }
 
@@ -37,13 +38,14 @@ func (r *DBBranchReader) Next() (*DBReaderEntry, error) {
 	if r.EOF {
 		return nil, nil
 	}
-	if len(r.buf) == 0 {
-		r.buf = r.initialBuf
+	if r.idx >= len(r.buf) {
 		q := sqBranchReaderSelectWithCommitID(r.branchID, r.commitID).Limit(uint64(r.bufSize)).Where("path > ?", r.after)
 		sql, args, err := q.PlaceholderFormat(sq.Dollar).ToSql()
 		if err != nil {
 			return nil, fmt.Errorf("next query format: %w", err)
 		}
+		r.idx = 0
+		r.buf = r.buf[:0]
 		err = r.tx.Select(&r.buf, sql, args...)
 		if err != nil {
 			return nil, fmt.Errorf("next select: %w", err)
@@ -53,12 +55,12 @@ func (r *DBBranchReader) Next() (*DBReaderEntry, error) {
 		r.EOF = true
 		return nil, nil
 	}
-	nextPk := r.buf[0]
+	nextPk := r.buf[r.idx]
+	r.idx++
 	// if entry was deleted after the max commit that can be read, it must be set to undeleted
 	if r.shouldAlignMaxCommit() && nextPk.MaxCommit >= r.commitID {
 		nextPk.MaxCommit = MaxCommitID
 	}
-	r.buf = r.buf[1:]
 	r.after = nextPk.Path
 	return nextPk, nil
 }
