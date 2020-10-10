657eb6f3e29577c88734c025f2aedc50bc62a4e9
tzahij <tzahij@hotmail.com>
2020-10-10 19:17:55 +0300

    with Linter fixes

--- catalog/db_read_entry.go
+++ catalog/db_read_entry.go
@@ -7,17 +7,21 @@ import (
 	"github.com/treeverse/lakefs/db"
 )
 
-func LineageSelect(tx db.Tx, branchID int64, commitID CommitID, filterDeleted bool, paths []string) (sq.SelectBuilder, error) {
+// EntryLineageSelect select path/s from a branch, including lineage
+// 1. Union all the branches in the lineage.
+// 2. if multiple branches have this path - select the one closest to the requested branch
+// 3. filterDeleted param = true will remove results that were deleted
+func EntryLineageSelect(tx db.Tx, branchID int64, commitID CommitID, filterDeleted bool, paths []string) (sq.SelectBuilder, error) {
 	lineage, err := getLineage(tx, branchID, commitID)
 	if err != nil {
 		return sq.SelectBuilder{}, err
 	}
-	unionSelect := singleBranchSelect(branchID, paths, commitID).Column("? as lineage_order", "0").
+	unionSelect := EntryBranchSelect(branchID, commitID, paths).Column("? as lineage_order", "0").
 		Prefix("(").Suffix(")")
 	for i, branch := range lineage {
-		unionSelect.SuffixExpr(sq.ConcatExpr(
+		unionSelect = unionSelect.SuffixExpr(sq.ConcatExpr(
 			" UNION ALL (",
-			singleBranchSelect(branch.BranchID, paths, branch.CommitID).Column("? as lineage_order", strconv.Itoa(i+1)),
+			EntryBranchSelect(branch.BranchID, branch.CommitID, paths).Column("? as lineage_order", strconv.Itoa(i+1)),
 			")"))
 	}
 	distinctSelect := sq.Select("*").
@@ -32,13 +36,20 @@ func LineageSelect(tx db.Tx, branchID int64, commitID CommitID, filterDeleted bo
 	return finalSelect, nil
 }
 
-func singleBranchSelect(branchID int64, paths []string, commitID CommitID) sq.SelectBuilder {
+//  EntryBranchSelect select path/s from a single branch.
+//  1. Get the requested commit
+//  2. If a path have multiple versions in various commits- Select only the relevant one -  with Highest min commit
+//	3. if the version was deleted after the requested commit - the row will be set to uncommitted
+func EntryBranchSelect(branchID int64, commitID CommitID, paths []string) sq.SelectBuilder {
 	rawSelect := sq.Select("path", "physical_address", "creation_date", "size", "checksum", "metadata", "is_expired").
 		Distinct().Options("ON (branch_id,path)").
 		From("catalog_entries").
 		Where("branch_id = ?", branchID).
 		OrderBy("branch_id", "path", "min_commit desc")
 	l := len(paths)
+	// the case of single path is handled differently than multiple pathes because my tests showed there is a significant
+	// difference between (about 25%) between "where path = X" and "where path in (X)". even though the optimization is
+	// the same. may be a driver issue in the client application
 	if l == 1 {
 		rawSelect = rawSelect.Where("path = ?", paths[0])
 	} else {
