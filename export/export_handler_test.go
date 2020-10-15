8bb9f38e460a9d453b24d620c3306801b0ec6602
Barak Amar <barak.amar@treeverse.io>
2020-10-15 14:46:32 +0300

    align export handler with task_generator

--- export/export_handler_test.go
+++ export/export_handler_test.go
@@ -11,100 +11,141 @@ import (
 	"testing"
 )
 
-func TestExportHandler_Handle(t *testing.T) {
-	tests := []struct {
-		name           string
-		Body           TaskBody
-		Action         string
-		blockstoreType string
-	}{
-		{
-			name:   "copy on mem",
-			Action: actionCopy,
-			Body: TaskBody{
-				DestinationNamespace: "mem://external-bucket",
-				DestinationID:        "one/two",
-				SourceNamespace:      "mem://lakefs-buck",
-				SourceID:             "one/two",
-			},
-			blockstoreType: mem.BlockstoreType,
-		},
-		{
-			name:   "delete on mem",
-			Action: actionDelete,
-			Body: TaskBody{
-				DestinationNamespace: "mem://external-bucket",
-				DestinationID:        "one/two",
-			},
-			blockstoreType: mem.BlockstoreType,
-		},
-		{
-			name:   "touch on mem",
-			Action: actionTouch,
-			Body: TaskBody{
-				DestinationNamespace: "mem://external-bucket",
-				DestinationID:        "one/two",
-			},
-			blockstoreType: mem.BlockstoreType,
-		},
+func TestCopy(t *testing.T) {
+	adapter := testutil.NewBlockAdapterByType(t, &block.NoOpTranslator{}, mem.BlockstoreType)
+	sourcePointer := block.ObjectPointer{
+		StorageNamespace: "mem://lakeFS-bucket",
+		Identifier:       "/one/two",
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			adapter := testutil.NewBlockAdapterByType(t, &block.NoOpTranslator{}, tt.blockstoreType)
-			sourcePointer := block.ObjectPointer{
-				StorageNamespace: tt.Body.SourceNamespace,
-				Identifier:       tt.Body.SourceID,
-			}
-			destinationPointer := block.ObjectPointer{
-				StorageNamespace: tt.Body.DestinationNamespace,
-				Identifier:       tt.Body.DestinationID,
-			}
-			// add to
-			testData := "this is the test Data"
-			testReader := strings.NewReader(testData)
-			if tt.Action == actionCopy {
-				err := adapter.Put(sourcePointer, testReader.Size(), testReader, block.PutOpts{})
-				if err != nil {
-					t.Fatal(err)
-				}
-			}
+	destinationPointer := block.ObjectPointer{
+		StorageNamespace: "mem://external-bucket",
+		Identifier:       "/one/two",
+	}
+	from := sourcePointer.StorageNamespace + sourcePointer.Identifier
+	to := destinationPointer.StorageNamespace + destinationPointer.Identifier
 
-			h := NewHandler(adapter)
-			taskBody, err := json.Marshal(tt.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-			taskBodyStr := string(taskBody)
-			task := parade.OwnedTaskData{
-				Action: tt.Action,
-				Body:   &taskBodyStr,
-			}
-			if res := h.Handle(task.Action, task.Body); res.StatusCode != parade.TaskCompleted {
-				t.Errorf("expected status code: %s, got: %s", parade.TaskCompleted, res.StatusCode)
-			}
-			// read Destination
-			reader, err := adapter.Get(destinationPointer, testReader.Size())
+	testData := "this is the test Data"
+	testReader := strings.NewReader(testData)
+	err := adapter.Put(sourcePointer, testReader.Size(), testReader, block.PutOpts{})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-			if err != nil {
-				if tt.Action == actionDelete {
-					return
-				}
-				t.Fatal(err)
-			}
-			if tt.Action == actionDelete {
-				t.Fatalf("expected to get error on get in action delete")
-			}
-			val, err := ioutil.ReadAll(reader)
-			if err != nil {
-				t.Fatal(err)
-			}
-			expect := testData
-			if tt.Action == actionTouch {
-				expect = ""
-			}
-			if string(val) != expect {
-				t.Errorf("expected %s, got %s\n", testData, string(val))
-			}
-		})
+	h := NewHandler(adapter)
+	taskBody, err := json.Marshal(&CopyData{
+		From: from,
+		To:   to,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	taskBodyStr := string(taskBody)
+	task := parade.OwnedTaskData{
+		Action: CopyAction,
+		Body:   &taskBodyStr,
+	}
+	if res := h.Handle(task.Action, task.Body); res.StatusCode != parade.TaskCompleted {
+		t.Errorf("expected status code: %s, got: %s", parade.TaskCompleted, res.StatusCode)
+	}
+	// read Destination
+	reader, err := adapter.Get(destinationPointer, testReader.Size())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	val, err := ioutil.ReadAll(reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect := testData
+	if string(val) != expect {
+		t.Errorf("expected %s, got %s\n", testData, string(val))
+	}
+}
+
+func TestDelete(t *testing.T) {
+	adapter := testutil.NewBlockAdapterByType(t, &block.NoOpTranslator{}, mem.BlockstoreType)
+
+	destinationPointer := block.ObjectPointer{
+		StorageNamespace: "mem://external-bucket",
+		Identifier:       "/one/two",
+	}
+	path := destinationPointer.StorageNamespace + destinationPointer.Identifier
+
+	testData := "this is the test Data"
+	testReader := strings.NewReader(testData)
+	err := adapter.Put(destinationPointer, testReader.Size(), testReader, block.PutOpts{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h := NewHandler(adapter)
+	taskBody, err := json.Marshal(&DeleteData{
+		File: path,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	taskBodyStr := string(taskBody)
+	task := parade.OwnedTaskData{
+		Action: DeleteAction,
+		Body:   &taskBodyStr,
+	}
+	if res := h.Handle(task.Action, task.Body); res.StatusCode != parade.TaskCompleted {
+		t.Errorf("expected status code: %s, got: %s", parade.TaskCompleted, res.StatusCode)
+	}
+	// read Destination
+	_, err = adapter.Get(destinationPointer, testReader.Size())
+	if err == nil {
+		t.Errorf("expected path get err file not found")
+	}
+
+}
+
+func TestTouch(t *testing.T) {
+	adapter := testutil.NewBlockAdapterByType(t, &block.NoOpTranslator{}, mem.BlockstoreType)
+	destinationPointer := block.ObjectPointer{
+		StorageNamespace: "mem://external-bucket",
+		Identifier:       "/one/two",
+	}
+	path := destinationPointer.StorageNamespace + destinationPointer.Identifier
+
+	testData := "this is the test Data"
+	testReader := strings.NewReader(testData)
+	err := adapter.Put(destinationPointer, testReader.Size(), testReader, block.PutOpts{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h := NewHandler(adapter)
+	taskBody, err := json.Marshal(&SuccessData{
+		File: path,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	taskBodyStr := string(taskBody)
+	task := parade.OwnedTaskData{
+		Action: TouchAction,
+		Body:   &taskBodyStr,
+	}
+	if res := h.Handle(task.Action, task.Body); res.StatusCode != parade.TaskCompleted {
+		t.Errorf("expected status code: %s, got: %s", parade.TaskCompleted, res.StatusCode)
+	}
+	// read Destination
+	reader, err := adapter.Get(destinationPointer, testReader.Size())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	val, err := ioutil.ReadAll(reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect := ""
+	if string(val) != expect {
+		t.Errorf("expected %s, got %s\n", testData, string(val))
 	}
 }
