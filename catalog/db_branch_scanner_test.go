6fce11e4882fb345902e7a2eca62e9dfdd20173b
Barak Amar <barak.amar@treeverse.io>
2020-10-15 16:56:05 +0300

    fix tests after merge with master

--- catalog/db_branch_scanner_test.go
+++ catalog/db_branch_scanner_test.go
@@ -23,9 +23,18 @@ func TestDBBranchScanner(t *testing.T) {
 	const baseBranchName = "b0"
 	repository := testCatalogerRepo(t, ctx, c, "repo", baseBranchName)
 
+	// get branch ID once
+	var branchID int64
+	_, _ = conn.Transact(func(tx db.Tx) (interface{}, error) {
+		var err error
+		branchID, err = getBranchID(tx, repository, "b0", LockTypeNone)
+		testutil.MustDo(t, "get branch id", err)
+		return nil, nil
+	})
+
 	t.Run("empty", func(t *testing.T) {
 		_, _ = conn.Transact(func(tx db.Tx) (interface{}, error) {
-			scanner := NewDBBranchScanner(tx, 1, UncommittedID, &DBScannerOptions{BufferSize: 64})
+			scanner := NewDBBranchScanner(tx, branchID, UncommittedID, &DBScannerOptions{BufferSize: 64})
 			firstNext := scanner.Next()
 			if firstNext {
 				t.Fatalf("first entry should be false")
@@ -42,7 +51,7 @@ func TestDBBranchScanner(t *testing.T) {
 
 	t.Run("additional_fields", func(t *testing.T) {
 		_, _ = conn.Transact(func(tx db.Tx) (interface{}, error) {
-			scanner := NewDBBranchScanner(tx, 1, UncommittedID, &DBScannerOptions{
+			scanner := NewDBBranchScanner(tx, branchID, UncommittedID, &DBScannerOptions{
 				AdditionalFields: []string{"checksum", "physical_address"},
 			})
 			testedSomething := false
@@ -69,7 +78,7 @@ func TestDBBranchScanner(t *testing.T) {
 	t.Run("additional_where", func(t *testing.T) {
 		_, _ = conn.Transact(func(tx db.Tx) (interface{}, error) {
 			p := fmt.Sprintf("Obj-%04d", numberOfObjects-5)
-			scanner := NewDBBranchScanner(tx, 1, UncommittedID, &DBScannerOptions{
+			scanner := NewDBBranchScanner(tx, branchID, UncommittedID, &DBScannerOptions{
 				AdditionalWhere: sq.Expr("path=?", p),
 			})
 			var ent *DBScannerEntry
@@ -90,9 +99,10 @@ func TestDBBranchScanner(t *testing.T) {
 		bufferSizes := []int{1, 2, 8, 64, 512, 1024 * 4}
 		for _, bufSize := range bufferSizes {
 			for branchNo, objSkipNo := range objSkip {
-				branchID := int64(branchNo + 1)
 				branchName := "b" + strconv.Itoa(branchNo)
 				_, _ = conn.Transact(func(tx db.Tx) (interface{}, error) {
+					branchID, err := getBranchID(tx, repository, branchName, LockTypeNone)
+					testutil.MustDo(t, "get branch ID", err)
 					scanner := NewDBBranchScanner(tx, branchID, UncommittedID, &DBScannerOptions{BufferSize: bufSize})
 					i := 0
 					for scanner.Next() {
