c8bed510320d68ad11bc4149840e6dcb7ce464d4
Barak Amar <barak.amar@treeverse.io>
2020-10-21 23:16:59 +0300

    add the commits index

--- ddl/000008_commit_idx.up.sql
+++ ddl/000008_commit_idx.up.sql
@@ -4,5 +4,5 @@ DROP INDEX IF EXISTS entries_uncommitted_branch_min_commit;
 DROP INDEX IF EXISTS entries_branch_min_commit;
 CREATE INDEX entries_branch_min_commit
     ON catalog_entries USING btree
-        (branch_id ASC NULLS LAST, min_commit ASC NULLS LAST)
+        (branch_id ASC NULLS LAST, min_commit ASC NULLS LAST);
 COMMIT;
