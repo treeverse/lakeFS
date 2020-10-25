2d54aea16c9930f9ee744aaba37c8e14ad235365
tzahij <tzahij@hotmail.com>
2020-10-25 16:50:50 +0200

    add the commits index

---
+++ catalog/cataloger_diff_scanners.go
@@ -0,0 +1,228 @@
+package catalog
+
+import (
+	"fmt"
+
+	"context"
+
+	"github.com/treeverse/lakefs/db"
+	"github.com/treeverse/lakefs/logging"
+)
+
+type diffScanner interface {
+	Next() bool
+	Value() *diffResultRecord
+	Error() error
+}
+
+type diffScannerBase struct {
+	diffParams                *doDiffParams
+	leftScanner, rightScanner *DBLineageScanner
+	err                       error
+	value                     *diffResultRecord
+	diffSummary               map[DifferenceType]int
+}
+
+type parentToChildScanner struct {
+	diffScannerBase
+	childLineage                []lineageCommit
+	childLastFromParentCommitID CommitID
+	lastChildEnt                *DBScannerEntry
+	rowsCounter                 int
+}
+
+func (c *cataloger) Diff(ctx context.Context, repository string, leftReference string, rightReference string, params DiffParams) (Differences, bool, error) {
+	if err := Validate(ValidateFields{
+		{Name: "repository", IsValid: ValidateRepositoryName(repository)},
+		{Name: "leftReference", IsValid: ValidateReference(leftReference)},
+		{Name: "rightReference", IsValid: ValidateReference(rightReference)},
+	}); err != nil {
+		return nil, false, err
+	}
+
+	// parse references
+	leftRef, err := ParseRef(leftReference)
+	if err != nil {
+		return nil, false, fmt.Errorf("left reference: %w", err)
+	}
+	rightRef, err := ParseRef(rightReference)
+	if err != nil {
+		return nil, false, fmt.Errorf("right reference: %w", err)
+	}
+
+	if params.Limit < 0 || params.Limit > DiffMaxLimit {
+		params.Limit = DiffMaxLimit
+	}
+
+	ctx, cancel := c.withDiffResultsContext(ctx)
+	defer cancel()
+
+	res, err := c.db.Transact(func(tx db.Tx) (interface{}, error) {
+		// get branch IDs
+		leftBranchID, err := c.getBranchIDCache(tx, repository, leftRef.Branch)
+		if err != nil {
+			return nil, fmt.Errorf("left ref branch: %w", err)
+		}
+		rightBranchID, err := c.getBranchIDCache(tx, repository, rightRef.Branch)
+		if err != nil {
+			return nil, fmt.Errorf("right ref branch: %w", err)
+		}
+
+		diffParams := &doDiffParams{
+			Repository:    repository,
+			LeftCommitID:  leftRef.CommitID,
+			LeftBranchID:  leftBranchID,
+			RightCommitID: rightRef.CommitID,
+			RightBranchID: rightBranchID,
+			DiffParams: DiffParams{
+				// we request additional one (without returning it) for pagination (hasMore)
+				Limit:            params.Limit + 1,
+				After:            params.After,
+				AdditionalFields: params.AdditionalFields,
+			},
+		}
+		scanner, err := c.newDiffScanner(tx, diffParams)
+		if err != nil {
+			return nil, err
+		}
+		differences := make(Differences, 0, params.Limit+1)
+		for scanner.Next() {
+			v := scanner.Value()
+			var d Difference
+			d.Path = v.Entry.Path
+			d.Type = v.DiffType
+			differences = append(differences, d)
+		}
+
+		return differences, nil
+	}, c.txOpts(ctx)...)
+	if err != nil {
+		return nil, false, err
+	}
+	differences := res.(Differences)
+	hasMore := paginateSlice(&differences, params.Limit)
+	return differences, hasMore, nil
+}
+
+func (c *cataloger) newDiffScanner(tx db.Tx, params *doDiffParams) (diffScanner, error) {
+	if params == nil {
+		return nil, fmt.Errorf("doDiff params are required: %w", ErrInvalidValue)
+	}
+	relation, err := c.getRefsRelationType(tx, params)
+	if err != nil {
+		return nil, err
+	}
+	switch relation {
+	case RelationTypeFromParent:
+		return c.newDiffFromParent(tx, params)
+	//case RelationTypeFromChild:
+	//	return c.diffFromChild(ctx, tx, params)
+	//case RelationTypeNotDirect:
+	//	return c.diffNonDirect(ctx, tx, params)
+	//case RelationTypeSame:
+	//	return c.diffSameBranch(ctx, tx, params)
+	default:
+		c.log.WithFields(logging.Fields{
+			"relation_type":   relation,
+			"repository":      params.Repository,
+			"left_branch_id":  params.LeftBranchID,
+			"left_commit_id":  params.LeftCommitID,
+			"right_branch_id": params.RightBranchID,
+			"right_commit_id": params.RightCommitID,
+		}).Debug("Diff by relation - unsupported type")
+		return nil, ErrFeatureNotSupported
+	}
+}
+
+func (c *cataloger) newDiffFromParent(tx db.Tx, params *doDiffParams) (diffScanner, error) {
+	// get child last commit of merge from parent
+	scanner := new(parentToChildScanner)
+	scanner.diffParams = params
+	query, args, err := psql.Select("MAX(commit_id) as max_child_commit").
+		From("catalog_commits").
+		Where("branch_id = ? AND merge_type = 'from_parent'", params.RightBranchID).
+		ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("get child last commit sql: %w", err)
+	}
+	err = tx.Get(&scanner.childLastFromParentCommitID, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("get child last commit failed: %w", err)
+	}
+
+	scannerOpts := DBScannerOptions{
+		After:            params.After,
+		AdditionalFields: prepareDiffAdditionalFields(params.AdditionalFields),
+	}
+	scanner.leftScanner = NewDBLineageScanner(tx, params.LeftBranchID, CommittedID, &scannerOpts)
+	scanner.rightScanner = NewDBLineageScanner(tx, params.RightBranchID, UncommittedID, &scannerOpts)
+	scanner.childLineage, err = scanner.rightScanner.ReadLineage()
+	if err != nil {
+		return nil, err
+	}
+	scanner.diffSummary = createSummaryMap()
+	return scanner, nil
+}
+
+func createSummaryMap() map[DifferenceType]int {
+	m := make(map[DifferenceType]int)
+	for i := 0; i < int(DifferenceTypeNone); i++ {
+		m[DifferenceType(i)] = 0
+	}
+	return m
+}
+
+func (c *parentToChildScanner) Next() bool {
+	var err error
+	if c.diffParams.Limit > -1 && c.rowsCounter >= c.diffParams.Limit {
+		return false
+	}
+	for c.leftScanner.Next() {
+		// is parent element is relevant
+		parentEnt := c.leftScanner.Value()
+		// get next child entry - scan until we match child's path to parent (or bigger)
+		c.lastChildEnt, err = ScanDBEntryUntil(c.rightScanner, c.lastChildEnt, parentEnt.Path)
+		if err != nil {
+			c.err = fmt.Errorf("scan next child element: %w", err)
+			return false
+		}
+		// point to matched child based on path
+		var matchedChild *DBScannerEntry
+		if c.lastChildEnt != nil && c.lastChildEnt.Path == parentEnt.Path {
+			matchedChild = c.lastChildEnt
+		}
+		// diff between entries
+		diffType := evaluateFromParentElementDiffType(c.diffParams.RightBranchID, c.childLastFromParentCommitID, c.childLineage, parentEnt, matchedChild)
+		if diffType == DifferenceTypeNone {
+			continue
+		}
+		c.value = &diffResultRecord{DiffType: diffType,
+			Entry: parentEnt.Entry,
+		}
+
+		// store ctid for copying in the merge step, under the following conditions:
+		// 1. the entry exists in the child branch
+		// 2. difference type is either changed or added.
+		// Then the entry has to appear in the child branch, but an older version exists in the child, so merge will copy it, and the ctid is needed for that
+		if matchedChild != nil && matchedChild.BranchID == c.diffParams.RightBranchID && (diffType == DifferenceTypeAdded || diffType == DifferenceTypeChanged) {
+			c.value.EntryCtid = &parentEnt.RowCtid
+		}
+		c.rowsCounter++
+		c.diffSummary[diffType]++
+		return true // exit for loop and function
+	}
+	c.err = c.leftScanner.Err()
+	return false
+}
+
+func (c *parentToChildScanner) Value() *diffResultRecord {
+	if c.err == nil {
+		return c.value
+	} else {
+		return nil // will happen only if the caller did not check error
+	}
+}
+
+func (c *parentToChildScanner) Error() error {
+	return c.err
+}
---
+++ catalog/cataloger_diff_scanners_test.go
@@ -0,0 +1,134 @@
+package catalog
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/go-test/deep"
+	"github.com/treeverse/lakefs/testutil"
+)
+
+func TestCataloger_Diff_Scanner_FromParentThreeBranches(t *testing.T) {
+	ctx := context.Background()
+	c := testCataloger(t)
+	//conn, _ := testutil.GetDB(t, databaseURI)
+	repository := testCatalogerRepo(t, ctx, c, "repo", "master")
+
+	const numberOfBranches = 3
+	const numberOfEntries = 3
+
+	// create 3 files on master
+	for j := 0; j < numberOfEntries; j++ {
+		p := fmt.Sprintf("file%d-%s", j, DefaultBranchName)
+		testCatalogerCreateEntry(t, ctx, c, repository, DefaultBranchName, p, nil, DefaultBranchName)
+	}
+	_, err := c.Commit(ctx, repository, DefaultBranchName, "commit changes to "+DefaultBranchName, "tester", nil)
+	testutil.MustDo(t, "initial branch commit", err)
+
+	// create 3 branches, create 3 files and commit. each branch branches from previous branch
+	prevBranch := DefaultBranchName
+	for i := 0; i < numberOfBranches; i++ {
+		branchName := fmt.Sprintf("branch%d", i)
+		testCatalogerBranch(t, ctx, c, repository, branchName, prevBranch)
+		for j := 0; j < numberOfEntries; j++ {
+			p := fmt.Sprintf("file%d-%s", j, branchName)
+			testCatalogerCreateEntry(t, ctx, c, repository, branchName, p, nil, branchName)
+		}
+		_, err := c.Commit(ctx, repository, branchName, "commit changes to "+branchName, "tester", nil)
+		testutil.MustDo(t, "initial branch commit", err)
+		prevBranch = branchName
+	}
+
+	// on master branch do: delete, update, entry and commit
+
+	// delete
+	err = c.DeleteEntry(ctx, repository, DefaultBranchName, "file1-"+DefaultBranchName)
+	testutil.MustDo(t, "delete entry from branch", err)
+
+	// update
+	testCatalogerCreateEntry(t, ctx, c, repository, DefaultBranchName, "file2-"+DefaultBranchName, nil, DefaultBranchName+"mod")
+
+	// add
+	testCatalogerCreateEntry(t, ctx, c, repository, DefaultBranchName, "fileX-"+DefaultBranchName, nil, DefaultBranchName)
+
+	// commit changes
+	_, err = c.Commit(ctx, repository, DefaultBranchName, "commit changes", "tester", nil)
+	testutil.MustDo(t, "commit branch changes", err)
+
+	//diff changes between master and branch0
+	res, more, err := c.Diff(ctx, repository, "master", "branch0", DiffParams{Limit: -1})
+
+	testutil.MustDo(t, "Diff changes from master to branch0", err)
+	if more {
+		t.Fatal("Diff has more differences, expected none")
+	}
+	//res, err := conn.Transact(func(tx db.Tx) (interface{}, error) {
+	//	diffScan, err := c.newDiffFromParent(tx, DiffParams{Limit: -1})
+	//})
+
+	if diff := deep.Equal(res, Differences{
+		Difference{Entry: Entry{Path: "file1-" + DefaultBranchName}, Type: DifferenceTypeRemoved},
+		Difference{Entry: Entry{Path: "file2-" + DefaultBranchName}, Type: DifferenceTypeChanged},
+		Difference{Entry: Entry{Path: "fileX-" + DefaultBranchName}, Type: DifferenceTypeAdded},
+	}); diff != nil {
+		t.Fatal("Diff unexpected differences:", diff)
+	}
+}
+
+func TestCataloger_Diff_AdditionalFields(t *testing.T) {
+	ctx := context.Background()
+	c := testCataloger(t)
+
+	// test flow
+	// 1. create repo (with master branch)
+	// 2. create branch based on master - branch1
+	// 3. create 3 entries and commit on master
+	// 4. run diff between master and branch1 - no additional fields - check physical address is empty
+	// 4. run diff between master and branch1 - physical address as additional field - check physical address is set
+
+	repository := testCatalogerRepo(t, ctx, c, "repo", "master")
+	testCatalogerBranch(t, ctx, c, repository, "branch1", "master")
+	const numOfEntries = 3
+	for i := 0; i < numOfEntries; i++ {
+		testCatalogerCreateEntry(t, ctx, c, repository, "master", fmt.Sprintf("file%d", i), nil, "")
+	}
+	_, err := c.Commit(ctx, repository, "master", "checking changes on master", "tester", nil)
+	testutil.Must(t, err)
+
+	res, hasMore, err := c.Diff(ctx, repository, "master", "branch1", DiffParams{Limit: numOfEntries})
+	testutil.MustDo(t, "diff changes", err)
+	if hasMore {
+		t.Fatal("Diff() hasMore should be false")
+	}
+	expectedLen := 3
+	if len(res) != expectedLen {
+		t.Fatalf("Diff() len of result %d, expected %d", len(res), expectedLen)
+	}
+	for _, d := range res {
+		if d.PhysicalAddress != "" {
+			t.Fatalf("Diff result entry should not have physical address set (%s)", d.PhysicalAddress)
+		}
+	}
+
+	res, hasMore, err = c.Diff(ctx, repository, "master", "branch1", DiffParams{
+		Limit:            numOfEntries,
+		AdditionalFields: []string{DBEntryFieldPhysicalAddress, DBEntryFieldChecksum},
+	})
+	testutil.MustDo(t, "diff changes", err)
+	if hasMore {
+		t.Fatal("Diff() hasMore should be false")
+	}
+	if len(res) != expectedLen {
+		t.Fatalf("Diff() len of result %d, expected %d", len(res), expectedLen)
+	}
+	for _, d := range res {
+		if d.PhysicalAddress != "" {
+			t.Fatalf("Diff result entry should not have physical address set (%s)", d.PhysicalAddress)
+		}
+		// verify that checksum - added by diff code is set on entry
+		if d.Checksum != "" {
+			t.Fatalf("Diff result entry should not have checksum address set (%s)", d.Checksum)
+		}
+	}
+}
