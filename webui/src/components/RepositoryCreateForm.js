093911297fe2595f1c37face42f25b9aa16d189e
Sufiyan Gani <sufigani@gmail.com>
2020-10-28 22:35:32 +0530

    added: client side validation of parameters while creating repository

--- webui/src/components/RepositoryCreateForm.js
+++ webui/src/components/RepositoryCreateForm.js
@@ -14,20 +14,33 @@ export const RepositoryCreateForm = connect(({ repositories }) => {
 })(({ error, onSubmit, onCancel, create, sm = 6 }) => {
 
     const fieldNameOffset = 3;
+    const repoValidityRegex = /^[a-z0-9][a-z0-9-]{2,62}$/;
+    const storageNamespaceValidityRegex = /^(s3|gs|mem|local|transient):\/.*$/;
 
     const [formValid, setFormValid] = useState(false);
+    const [repoValid, setRepoValid] = useState(true);
+    const [storageNamespaceValid, setStorageNamespaceValid] = useState(true);
+    const [defaultBranchValid, setDefaultBranchValid] = useState(true);
     const storageNamespaceField = useRef(null);
     const defaultBranchField = useRef(null);
     const repoIdField = useRef(null);
 
-    const checkValidity = () => {
-        if (repoIdField.current.value.length === 0 ||
-            storageNamespaceField.current.value.length === 0 ||
-            defaultBranchField.current.value.length === 0) {
-            setFormValid(false);
-            return;
-        }
-        setFormValid(true);
+    const checkRepoValidity = () => {
+        const isRepoValid = repoValidityRegex.test(repoIdField.current.value)
+        setRepoValid(isRepoValid);
+        setFormValid(isRepoValid);
+    };
+
+    const checkStorageNamespaceValidity = () => {
+        const isStorageNamespaceValid = storageNamespaceValidityRegex.test(storageNamespaceField.current.value)
+        setStorageNamespaceValid(isStorageNamespaceValid);
+        setFormValid(isStorageNamespaceValid);
+    };
+
+    const checkDefaultBranchValidity = () => {
+        const isBranchValid = defaultBranchField.current.value.length;
+        setDefaultBranchValid(isBranchValid);
+        setFormValid(isBranchValid);
     };
 
     return (
@@ -45,20 +58,35 @@ export const RepositoryCreateForm = connect(({ repositories }) => {
             <Form.Group as={Row} controlId="id">
                 <Form.Label column sm={fieldNameOffset}>Repository ID</Form.Label>
                 <Col sm={sm}>
-                    <Form.Control type="text" autoFocus ref={repoIdField} onChange={checkValidity}/>
+                    <Form.Control type="text" autoFocus ref={repoIdField} onChange={checkRepoValidity}/>
+                    {!repoValid &&
+                        <Form.Text className="text-danger">
+                            Min 2 characters. Only Alpahnumeric characters and '-' allowed.
+                        </Form.Text>
+                    }
                 </Col>
             </Form.Group>
 
             <Form.Group as={Row}>
                 <Form.Label column sm={fieldNameOffset}>Storage Namespace</Form.Label>
                     <Col sm={sm}>
-                        <Form.Control type="text" ref={storageNamespaceField} placeholder="i.e. s3://example-bucket/" onChange={checkValidity}/>
+                        <Form.Control type="text" ref={storageNamespaceField} placeholder="i.e. s3://example-bucket/" onChange={checkStorageNamespaceValidity}/>
+                        {!storageNamespaceValid &&
+                            <Form.Text className="text-danger">
+                                Invalid Storage Namespace.
+                            </Form.Text>
+                        }
                     </Col>
             </Form.Group>
             <Form.Group as={Row} controlId="defaultBranch">
                 <Form.Label column sm={fieldNameOffset}>Default Branch</Form.Label>
                 <Col sm={sm}>
-                    <Form.Control type="text" ref={defaultBranchField} placeholder="defaultBranch" defaultValue={"master"} onChange={checkValidity}/>
+                    <Form.Control type="text" ref={defaultBranchField} placeholder="defaultBranch" defaultValue={"master"} onChange={checkDefaultBranchValidity}/>
+                    {!defaultBranchValid &&
+                        <Form.Text className="text-danger">
+                            Invalid Branch.
+                        </Form.Text>
+                    }
                 </Col>
             </Form.Group>
 
