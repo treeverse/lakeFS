529ab7e6134b615e66fa8d6e9ad5286a9683289e
Barak Amar <barak.amar@treeverse.io>
2020-10-28 15:51:30 +0200

    revert back the force failure

--- cmd/lakefs/cmd/run.go
+++ cmd/lakefs/cmd/run.go
@@ -53,8 +53,10 @@ var runCmd = &cobra.Command{
 		// validate service names and turn on the right flags
 		dbParams := cfg.GetDatabaseParams()
 
-		if err := db.ValidateSchemaUpToDate(dbParams); err != nil {
-			logger.WithError(err).Fatal("Failed on schema validation")
+		if err := db.ValidateSchemaUpToDate(dbParams); errors.Is(err, db.ErrSchemaNotCompatible) {
+			logger.WithError(err).Fatal("Migration version mismatch")
+		} else if err != nil {
+			logger.WithError(err).Warn("Failed on schema validation")
 		}
 		dbPool := db.BuildDatabaseConnection(dbParams)
 		defer func() {
