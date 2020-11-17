6985dbad0ca1956bcc84578065cef6d21020e314
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2020-11-17 14:59:51 +0200

    [CR] Use cobra.ExactArgs, SQL IF [NOT] EXISTS

--- ddl/000012_export_continuous.up.sql
+++ ddl/000012_export_continuous.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE catalog_branches_export ADD COLUMN continuous BOOLEAN;
+ALTER TABLE catalog_branches_export ADD COLUMN IF NOT EXISTS continuous BOOLEAN;
 UPDATE catalog_branches_export SET continuous=false;
 ALTER TABLE catalog_branches_export ALTER COLUMN continuous SET NOT NULL;
 
