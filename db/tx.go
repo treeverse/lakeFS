62de3b9f8ea48d2ac59cd24b1cd0e0c7979ac3af
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2020-11-24 11:34:56 +0200

    [bugfix] return ErrNotFound correctly
    Package `scany` depends on a different version of `pgx` than the rest of lakeFS.  So
    `errors.Is(err, pgx.ErrNoRows)` fails.  Luckily it (sort-of) knows of this issue and
    wraps this call inside it as `pgxscan.NotFound`.
    
    Also make `ErrNotFound` wrap `pgx.ErrNoRows` rather than a new error.

--- db/tx.go
+++ db/tx.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -75,8 +74,6 @@ func (d *dbTx) Get(dest interface{}, query string, args ...interface{}) error {
 	})
 	err := pgxscan.Get(context.Background(), d.tx, dest, query, args...)
 	if pgxscan.NotFound(err) {
-		// Don't wrap this err: it might come from a different version of pgx and then
-		// !errors.Is(err, pgx.ErrNoRows).
 		log.Trace("SQL query returned no results")
 		return ErrNotFound
 	}
@@ -99,7 +96,9 @@ func (d *dbTx) GetPrimitive(dest interface{}, query string, args ...interface{})
 	})
 	row := d.tx.QueryRow(context.Background(), query, args...)
 	err := row.Scan(dest)
-	if errors.Is(err, pgx.ErrNoRows) {
+	if pgxscan.NotFound(err) {
+		// Don't wrap err: it might come from a different version of pgx and then
+		// !errors.Is(err, pgx.ErrNoRows).
 		log.Trace("SQL query returned no results")
 		return ErrNotFound
 	}
