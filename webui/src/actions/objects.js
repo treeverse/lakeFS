1924b2728e361fb50d15fbe7ab43b291a140ab58
Khushi Agarwal <46860508+hi-18-K@users.noreply.github.com>
2020-11-26 16:52:26 +0530

    Feature/added scrollto after object is deleted
    added scrollTo feature. When any object is deleted its scroll position is stored and after it is deleted scrollTo is triggered to restore previous scroll state.

--- webui/src/actions/objects.js
+++ webui/src/actions/objects.js
@@ -1,5 +1,7 @@
 import * as api from "./api";
 import {AsyncActionType} from "./request";
+import { Link,  Element, Events, scrollSpy, scroller, animateScroll as scroll } from "react-scroll";
+import * as Scroll from 'react-scroll';
 
 export const PAGINATION_AMOUNT = 300;
 export const OBJECTS_LIST_TREE = new AsyncActionType('OBJECTS_GET_TREE');
@@ -31,14 +33,31 @@ export const uploadDone = () => {
     return OBJECTS_UPLOAD.resetAction();
 };
 
+let yoffset = 0;
 
 export const deleteObject = (repoId, branchId, path) => {
+    constructor(props) {
+      super(props);
+      this.state = {yoffset: 0};
+    }
+    storeScroll = () => {
+      console.log(window.pageYOffset);
+      yoffset = window.pageYOffset;
+      this.setState({yoffset: window.pageYOffset});
+    };
     return OBJECTS_DELETE.execute(async () => {
+        this.storeScroll;
         return await api.objects.delete(repoId, branchId, path);
     });
 };
 
 export const deleteObjectDone = () => {
-    return OBJECTS_DELETE.resetAction();
+    scrollToPageOffset = offset_val => e =>  {
+      scroll.scrollTo(offset_val);
+    };
+    return (
+      OBJECTS_DELETE.resetAction();
+      this.scrollToPageOffset(yoffset);
+      yoffset = 0;
+    )
 };
-
