4ad75000c9e34ada7be24a98b86b69f66d24788e
johnnyaug <yoni.augarten@treeverse.io>
2020-12-06 12:25:25 +0200

    revert entry catalog change

--- catalog/rocks/entry_catalog.go
+++ catalog/rocks/entry_catalog.go
@@ -1,8 +1,10 @@
-package graveler
+package rocks
 
 import (
 	"context"
 	"time"
+
+	"github.com/treeverse/lakefs/graveler"
 )
 
 type Path string
@@ -11,7 +13,7 @@ type Path string
 type Entry struct {
 	LastModified time.Time
 	Address      string
-	Metadata     Metadata
+	Metadata     graveler.Metadata
 	ETag         string
 	Size         int64
 }
@@ -37,21 +39,21 @@ type EntryListingIterator interface {
 
 // EntryCatalog
 type EntryCatalog interface {
-	VersionController
+	graveler.VersionController
 
 	// Get returns entry from repository / reference by path, nil entry is a valid entry for tombstone
 	// returns error if entry does not exist
-	GetEntry(ctx context.Context, repositoryID RepositoryID, ref Ref, path Path) (*Entry, error)
+	GetEntry(ctx context.Context, repositoryID graveler.RepositoryID, ref graveler.Ref, path Path) (*Entry, error)
 
 	// Set stores entry on repository / branch by path. nil entry is a valid entry for tombstone
-	SetEntry(ctx context.Context, repositoryID RepositoryID, branchID BranchID, path Path, entry *Entry) error
+	SetEntry(ctx context.Context, repositoryID graveler.RepositoryID, branchID graveler.BranchID, path Path, entry *Entry) error
 
 	// DeleteEntry deletes entry from repository / branch by path
-	DeleteEntry(ctx context.Context, repositoryID RepositoryID, branchID BranchID, path Path) error
+	DeleteEntry(ctx context.Context, repositoryID graveler.RepositoryID, branchID graveler.BranchID, path Path) error
 
 	// List lists entries on repository / ref will filter by prefix, from path 'from'.
 	//   When 'delimiter' is set the listing will include common prefixes based on the delimiter
-	ListEntries(ctx context.Context, repositoryID RepositoryID, ref Ref, prefix, from, delimiter Path) (EntryListingIterator, error)
+	ListEntries(ctx context.Context, repositoryID graveler.RepositoryID, ref graveler.Ref, prefix, from, delimiter Path) (EntryListingIterator, error)
 }
 
 func NewPath(id string) (Path, error) {
