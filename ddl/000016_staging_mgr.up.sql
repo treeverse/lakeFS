692afdb3ad86e3bbaa8b2951265c40c21d635429
johnnyaug <yoni.augarten@treeverse.io>
2020-12-06 12:22:24 +0200

    disallow nil values

--- ddl/000016_staging_mgr.up.sql
+++ ddl/000016_staging_mgr.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS staging_kv
     staging_token varchar not null,
     key bytea not null,
     identity bytea not null,
-    data bytea not null
+    data bytea
 ) PARTITION BY HASH (staging_token);
 CREATE TABLE IF NOT EXISTS staging_kv_p0 PARTITION OF staging_kv FOR VALUES WITH (MODULUS 10, REMAINDER 0);
 CREATE TABLE IF NOT EXISTS staging_kv_p1 PARTITION OF staging_kv FOR VALUES WITH (MODULUS 10, REMAINDER 1);
