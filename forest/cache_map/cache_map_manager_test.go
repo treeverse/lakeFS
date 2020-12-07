a5d874e7fa335ab36a1d033ecdace1bcadbd6322
tzahij <tzahij@hotmail.com>
2020-12-07 16:39:00 +0200

    cache map ready for PR

---
+++ forest/cache_map/cache_map_manager_test.go
@@ -0,0 +1,71 @@
+package cache_map
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+type cacheTest struct {
+	name             string
+	cacheMaxSize     int
+	cacheTrimSize    int
+	initialWeight    int64
+	additionalWeight int64
+	trimFactor       int
+	randomOpsFactor  int
+}
+
+var tests = []cacheTest{
+	{"initialTest", 100, 20, 64, 16, 1, 10},
+	{"bigTest", 1000, 200, 64, 16, 2, 20},
+	{"largeTrim", 500, 300, 64, 16, 3, 30},
+	{"smallWeight", 500, 300, 8, 2, 4, 40},
+	{"minimalTrim", 500, 30, 64, 4, 4, 40},
+}
+
+func TestTrim(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			k := newCacheMap(test.cacheMaxSize, test.cacheTrimSize, test.initialWeight, test.additionalWeight, test.trimFactor, test.trimFactor)
+			for i := 0; i < k.cacheMaxSize; i++ {
+				key := fmt.Sprintf("%05d", i)
+				k.Set(key, nil)
+			}
+			for i := 0; i < k.cacheMaxSize*test.randomOpsFactor; i++ {
+				key := fmt.Sprintf("%05d", rand.Int31n(int32(k.cacheMaxSize)))
+				k.Get(key)
+			}
+			cacheCopy := deepCopyMap(k.cache)
+			k.Set("spillValue", nil)
+			maxRemoved := maxRemoved(k.cache, cacheCopy)
+			for _, v := range k.cache {
+				if v.weight < maxRemoved {
+					t.Errorf("eviction erroe, max removed: %d, weight of not removed:%d", maxRemoved, v.weight)
+				}
+			}
+		})
+
+	}
+}
+
+func deepCopyMap(inp map[string]*cacheEntry) map[string]*cacheEntry {
+	out := make(map[string]*cacheEntry, len(inp))
+	for k, v := range inp {
+		out[k] = v
+	}
+	return out
+}
+func maxRemoved(a, b map[string]*cacheEntry) int64 {
+	var maxRemoved int64
+	for k, v := range b {
+		_, exist := a[k]
+		if !exist {
+			w := v.weight
+			if w > maxRemoved {
+				maxRemoved = w
+			}
+		}
+	}
+	return maxRemoved
+}
---
+++ forest/cache_map/cache_map_manger.go
@@ -0,0 +1,112 @@
+package cache_map
+
+import (
+	"container/heap"
+)
+
+type cacheEntry struct {
+	key     string
+	weight  int64
+	content *interface{}
+}
+
+type cacheMap struct {
+	cacheMaxSize     int
+	cacheTrimSize    int
+	initialWeight    int64
+	additionalWeight int64
+	opsNumber        int
+	trimFactor       int
+	cache            map[string]*cacheEntry
+}
+
+func newCacheMap(cacheMaxSize, cacheTrimSize int, initialWeight, additionalWeight int64, trimFactor int, OpsBeforeTrim int) cacheMap {
+	return cacheMap{
+		cacheMaxSize:     cacheMaxSize,
+		cacheTrimSize:    cacheTrimSize,
+		initialWeight:    initialWeight,
+		additionalWeight: additionalWeight,
+		trimFactor:       trimFactor,
+		cache:            make(map[string]*cacheEntry, 0),
+	}
+}
+
+func (c *cacheMap) Get(k string) (*interface{}, bool) {
+	t, ok := c.cache[k]
+	if ok {
+		t.weight += c.additionalWeight
+		c.decayWeightWhenNeeded()
+		return t.content, true
+	}
+	return nil, false
+}
+
+func (c *cacheMap) decayWeightWhenNeeded() {
+	c.opsNumber++
+	if c.opsNumber > c.cacheMaxSize*c.trimFactor {
+		for _, v := range c.cache {
+			v.weight /= 2
+		}
+		c.opsNumber = 0
+	}
+}
+
+func (c *cacheMap) Set(k string, val *interface{}) {
+	c.cache[k] = &cacheEntry{
+		key:     k,
+		weight:  c.initialWeight,
+		content: val,
+	}
+	if len(c.cache) > c.cacheMaxSize {
+		c.trimCache()
+	}
+}
+
+// implementing partial sort using hash
+type heapItem struct {
+	key    string
+	weight int64
+}
+type heapItemsType []heapItem
+
+func (c *cacheMap) trimCache() {
+	limit := c.cacheTrimSize
+	heapItems := make(heapItemsType, limit)
+	var counter int
+	for k, v := range c.cache {
+		if counter < limit {
+			heapItems[counter] = heapItem{
+				key:    k,
+				weight: v.weight}
+		}
+		if counter == limit {
+			heap.Init(&heapItems)
+		}
+		if counter >= limit {
+			if heapItems[0].weight > v.weight {
+				heap.Pop(&heapItems)
+				heap.Push(&heapItems, heapItem{
+					key:    k,
+					weight: v.weight})
+			}
+		}
+		counter++
+	}
+	for _, minItem := range heapItems {
+		delete(c.cache, minItem.key)
+	}
+}
+
+func (h heapItemsType) Len() int           { return len(h) }
+func (h heapItemsType) Less(i, j int) bool { return h[i].weight > h[j].weight }
+func (h heapItemsType) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *heapItemsType) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+func (h *heapItemsType) Push(x interface{}) {
+	*h = append(*h, x.(heapItem))
+}
