d1f1d9fb41445ddd33873b7b5f7f4c5137700b55
tzahij <tzahij@hotmail.com>
2020-12-13 12:24:11 +0200

    fix review comments

--- forest/tree/tree.go
+++ forest/tree/tree.go
@@ -1,21 +1,22 @@
 package tree
 
 import (
-	cache "github.com/treeverse/lakefs/forest/cache_map"
-	gr "github.com/treeverse/lakefs/graveler"
+	//cache "github.com/treeverse/lakefs/forest/cache_map"
+	"github.com/treeverse/lakefs/graveler"
 	"github.com/treeverse/lakefs/graveler/committed/sstable"
 )
 
 type TreePart struct {
-	PartName sstable.ID `json:"part_name"`
-	MaxKey   gr.Key     `json:"max_path"`
+	PartName sstable.ID   `json:"part_name"`
+	MaxKey   graveler.Key `json:"max_path"`
 }
 type TreeSlice struct {
 	treeSlice []TreePart
 }
+type dummyMap map[string]string // place holder for cache_map package that will be merged soon
 
 type treesRepo struct {
-	treesMap   cache.CacheMap
+	treesMap   dummyMap
 	partManger sstable.Manager
 }
 
@@ -25,23 +26,31 @@ type treesRepo struct {
 type InitTreesRepository func(manager sstable.Manager) TreeRepo
 
 type TreeRepo interface {
-	// NewTreeWriter returns a writer that uses the part managet to create a new tree
+	// NewTreeWriter returns a writer that uses the part manager to create a new tree
 	NewTreeWriter(splitFactor int, // average number of keys that we want to stored in a part
 		// for more detail, look at "IsSplitKey"
 		closeAsync sstable.BatchWriterCloser, // component used to close part asynchronously, and wait for all part
 		// completions when tree writing completes
 	) TreeWriter
-	// NewScannerFromTreeID accepts a tree ID, and returnes an iterator over the tree
-	NewIteratorFromTreeID(treeID gr.TreeID, start gr.Key) (gr.ValueIterator, error)
-	// NewIteratorFromTreeParts accept a tree in memory, returnes iferator over the tree
-	NewIteratorFromTreeSlice(treeSlice TreeSlice, start gr.Key) (gr.ValueIterator, error)
+	// NewScannerFromTreeID accepts a tree ID, and returns an iterator over the tree
+	NewIteratorFromTreeID(treeID graveler.TreeID, start graveler.Key) (graveler.ValueIterator, error)
+	// NewIteratorFromTreeParts accept a tree in memory, returns iterator over the tree
+	NewIteratorFromTreeSlice(treeSlice TreeSlice, start graveler.Key) (graveler.ValueIterator, error)
 	// GetPartManager give components of tree package to the configured part manager.
 	// will probably change later
 	GetPartManger() sstable.Manager
 }
 
 type TreeWriter interface {
-	WriteEntry(record gr.ValueRecord) error
-	FlushIterToTree(iter gr.ValueIterator) error
-	SaveTree(reuseTree TreeSlice) (gr.TreeID, error)
+	WriteEntry(record graveler.ValueRecord) error
+	// FlushIterToTree writes the content of an iterator to the tree.
+	FlushIterToTree(iter graveler.ValueIterator) error
+	// SaveTree stores the tree to tierFS. During tree writing, parts are closed asynchronously and copied by tierFS
+	// while writing continues. SaveTree waits until closing and copying all parts
+	SaveTree(reuseTree TreeSlice, // A tree may be saved with additional parts that are "reused" from a base tree.
+	// these are parts that exist in a source tree, and are merged into the destination tree.
+	// an example of using it is in the apply process, which creates a new tree from a base tree and an input iterator.
+	// those parts of the base tree that were not modified by it input iterator will be merged into the resulting tree
+	// by passing them in the  reuseTree parameter.
+	) (graveler.TreeID, error)
 }
