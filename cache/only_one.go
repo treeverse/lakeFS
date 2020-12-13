ad1d7b0b4bd33928e6e351049dde12d629454916
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2020-12-13 16:58:35 +0200

    Compute values for cache once and return them on concurrent accesses
    When computing a new value to place in the cache, return it from all concurrent GetOrSet
    calls.
    
    Also remove ErrCacheItemNotFound, it can no longer be generated.  Cached values (always)
    can always outlive their lifetimes in the cache itself!  The actual values returned from
    GetOrSet are no longer controlled by the cache.

---
+++ cache/only_one.go
@@ -0,0 +1,39 @@
+package cache
+
+import "sync"
+
+// OnlyOne ensures only one concurrent evaluation of a keyed expression.
+type OnlyOne interface {
+	// Compute returns the value of calling fn(), but only calls fn once concurrently for
+	// each k.
+	Compute(k interface{}, fn func() interface{})
+}
+
+type ChanOnlyOne struct {
+	m *sync.Map
+}
+
+func NewChanOnlyOne() *ChanOnlyOne {
+	return &ChanOnlyOne{
+		m: &sync.Map{},
+	}
+}
+
+type chanAndResult struct {
+	ch    chan struct{}
+	value interface{}
+	err   error
+}
+
+func (c *ChanOnlyOne) Compute(k interface{}, fn func() (interface{}, error)) (interface{}, error) {
+	stop := chanAndResult{ch: make(chan struct{})}
+	actual, inFlight := c.m.LoadOrStore(k, &stop)
+	actualStop := actual.(*chanAndResult)
+	if inFlight {
+		<-actualStop.ch
+	} else {
+		actualStop.value, actualStop.err = fn()
+		close(actualStop.ch)
+	}
+	return actualStop.value, actualStop.err
+}
