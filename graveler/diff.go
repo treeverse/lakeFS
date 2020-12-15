59536c3bd6d16178c48846a7a8a77d619255f4b6
johnnyaug <yoni.augarten@treeverse.io>
2020-12-15 15:14:22 +0200

    simplify diff logic

--- graveler/diff.go
+++ graveler/diff.go
@@ -18,49 +18,46 @@ func NewDiffIterator(left ValueIterator, right ValueIterator) DiffIterator {
 	return it
 }
 
+func (d *diffIterator) compareKeys() int {
+	if d.leftNext && !d.rightNext {
+		return -1
+	}
+	if !d.leftNext && d.rightNext {
+		return 1
+	}
+	return bytes.Compare(d.left.Value().Key, d.right.Value().Key)
+}
+
 func (d *diffIterator) Next() bool {
-	for {
+	for d.leftNext || d.rightNext {
 		if d.left.Err() != nil || d.right.Err() != nil {
 			return false
 		}
-		if d.rightNext && d.leftNext {
-			leftKey := d.left.Value().Key
-			rightKey := d.right.Value().Key
-			if bytes.Equal(leftKey, rightKey) {
-				leftVal := d.left.Value()
-				rightVal := d.right.Value()
-				d.rightNext = d.right.Next()
-				d.leftNext = d.left.Next()
-				if !bytes.Equal(leftVal.Value.Identity, rightVal.Value.Identity) {
-					d.currentVal = rightVal
-					d.currentType = DiffTypeChanged
-					return true
-				}
-				continue // identical values
-			} else if bytes.Compare(leftKey, rightKey) < 0 {
-				d.currentVal = d.left.Value()
-				d.leftNext = d.left.Next()
-				d.currentType = DiffTypeRemoved
-				return true
-			} else { // leftKey > rightKey
-				d.currentVal = d.right.Value()
-				d.rightNext = d.right.Next()
-				d.currentType = DiffTypeAdded
+		comp := d.compareKeys()
+		switch comp {
+		case 0:
+			leftVal := d.left.Value()
+			rightVal := d.right.Value()
+			d.rightNext = d.right.Next()
+			d.leftNext = d.left.Next()
+			if !bytes.Equal(leftVal.Value.Identity, rightVal.Value.Identity) {
+				d.currentVal = rightVal
+				d.currentType = DiffTypeChanged
 				return true
 			}
-		} else if d.leftNext {
+		case -1:
 			d.currentVal = d.left.Value()
 			d.leftNext = d.left.Next()
 			d.currentType = DiffTypeRemoved
 			return true
-		} else if d.rightNext {
+		default: // leftKey > rightKey
 			d.currentVal = d.right.Value()
 			d.rightNext = d.right.Next()
 			d.currentType = DiffTypeAdded
 			return true
 		}
-		return false
 	}
+	return false
 }
 
 func (d *diffIterator) SeekGE(id Key) {
