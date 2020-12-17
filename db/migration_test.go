090de1f00e1f25cbd203968c9d72e46a0e40d1ca
guyhardonag <guy.hardonag@treeverse.io>
2020-12-17 15:50:01 +0200

    Add test for migrate down

--- db/migration_test.go
+++ db/migration_test.go
@@ -1,1 +1,23 @@
-package db
+package db_test
+
+import (
+	"testing"
+
+	"github.com/treeverse/lakefs/db"
+
+	"github.com/treeverse/lakefs/db/params"
+	"github.com/treeverse/lakefs/testutil"
+)
+
+func TestMigrations(t *testing.T) {
+	databaseURI, closer := testutil.GetDBInstance(pool)
+	defer closer()
+	err := db.MigrateUp(params.Database{ConnectionString: databaseURI})
+	if err != nil {
+		t.Fatal("failed running migrate up:", err)
+	}
+	err = db.MigrateDown(params.Database{ConnectionString: databaseURI})
+	if err != nil {
+		t.Fatal("failed running migrate down", err)
+	}
+}
