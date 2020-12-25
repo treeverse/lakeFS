7f30f17081deef3d0f919d911357415e482bd73d
tzahij <tzahij@hotmail.com>
2020-12-26 00:36:52 +0200

    tzahi final push of apply

--- graveler/committed/tree/base_tree_manager.go
+++ graveler/committed/tree/base_tree_manager.go
@@ -7,14 +7,14 @@ import (
 	"github.com/treeverse/lakefs/graveler/committed/sstable"
 )
 
-type baseTreeManagerType struct {
-	baseTree      Tree
-	partsForReuse []part
-	baseIndex     int
-	partManager   sstable.Manager
+type BaseTreeManager struct {
+	baseTree      Tree            // tree on which the input iterator will be applied, to create a new tree
+	partsForReuse []part          // part that can be copied directly from base tree to new tree
+	baseIndex     int             // next base part that should be processed or re-used
+	partManager   sstable.Manager // for lack of better place to connect to sstable
 }
 
-func (trees *treeRepo) newBaseTreeManager(treeID gr.TreeID) (*baseTreeManagerType, error) {
+func (trees *treeRepo) newBaseTreeManager(treeID gr.TreeID) (*BaseTreeManager, error) {
 	var baseParts Tree
 	var err error
 	if treeID == "" {
@@ -25,22 +25,24 @@ func (trees *treeRepo) newBaseTreeManager(treeID gr.TreeID) (*baseTreeManagerTyp
 			return nil, err
 		}
 	}
-	return &baseTreeManagerType{
+	return &BaseTreeManager{
 		baseTree:      baseParts,
 		partsForReuse: make([]part, 0),
 		partManager:   trees.partManger,
 	}, nil
 }
 
-func (bm *baseTreeManagerType) isEndOfBase() bool {
+func (bm *BaseTreeManager) isEndOfBase() bool {
 	return bm.baseIndex >= len(bm.baseTree.treeSlice)
 }
 
-func (bm *baseTreeManagerType) getBasePartForPath(key gr.Key) (*pushBackValueIterator, gr.Key, error) {
+// getBasePartForPath is the most important method
+// it accepts the next key from the input iterator, and finds the base part where it should be added or updated.
+//
+func (bm *BaseTreeManager) getBasePartForPath(key gr.Key) (*pushBackValueIterator, gr.Key, error) {
 	baseSlice := bm.baseTree.treeSlice
 	lenBaseTree := len(baseSlice)
-	for ; bm.baseIndex < lenBaseTree &&
-		bytes.Compare(baseSlice[bm.baseIndex].MaxKey, key) < 0; bm.baseIndex++ {
+	for ; bm.baseIndex < lenBaseTree && bytes.Compare(baseSlice[bm.baseIndex].MaxKey, key) < 0; bm.baseIndex++ {
 		bm.partsForReuse = append(bm.partsForReuse, baseSlice[bm.baseIndex])
 	}
 	if len(baseSlice) <= bm.baseIndex {
@@ -54,14 +56,14 @@ func (bm *baseTreeManagerType) getBasePartForPath(key gr.Key) (*pushBackValueIte
 	bm.baseIndex++
 	return newPushbackEntryIterator(basePartIter), p.MaxKey, nil
 }
-func (bm *baseTreeManagerType) getPartsForReuse() *[]part {
+func (bm *BaseTreeManager) getPartsForReuse() *[]part {
 	if bm.baseIndex < len(bm.baseTree.treeSlice) { // the apply loop did not reach the last parts of base, they will be added to reused
 		bm.partsForReuse = append(bm.partsForReuse, bm.baseTree.treeSlice[bm.baseIndex:]...)
 	}
 	return &bm.partsForReuse
 }
 
-func (bm *baseTreeManagerType) isPathInNextPart(path gr.Key) bool {
+func (bm *BaseTreeManager) isPathInNextPart(path gr.Key) bool {
 	if bm.isEndOfBase() {
 		return true // last part of base is the active now. the new path wil be written to it
 	} else {
@@ -69,15 +71,15 @@ func (bm *baseTreeManagerType) isPathInNextPart(path gr.Key) bool {
 	}
 }
 
-func (bm *baseTreeManagerType) getBaseMaxKey() gr.Key {
+func (bm *BaseTreeManager) getBaseMaxKey() gr.Key {
 	return bm.baseTree.treeSlice[len(bm.baseTree.treeSlice)-1].MaxKey
 }
 
-func (bm *baseTreeManagerType) wasLastPartProcessed() bool {
+func (bm *BaseTreeManager) wasLastPartProcessed() bool {
 	return len(bm.baseTree.treeSlice) <= bm.baseIndex
 }
 
-func (bm *baseTreeManagerType) getLastPartIter() (*pushBackValueIterator, error) {
+func (bm *BaseTreeManager) getLastPartIter() (*pushBackValueIterator, error) {
 	baseIter, _, err := bm.getBasePartForPath(bm.baseTree.treeSlice[len(bm.baseTree.treeSlice)-1].MaxKey)
 	return baseIter, err
 }
--- graveler/committed/tree/tree_apply.go
+++ graveler/committed/tree/tree_apply.go
@@ -35,60 +35,9 @@ func (trees treeRepo) Apply(baseTreeID graveler.TreeID, inputIter graveler.Value
 		input := pushbackInputIter.Value()
 		// adjust the input to the correct base and output parts
 		if bytes.Compare(maxCurrentBaseKey, input.Key) < 0 { // not is current base part
-			// flush all updates that remained in  current base part
-			if basePartIter != nil { // nil  basePartIter indicates this is first iteration
-				err = treeWriter.FlushIterToTree(basePartIter)
-				if err != nil {
-					return "", err
-				}
-				basePartIter.Close()
-				basePartIter = nil
-			}
-			// indicates that writing to this part did not close naturally with a splitter,even though a base part was finished
-			if treeWriter.HasOpenWriter() {
-				// next update will go past the next part of base tree. This means that the next part has no
-				// updates and can be reused in the new tree. for that to happen - the current output part must be closed
-				// so we close the current part
-				// a special case is when the current base part is the last in the tree, and the new path is bigger than
-				// any path in the tree. This too is considered as it the path is in next part
-				if !baseTreeManager.isPathInNextPart(input.Key) {
-					treeWriter.ClosePart()
-				}
-			}
-			if bytes.Compare(baseTreeMaxPath, input.Key) >= 0 {
-				// common case - input key falls within the range of keys in base tree ( less than maximum key in tree)
-				basePartIter, maxCurrentBaseKey, err = baseTreeManager.getBasePartForPath(input.Key)
-				if err != nil {
-					return "", err
-				}
-			} else {
-				// a special case when the input path is bigger than maximum part in the base tree
-				// Insert the new entries into the last part of the base tree, and dont create a tiny last part
-				// when some keys  are bigger than keys accepted before (e.g. when the keys are based on date)
-				// one thing we know about the last part: there is very tiny chance the last key is a split key. so adding keys
-				// to the last part make sense.
-				// There are two possibilities:
-				// 1. the input had entries that went into the last part. so last part should be open for writing now.
-				// 2. the last input key was smaller than the minimum key in the last part, so last base part should be opened
-				//    and copied into the output tree. After that- exit the loop and finish writing the input iterator
-				//    into the new tree
-				if !baseTreeManager.wasLastPartProcessed() {
-					basePartIter, err = baseTreeManager.getLastPartIter()
-					if err != nil {
-						return "", err
-					}
-					err = treeWriter.FlushIterToTree(basePartIter)
-					if err != nil {
-						return "", err
-					}
-					basePartIter.Close()
-					basePartIter = nil
-				}
-				err = pushbackInputIter.pushBack()
-				if err != nil {
-					return "", err
-				}
-				break // exit the main loop after all base was read
+			basePartIter, err, id, err2 := baseTreeManager.advanceBasePart(basePartIter, treeWriter, input, baseTreeMaxPath, maxCurrentBaseKey, pushbackInputIter)
+			if err2 != nil {
+				return id, err2
 			}
 		}
 		// handle single input update
@@ -130,4 +79,64 @@ func (trees treeRepo) Apply(baseTreeID graveler.TreeID, inputIter graveler.Value
 		return "", fmt.Errorf(" input flushing error : %w", err)
 	}
 	return treeWriter.saveTreeWithReuseParts(baseTreeManager.getPartsForReuse())
+}
+
+func (base *BaseTreeManager) advanceBasePart(basePartIter *pushBackValueIterator, treeWriter TreeWriter, input *graveler.ValueRecord, baseTreeMaxPath graveler.Key, maxCurrentBaseKey graveler.Key, pushbackInputIter *pushBackValueIterator) (*pushBackValueIterator, error, graveler.TreeID, error) {
+	var err error
+	// flush all updates that remained in  current base part
+	if basePartIter != nil { // nil  basePartIter indicates this is first iteration
+		err = treeWriter.FlushIterToTree(basePartIter)
+		if err != nil {
+			return nil, nil, "", err
+		}
+		basePartIter.Close()
+		basePartIter = nil
+	}
+	// indicates that writing to this part did not close naturally with a splitter,even though a base part was finished
+	if treeWriter.HasOpenWriter() {
+		// next update will go past the next part of base tree. This means that the next part has no
+		// updates and can be reused in the new tree. for that to happen - the current output part must be closed
+		// so we close the current part
+		// a special case is when the current base part is the last in the tree, and the new path is bigger than
+		// any path in the tree. This too is considered as it the path is in next part
+		if !base.isPathInNextPart(input.Key) {
+			treeWriter.ClosePart()
+		}
+	}
+	if bytes.Compare(baseTreeMaxPath, input.Key) >= 0 {
+		// common case - input key falls within the range of keys in base tree ( less than maximum key in tree)
+		basePartIter, maxCurrentBaseKey, err = base.getBasePartForPath(input.Key)
+		if err != nil {
+			return nil, nil, "", err
+		}
+	} else {
+		// a special case when the input path is bigger than maximum part in the base tree
+		// Insert the new entries into the last part of the base tree, and dont create a tiny last part
+		// when some keys  are bigger than keys accepted before (e.g. when the keys are based on date)
+		// one thing we know about the last part: there is very tiny chance the last key is a split key. so adding keys
+		// to the last part make sense.
+		// There are two possibilities:
+		// 1. the input had entries that went into the last part. so last part should be open for writing now.
+		// 2. the last input key was smaller than the minimum key in the last part, so last base part should be opened
+		//    and copied into the output tree. After that- exit the loop and finish writing the input iterator
+		//    into the new tree
+		if !base.wasLastPartProcessed() {
+			basePartIter, err = base.getLastPartIter()
+			if err != nil {
+				return nil, nil, "", err
+			}
+			err = treeWriter.FlushIterToTree(basePartIter)
+			if err != nil {
+				return nil, nil, "", err
+			}
+			basePartIter.Close()
+			basePartIter = nil
+		}
+		err = pushbackInputIter.pushBack()
+		if err != nil {
+			return nil, nil, "", err
+		}
+		break // exit the main loop after all base was read
+	}
+	return basePartIter, err, "", nil
 }
