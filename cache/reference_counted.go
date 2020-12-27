0673ccd46f6eaff3e0f8f992bf72b0c4df05cda4
Barak Amar <barak.amar@treeverse.io>
2020-12-27 13:14:35 +0200

    Cache SSTables (#1087)
    * Cache open SSTables in forest
    
    * Make shardedCacheWithDisposal implement its desired interface
    
    * Test SSTable cache
    
    - Modify cache to hold a testable interface
    - Add a basic test: it does *not* test the underlying cache, only that opens and closes are
      called reasonably.
    - Break an import cycle by moving sstable tests to package sstable_test
    
    * [CR] Remove support for external cache OnDispose; name cache shards
    
    * Eschew abbreviations: rename refcnted -> reference_counted
    
    There is little point in using a possibly confusing abbreviation when it never actually
    reduces the number of characters anyone except the author of the file needs to type.  I
    changed it to be more readable.

--- cache/reference_counted.go
+++ cache/reference_counted.go
@@ -38,6 +38,7 @@ type ParamsWithDisposal struct {
 // keyus have a String() method that is compatible with their equality, i.e. k1.GoString() ==
 // k2.GoString() implies k1 equals k2 in the sense of map comparison.
 type shardedCacheWithDisposal struct {
+	name   string
 	Seed   maphash.Seed
 	Shards []CacheWithDisposal
 }
@@ -50,7 +51,11 @@ func NewCacheWithDisposal(p ParamsWithDisposal) *shardedCacheWithDisposal {
 	for i := 0; i < p.Shards; i++ {
 		shards[i] = NewSingleThreadedCacheWithDisposal(p)
 	}
-	return &shardedCacheWithDisposal{Seed: maphash.MakeSeed(), Shards: shards}
+	return &shardedCacheWithDisposal{name: p.Name, Seed: maphash.MakeSeed(), Shards: shards}
+}
+
+func (s *shardedCacheWithDisposal) Name() string {
+	return s.name
 }
 
 func (s *shardedCacheWithDisposal) GetOrSet(k interface{}, setFn SetFn) (interface{}, Derefer, error) {
