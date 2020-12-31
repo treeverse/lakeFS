29b6aa141a10a4099887a61c81c8362edbd9e703
Barak Amar <barak.amar@treeverse.io>
2020-12-31 17:45:25 +0200

    Document nil return value of UpperBoundForPrefix (#1137)

--- graveler/prefix.go
+++ graveler/prefix.go
@@ -2,9 +2,10 @@ package graveler
 
 import "math"
 
-// UpperBoundForPrefix returns, given a prefix `p`, an array 'q' such that a byte array `s` starts with `p`
+// UpperBoundForPrefix returns, given a prefix `p`, a slice 'q' such that a byte slice `s` starts with `p`
 // if and only if p <= s < q. Namely, it returns an exclusive upper bound for the set of all byte arrays
-// that start with this prefix.
+// that start with this prefix. It returns nil if there is no such byte slice because all bytes of `p` are
+// maximal.
 func UpperBoundForPrefix(prefix []byte) []byte {
 	idx := len(prefix) - 1
 	for idx >= 0 && prefix[idx] == math.MaxUint8 {
