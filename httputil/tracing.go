b0b6c387dbb90291741aa73ada4434088ee533a3
Barak Amar <barak.amar@treeverse.io>
2021-01-17 13:56:21 +0200

    Correct slice body when reporting a response for trace (#1205)

--- httputil/tracing.go
+++ httputil/tracing.go
@@ -100,6 +100,13 @@ func (r *requestBodyTracer) Close() error {
 	return r.body.Close()
 }
 
+func presentBody(body []byte) string {
+	if len(body) > MaxBodyBytes {
+		body = body[:MaxBodyBytes]
+	}
+	return string(body)
+}
+
 func TracingMiddleware(requestIDHeaderName string, fields logging.Fields, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
@@ -131,7 +138,7 @@ func TracingMiddleware(requestIDHeaderName string, fields logging.Fields, next h
 			"sent_bytes":       responseWriter.ResponseSize,
 			"request_body":     requestBodyTracer.bodyRecorder.Buffer,
 			"request_headers":  r.Header,
-			"response_body":    string(responseWriter.BodyRecorder.Buffer[:MaxBodyBytes]),
+			"response_body":    presentBody(responseWriter.BodyRecorder.Buffer),
 			"response_headers": responseWriter.Header(),
 		}).Trace("HTTP call ended")
 	})
