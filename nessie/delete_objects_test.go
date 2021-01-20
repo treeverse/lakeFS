bb496fddef4d3b9ff27aed36144d7e8cf6da6b84
Itai Admi <itaiad200@gmail.com>
2021-01-20 18:59:50 +0200

    Try to repro delete objects no permissions bug

---
+++ nessie/delete_objects_test.go
@@ -0,0 +1,55 @@
+package nessie
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const numOfObjects = 10
+
+func TestDeleteObjects(t *testing.T) {
+	ctx, _, repo := setupTest(t)
+
+	files := make([]string, 0, numOfObjects)
+	identifiers := make([]*s3.ObjectIdentifier, 0, numOfObjects)
+
+	for i := 1; i <= numOfObjects; i++ {
+		file := strconv.Itoa(i) + ".txt"
+		files = append(files, file)
+		identifiers = append(identifiers, &s3.ObjectIdentifier{
+			Key: aws.String(masterBranch + "/" + file),
+		})
+		_, _ = uploadFileRandomData(ctx, t, repo, masterBranch, file)
+	}
+
+	listOut, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(repo),
+		Prefix: aws.String(masterBranch + "/"),
+	})
+
+	assert.NoError(t, err)
+	assert.Len(t, listOut.Contents, numOfObjects)
+
+	deleteOut, err := svc.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: aws.String(repo),
+		Delete: &s3.Delete{
+			Objects: identifiers,
+		},
+	})
+
+	assert.NoError(t, err)
+	assert.Len(t, deleteOut.Deleted, numOfObjects)
+
+	listOut, err = svc.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(repo),
+		Prefix: aws.String(masterBranch + "/"),
+	})
+
+	assert.NoError(t, err)
+	assert.Len(t, listOut.Contents, 0)
+}
