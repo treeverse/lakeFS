c0a7e2c91b95990033744f5b788948ca8641dc0d
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-01-24 09:15:55 +0200

    Add struct-as-key test to OnlyOne

--- cache/only_one_test.go
+++ cache/only_one_test.go
@@ -2,6 +2,7 @@ package cache_test
 
 import (
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -85,5 +86,48 @@ func TestOnlyOne_ComputeConcurrentlyOnce(t *testing.T) {
 	}
 	if did5 {
 		t.Error("did not expect to run concurrent compute after 5ms")
+	}
+}
+
+func TestOnlyOne_ComputeConcurrentlyOnceKeyedByStruct(t *testing.T) {
+	c := cache.NewChanOnlyOne()
+
+	type Key struct {
+		a, b int
+	}
+	const (
+		size        = 3
+		repeats     = 50
+		parallelism = 9
+	)
+
+	for i := 0; i < repeats; i++ {
+		var wg sync.WaitGroup
+		wg.Add(size * size)
+		ch := make(chan struct{})
+		numExec := int32(0)
+		for j := 0; j < parallelism; j++ {
+			go func() {
+				<-ch
+				for k := 0; k < size; k++ {
+					for l := 0; l < size; l++ {
+						v, err := c.Compute(Key{k, l}, func() (interface{}, error) {
+							atomic.AddInt32(&numExec, 1)
+							return Key{k + 1, l + 1}, nil
+						})
+						if err != nil {
+							t.Error(err)
+							continue
+						}
+						got := v.(Key)
+						expected := Key{a: k + 1, b: l + 1}
+						if got != expected {
+							t.Errorf("got %v instead of [%d %d]", got, k+1, l+1)
+						}
+					}
+				}
+			}()
+		}
+		close(ch)
 	}
 }
