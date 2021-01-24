a4591a9faed445a426d86b13915ecb0d8819ae9f
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-01-24 08:49:24 +0200

    Add a different racing opportunity to reference_counted_test

--- cache/reference_counted_test.go
+++ cache/reference_counted_test.go
@@ -13,88 +13,100 @@ import (
 )
 
 func TestCacheWithDisposal(t *testing.T) {
-	const (
-		size        = 7
-		parallelism = 100
-		repeats     = 2000
-	)
 	type record struct {
 		key      int
 		disposed int32
 		live     int32
 	}
 	ctx := context.Background()
-	var elements [size]*record
-	ch := make(chan error, parallelism*2)
-	allErrs := make(chan []error)
-	go func() {
-		errs := make([]error, 0)
-		for err := range ch {
-			errs = append(errs, err)
-		}
-		allErrs <- errs
-		close(allErrs)
-	}()
-	p := cache.ParamsWithDisposal{
-		Name:   t.Name(),
-		Logger: logging.Default().WithField("testing", true),
-		Shards: 3,
-		Size:   size,
-		OnDispose: func(v interface{}) error {
-			element := v.(*record)
-			if n := atomic.AddInt32(&element.disposed, 1); n != 1 {
-				ch <- fmt.Errorf("%d disposals of %+v", n, *element)
-			}
-			if l := atomic.CompareAndSwapInt32(&element.live, 1, 0); !l {
-				ch <- fmt.Errorf("disposal of already-dead %+v", *element)
-			}
-			return nil
-		},
+
+	cases := []struct {
+		name                       string
+		size, parallelism, repeats int
+		delay                      time.Duration
+	}{
+		{name: "full speed", size: 7, parallelism: 100, repeats: 2000},
+		{name: "try to race", size: 7, parallelism: 9, repeats: 500, delay: time.Millisecond},
 	}
-	c := cache.NewCacheWithDisposal(p)
 
-	numCreated := int32(0)
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var elements = make([]*record, tc.size)
+			ch := make(chan error, tc.parallelism*2)
+			allErrs := make(chan []error, tc.parallelism*2)
+			go func() {
+				errs := make([]error, 0)
+				for err := range ch {
+					errs = append(errs, err)
+				}
+				allErrs <- errs
+				close(allErrs)
+			}()
+			p := cache.ParamsWithDisposal{
+				Name:   t.Name(),
+				Logger: logging.Default().WithField("testing", true),
+				Shards: 3,
+				Size:   tc.size,
+				OnDispose: func(v interface{}) error {
+					element := v.(*record)
+					if n := atomic.AddInt32(&element.disposed, 1); n != 1 {
+						ch <- fmt.Errorf("%d disposals of %+v", n, *element)
+					}
+					if l := atomic.CompareAndSwapInt32(&element.live, 1, 0); !l {
+						ch <- fmt.Errorf("disposal of already-dead %+v", *element)
+					}
+					return nil
+				},
+			}
+			c := cache.NewCacheWithDisposal(p)
 
-	wg := sync.WaitGroup{}
-	for i := 0; i < parallelism; i++ {
-		wg.Add(1)
-		go func(i int) {
-			for j := 0; j < repeats; j++ {
-				k := j % size
-				v, release, err := c.GetOrSet(
-					ctx,
-					k, func() (interface{}, error) {
-						e := &record{
-							key:      k,
-							disposed: 0,
-							live:     1,
+			numCreated := int32(0)
+
+			wg := sync.WaitGroup{}
+			for i := 0; i < tc.parallelism; i++ {
+				wg.Add(1)
+				go func(i int) {
+					for j := 0; j < tc.repeats; j++ {
+						k := j % tc.size
+						v, release, err := c.GetOrSet(
+							ctx,
+							k, func() (interface{}, error) {
+								if tc.delay > 0 {
+									time.Sleep(tc.delay)
+								}
+								e := &record{
+									key:      k,
+									disposed: 0,
+									live:     1,
+								}
+								elements[k] = e
+								atomic.AddInt32(&numCreated, int32(1))
+								return e, nil
+							})
+						if err != nil {
+							ch <- err
 						}
-						elements[k] = e
-						atomic.AddInt32(&numCreated, int32(1))
-						return e, nil
-					})
-				if err != nil {
-					ch <- err
-				}
-				e := v.(*record)
-				if e.key != k {
-					ch <- fmt.Errorf("got %v not %d", v, k)
-				}
-				if e.live == 0 {
-					ch <- fmt.Errorf("got dead element %+v at %d", v, k)
-				}
-				if (j*repeats+i)%113 == 17 {
-					time.Sleep(17 * time.Millisecond)
-				}
-				release()
+						e := v.(*record)
+						if e.key != k {
+							ch <- fmt.Errorf("got %v not %d", v, k)
+						}
+						if e.live == 0 {
+							ch <- fmt.Errorf("got dead element %+v at %d", v, k)
+						}
+						if (j*tc.repeats+i)%113 == 17 {
+							time.Sleep(17 * time.Millisecond)
+						}
+						release()
+					}
+					wg.Done()
+				}(i)
 			}
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
-	close(ch)
-	errs := <-allErrs
-	for _, err := range errs {
-		t.Error(err)
+			wg.Wait()
+			close(ch)
+			errs := <-allErrs
+			for _, err := range errs {
+				t.Error(err)
+			}
+		})
 	}
 }
