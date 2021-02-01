a840b2c7466df5624856f1a0bae51c0fe2ee9232
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-02-01 11:19:57 +0200

    Bring back a missing "import ... deep" line
    Broke build.

--- api/controller_test.go
+++ api/controller_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/go-openapi/runtime"
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/swag"
+	"github.com/go-test/deep"
 	"github.com/treeverse/lakefs/api/gen/client/auth"
 	"github.com/treeverse/lakefs/api/gen/client/branches"
 	"github.com/treeverse/lakefs/api/gen/client/commits"
