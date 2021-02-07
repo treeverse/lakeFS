2865e36304af8e8d2684361743438a3d8e4dbb32
johnnyaug <yoni.augarten@treeverse.io>
2021-02-07 15:16:50 +0200

    implement interfaces

--- clients/src/main/scala/io/treeverse/clients/Main.scala
+++ clients/src/main/scala/io/treeverse/clients/Main.scala
@@ -2,33 +2,24 @@ package io.treeverse.clients
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.services.s3.model.GetObjectRequest
-import io.treeverse.clients.SSTableReader
-
-import java.io.File
+import io.treeverse.clients.s3.{S3MetaRangeReader, S3RangeReader}
 
 object Main {
   val REGION = "us-east-1"
   val PROFILE = "yoni"
 
   def main(args: Array[String]): Unit = {
-    var metaRangeID = "c6e1391c3677c56119a62827939ebafe4b812200039ddd490f2b5625ca105b5c"
-    var s3Client = AmazonS3ClientBuilder.standard().withRegion(REGION).withCredentials(new ProfileCredentialsProvider(PROFILE)).build()
-    var metarangeFile = File.createTempFile("lakefs", "metarange")
-    metarangeFile.deleteOnExit()
-    var obj = s3Client.getObject(new GetObjectRequest("yoni-test3", "javajava/_lakefs/c6e1391c3677c56119a62827939ebafe4b812200039ddd490f2b5625ca105b5c"), metarangeFile)
-    var reader = new SSTableReader
-    var ranges = reader.getRanges(metarangeFile.getAbsolutePath)
+    val metaRangeID = "c6e1391c3677c56119a62827939ebafe4b812200039ddd490f2b5625ca105b5c"
+    val repoPrefix = "javajava"
+    val s3 = AmazonS3ClientBuilder.standard().withRegion(REGION).withCredentials(new ProfileCredentialsProvider(PROFILE)).build()
+    val metaRangeReader = new S3MetaRangeReader(s3, "yoni-test3", repoPrefix)
+    val rangeReader = new S3RangeReader(s3, "yoni-test3", repoPrefix)
+    val ranges = metaRangeReader.getRanges(metaRangeID)
     for (r <- ranges) {
-      val rangeFile = File.createTempFile("lakefs", "metarange")
-      rangeFile.deleteOnExit()
-      obj = s3Client.getObject(new GetObjectRequest("yoni-test3", String.format("javajava/_lakefs/%s", r.identity)), rangeFile)
-      var records = reader.getEntries(rangeFile.getAbsolutePath)
+      val records = rangeReader.getEntries(r.identity)
       for (rec <- records) {
-        println(rec.key + " = " + rec.identity + "\n\t" + rec.entry);
+        println(rec.key + " = " + rec.identity + "\n\t" + rec.entry.address);
       }
-      rangeFile.delete();
     }
   }
-
 }
---
+++ clients/src/main/scala/io/treeverse/clients/s3/S3RangeReader.scala
@@ -0,0 +1,23 @@
+package io.treeverse.clients.s3
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.GetObjectRequest
+import io.treeverse.clients.{EntryRecord, RangeReader, SSTableReader}
+
+import java.io.File
+
+class S3RangeReader(var s3: AmazonS3, var bucket: String, var repositoryPrefix: String) extends RangeReader {
+  val reader = new SSTableReader
+
+  override def getEntries(rangeID: String): List[EntryRecord] = {
+    val rangeFile = File.createTempFile("lakefs", "metarange")
+    rangeFile.deleteOnExit()
+    s3.getObject(new GetObjectRequest(bucket, "%s/_lakefs/%s".format(repositoryPrefix, rangeID)), rangeFile)
+    try {
+      reader.getEntries(rangeFile.getAbsolutePath)
+    }
+    finally {
+      rangeFile.delete();
+    }
+  }
+}
