8ae3ddcb625aff7051cf433421b992f2b20cf55e
Oz Katz <oz.katz@treeverse.io>
2021-02-21 14:52:04 +0200

    fixed diff throwing 500 instead of 404 when ref/repo not found

--- catalog/errors.go
+++ catalog/errors.go
@@ -8,11 +8,12 @@ import (
 )
 
 var (
+	ErrNotFound                 = db.ErrNotFound
 	ErrInvalidMetadataSrcFormat = errors.New("invalid metadata src format")
 	ErrExpired                  = errors.New("expired from storage")
 	ErrFeatureNotSupported      = errors.New("feature not supported")
-	ErrBranchNotFound           = fmt.Errorf("branch %w", db.ErrNotFound)
-	ErrRepositoryNotFound       = fmt.Errorf("repository %w", db.ErrNotFound)
+	ErrBranchNotFound           = fmt.Errorf("branch %w", ErrNotFound)
+	ErrRepositoryNotFound       = fmt.Errorf("repository %w", ErrNotFound)
 	ErrInvalidValue             = errors.New("invalid value")
 	ErrNoDifferenceWasFound     = errors.New("no difference was found")
 	ErrConflictFound            = errors.New("conflict found")
