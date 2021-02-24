bab211b51177cbe60e1bd0a9c63e67e6f94c857f
Itai Admi <itaiad200@gmail.com>
2021-02-24 14:03:25 +0200

    Change webhook's Metadata to CommitMetadata

--- actions/webhook.go
+++ actions/webhook.go
@@ -20,16 +20,16 @@ type Webhook struct {
 }
 
 type WebhookEventInfo struct {
-	EventType     string            `json:"event_type"`
-	EventTime     string            `json:"event_time"`
-	ActionName    string            `json:"action_name"`
-	HookID        string            `json:"hook_id"`
-	RepositoryID  string            `json:"repository_id"`
-	BranchID      string            `json:"branch_id"`
-	SourceRef     string            `json:"source_ref,omitempty"`
-	CommitMessage string            `json:"commit_message"`
-	Committer     string            `json:"committer"`
-	Metadata      map[string]string `json:"metadata,omitempty"`
+	EventType      string            `json:"event_type"`
+	EventTime      string            `json:"event_time"`
+	ActionName     string            `json:"action_name"`
+	HookID         string            `json:"hook_id"`
+	RepositoryID   string            `json:"repository_id"`
+	BranchID       string            `json:"branch_id"`
+	SourceRef      string            `json:"source_ref,omitempty"`
+	CommitMessage  string            `json:"commit_message"`
+	Committer      string            `json:"committer"`
+	CommitMetadata map[string]string `json:"metadata,omitempty"`
 }
 
 const webhookClientTimeout = 5 * time.Minute
@@ -103,16 +103,16 @@ func (w *Webhook) Run(ctx context.Context, record graveler.HookRecord, writer *H
 func (w *Webhook) marshalEventInformation(record graveler.HookRecord) ([]byte, error) {
 	now := time.Now()
 	info := WebhookEventInfo{
-		EventType:     string(record.EventType),
-		EventTime:     now.UTC().Format(time.RFC3339),
-		ActionName:    w.ActionName,
-		HookID:        w.ID,
-		RepositoryID:  record.RepositoryID.String(),
-		BranchID:      record.BranchID.String(),
-		SourceRef:     record.SourceRef.String(),
-		CommitMessage: record.Commit.Message,
-		Committer:     record.Commit.Committer,
-		Metadata:      record.Commit.Metadata,
+		EventType:      string(record.EventType),
+		EventTime:      now.UTC().Format(time.RFC3339),
+		ActionName:     w.ActionName,
+		HookID:         w.ID,
+		RepositoryID:   record.RepositoryID.String(),
+		BranchID:       record.BranchID.String(),
+		SourceRef:      record.SourceRef.String(),
+		CommitMessage:  record.Commit.Message,
+		Committer:      record.Commit.Committer,
+		CommitMetadata: record.Commit.Metadata,
 	}
 	return json.Marshal(info)
 }
