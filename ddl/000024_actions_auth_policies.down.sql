8533df60e499a3e624fffd2eca70ae108042caed
Barak Amar <barak.amar@treeverse.io>
2021-02-28 11:24:26 +0200

    code review

--- ddl/000024_actions_auth_policies.down.sql
+++ ddl/000024_actions_auth_policies.down.sql
@@ -1,15 +1,15 @@
 BEGIN;
 
-update auth_policies
-set statement = statement - (select ordinality-1 as index
-        from public.auth_policies cross join jsonb_array_elements(statement) with ordinality
-        where display_name = 'RepoManagementReadAll' and value = '{"Action": ["ci:Read*"], "Effect": "allow", "Resource": "*"}')::int
-    where display_name = 'RepoManagementReadAll' AND statement @> '[{"Action": ["ci:Read*"], "Effect": "allow", "Resource": "*"}]'::jsonb ;
+UPDATE auth_policies
+SET statement = statement - (SELECT ordinality-1 AS index
+        FROM auth_policies CROSS JOIN jsonb_array_elements(statement) WITH ORDINALITY
+        WHERE display_name = 'RepoManagementReadAll' AND value = '{"Action": ["ci:Read*"], "Effect": "allow", "Resource": "*"}')::int
+    WHERE display_name = 'RepoManagementReadAll' AND statement @> '[{"Action": ["ci:Read*"], "Effect": "allow", "Resource": "*"}]'::jsonb;
 
-update auth_policies
-set statement = statement - (select ordinality-1 as index
-        from public.auth_policies cross join jsonb_array_elements(statement) with ordinality
-        where display_name = 'RepoManagementFullAccess' and value = '{"Action": ["ci:*"], "Effect": "allow", "Resource": "*"}')::int
-    where display_name = 'RepoManagementFullAccess' AND statement @> '[{"Action": ["ci:*"], "Effect": "allow", "Resource": "*"}]'::jsonb ;
+UPDATE auth_policies
+SET statement = statement - (SELECT ordinality-1 AS index
+        FROM auth_policies CROSS JOIN jsonb_array_elements(statement) WITH ORDINALITY
+        WHERE display_name = 'RepoManagementFullAccess' AND value = '{"Action": ["ci:*"], "Effect": "allow", "Resource": "*"}')::int
+    WHERE display_name = 'RepoManagementFullAccess' AND statement @> '[{"Action": ["ci:*"], "Effect": "allow", "Resource": "*"}]'::jsonb;
 
 COMMIT;
--- ddl/000024_actions_auth_policies.up.sql
+++ ddl/000024_actions_auth_policies.up.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
 UPDATE auth_policies
-SET statement = statement || '{"Action": ["ci:Read*"], "Effect": "allow", "Resource": "*"}'::jsonb
+SET statement = statement || '[{"Action": ["ci:Read*"], "Effect": "allow", "Resource": "*"}]'::jsonb
 WHERE display_name = 'RepoManagementReadAll' AND NOT statement @> '[{"Action": ["ci:Read*"], "Effect": "allow", "Resource": "*"}]'::jsonb;
 
 UPDATE auth_policies
-SET statement = statement || '{"Action": ["ci:*"], "Effect": "allow", "Resource": "*"}'::jsonb
+SET statement = statement || '[{"Action": ["ci:*"], "Effect": "allow", "Resource": "*"}]'::jsonb
 WHERE display_name = 'RepoManagementFullAccess' AND NOT statement @> '[{"Action": ["ci:*"], "Effect": "allow", "Resource": "*"}]'::jsonb;
 
 COMMIT;
