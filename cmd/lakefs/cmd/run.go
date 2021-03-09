91fb35792eecee1fa32daefbf19b74f4f0b2e3e9
johnnyaug <yoni.augarten@treeverse.io>
2021-03-09 14:34:12 +0200

    fix url in docs (#1592)
    * fix url in docs
    
    * fix phrasing in upgrade page

--- cmd/lakefs/cmd/run.go
+++ cmd/lakefs/cmd/run.go
@@ -56,7 +56,7 @@ var runCmd = &cobra.Command{
 		dbParams := cfg.GetDatabaseParams()
 
 		if err := db.ValidateSchemaUpToDate(ctx, dbParams); errors.Is(err, db.ErrSchemaNotCompatible) {
-			logger.WithError(err).Fatal("Migration version mismatch, for more information see https://docs.lakefs.io/deploying/upgrade.html")
+			logger.WithError(err).Fatal("Migration version mismatch, for more information see https://docs.lakefs.io/deploying-aws/upgrade.html")
 		} else if errors.Is(err, migrate.ErrNilVersion) {
 			logger.Debug("No migration, setup required")
 		} else if err != nil {
