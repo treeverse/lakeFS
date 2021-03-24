97668e45c39756817098740905a18549c8d15c6f
Itai Admi <itaiad200@gmail.com>
2021-03-24 12:00:32 +0200

    More improvements

--- clients/spark/core/src/main/scala/io/treeverse/clients/Exporter.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/Exporter.scala
@@ -1,7 +1,10 @@
 package io.treeverse.clients
-
+import org.apache.hadoop.fs.Path
 import org.apache.spark.SerializableWritable
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import java.net.{URI, URL}
+import scala.util.Random
 
 class Exporter(spark : SparkSession, apiClient: ApiClient, repoName: String, dstRoot: String) {
   def exportAllFromBranch(branch: String): Unit = {
@@ -12,14 +15,15 @@ class Exporter(spark : SparkSession, apiClient: ApiClient, repoName: String, dst
   def exportAllFromCommit(commitID: String): Unit = {
     val ns = apiClient.getStorageNamespace(repoName)
     val df = LakeFSContext.newDF(spark, repoName, commitID)
-    df.createOrReplaceTempView("commit")
+
+    val tableName = Random.alphanumeric.dropWhile(_.isDigit).take(8).mkString("") + "_commit"
+    df.createOrReplaceTempView(tableName)
 
     // pin Exporter field to avoid serialization
     val dst = dstRoot
-    val actionsDF = spark.sql("SELECT 'copy' as action, * FROM commit")
+    val actionsDF = spark.sql(s"SELECT 'copy' as action, * FROM ${tableName}")
 
     export(ns, dst, actionsDF)
-    spark.sparkContext.stop()
   }
 
   private def export(ns: String, rel: String, actionsDF: DataFrame) =  {
@@ -38,38 +42,43 @@ class Exporter(spark : SparkSession, apiClient: ApiClient, repoName: String, dst
     val newDF = LakeFSContext.newDF(spark, repoName, commitID)
     val prevDF = LakeFSContext.newDF(spark, repoName, prevCommitID)
 
-    newDF.createOrReplaceTempView("new_commit")
-    prevDF.createOrReplaceTempView("prev_commit")
+    val gen = Random.alphanumeric.dropWhile(_.isDigit)
+    val newTableName =  gen.take(8).mkString("") +"_new_commit"
+    val prevTableName =  gen.take(8).mkString("") +"_prev_commit"
+    newDF.createOrReplaceTempView(newTableName)
+    prevDF.createOrReplaceTempView(prevTableName)
 
     // pin Exporter field to avoid serialization
     val dst = dstRoot
 
-    val actionsDF = spark.sql("WITH raw_rows AS (SELECT n.key as nkey, n.address as naddress, n.etag as netag, " +
-      "p.key as pkey, p.address as paddress, p.etag as petag " +
-      "FROM new_commit n " +
-      "FULL OUTER JOIN prev_commit p " +
-      "ON n.key = p.key " +
-      "WHERE n.etag <> p.etag OR n.etag is null or p.etag is null) " +
-      "SELECT " +
-      "CASE WHEN nkey is null THEN 'delete' ELSE 'copy' END as action, " +
-      "CASE WHEN nkey is null THEN pkey ELSE nkey END as key, " +
-      "CASE WHEN naddress is null THEN paddress ELSE naddress END as address, " +
-      "CASE WHEN netag is null THEN petag ELSE netag END as etag ")
+    val actionsDF = spark.sql(s"""
+    SELECT
+      CASE WHEN nkey is null THEN 'delete' ELSE 'copy' END as action,
+      CASE WHEN nkey is null THEN pkey ELSE nkey END as key,
+      CASE WHEN naddress is null THEN paddress ELSE naddress END as address,
+      CASE WHEN netag is null THEN petag ELSE netag END as etag
+    FROM
+    (SELECT n.key as nkey, n.address as naddress, n.etag as netag,
+      p.key as pkey, p.address as paddress, p.etag as petag
+      FROM ${newTableName} n
+      FULL OUTER JOIN ${prevTableName} p
+      ON n.key = p.key
+      WHERE n.etag <> p.etag OR n.etag is null or p.etag is null)
+    """)
 
     export(ns, dst, actionsDF)
-    spark.sparkContext.stop()
   }
 }
 
 object Exporter {
-  private def handleRow(ns: String, rel: String, serializedConf: SerializableWritable[org.apache.hadoop.conf.Configuration], row: Row): Unit =  {
+  private def handleRow(ns: String, rootDst: String, serializedConf: SerializableWritable[org.apache.hadoop.conf.Configuration], row: Row): Unit =  {
     val action = row(0)
-    val key = row(1)
+    val key = row(1).toString()
     val address = row(2).toString()
     val conf = serializedConf.value
 
-    val srcPath = new org.apache.hadoop.fs.Path( if (address.contains("://")) address else ns + "/" + address)
-    val dstPath = new org.apache.hadoop.fs.Path(rel + "/" + key)
+    val srcPath = resolveURL(new URL(ns), address)
+    val dstPath = resolveURL(new URL(rootDst), key)
 
     val dstFS = dstPath.getFileSystem(conf)
 
@@ -90,5 +99,12 @@ object Exporter {
         ) : Unit
       }
     }
+  }
+
+  private def resolveURL(baseUrl :URL, extraPath: String): Path = {
+    val uri = baseUrl.toURI
+    val newPath: String = uri.getPath + '/' + extraPath
+
+    new Path(uri.resolve(newPath))
   }
 }
