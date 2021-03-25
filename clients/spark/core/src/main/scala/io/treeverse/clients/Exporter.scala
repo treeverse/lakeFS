8b2e806bf9924bf7ff3dc44e9e589b1520dea3fd
itaiad200 <itaiad200@gmail.com>
2021-03-25 19:06:24 +0200

    Improve export (sql query, file ordering, errors log) (#1666)

--- clients/spark/core/src/main/scala/io/treeverse/clients/Exporter.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/Exporter.scala
@@ -1,9 +1,20 @@
 package io.treeverse.clients
-
+import io.treeverse.clients.Exporter.resolveURL
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
 import org.apache.spark.SerializableWritable
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import java.io.IOException
+import java.net.URL
+import scala.util.Random
 
-class Exporter(spark : SparkSession, apiClient: ApiClient, repoName: String, dstRoot: String) {
+
+class Exporter(spark : SparkSession, apiClient: ApiClient, filter: KeyFilter, repoName: String, dstRoot: String) {
+
+  def this(spark : SparkSession, apiClient: ApiClient, repoName: String, dstRoot: String) {
+    this(spark, apiClient, new SparkFilter(), repoName, dstRoot)
+  }
+
   def exportAllFromBranch(branch: String): Unit = {
     val commitID = apiClient.getBranchHEADCommit(repoName, branch)
     exportAllFromCommit(commitID)
@@ -12,79 +23,147 @@ class Exporter(spark : SparkSession, apiClient: ApiClient, repoName: String, dst
   def exportAllFromCommit(commitID: String): Unit = {
     val ns = apiClient.getStorageNamespace(repoName)
     val df = LakeFSContext.newDF(spark, repoName, commitID)
-    df.createOrReplaceTempView("commit")
+
+    val tableName = randPrefix() + "_commit"
+    df.createOrReplaceTempView(tableName)
 
     // pin Exporter field to avoid serialization
     val dst = dstRoot
-    val actionsDF = spark.sql("SELECT 'copy' as action, * FROM commit")
+    val actionsDF = spark.sql(s"SELECT 'copy' as action, * FROM ${tableName}")
 
-    export(ns, dst, actionsDF)
-    spark.sparkContext.stop()
+    actOnActions(ns, dst, commitID, actionsDF)
   }
 
-  private def export(ns: String, rel: String, actionsDF: DataFrame) =  {
+  final private val maxLoggedErrors = 10000
+  private def export(round: Int, ns: String, rel: String, commitID: String, actionsDF: DataFrame) : Boolean =  {
+    import spark.implicits._
+
     val hadoopConf = spark.sparkContext.hadoopConfiguration
     val serializedConf = new SerializableWritable(hadoopConf)
+    val f = filter
+    val errs = actionsDF.map(
+      row =>
+        Exporter.handleRow(f, round, ns, rel, serializedConf, row)
+    ).filter(err => err != null)
 
-    actionsDF.foreach { row =>
-      Exporter.handleRow(ns, rel, serializedConf, row)
+    if (errs.isEmpty){
+      return true
     }
-}
-def exportFrom(branch: String, prevCommitID: String): Unit = {
+
+    writeSummaryFile(false, commitID, errs.take(maxLoggedErrors).reduce(_+"\n"+_))
+    false
+  }
+
+  def exportFrom(branch: String, prevCommitID: String): Unit = {
     val commitID = apiClient.getBranchHEADCommit(repoName, branch)
     val ns = apiClient.getStorageNamespace(repoName)
 
     val newDF = LakeFSContext.newDF(spark, repoName, commitID)
     val prevDF = LakeFSContext.newDF(spark, repoName, prevCommitID)
 
-    newDF.createOrReplaceTempView("new_commit")
-    prevDF.createOrReplaceTempView("prev_commit")
+    val newTableName =  randPrefix() + "_new_commit"
+    val prevTableName =  randPrefix() + "_prev_commit"
+    newDF.createOrReplaceTempView(newTableName)
+    prevDF.createOrReplaceTempView(prevTableName)
 
     // pin Exporter field to avoid serialization
     val dst = dstRoot
-    val actionsDF = spark.sql("SELECT 'copy' as action, new_commit.* FROM new_commit " +
-      "LEFT JOIN prev_commit " +
-      "ON (new_commit.etag = prev_commit.etag AND new_commit.key = prev_commit.key) " +
-      "WHERE prev_commit.key is NULL " +
-      "UNION " +
-      "SELECT 'delete' as action, prev_commit.* FROM prev_commit " +
-      "LEFT OUTER JOIN new_commit " +
-      "ON (new_commit.key = prev_commit.key) " +
-      "WHERE new_commit.key is NULL")
 
-    export(ns, dst, actionsDF)
-    spark.sparkContext.stop()
+    val actionsDF = spark.sql(s"""
+    SELECT
+      CASE WHEN nkey is null THEN 'delete' ELSE 'copy' END as action,
+      CASE WHEN nkey is null THEN pkey ELSE nkey END as key,
+      CASE WHEN naddress is null THEN paddress ELSE naddress END as address,
+      CASE WHEN netag is null THEN petag ELSE netag END as etag
+    FROM
+    (SELECT n.key as nkey, n.address as naddress, n.etag as netag,
+      p.key as pkey, p.address as paddress, p.etag as petag
+      FROM ${newTableName} n
+      FULL OUTER JOIN ${prevTableName} p
+      ON n.key = p.key
+      WHERE n.etag <> p.etag OR n.etag is null or p.etag is null)
+    """)
+
+    actOnActions(ns, dst, commitID, actionsDF)
+  }
+
+  final private val successMsg = "Export completed successfully!"
+  private def actOnActions(ns: String, dst: String, commitID: String, actionsDF: DataFrame): Unit = {
+    for( i <- 1 to filter.rounds()){
+      if (!export(i, ns, dst, commitID, actionsDF)) {
+        // failed to export files, don't export next round
+        return
+      }
+    }
+
+    writeSummaryFile(true, commitID, successMsg)
+  }
+
+  private def writeSummaryFile(success: Boolean, commitID: String, content : String) = {
+    val suffix = if(success) "SUCCESS" else "FAILURE"
+    val dstPath = resolveURL(new URL(dstRoot), s"EXPORT_${commitID}_${suffix}")
+    val dstFS = dstPath.getFileSystem(spark.sparkContext.hadoopConfiguration)
+
+    val stream = dstFS.create(dstPath)
+    stream.writeChars(content)
+    stream.close()
+  }
+
+  final private val prefixLen = 8
+  private def randPrefix() : String = {
+    val gen = Random.alphanumeric.dropWhile(_.isDigit)
+    gen.take(prefixLen).mkString("")
   }
 }
 
 object Exporter {
-  private def handleRow(ns: String, rel: String, serializedConf: SerializableWritable[org.apache.hadoop.conf.Configuration], row: Row): Unit =  {
+  private def handleRow(filter: KeyFilter, round:Int, ns: String, rootDst: String, serializedConf: SerializableWritable[Configuration], row: Row) : String =  {
     val action = row(0)
-    val key = row(1)
+    val key = row(1).toString()
     val address = row(2).toString()
-    val conf = serializedConf.value
 
-    val srcPath = new org.apache.hadoop.fs.Path( if (address.contains("://")) address else ns + "/" + address)
-    val dstPath = new org.apache.hadoop.fs.Path(rel + "/" + key)
+    if (filter.roundForKey(key) != round){
+      // skip this round
+      return null
+    }
+
+    val conf = serializedConf.value
+    val srcPath = resolveURL(new URL(ns), address)
+    val dstPath = resolveURL(new URL(rootDst), key)
 
     val dstFS = dstPath.getFileSystem(conf)
 
-    action match {
-      case "delete" => {
-        dstFS.delete(dstPath,false) : Unit
-      }
+      action match {
+        case "delete" => {
+          try {
+            dstFS.delete(dstPath, false)
+            null
+          } catch {
+            case e : (IOException) =>  s"Unable to delete file ${dstPath.toString}: ${e.toString}"
+          }
+        }
 
-      case "copy" =>{
-        org.apache.hadoop.fs.FileUtil.copy(
-
-          srcPath.getFileSystem(conf),
-          srcPath,
-          dstFS,
-          dstPath,
-          false,
-          conf
-        ) : Unit
+        case "copy" =>{
+          try {
+            org.apache.hadoop.fs.FileUtil.copy(
+              srcPath.getFileSystem(conf),
+              srcPath,
+              dstFS,
+              dstPath,
+              false,
+              conf)
+            null
+          } catch {
+            case e : (IOException) =>  s"Unable to copy file ${dstPath.toString} from source ${srcPath.toString}: ${e.toString}"
+          }
+        }
       }
-    }
+  }
+
+  private def resolveURL(baseUrl :URL, extraPath: String): Path = {
+    val uri = baseUrl.toURI
+    val newPath: String = uri.getPath + '/' + extraPath
+
+    new Path(uri.resolve(newPath))
   }
 }
