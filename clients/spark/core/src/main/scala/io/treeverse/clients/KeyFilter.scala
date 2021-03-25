61e24af2afb0fdb90c20ca1f0193c7f3beec1b77
Barak Amar <barak.amar@treeverse.io>
2021-03-25 19:06:24 +0200

    Improve export (sql query, file ordering, errors log) (#1666)

---
+++ clients/spark/core/src/main/scala/io/treeverse/clients/KeyFilter.scala
@@ -0,0 +1,12 @@
+package io.treeverse.clients
+
+trait KeyFilter extends Serializable {
+  // Number of rounds for this matcher.
+  // Rounds are counted from 1, allowing filter logic could be changed based on the number of rounds.
+  // For example, export all non-success file on the first round,
+  // and all success files on the seconds round.
+  def rounds(): Int
+
+  // Return the matching round for the key
+  def roundForKey(key: String) : Int
+}
