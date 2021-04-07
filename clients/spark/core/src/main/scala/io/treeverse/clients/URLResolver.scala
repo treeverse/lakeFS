bef42a49d5f150b60085f355b75e37ab84b4eeff
Itai Admi <itaiad200@gmail.com>
2021-04-07 13:41:59 +0300

    Fix url parsing

--- clients/spark/core/src/main/scala/io/treeverse/clients/URLResolver.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/URLResolver.scala
@@ -4,15 +4,22 @@ import java.net.URI
 import org.apache.hadoop.fs.Path
 
 object URLResolver extends Serializable{
-
-  /** resolves a url which is relative to the full uri
-   *
-   * @param uri includes schema and may include a path
-   * @param extraPath must be a relative url
-   * @return the concatenated path
+  /** resolves a url given the path.
+   *  If the path is a full url to the object store return that.
+   *  Else, concatenates the path to the url.
    */
-  def resolveURL(uri: URI, extraPath: String): Path = {
-    val newPath: String = uri.getPath + '/' + extraPath
-    new Path(uri.resolve(newPath))
+  def resolveURL(uri: URI, path: String): Path = {
+    if (path.contains("://")) {
+      return new Path(path)
+    }
+
+    // uri should end with a trailing '/'
+    var uriPath =  uri.toString
+    uriPath = if (uriPath.endsWith("/")) uriPath else uriPath.concat("/")
+
+    // path should not start with a leading '/'
+    val extraPath = if (path.startsWith("/")) path.substring(1) else path
+
+    new Path(uriPath + extraPath)
   }
 }
