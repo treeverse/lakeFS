0a9bbe45cf3d3ce4dee1bd6770e9e29e4daf7e0d
Barak Amar <barak.amar@treeverse.io>
2021-04-12 17:36:58 +0300

    python client class ensure server endpoint incldue default path api/v1

--- clients/python/lakefs_client/client.py
+++ clients/python/lakefs_client/client.py
@@ -1,9 +1,18 @@
+from urllib.parse import urlparse, urlunparse
+
 import lakefs_client
-from lakefs_client.apis import ActionsApi, AuthApi, BranchesApi, CommitsApi, ConfigApi, HealthCheckApi, MetadataApi, ObjectsApi, RefsApi, RepositoriesApi, StagingApi, TagsApi
+from lakefs_client.apis import ActionsApi, AuthApi, BranchesApi, CommitsApi, ConfigApi, HealthCheckApi, MetadataApi, \
+    ObjectsApi, RefsApi, RepositoriesApi, StagingApi, TagsApi
+
+DEFAULT_BASE_URL_PATH = "/api/v1"
+
 
 class LakeFSClient:
     def __init__(self, configuration=None, header_name=None, header_value=None, cookie=None, pool_threads=1):
-        self._api = lakefs_client.ApiClient(configuration=configuration, header_name=header_name, header_value=header_value, cookie=cookie, pool_threads=pool_threads)
+        if configuration:
+            configuration.host = LakeFSClient._ensure_endpoint(configuration.host)
+        self._api = lakefs_client.ApiClient(configuration=configuration, header_name=header_name,
+                                            header_value=header_value, cookie=cookie, pool_threads=pool_threads)
         self.actions = ActionsApi(self._api)
         self.auth = AuthApi(self._api)
         self.branches = BranchesApi(self._api)
@@ -16,3 +25,13 @@ class LakeFSClient:
         self.repositories = RepositoriesApi(self._api)
         self.staging = StagingApi(self._api)
         self.tags = TagsApi(self._api)
+
+    @staticmethod
+    def _ensure_endpoint(endpoint):
+        try:
+            o = urlparse(endpoint)
+            if not o.path or o.path == '/':
+                return urlunparse((o.scheme, o.netloc, DEFAULT_BASE_URL_PATH, o.params, o.query, o.fragment))
+        except ValueError:
+            pass
+        return endpoint
