7b2e7bb8cb23997e0af38dceafb6a182eb05a13a
Barak Amar <barak.amar@treeverse.io>
2021-04-12 11:07:02 +0300

    Specify key structure type (relative/full) in entry model (#1733)

--- pkg/gateway/operations/operation_utils.go
+++ pkg/gateway/operations/operation_utils.go
@@ -8,12 +8,18 @@ import (
 	"github.com/treeverse/lakefs/pkg/logging"
 )
 
-func (o *PathOperation) finishUpload(req *http.Request, checksum, physicalAddress string, size int64) error {
+func (o *PathOperation) finishUpload(req *http.Request, checksum, physicalAddress string, size int64, relative bool) error {
+	addressType := catalog.AddressTypeRelative
+	if !relative {
+		addressType = catalog.AddressTypeFull
+	}
+
 	// write metadata
 	writeTime := time.Now()
 	entry := catalog.DBEntry{
 		Path:            o.Path,
 		PhysicalAddress: physicalAddress,
+		AddressType:     addressType,
 		Checksum:        checksum,
 		Metadata:        nil, // TODO: Read whatever metadata came from the request headers/params and add here
 		Size:            size,
