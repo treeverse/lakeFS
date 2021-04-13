f564ea3eae78b3b64f1fb3b4e1fd3c9bb801a589
Barak Amar <barak.amar@treeverse.io>
2021-04-13 16:37:39 +0300

    extract endpoint from host configuration

--- clients/python/lakefs_client/client.py
+++ clients/python/lakefs_client/client.py
@@ -4,13 +4,10 @@ import lakefs_client
 from lakefs_client.apis import ActionsApi, AuthApi, BranchesApi, CommitsApi, ConfigApi, HealthCheckApi, MetadataApi, \
     ObjectsApi, RefsApi, RepositoriesApi, StagingApi, TagsApi
 
-DEFAULT_BASE_URL_PATH = "/api/v1"
-
-
 class LakeFSClient:
     def __init__(self, configuration=None, header_name=None, header_value=None, cookie=None, pool_threads=1):
         if configuration:
-            configuration.host = LakeFSClient._ensure_endpoint(configuration.host)
+            configuration = LakeFSClient._ensure_endpoint(configuration)
         self._api = lakefs_client.ApiClient(configuration=configuration, header_name=header_name,
                                             header_value=header_value, cookie=cookie, pool_threads=pool_threads)
         self.actions = ActionsApi(self._api)
@@ -27,11 +24,15 @@ class LakeFSClient:
         self.tags = TagsApi(self._api)
 
     @staticmethod
-    def _ensure_endpoint(endpoint):
-        try:
-            o = urlparse(endpoint)
-            if not o.path or o.path == '/':
-                return urlunparse((o.scheme, o.netloc, DEFAULT_BASE_URL_PATH, o.params, o.query, o.fragment))
-        except ValueError:
-            pass
-        return endpoint
+    def _ensure_endpoint(configuration):
+        if configuration.host:
+            try:
+                o = urlparse(configuration.host)
+                if not o.path or o.path == '/':
+                    settings = configuration.get_host_settings()
+                    if settings:
+                        base_path = urlparse(settings[0].get('url')).path
+                        configuration.host = urlunparse((o.scheme, o.netloc, base_path, o.params, o.query, o.fragment))
+            except ValueError:
+                pass
+        return configuration
