24a028ab96e7f6d9291b672fb5612f2c8dca2052
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-04-22 11:40:34 +0300

    Test using defined ops sequence; run under a prefix (and clean up)

--- clients/spark/examples/src/main/scala/io/treeverse/clients/examples/S3AUser.scala
+++ clients/spark/examples/src/main/scala/io/treeverse/clients/examples/S3AUser.scala
@@ -13,24 +13,30 @@ import org.apache.hadoop.fs
 import org.apache.hadoop.fs.s3a
 import org.apache.hadoop.conf.Configuration
 
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request
-import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.s3.model.{DeleteObjectRequest, ListObjectsV2Request, PutObjectRequest}
 import software.amazon.awssdk.services.s3.S3Client
 import scala.collection.immutable.Stream
+import scala.collection.mutable
+
+trait Store {
+  def upload(key: String, contents: String)
+  def download(key: String): String
+  def delete(key: String)
+}
 
 /** Exercise an underlying FileSystem. */
-class UnderlyingFS(s3aFS: s3a.S3AFileSystem, bucket: String) {
+class UnderlyingFS(s3aFS: s3a.S3AFileSystem, bucket: String, prefix: String) extends Store {
   val utf8 = Charset.forName("UTF-8")
 
   def upload(key: String, contents: String) = {
-    val path = new fs.Path("s3a", bucket, key)
+    val path = new fs.Path("s3a", bucket, prefix + key)
     val os = s3aFS.create(path)
     os.write(contents.getBytes(utf8))
     os.close()
   }
 
   def download(key: String): String = {
-    val path = new fs.Path("s3a", bucket, key)
+    val path = new fs.Path("s3a", bucket, prefix + key)
     val is = s3aFS.open(path, 16384)
     val bytes = Stream.continually({
       val buf = new Array[Byte](10)
@@ -39,6 +45,45 @@ class UnderlyingFS(s3aFS: s3a.S3AFileSystem, bucket: String) {
     }).takeWhile(buf => buf != null)
       .fold(new Array[Byte](0))(_ ++ _)
     new String(bytes)
+  }
+
+  def delete(key: String) = {
+    val path = new fs.Path("s3a", bucket, prefix + key)
+    s3aFS.delete(path)
+  }
+}
+
+/** Store of currently existing keys */
+class KeyStore extends Store {
+  val store = mutable.Map[String, String]()
+
+  def list(): Seq[String] = store.keys.toList.sorted
+
+  def upload(key: String, contents: String) = store.update(key, contents)
+
+  def download(key: String): String = store.get(key).get
+
+  def delete(key: String) = store.remove(key)
+}
+
+class CompareStore(val a: Store, val b: Store) extends Store {
+  /** Upload key -> contents to each store. */
+  def upload(key: String, contents: String) = {
+    a.upload(key, contents)
+    b.upload(key, contents)
+  }
+
+  /** Download key from each store, if different return a non-empty explanation */
+  def download(key: String): String = {
+    val aValue = a.download(key)
+    val bValue = b.download(key)
+    if (aValue != bValue) s""""${aValue}" != "${bValue}"""" else ""
+  }
+
+  /** Delete key from each store. */
+  def delete(key: String) = {
+    a.delete(key)
+    b.delete(key)
   }
 }
 
@@ -50,12 +95,22 @@ class ObjectStore(val s3: S3Client, val bucket: String) {
     * @bug no support for paging.
     */
   def list(prefix: String): Seq[String] = {
+    val sPrefix = prefix.stripPrefix("/")
     val req = ListObjectsV2Request.builder
       .bucket(bucket)
-      .prefix(prefix)
+      .prefix(sPrefix)
       .build
     val res = s3.listObjectsV2(req)
-    res.contents.asScala.map(_.key)
+    res.contents.asScala.map(_.key).map("/" + _.stripPrefix(sPrefix))
+  }
+
+  def delete(prefix: String, key: String) = {
+    val path = s"${prefix.stripPrefix("/")}${key.stripPrefix("/")}"
+    val req = DeleteObjectRequest.builder
+      .bucket(bucket)
+      .key(path)
+      .build
+    val res = s3.deleteObject(req)
   }
 }
 
@@ -74,9 +129,6 @@ object S3AUser extends App {
       Console.err.println("Usage: ... s3://bucket/prefix/to/write")
       System.exit(1)
     }
-
-
-    // Not running as part of Spark, setup a similar Hadoop configuration.
 
     System.setProperty(SDKGlobalConfiguration.ENABLE_S3_SIGV4_SYSTEM_PROPERTY, "true")
 
@@ -107,39 +159,59 @@ object S3AUser extends App {
         val bucket = baseURI.getHost
         val prefix = baseURI.getPath
 
+        def failDownload(key: String, stores: Store*): String =
+          stores.map(store =>
+            Try(store.download(key)) match {
+              case Failure(_: java.io.FileNotFoundException | _: java.util.NoSuchElementException) => null
+              case Failure(e) => s"unexpected exception ${e}"
+              case Success(contents) => s"value ${contents}, expected deleted"
+            })
+            .filter(_ != null)
+            .mkString(", ")
+
+        def checkObjectStore(store: KeyStore, objects: ObjectStore, prefix: String): String = {
+          val expected = store.list()
+          val actual = objects.list(prefix).toList
+          val tooMany = actual diff expected
+          val tooFew = expected diff actual
+          Seq(
+            if (tooMany.isEmpty) null else  s"""unexpected objects ${tooMany.mkString(", ")}""",
+            if (tooFew.isEmpty) null else s"""missing objects ${tooFew.mkString(", ")}""",
+          ).filter(_ != null).mkString("; ")
+        }
+
+        def isNonemptyString(s: Any) = s match { case s: String => s != ""; case _ => false}
+
         val s3aFS = makeS3A(conf, bucket)
-        val fs = new UnderlyingFS(s3aFS, bucket)
+        val fs = new UnderlyingFS(s3aFS, bucket, prefix)
+        val mem = new KeyStore
+        val compare = new CompareStore(fs, mem)
 
         val s3 = S3Client.builder.build
-        val store = new ObjectStore(s3, bucket)
+        val objects = new ObjectStore(s3, bucket)
 
-        fs.upload(prefix + "/abc", "quick brown fox")
-        fs.upload(prefix + "/d/e/f/xyz", "foo bar")
+        val actions = Seq(
+          () => compare.upload("/abc", "quick brown fox"),
+          () => compare.upload("/d/e/f/xyz", "foo bar"),
+          () => checkObjectStore(mem, objects, prefix),
+          () => compare.upload("/abc/g", "slow white lemming"),
+          () => compare.download("/abc"),
+          () => compare.download("/d/e/f/xyz"),
+          () => compare.download("/abc/g"),
+          () => compare.delete("/abc"),
+          () => failDownload("/abc", fs, mem),
+          () => compare.download("/abc/g"),
+          () => checkObjectStore(mem, objects, prefix),
+        )
 
-        val abcContents = fs.download(prefix + "/abc")
-        if (abcContents != "quick brown fox") {
-          Console.err.println(s"""got /abc = "${abcContents}" (len ${abcContents.size})""")
+        // TODO(ariels): Compare current state on ObjectStore after each action.
+        actions.map(_()).zipWithIndex.filter({ case (res, _) => isNonemptyString(res) }).foreach({ case (err, idx) => {
+          Console.err.println(s"[${idx}] ${err}\n")
           numFailures += 1
-        }
-        val xyzContents = fs.download(prefix + "/d/e/f/xyz")
-        if (xyzContents != "foo bar") {
-          Console.err.println(s"""got /d/e/f/xyz = "${xyzContents}" (len ${xyzContents.size})""")
-          numFailures += 1
-        }
+        }})
 
-        val prefix2 = prefix.stripPrefix("/") // S3 keys don't have that leading slash.
-        val listing = store.list(prefix2).map(_.stripPrefix(prefix2)).toList
-        val expected = scala.List("abc", "d/e/f/xyz")
-        val tooMany = listing diff expected
-        if (!tooMany.isEmpty) {
-          Console.err.printf("unexpected objects on S3: %s\n", tooMany)
-          numFailures += 1
-        }
-        val tooFew = expected diff listing
-        if (!tooFew.isEmpty) {
-          Console.err.printf("missing objects on S3: %s\n", tooFew)
-          numFailures += 1
-        }
+        // Clean up (_only_ known files).
+        mem.list().foreach(key => objects.delete(prefix, key))
 
         s3aFS.close()
       }
