49659553d34207c24dddf60155b019e7e78baf5c
Barak Amar <barak.amar@treeverse.io>
2021-05-05 10:45:01 +0300

    hadoopfs assembly, handle workdir and lakefs client

--- clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
@@ -1,6 +1,10 @@
 package io.lakefs;
 
+import io.lakefs.clients.api.ApiException;
+import io.lakefs.clients.api.RepositoriesApi;
 import io.lakefs.clients.api.auth.HttpBasicAuth;
+import io.lakefs.clients.api.model.Repository;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
 import org.apache.hadoop.fs.permission.FsPermission;
