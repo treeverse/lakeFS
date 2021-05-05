a4a31cf5cfe66d7a4b7bc71cbfe748d8749b55e9
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-05-05 16:58:48 +0300

    Add "open" method
    Untested -- need an integration test (vs. e.g. minIO and/or true S3).

--- clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
@@ -66,32 +66,32 @@ public class LakeFSFileSystem extends FileSystem {
 
     @Override
     public void initialize(URI name, Configuration conf) throws IOException {
-        super.initialize(name, conf);
+	super.initialize(name, conf);
 	this.conf = conf;
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ initialize: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$", name);
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ initialize: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$", name);
 
-        String host = name.getHost();
-        if (host == null) {
-            throw new IOException("Invalid repository specified");
-        }
-        setConf(conf);
-        this.uri = name;
+	String host = name.getHost();
+	if (host == null) {
+	    throw new IOException("Invalid repository specified");
+	}
+	setConf(conf);
+	this.uri = name;
 
-        // setup lakeFS api client
-        String endpoint = conf.get(FS_LAKEFS_ENDPOINT, "http://localhost:8000/api/v1");
-        String accessKey = conf.get(FS_LAKEFS_ACCESS_KEY);
-        if (accessKey == null) {
-            throw new IOException("Missing lakeFS access key");
-        }
-        String secretKey = conf.get(FS_LAKEFS_SECRET_KEY);
-        if (secretKey == null) {
-            throw new IOException("Missing lakeFS secret key");
-        }
-        this.apiClient = io.lakefs.clients.api.Configuration.getDefaultApiClient();
-        this.apiClient.setBasePath(endpoint);
-        HttpBasicAuth basicAuth = (HttpBasicAuth)this.apiClient.getAuthentication(BASIC_AUTH);
-        basicAuth.setUsername(accessKey);
-        basicAuth.setPassword(secretKey);
+	// setup lakeFS api client
+	String endpoint = conf.get(FS_LAKEFS_ENDPOINT, "http://localhost:8000/api/v1");
+	String accessKey = conf.get(FS_LAKEFS_ACCESS_KEY);
+	if (accessKey == null) {
+	    throw new IOException("Missing lakeFS access key");
+	}
+	String secretKey = conf.get(FS_LAKEFS_SECRET_KEY);
+	if (secretKey == null) {
+	    throw new IOException("Missing lakeFS secret key");
+	}
+	this.apiClient = io.lakefs.clients.api.Configuration.getDefaultApiClient();
+	this.apiClient.setBasePath(endpoint);
+	HttpBasicAuth basicAuth = (HttpBasicAuth)this.apiClient.getAuthentication(BASIC_AUTH);
+	basicAuth.setUsername(accessKey);
+	basicAuth.setPassword(secretKey);
     }
 
 
@@ -126,75 +126,75 @@ public class LakeFSFileSystem extends FileSystem {
      */
     @Override
     public FSDataOutputStream create(Path path, FsPermission fsPermission, boolean b, int i, short i1, long l,
-                                     Progressable progressable) throws IOException {
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ create path: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", path.toString());
-        return new FSDataOutputStream(System.out, null);
+				     Progressable progressable) throws IOException {
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ create path: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", path.toString());
+	return new FSDataOutputStream(System.out, null);
     }
 
     @Override
     public FSDataOutputStream append(Path path, int i, Progressable progressable) throws IOException {
-        throw new UnsupportedOperationException("Append is not supported by LakeFSFileSystem");
+	throw new UnsupportedOperationException("Append is not supported by LakeFSFileSystem");
     }
 
     @Override
     public boolean rename(Path path, Path path1) throws IOException {
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ rename $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
-        return false;
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ rename $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
+	return false;
     }
 
     @Override
     public boolean delete(Path path, boolean b) throws IOException {
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ delete $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
-        return false;
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ delete $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
+	return false;
     }
 
     @Override
     public FileStatus[] listStatus(Path path) throws FileNotFoundException, IOException {
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ List status is called for: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$", path.toString());
-        return new FileStatus[0];
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ List status is called for: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$", path.toString());
+	return new FileStatus[0];
     }
 
     @Override
     public void setWorkingDirectory(Path path) {
-        this.workingDirectory = path;
+	this.workingDirectory = path;
     }
 
     @Override
     public Path getWorkingDirectory() {
-        return this.workingDirectory;
+	return this.workingDirectory;
     }
 
     @Override
     public boolean mkdirs(Path path, FsPermission fsPermission) throws IOException {
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ mkdirs, path: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", path.toString());
-        return false;
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ mkdirs, path: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", path.toString());
+	return false;
     }
 
     @Override
     public FileStatus getFileStatus(Path path) throws IOException {
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ getFileStatus, path: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", path.toString());
-        ObjectLocation loc = pathToObjectLocation(path);
-        if (loc == null) {
-            throw new FileNotFoundException(path.toString());
-        }
-        try {
-            ObjectsApi objectsApi = new ObjectsApi(this.apiClient);
-            ObjectStats objectStat = objectsApi.statObject(loc.getRepository(), loc.getRef(), loc.getPath());
-            long length = 0;
-            Long sizeBytes = objectStat.getSizeBytes();
-            if (sizeBytes != null) {
-                length = sizeBytes;
-            }
-            long modificationTime = 0;
-            Long mtime = objectStat.getMtime();
-            if (mtime != null) {
-                modificationTime = TimeUnit.SECONDS.toMillis(mtime);
-            }
-            Path filePath = path.makeQualified(this.uri, this.workingDirectory);
-            return new FileStatus(length, false, 0, 0, modificationTime, filePath);
-        } catch (ApiException e) {
-            throw new IOException("statObject", e);
-        }
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ getFileStatus, path: {} $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", path.toString());
+	ObjectLocation loc = pathToObjectLocation(path);
+	if (loc == null) {
+	    throw new FileNotFoundException(path.toString());
+	}
+	try {
+	    ObjectsApi objectsApi = new ObjectsApi(this.apiClient);
+	    ObjectStats objectStat = objectsApi.statObject(loc.getRepository(), loc.getRef(), loc.getPath());
+	    long length = 0;
+	    Long sizeBytes = objectStat.getSizeBytes();
+	    if (sizeBytes != null) {
+		length = sizeBytes;
+	    }
+	    long modificationTime = 0;
+	    Long mtime = objectStat.getMtime();
+	    if (mtime != null) {
+		modificationTime = TimeUnit.SECONDS.toMillis(mtime);
+	    }
+	    Path filePath = path.makeQualified(this.uri, this.workingDirectory);
+	    return new FileStatus(length, false, 0, 0, modificationTime, filePath);
+	} catch (ApiException e) {
+	    throw new IOException("statObject", e);
+	}
     }
 
     /**
@@ -204,17 +204,17 @@ public class LakeFSFileSystem extends FileSystem {
      */
     @Override
     public String getScheme() {
-        return SCHEME;
+	return SCHEME;
     }
 
     @Override
     public FileStatus[] globStatus(Path pathPattern) throws IOException {
-        LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ globStatus $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
-        FileStatus fStatus = new FileStatus(0, false, 1, 20, 1,
-                new Path("tal-test"));
-        FileStatus[] res = new FileStatus[1];
-        res[0] = fStatus;
-        return res;
+	LOG.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$ globStatus $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
+	FileStatus fStatus = new FileStatus(0, false, 1, 20, 1,
+		new Path("tal-test"));
+	FileStatus[] res = new FileStatus[1];
+	res[0] = fStatus;
+	return res;
     }
 
     /**
@@ -223,59 +223,64 @@ public class LakeFSFileSystem extends FileSystem {
      * @return lakeFS Location with repository, ref and path
      */
     private ObjectLocation pathToObjectLocation(Path path) {
-        if (!path.isAbsolute()) {
-            path = new Path(this.workingDirectory, path);
-        }
+	if (!path.isAbsolute()) {
+	    path = new Path(this.workingDirectory, path);
+	}
 
-        URI uri = path.toUri();
-        if (uri.getScheme() != null && uri.getPath().isEmpty()) {
-            return null;
-        }
+	URI uri = path.toUri();
+	if (uri.getScheme() != null && uri.getPath().isEmpty()) {
+	    return null;
+	}
 
-        ObjectLocation loc = new ObjectLocation();
-        loc.setRepository(uri.getHost());
-        // extract ref and rest of the path after removing the '/' prefix
-        String s = uri.getPath();
-        if (s.startsWith(SEPARATOR)) {
-            s = s.substring(1);
-        }
-        int i = s.indexOf(SEPARATOR);
-        if (i == -1) {
-            loc.setRef(s);
-        } else {
-            loc.setRef(s.substring(0, i));
-            loc.setPath(s.substring(i+1));
-        }
-        return loc;
+	ObjectLocation loc = new ObjectLocation();
+	loc.setRepository(uri.getHost());
+	// extract ref and rest of the path after removing the '/' prefix
+	String s = uri.getPath();
+	if (s.startsWith(SEPARATOR)) {
+	    s = s.substring(1);
+	}
+	int i = s.indexOf(SEPARATOR);
+	if (i == -1) {
+	    loc.setRef(s);
+	} else {
+	    loc.setRef(s.substring(0, i));
+	    loc.setPath(s.substring(i+1));
+	}
+	return loc;
     }
 
     private static class ObjectLocation {
-        private String repository;
-        private String ref;
-        private String path;
-        
-        public String getRepository() {
-            return repository;
-        }
+	private String repository;
+	private String ref;
+	private String path;
 
-        public void setRepository(String repository) {
-            this.repository = repository;
-        }
+	public String getRepository() {
+	    return repository;
+	}
 
-        public String getRef() {
-            return ref;
-        }
+	public void setRepository(String repository) {
+	    this.repository = repository;
+	}
 
-        public void setRef(String ref) {
-            this.ref = ref;
-        }
+	public String getRef() {
+	    return ref;
+	}
 
-        public String getPath() {
-            return path;
-        }
+	public void setRef(String ref) {
+	    this.ref = ref;
+	}
 
-        public void setPath(String path) {
-            this.path = path;
-        }
+	public String getPath() {
+	    return path;
+	}
+
+	public void setPath(String path) {
+	    this.path = path;
+	}
+    }
+
+    @Override
+    public boolean exists(Path f) throws IOException {
+	return false;
     }
 }
