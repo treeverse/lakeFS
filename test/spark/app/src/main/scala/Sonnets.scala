6d69d462df9acd1ad8a3fb8e1bd151e46e3a0941
arielshaqed <ariels@treeverse.io>
2021-05-13 12:25:45 +0300

    Test lakeFS with direct-access "thick" Spark client (#1924)
    * Test lakeFS with direct-access "thick" Spark client
    
    Fixes #1921.
    
    * Split out test setup and run it once for both tests
    
    Otherwise cannot setup Spark twice.
    
    * Mount compiled client.jar inside Spark test container
    
    * Pass AWS S3 credentials into spark-submit
    
    Using spark-submit options doesn't appear to work, use AWS environment variables.  (This is
    related to S3A *not* lakeFSFS: it is a configuration issue in the integration test, not the
    client.
    
    * Skip thick client integration test failures
    
    The thick client does not work yet.  Allow us to develop it without breaking trunk.
    
    * [CR] remove outdated comment
    
    * Post-rebase update exceptions and calls, pass blockSize again

--- test/spark/app/src/main/scala/Sonnets.scala
+++ test/spark/app/src/main/scala/Sonnets.scala
@@ -6,16 +6,16 @@ object Sonnets {
       .builder()
       .appName("SonnetsApp")
       .getOrCreate()
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.docker.lakefs.io:8000")
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.connection.ssl.enabled", "false")
-    var sc = spark.sparkContext
+    val input = args.applyOrElse(0, (_: Any) => "s3a://example/main/sonnets.txt")
+    val output = args.applyOrElse(1, (_: Any) => "s3a://example/main/sonnets-wordcount.csv")
+    val sc = spark.sparkContext
     sc.setLogLevel("INFO")
     import spark.implicits._
-    val sonnets = sc.textFile("s3a://example/main/sonnets.txt")
+    val sonnets = sc.textFile(input)
     val counts = sonnets.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).toDF
-    counts.write.format("csv").save("s3a://example/main/sonnets-wordcount.csv")
+    counts.write.format("csv").save(output)
 
-    val wc = spark.read.format("csv").load("s3a://example/main/sonnets-wordcount.csv")
+    val wc = spark.read.format("csv").load(output)
     val life = wc.filter(x => x(1) == "42").map(x => x(0).toString).collect.sorted.mkString(",")
     if (life != "can,or") {
       println(s"Words found 42 times doesn't match '$life'")
