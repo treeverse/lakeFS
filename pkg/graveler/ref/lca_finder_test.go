89495b45a6c0645912dc0f18b887cca96cb42e3c
johnnyaug <yoni.augarten@treeverse.io>
2021-05-19 19:16:28 +0300

    better testing

--- pkg/graveler/ref/lca_finder_test.go
+++ pkg/graveler/ref/lca_finder_test.go
@@ -7,12 +7,10 @@ import (
 
 	"github.com/treeverse/lakefs/pkg/graveler"
 	"github.com/treeverse/lakefs/pkg/graveler/ref"
-	"github.com/treeverse/lakefs/pkg/ident"
 	"github.com/treeverse/lakefs/pkg/testutil"
 )
 
 type MockCommitGetter struct {
-	byHumanID  map[string]*graveler.Commit
 	byCommitID map[graveler.CommitID]*graveler.Commit
 	visited    map[graveler.CommitID]interface{}
 }
@@ -43,34 +41,22 @@ func computeGeneration(byCommitID map[graveler.CommitID]*graveler.Commit, commit
 	return commit.Generation
 }
 
-func newReader(kv map[string]*graveler.Commit) *MockCommitGetter {
-	byCommitID := make(map[graveler.CommitID]*graveler.Commit)
-	for _, commit := range kv {
-		byCommitID[caddr(commit)] = commit
-	}
+func newReader(kv map[graveler.CommitID]*graveler.Commit) *MockCommitGetter {
 	for _, v := range kv {
-		v.Generation = computeGeneration(byCommitID, v)
+		v.Generation = computeGeneration(kv, v)
 	}
 	return &MockCommitGetter{
-		byHumanID:  kv,
-		byCommitID: byCommitID,
+		byCommitID: kv,
 		visited:    make(map[graveler.CommitID]interface{}),
 	}
 
 }
 
-func caddr(commit *graveler.Commit) graveler.CommitID {
-	if commit == nil {
-		return ""
-	}
-	return graveler.CommitID(ident.NewHexAddressProvider().ContentAddress(commit))
-}
-
 func TestFindLowestCommonAncestor(t *testing.T) {
 	cases := []struct {
 		Name     string
-		Left     string
-		Right    string
+		Left     graveler.CommitID
+		Right    graveler.CommitID
 		Getter   func() *MockCommitGetter
 		Expected []string
 	}{
@@ -79,15 +65,15 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Left:  "c7",
 			Right: "c6",
 			Getter: func() *MockCommitGetter {
-				c0 := &graveler.Commit{Message: "0", Parents: []graveler.CommitID{}}
-				c1 := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{caddr(c0)}}
-				c2 := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(c0)}}
-				c3 := &graveler.Commit{Message: "3", Parents: []graveler.CommitID{caddr(c1)}}
-				c4 := &graveler.Commit{Message: "4", Parents: []graveler.CommitID{caddr(c2)}}
-				c5 := &graveler.Commit{Message: "5", Parents: []graveler.CommitID{caddr(c3)}}
-				c6 := &graveler.Commit{Message: "6", Parents: []graveler.CommitID{caddr(c4)}}
-				c7 := &graveler.Commit{Message: "7", Parents: []graveler.CommitID{caddr(c5)}}
-				return newReader(map[string]*graveler.Commit{
+				c0 := &graveler.Commit{Message: "c0", Parents: []graveler.CommitID{}}
+				c1 := &graveler.Commit{Message: "c1", Parents: []graveler.CommitID{"c0"}}
+				c2 := &graveler.Commit{Message: "c2", Parents: []graveler.CommitID{"c0"}}
+				c3 := &graveler.Commit{Message: "c3", Parents: []graveler.CommitID{"c1"}}
+				c4 := &graveler.Commit{Message: "c4", Parents: []graveler.CommitID{"c2"}}
+				c5 := &graveler.Commit{Message: "c5", Parents: []graveler.CommitID{"c3"}}
+				c6 := &graveler.Commit{Message: "c6", Parents: []graveler.CommitID{"c4"}}
+				c7 := &graveler.Commit{Message: "c7", Parents: []graveler.CommitID{"c5"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"c0": c0, "c1": c1, "c2": c2, "c3": c3, "c4": c4, "c5": c5, "c6": c6, "c7": c7,
 				})
 			},
@@ -98,12 +84,12 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Left:  "c3",
 			Right: "c4",
 			Getter: func() *MockCommitGetter {
-				c0 := &graveler.Commit{Message: "0", Parents: []graveler.CommitID{}}
-				c1 := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{caddr(c0)}}
-				c2 := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(c1)}}
-				c3 := &graveler.Commit{Message: "3", Parents: []graveler.CommitID{caddr(c2)}}
-				c4 := &graveler.Commit{Message: "4", Parents: []graveler.CommitID{caddr(c2)}}
-				return newReader(map[string]*graveler.Commit{
+				c0 := &graveler.Commit{Message: "c0", Parents: []graveler.CommitID{}}
+				c1 := &graveler.Commit{Message: "c1", Parents: []graveler.CommitID{"c0"}}
+				c2 := &graveler.Commit{Message: "c2", Parents: []graveler.CommitID{"c1"}}
+				c3 := &graveler.Commit{Message: "c3", Parents: []graveler.CommitID{"c2"}}
+				c4 := &graveler.Commit{Message: "c4", Parents: []graveler.CommitID{"c2"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"c0": c0, "c1": c1, "c2": c2, "c3": c3, "c4": c4,
 				})
 			},
@@ -114,14 +100,14 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Left:  "c5",
 			Right: "c6",
 			Getter: func() *MockCommitGetter {
-				c0 := &graveler.Commit{Message: "0", Parents: []graveler.CommitID{}}
-				c1 := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{caddr(c0)}}
-				c2 := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(c0)}}
-				c3 := &graveler.Commit{Message: "3", Parents: []graveler.CommitID{caddr(c1), caddr(c2)}}
-				c4 := &graveler.Commit{Message: "4", Parents: []graveler.CommitID{caddr(c1), caddr(c2)}}
-				c5 := &graveler.Commit{Message: "5", Parents: []graveler.CommitID{caddr(c3)}}
-				c6 := &graveler.Commit{Message: "6", Parents: []graveler.CommitID{caddr(c4)}}
-				return newReader(map[string]*graveler.Commit{
+				c0 := &graveler.Commit{Message: "c0", Parents: []graveler.CommitID{}}
+				c1 := &graveler.Commit{Message: "c1", Parents: []graveler.CommitID{"c0"}}
+				c2 := &graveler.Commit{Message: "c2", Parents: []graveler.CommitID{"c0"}}
+				c3 := &graveler.Commit{Message: "c3", Parents: []graveler.CommitID{"c1", "c2"}}
+				c4 := &graveler.Commit{Message: "c4", Parents: []graveler.CommitID{"c1", "c2"}}
+				c5 := &graveler.Commit{Message: "c5", Parents: []graveler.CommitID{"c3"}}
+				c6 := &graveler.Commit{Message: "c6", Parents: []graveler.CommitID{"c4"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"c0": c0, "c1": c1, "c2": c2, "c3": c3, "c4": c4, "c5": c5, "c6": c6,
 				})
 			},
@@ -132,10 +118,10 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Left:  "c2",
 			Right: "c1",
 			Getter: func() *MockCommitGetter {
-				c0 := &graveler.Commit{Message: "0", Parents: []graveler.CommitID{}}
-				c1 := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{caddr(c0)}}
-				c2 := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(c1)}}
-				return newReader(map[string]*graveler.Commit{
+				c0 := &graveler.Commit{Message: "c0", Parents: []graveler.CommitID{}}
+				c1 := &graveler.Commit{Message: "c1", Parents: []graveler.CommitID{"c0"}}
+				c2 := &graveler.Commit{Message: "c2", Parents: []graveler.CommitID{"c1"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"c0": c0, "c1": c1, "c2": c2,
 				})
 			},
@@ -146,15 +132,15 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Left:  "c7",
 			Right: "c3",
 			Getter: func() *MockCommitGetter {
-				c0 := &graveler.Commit{Message: "0", Parents: []graveler.CommitID{}}
-				c1 := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{caddr(c0)}}
-				c2 := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(c1)}}
-				c3 := &graveler.Commit{Message: "3", Parents: []graveler.CommitID{caddr(c2)}}
-				c4 := &graveler.Commit{Message: "4", Parents: []graveler.CommitID{}}
-				c5 := &graveler.Commit{Message: "5", Parents: []graveler.CommitID{caddr(c4)}}
-				c6 := &graveler.Commit{Message: "6", Parents: []graveler.CommitID{caddr(c5)}}
-				c7 := &graveler.Commit{Message: "7", Parents: []graveler.CommitID{caddr(c6)}}
-				return newReader(map[string]*graveler.Commit{
+				c0 := &graveler.Commit{Message: "c0", Parents: []graveler.CommitID{}}
+				c1 := &graveler.Commit{Message: "c1", Parents: []graveler.CommitID{"c0"}}
+				c2 := &graveler.Commit{Message: "c2", Parents: []graveler.CommitID{"c1"}}
+				c3 := &graveler.Commit{Message: "c3", Parents: []graveler.CommitID{"c2"}}
+				c4 := &graveler.Commit{Message: "c4", Parents: []graveler.CommitID{}}
+				c5 := &graveler.Commit{Message: "c5", Parents: []graveler.CommitID{"c4"}}
+				c6 := &graveler.Commit{Message: "c6", Parents: []graveler.CommitID{"c5"}}
+				c7 := &graveler.Commit{Message: "c7", Parents: []graveler.CommitID{"c6"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"c0": c0, "c1": c1, "c2": c2, "c3": c3, "c4": c4, "c5": c5, "c6": c6, "c7": c7,
 				})
 			},
@@ -165,12 +151,12 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Left:  "c3",
 			Right: "c4",
 			Getter: func() *MockCommitGetter {
-				c0 := &graveler.Commit{Message: "0", Parents: []graveler.CommitID{}}
-				c2 := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(c0)}}
-				c1 := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{caddr(c0), caddr(c2)}}
-				c3 := &graveler.Commit{Message: "3", Parents: []graveler.CommitID{caddr(c1)}}
-				c4 := &graveler.Commit{Message: "5", Parents: []graveler.CommitID{caddr(c2)}}
-				return newReader(map[string]*graveler.Commit{
+				c0 := &graveler.Commit{Message: "c0", Parents: []graveler.CommitID{}}
+				c2 := &graveler.Commit{Message: "c2", Parents: []graveler.CommitID{"c0"}}
+				c1 := &graveler.Commit{Message: "c1", Parents: []graveler.CommitID{"c0", "c2"}}
+				c3 := &graveler.Commit{Message: "c3", Parents: []graveler.CommitID{"c1"}}
+				c4 := &graveler.Commit{Message: "c5", Parents: []graveler.CommitID{"c2"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"c0": c0, "c1": c1, "c2": c2, "c3": c3, "c4": c4,
 				})
 			},
@@ -181,13 +167,13 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Left:  "x",
 			Right: "y",
 			Getter: func() *MockCommitGetter {
-				c1 := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{}}
-				c2 := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(c1)}}
-				c3 := &graveler.Commit{Message: "3", Parents: []graveler.CommitID{caddr(c2)}}
-				c4 := &graveler.Commit{Message: "4", Parents: []graveler.CommitID{caddr(c3)}}
-				x := &graveler.Commit{Message: "x", Parents: []graveler.CommitID{caddr(c4), caddr(c1)}}
-				y := &graveler.Commit{Message: "y", Parents: []graveler.CommitID{caddr(c2)}}
-				return newReader(map[string]*graveler.Commit{
+				c1 := &graveler.Commit{Message: "c1", Parents: []graveler.CommitID{}}
+				c2 := &graveler.Commit{Message: "c2", Parents: []graveler.CommitID{"c1"}}
+				c3 := &graveler.Commit{Message: "c3", Parents: []graveler.CommitID{"c2"}}
+				c4 := &graveler.Commit{Message: "c4", Parents: []graveler.CommitID{"c3"}}
+				x := &graveler.Commit{Message: "x", Parents: []graveler.CommitID{"c4", "c1"}}
+				y := &graveler.Commit{Message: "y", Parents: []graveler.CommitID{"c2"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"c1": c1, "c2": c2, "c3": c3, "c4": c4, "x": x, "y": y,
 				})
 			},
@@ -204,15 +190,15 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 			Right: "h",
 
 			Getter: func() *MockCommitGetter {
-				e := &graveler.Commit{Message: "1", Parents: []graveler.CommitID{}}
-				d := &graveler.Commit{Message: "2", Parents: []graveler.CommitID{caddr(e)}}
-				f := &graveler.Commit{Message: "3", Parents: []graveler.CommitID{caddr(e)}}
-				c := &graveler.Commit{Message: "4", Parents: []graveler.CommitID{caddr(d)}}
-				b := &graveler.Commit{Message: "5", Parents: []graveler.CommitID{caddr(c)}}
-				a := &graveler.Commit{Message: "6", Parents: []graveler.CommitID{caddr(b)}}
-				g := &graveler.Commit{Message: "7", Parents: []graveler.CommitID{caddr(b), caddr(e)}}
-				h := &graveler.Commit{Message: "8", Parents: []graveler.CommitID{caddr(a), caddr(f)}}
-				return newReader(map[string]*graveler.Commit{
+				e := &graveler.Commit{Message: "e", Parents: []graveler.CommitID{}}
+				d := &graveler.Commit{Message: "d", Parents: []graveler.CommitID{"e"}}
+				f := &graveler.Commit{Message: "f", Parents: []graveler.CommitID{"e"}}
+				c := &graveler.Commit{Message: "c", Parents: []graveler.CommitID{"d"}}
+				b := &graveler.Commit{Message: "b", Parents: []graveler.CommitID{"c"}}
+				a := &graveler.Commit{Message: "a", Parents: []graveler.CommitID{"b"}}
+				g := &graveler.Commit{Message: "g", Parents: []graveler.CommitID{"b", "e"}}
+				h := &graveler.Commit{Message: "h0", Parents: []graveler.CommitID{"a", "f"}}
+				return newReader(map[graveler.CommitID]*graveler.Commit{
 					"e": e, "d": d, "f": f, "c": c, "b": b, "a": a, "g": g, "h": h,
 				})
 			},
@@ -222,20 +208,19 @@ func TestFindLowestCommonAncestor(t *testing.T) {
 	for _, cas := range cases {
 		t.Run(cas.Name, func(t *testing.T) {
 			getter := cas.Getter()
-			base, err := ref.FindLowestCommonAncestor(
-				context.Background(), getter, "", caddr(getter.byHumanID[cas.Left]), caddr(getter.byHumanID[cas.Right]))
+			base, err := ref.FindLowestCommonAncestor(context.Background(), getter, "", cas.Left, cas.Right)
 			if err != nil {
 				t.Fatalf("unexpected error %v", err)
 			}
-			verifyResult(t, base, getter, cas.Expected)
+			verifyResult(t, base, cas.Expected)
 
 			// flip right and left and expect the same result
 			base, err = ref.FindLowestCommonAncestor(
-				context.Background(), getter, "", caddr(getter.byHumanID[cas.Right]), caddr(getter.byHumanID[cas.Left]))
+				context.Background(), getter, "", cas.Right, cas.Left)
 			if err != nil {
 				t.Fatalf("unexpected error %v", err)
 			}
-			verifyResult(t, base, getter, cas.Expected)
+			verifyResult(t, base, cas.Expected)
 
 		})
 	}
@@ -257,54 +242,50 @@ func TestGrid(t *testing.T) {
 	//              \    /
 	//              (1,1)
 	grid := make([][]*graveler.Commit, 10)
-	kv := make(map[string]*graveler.Commit)
+	kv := make(map[graveler.CommitID]*graveler.Commit)
 	for i := 0; i < 10; i++ {
 		grid[i] = make([]*graveler.Commit, 10)
 		for j := 0; j < 10; j++ {
 			parents := make([]graveler.CommitID, 0, 2)
 			if i > 0 {
-				parents = append(parents, caddr(grid[i-1][j]))
+				parents = append(parents, graveler.CommitID(fmt.Sprintf("%d-%d", i-1, j)))
 			}
 			if j > 0 {
-				parents = append(parents, caddr(grid[i][j-1]))
+				parents = append(parents, graveler.CommitID(fmt.Sprintf("%d-%d", i, j-1)))
 			}
 			grid[i][j] = &graveler.Commit{Message: fmt.Sprintf("%d-%d", i, j), Parents: parents}
-			kv[fmt.Sprintf("%d-%d", i, j)] = grid[i][j]
+			kv[graveler.CommitID(fmt.Sprintf("%d-%d", i, j))] = grid[i][j]
 		}
 	}
 	getter := newReader(kv)
-	c, err := ref.FindLowestCommonAncestor(context.Background(), getter, "", caddr(grid[7][4]), caddr(grid[5][6]))
+	c, err := ref.FindLowestCommonAncestor(context.Background(), getter, "", "7-4", "5-6")
 	testutil.Must(t, err)
-	verifyResult(t, c, getter, []string{"5-4"})
+	verifyResult(t, c, []string{"5-4"})
 
-	c, err = ref.FindLowestCommonAncestor(context.Background(), getter, "", caddr(grid[1][2]), caddr(grid[2][1]))
+	c, err = ref.FindLowestCommonAncestor(context.Background(), getter, "", "1-2", "2-1")
 	testutil.Must(t, err)
-	verifyResult(t, c, getter, []string{"1-1"})
+	verifyResult(t, c, []string{"1-1"})
 
-	c, err = ref.FindLowestCommonAncestor(context.Background(), getter, "", caddr(grid[0][9]), caddr(grid[9][0]))
+	c, err = ref.FindLowestCommonAncestor(context.Background(), getter, "", "0-9", "9-0")
 	testutil.Must(t, err)
-	verifyResult(t, c, getter, []string{"0-0"})
+	verifyResult(t, c, []string{"0-0"})
 
-	c, err = ref.FindLowestCommonAncestor(context.Background(), getter, "", caddr(grid[6][9]), caddr(grid[9][6]))
+	c, err = ref.FindLowestCommonAncestor(context.Background(), getter, "", "6-9", "9-6")
 	testutil.Must(t, err)
-	verifyResult(t, c, getter, []string{"6-65"})
+	verifyResult(t, c, []string{"6-6"})
 }
 
-func verifyResult(t *testing.T, base *graveler.Commit, getter *MockCommitGetter, expected []string) {
-	var addr graveler.CommitID
+func verifyResult(t *testing.T, base *graveler.Commit, expected []string) {
 	if base == nil {
 		if len(expected) != 0 {
 			t.Fatalf("got nil result, expected %s", expected)
 		}
 		return
 	}
-	addr = caddr(base)
-	expectedCommitIDs := make([]graveler.CommitID, 0, len(expected))
 	for _, expectedKey := range expected {
-		expectedCommitIDs = append(expectedCommitIDs, caddr(getter.byHumanID[expectedKey]))
-		if caddr(getter.byHumanID[expectedKey]) == addr {
+		if base.Message == expectedKey {
 			return
 		}
 	}
-	t.Fatalf("expected one of (%v) got (%v)", expectedCommitIDs, addr)
+	t.Fatalf("expected one of (%v) got (%v)", expected, base.Message)
 }
