65b08a04353108ebd72e9f286b275e60a864f51f
Barak Amar <barak.amar@treeverse.io>
2021-05-24 10:14:40 +0300

    [filesystem] directory support for delete status and exists operations (#1970)

--- clients/hadoopfs/src/main/java/io/lakefs/Constants.java
+++ clients/hadoopfs/src/main/java/io/lakefs/Constants.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 
 class Constants {
     public static final String URI_SCHEME = "lakefs";
-    public static final String URI_SEPARATOR = "/";
     public static final String FS_LAKEFS_ENDPOINT_KEY = "fs.lakefs.endpoint";
     public static final String FS_LAKEFS_ACCESS_KEY = "fs.lakefs.access.key";
     public static final String FS_LAKEFS_SECRET_KEY = "fs.lakefs.secret.key";
--- clients/hadoopfs/src/main/java/io/lakefs/ObjectLocation.java
+++ clients/hadoopfs/src/main/java/io/lakefs/ObjectLocation.java
@@ -5,6 +5,10 @@ class ObjectLocation {
     private String ref;
     private String path;
 
+    public static String formatPath(String repository, String ref, String path) {
+        return String.format("%s://%s/%s/%s", Constants.URI_SCHEME,repository, ref, path);
+    }
+
     public String getRepository() {
         return repository;
     }
@@ -30,7 +34,7 @@ class ObjectLocation {
     }
 
     static String trimLeadingSlash(String s) {
-        if (s.startsWith(Constants.URI_SEPARATOR)) {
+        if (s.startsWith(Constants.SEPARATOR)) {
             return s.substring(1);
         }
         return s;
@@ -58,5 +62,10 @@ class ObjectLocation {
      */
     public boolean onSameBranch(ObjectLocation otherObjLoc) {
         return this.repository.equals(otherObjLoc.getRepository()) && this.ref.equals(otherObjLoc.getRef());
+    }
+
+    @Override
+    public String toString() {
+        return formatPath(repository, ref, path);
     }
 }
