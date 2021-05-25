edf41939b164bcb30c529f2663ea76ef270b4cae
Barak Amar <barak.amar@treeverse.io>
2021-05-25 17:46:43 +0300

    spark test app write multiple formats

--- test/spark/app/src/main/scala/Sonnets.scala
+++ test/spark/app/src/main/scala/Sonnets.scala
@@ -7,15 +7,21 @@ object Sonnets {
       .appName("SonnetsApp")
       .getOrCreate()
     val input = args.applyOrElse(0, (_: Any) => "s3a://example/main/sonnets.txt")
-    val output = args.applyOrElse(1, (_: Any) => "s3a://example/main/sonnets-wordcount.csv")
+    val output = args.applyOrElse(1, (_: Any) => "s3a://example/main/sonnets-wordcount")
     val sc = spark.sparkContext
     sc.setLogLevel("INFO")
     import spark.implicits._
     val sonnets = sc.textFile(input)
     val counts = sonnets.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).toDF
-    counts.write.format("csv").save(output)
 
-    val wc = spark.read.format("csv").load(output)
+    for (fmt <- Seq("csv", "parquet", "json", "orc")) {
+      val fname = "%s.%s".format(output, fmt)
+      println("Write counts to", fname)
+      counts.write.format(fmt).save(fname)
+    }
+
+    println("Load counts from csv")
+    val wc = spark.read.format("csv").load(output + ".csv")
     val life = wc.filter(x => x(1) == "42").map(x => x(0).toString).collect.sorted.mkString(",")
     if (life != "can,or") {
       println(s"Words found 42 times doesn't match '$life'")
--- test/spark/run-test.sh
+++ test/spark/run-test.sh
@@ -12,7 +12,7 @@
 STORAGE_NAMESPACE=${STORAGE_NAMESPACE:-local://}
 
 export input="lakefs://${REPO}/main/sonnets.txt"
-export output="lakefs://${REPO}/main/sonnets-wordcount.csv"
+export output="lakefs://${REPO}/main/sonnets-wordcount"
 
 docker-compose exec -T lakefs lakectl repo create "lakefs://${REPO}" ${STORAGE_NAMESPACE} -d main
 
@@ -22,6 +22,6 @@ if [ -v USE_DIRECT_ACCESS ]; then # Direct access using thick Spark client.
     docker-compose run -v $PWD/../../clients/hadoopfs/target/:/target/ -T --no-deps --rm spark-submit sh -c 'spark-submit --master spark://spark:7077 --jars /target/client.jar -c "spark.hadoop.fs.lakefs.impl=io.lakefs.LakeFSFileSystem" --conf=spark.driver.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true --conf=spark.executor.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true  -c spark.hadoop.fs.lakefs.endpoint=http://lakefs:8000/api/v1 -c "spark.hadoop.fs.lakefs.access.key=${TESTER_ACCESS_KEY_ID}" -c "spark.hadoop.fs.lakefs.secret.key=${TESTER_SECRET_ACCESS_KEY}" -c "spark.hadoop.fs.s3a.access.key=${AWS_ACCESS_KEY_ID}" -c "spark.hadoop.fs.s3a.secret.key=${AWS_SECRET_ACCESS_KEY}" -c "spark.hadoop.fs.s3a.region=${AWS_REGION}" --class Sonnets /local/app/target/scala-2.12/sonnets_2.12-0.1.jar ${input} ${output}'
 else				# Access via S3 gateway using regular Spark client.
     export s3input="s3a://${REPO}/main/sonnets.txt"
-    export s3output="s3a://${REPO}/main/sonnets-wordcount.csv"
+    export s3output="s3a://${REPO}/main/sonnets-wordcount"
     docker-compose run -T --no-deps --rm spark-submit sh -c 'spark-submit --master spark://spark:7077 -c "spark.hadoop.fs.s3a.access.key=${TESTER_ACCESS_KEY_ID}" -c "spark.hadoop.fs.s3a.secret.key=${TESTER_SECRET_ACCESS_KEY}" -c spark.hadoop.fs.s3a.endpoint=s3.docker.lakefs.io:8000 -c spark.hadoop.fs.s3a.connection.ssl.enabled=false --class Sonnets /local/app/target/scala-2.12/sonnets_2.12-0.1.jar ${s3input} ${s3output}'
 fi
