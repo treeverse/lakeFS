c0e647d5f00be6e1e1d56c38d14ba3bb9051df52
Tal Sofer <tal.sofer@treeverse.io>
2021-06-03 16:09:46 +0300

    throw FNFE when src does not exist

--- clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
@@ -284,13 +284,9 @@ public class LakeFSFileSystem extends FileSystem {
         }
 
         LakeFSFileStatus srcStatus;
-        try {
-            srcStatus = getFileStatus(src);
-        } catch (FileNotFoundException e) {
-            //TODO: throw FNFE, as s3a does in https://github.com/apache/hadoop/blob/2960d83c255a00a549f8809882cd3b73a6266b6d/hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java#L1505
-            LOG.error("rename: src {} does not exist, rename failed.", src, e);
-            return false;
-        }
+        // Throws FileNotFoundException when src does not exist. mimics s3a's behaviour in
+        // https://github.com/apache/hadoop/blob/2960d83c255a00a549f8809882cd3b73a6266b6d/hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java#L1505
+        srcStatus = getFileStatus(src);
         if (!srcStatus.isDirectory()) {
             return renameFile(srcStatus, dst);
         }
