477f01c4282f8b8e26a474e9bb0fad6965ed55e1
Barak Amar <barak.amar@treeverse.io>
2021-07-22 17:20:37 +0300

    Hadoop filesystem fake empty directory object support (#2238)

--- clients/hadoopfs/src/main/java/io/lakefs/LinkOnCloseOutputStream.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LinkOnCloseOutputStream.java
@@ -1,9 +1,7 @@
 package io.lakefs;
 
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import io.lakefs.clients.api.StagingApi;
 import io.lakefs.clients.api.model.StagingLocation;
-import io.lakefs.clients.api.model.StagingMetadata;
+import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,23 +11,24 @@ import java.net.URI;
  * Wraps a FSDataOutputStream to link file on staging when done writing
  */
 class LinkOnCloseOutputStream extends OutputStream {
-    private StagingApi staging;
-    private StagingLocation stagingLoc;
-    private ObjectLocation objectLoc;
-    private URI physicalUri;
+    private final LakeFSFileSystem lfs;
+    private final StagingLocation stagingLoc;
+    private final ObjectLocation objectLoc;
+    private final URI physicalUri;
     private final MetadataClient metadataClient;
-    private OutputStream out;
+    private final OutputStream out;
 
     /**
-     * @param staging client used to access metadata on lakeFS.
+     * @param lfs LakeFS file system
      * @param stagingLoc physical location of object data on S3.
      * @param objectLoc location of object on lakeFS.
      * @param physicalUri translated physical location of object data for underlying FileSystem.
      * @param metadataClient client used to request metadata information from the underlying FileSystem.
      * @param out stream on underlying filesystem to wrap.
      */
-    LinkOnCloseOutputStream(StagingApi staging, StagingLocation stagingLoc, ObjectLocation objectLoc, URI physicalUri, MetadataClient metadataClient, OutputStream out) {
-        this.staging = staging;
+    LinkOnCloseOutputStream(LakeFSFileSystem lfs, StagingLocation stagingLoc, ObjectLocation objectLoc,
+                            URI physicalUri, MetadataClient metadataClient, OutputStream out) {
+        this.lfs = lfs;
         this.stagingLoc = stagingLoc;
         this.objectLoc = objectLoc;
         this.physicalUri = physicalUri;
@@ -63,18 +62,11 @@ class LinkOnCloseOutputStream extends OutputStream {
 
         // Now the object is on the underlying store, find its parameters (sadly lost by
         // the underlying Hadoop FileSystem) so we can link it on lakeFS.
-        ObjectMetadata objectMetadata = metadataClient.getObjectMetadata(physicalUri);
-        // TODO(ariels): Can we add metadata here?
-        StagingMetadata metadata = new StagingMetadata()
-                .staging(stagingLoc)
-                .checksum(objectMetadata.getETag())
-                .sizeBytes(objectMetadata.getContentLength());
         try {
-            staging.linkPhysicalAddress(objectLoc.getRepository(), objectLoc.getRef(), objectLoc.getPath(), metadata);
+            lfs.linkPhysicalAddress(objectLoc, stagingLoc, physicalUri, metadataClient);
         } catch (io.lakefs.clients.api.ApiException e) {
             throw new IOException("link lakeFS path to physical address", e);
         }
+        lfs.deleteEmptyDirectoryMarkers(new Path(objectLoc.toString()).getParent());
     }
-
-
 }
