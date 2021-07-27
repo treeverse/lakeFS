ec181e83e9371d0b0c197598efa22ad0f859ef26
johnnyaug <yoni.augarten@treeverse.io>
2021-07-27 18:10:27 +0300

    remove typo

--- test/lakefsfs_contract/run-test.sh
+++ test/lakefsfs_contract/run-test.sh
@@ -4,6 +4,5 @@ REPOSITORY=${REPOSITORY:-example}
 
 docker-compose exec -T lakefs lakectl repo create "lakefs://${REPOSITORY}" ${STORAGE_NAMESPACE} -d main
 
-docker-compose exec -T lakefs lakectl fs upload -s /local/app/data-sets/sonnets.txt "lakefs://${REPOSITORY}/main/sonnets.txt"
-
 mvn test
+l
