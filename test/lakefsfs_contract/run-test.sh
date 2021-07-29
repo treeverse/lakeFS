d4b830bdfca1884509348a14b6d9441c8c765a45
johnnyaug <yoni.augarten@treeverse.io>
2021-07-29 14:40:11 +0300

    fix ecr step

--- test/lakefsfs_contract/run-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-REPOSITORY=${REPOSITORY:-example}
-
-docker-compose exec -T lakefs lakectl repo create "lakefs://${REPOSITORY}" ${STORAGE_NAMESPACE} -d main
-
-cd ../../clients/hadoopfs
-mvn test -Dtest=io.lakefs.contract.*
