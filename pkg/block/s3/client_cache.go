aa65ae734d9a5edbf668d9b04e2b2696b8bd9d41
johnnyaug <yoni.augarten@treeverse.io>
2021-08-09 13:56:43 +0300

    test step1

--- pkg/block/s3/client_cache.go
+++ pkg/block/s3/client_cache.go
@@ -5,7 +5,6 @@ import (
 	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
@@ -14,31 +13,42 @@ import (
 	"github.com/treeverse/lakefs/pkg/logging"
 )
 
-type clientFactory func(awsSession client.ConfigProvider, cfgs ...*aws.Config) s3iface.S3API
+type clientFactory func(awsSession *session.Session, cfgs ...*aws.Config) s3iface.S3API
+type s3RegionGetter func(ctx context.Context, sess *session.Session, bucket string) (string, error)
 
 type ClientCache struct {
 	regionToS3Client sync.Map
 	bucketToRegion   sync.Map
 	awsSession       *session.Session
-	defaultClient    s3iface.S3API
-	clientFactory    func(awsSession client.ConfigProvider, cfgs ...*aws.Config) s3iface.S3API
+
+	clientFactory  clientFactory
+	s3RegionGetter s3RegionGetter
 }
 
-func newS3Client(p client.ConfigProvider, cfgs ...*aws.Config) s3iface.S3API {
-	return s3.New(p, cfgs...)
+func getBucketRegionFromS3(ctx context.Context, sess *session.Session, bucket string) (string, error) {
+	return s3manager.GetBucketRegion(ctx, sess, bucket, "")
+}
+
+func newS3Client(sess *session.Session, cfgs ...*aws.Config) s3iface.S3API {
+	return s3.New(sess, cfgs...)
 }
 
 func NewClientCache(awsSession *session.Session) *ClientCache {
-	return &ClientCache{
-		awsSession:    awsSession,
-		clientFactory: newS3Client,
-		defaultClient: newS3Client(awsSession),
+	cc := &ClientCache{
+		awsSession:     awsSession,
+		clientFactory:  newS3Client,
+		s3RegionGetter: getBucketRegionFromS3,
 	}
+	return cc
 }
 
 func (c *ClientCache) WithClientFactory(clientFactory clientFactory) *ClientCache {
 	c.clientFactory = clientFactory
-	c.defaultClient = clientFactory(c.awsSession)
+	return c
+}
+
+func (c *ClientCache) WithS3RegionGetter(s3RegionGetter s3RegionGetter) *ClientCache {
+	c.s3RegionGetter = s3RegionGetter
 	return c
 }
 
@@ -47,7 +57,7 @@ func (c *ClientCache) getBucketRegion(ctx context.Context, bucket string) string
 		return region.(string)
 	}
 	logging.FromContext(ctx).WithField("bucket", bucket).Debug("requesting region for bucket")
-	region, err := s3manager.GetBucketRegionWithClient(ctx, c.defaultClient, bucket)
+	region, err := c.s3RegionGetter(ctx, c.awsSession, bucket)
 	if err != nil {
 		logging.FromContext(ctx).WithError(err).Error("failed to get region for bucket, falling back to default region")
 		region = *c.awsSession.Config.Region
@@ -56,6 +66,7 @@ func (c *ClientCache) getBucketRegion(ctx context.Context, bucket string) string
 	return region
 }
 
+// Get returns an AWS client configured to the region of the given bucket.
 func (c *ClientCache) Get(ctx context.Context, bucket string) s3iface.S3API {
 	region := c.getBucketRegion(ctx, bucket)
 	if _, hasClient := c.regionToS3Client.Load(region); !hasClient {
--- pkg/block/s3/client_cache_test.go
+++ pkg/block/s3/client_cache_test.go
@@ -1,41 +1,95 @@
 package s3_test
 
-//
-//import (
-//	"context"
-//	"testing"
-//
-//	"github.com/aws/aws-sdk-go/aws/client/metadata"
-//
-//	"github.com/aws/aws-sdk-go/aws/request"
-//	awss3 "github.com/aws/aws-sdk-go/service/s3"
-//
-//	"github.com/aws/aws-sdk-go/aws"
-//	"github.com/aws/aws-sdk-go/aws/client"
-//	"github.com/aws/aws-sdk-go/aws/session"
-//	"github.com/aws/aws-sdk-go/service/s3/s3iface"
-//	s3 "github.com/treeverse/lakefs/pkg/block/s3"
-//	"github.com/treeverse/lakefs/pkg/testutil"
-//)
-//
-//type s3ClientMock struct {
-//	s3iface.S3API
-//	headCalls int
-//}
-//
-//func (s *s3ClientMock) HeadBucketRequest(*awss3.HeadBucketInput) (*request.Request, *awss3.HeadBucketOutput) {
-//	s.headCalls++
-//	return request.New(aws.Config{}, metadata.ClientInfo{}, request.Handlers{Send: request.HandlerList{}, nil, &request.Operation{}, nil, nil), nil
-//}
-//
-//func TestCache(t *testing.T) {
-//	sess, err := session.NewSession() // this session will be discarded
-//	testutil.Must(t, err)
-//	c := s3.NewClientCache(sess)
-//	s3Mock := &s3ClientMock{}
-//	c = c.WithClientFactory(func(awsSession client.ConfigProvider, cfgs ...*aws.Config) s3iface.S3API {
-//		return s3Mock
-//	})
-//	c.Get(context.Background(), "boo")
-//
-//}
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/go-test/deep"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	s3 "github.com/treeverse/lakefs/pkg/block/s3"
+	"github.com/treeverse/lakefs/pkg/testutil"
+)
+
+var regionError = errors.New("failed to get region")
+
+type s3ClientMock struct {
+	s3iface.S3API
+}
+
+func TestCache(t *testing.T) {
+	defaultRegion := "us-west-2"
+	sess, err := session.NewSession(&aws.Config{Region: &defaultRegion})
+	testutil.Must(t, err)
+	s3Mock := &s3ClientMock{}
+	tests := map[string]struct {
+		bucketToRegion      map[string]string
+		calls               []string
+		regionErrorsIndexes map[int]bool
+	}{
+		"two_buckets_two_regions": {
+			bucketToRegion: map[string]string{"us-bucket": "us-east-1", "eu-bucket": "eu-west-1"},
+			calls:          []string{"us-bucket", "us-bucket", "us-bucket", "eu-bucket", "eu-bucket", "eu-bucket"},
+		},
+		"multiple_buckets_two_regions": {
+			bucketToRegion: map[string]string{"us-bucket-1": "us-east-1", "us-bucket-2": "us-east-1", "us-bucket-3": "us-east-1", "eu-bucket-1": "eu-west-1", "eu-bucket-2": "eu-west-1"},
+			calls:          []string{"us-bucket-1", "us-bucket-2", "us-bucket-3", "eu-bucket-1", "eu-bucket-2"},
+		},
+		"error_on_get_region": {
+			bucketToRegion:      map[string]string{"us-bucket": "us-east-1", "eu-bucket": "eu-west-1"},
+			calls:               []string{"us-bucket", "us-bucket", "us-bucket", "eu-bucket", "eu-bucket", "eu-bucket"},
+			regionErrorsIndexes: map[int]bool{3: true},
+		},
+	}
+	for name, tst := range tests {
+		t.Run(name, func(t *testing.T) {
+			actualClientCacheMissCount := make(map[string]int)
+			actualRegionCacheMissCount := make(map[string]int)
+			expectedClientCacheMissCount := make(map[string]int)
+			expectedRegionCacheMissCount := make(map[string]int)
+
+			c := s3.NewClientCache(sess)
+			var callIdx int
+			var call string
+			c = c.WithClientFactory(func(sess *session.Session, cfgs ...*aws.Config) s3iface.S3API {
+				region := sess.Config.Region
+				for _, cfg := range cfgs {
+					if cfg.Region != nil {
+						region = cfg.Region
+					}
+				}
+				actualClientCacheMissCount[*region]++
+				return s3Mock
+			})
+			c = c.WithS3RegionGetter(func(ctx context.Context, sess *session.Session, bucket string) (string, error) {
+				actualRegionCacheMissCount[bucket]++
+				if tst.regionErrorsIndexes[callIdx] {
+					return "", regionError
+				}
+				return tst.bucketToRegion[bucket], nil
+			})
+			bucketsInCache := make(map[string]bool)
+			for callIdx, call = range tst.calls {
+				expectedRegionCacheMissCount[call] = 1
+				if _, ok := bucketsInCache[call]; !ok {
+					if tst.regionErrorsIndexes[callIdx] {
+						expectedClientCacheMissCount[defaultRegion] = 1
+					} else {
+						expectedClientCacheMissCount[tst.bucketToRegion[call]] = 1
+					}
+				}
+				bucketsInCache[call] = true
+				c.Get(context.Background(), call)
+			}
+			if diff := deep.Equal(expectedClientCacheMissCount, actualClientCacheMissCount); diff != nil {
+				t.Fatal("unexpected client cache miss count. diff: ", diff)
+			}
+			if diff := deep.Equal(expectedRegionCacheMissCount, actualRegionCacheMissCount); diff != nil {
+				t.Fatal("unexpected region cache miss count. diff: ", diff)
+			}
+		})
+	}
+}
