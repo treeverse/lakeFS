db7e25b8b3ab1bff740e31a0d5155d1c4f3a96ed
johnnyaug <yoni.augarten@treeverse.io>
2021-08-09 14:17:05 +0300

    finish test

--- pkg/block/s3/client_cache_test.go
+++ pkg/block/s3/client_cache_test.go
@@ -5,55 +5,58 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/go-test/deep"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
-	s3 "github.com/treeverse/lakefs/pkg/block/s3"
+	"github.com/go-test/deep"
+	"github.com/treeverse/lakefs/pkg/block/s3"
 	"github.com/treeverse/lakefs/pkg/testutil"
 )
 
 var regionError = errors.New("failed to get region")
 
-type s3ClientMock struct {
-	s3iface.S3API
-}
-
-func TestCache(t *testing.T) {
+func TestClientCache(t *testing.T) {
 	defaultRegion := "us-west-2"
 	sess, err := session.NewSession(&aws.Config{Region: &defaultRegion})
 	testutil.Must(t, err)
-	s3Mock := &s3ClientMock{}
 	tests := map[string]struct {
 		bucketToRegion      map[string]string
-		calls               []string
+		bucketCalls         []string
 		regionErrorsIndexes map[int]bool
 	}{
 		"two_buckets_two_regions": {
 			bucketToRegion: map[string]string{"us-bucket": "us-east-1", "eu-bucket": "eu-west-1"},
-			calls:          []string{"us-bucket", "us-bucket", "us-bucket", "eu-bucket", "eu-bucket", "eu-bucket"},
+			bucketCalls:    []string{"us-bucket", "us-bucket", "us-bucket", "eu-bucket", "eu-bucket", "eu-bucket"},
 		},
 		"multiple_buckets_two_regions": {
 			bucketToRegion: map[string]string{"us-bucket-1": "us-east-1", "us-bucket-2": "us-east-1", "us-bucket-3": "us-east-1", "eu-bucket-1": "eu-west-1", "eu-bucket-2": "eu-west-1"},
-			calls:          []string{"us-bucket-1", "us-bucket-2", "us-bucket-3", "eu-bucket-1", "eu-bucket-2"},
+			bucketCalls:    []string{"us-bucket-1", "us-bucket-2", "us-bucket-3", "eu-bucket-1", "eu-bucket-2"},
 		},
 		"error_on_get_region": {
 			bucketToRegion:      map[string]string{"us-bucket": "us-east-1", "eu-bucket": "eu-west-1"},
-			calls:               []string{"us-bucket", "us-bucket", "us-bucket", "eu-bucket", "eu-bucket", "eu-bucket"},
+			bucketCalls:         []string{"us-bucket", "us-bucket", "us-bucket", "eu-bucket", "eu-bucket", "eu-bucket"},
 			regionErrorsIndexes: map[int]bool{3: true},
+		},
+		"all_errors": {
+			bucketToRegion:      map[string]string{"us-bucket-1": "us-east-1", "us-bucket-2": "us-east-1", "us-bucket-3": "us-east-1", "eu-bucket-1": "eu-west-1", "eu-bucket-2": "eu-west-1"},
+			bucketCalls:         []string{"us-bucket-1", "us-bucket-2", "us-bucket-3", "eu-bucket-1", "eu-bucket-2"},
+			regionErrorsIndexes: map[int]bool{0: true, 1: true, 2: true, 3: true, 4: true},
+		},
+		"alternating_regions": {
+			bucketToRegion: map[string]string{"us-bucket-1": "us-east-1", "us-bucket-2": "us-east-1", "us-bucket-3": "us-east-1", "eu-bucket-1": "eu-west-1", "eu-bucket-2": "eu-west-1"},
+			bucketCalls:    []string{"us-bucket-1", "eu-bucket-1", "us-bucket-2", "eu-bucket-2", "us-bucket-3", "us-bucket-1", "eu-bucket-1", "us-bucket-2", "eu-bucket-2", "us-bucket-3"},
 		},
 	}
 	for name, tst := range tests {
 		t.Run(name, func(t *testing.T) {
-			actualClientCacheMissCount := make(map[string]int)
-			actualRegionCacheMissCount := make(map[string]int)
-			expectedClientCacheMissCount := make(map[string]int)
-			expectedRegionCacheMissCount := make(map[string]int)
-
-			c := s3.NewClientCache(sess)
 			var callIdx int
-			var call string
+			var bucket string
+			actualClientsCreated := make(map[string]bool)
+			expectedClientsCreated := make(map[string]bool)
+			actualRegionFetch := make(map[string]bool)
+			expectedRegionFetch := make(map[string]bool)
+			c := s3.NewClientCache(sess)
+
 			c = c.WithClientFactory(func(sess *session.Session, cfgs ...*aws.Config) s3iface.S3API {
 				region := sess.Config.Region
 				for _, cfg := range cfgs {
@@ -61,33 +64,41 @@ func TestCache(t *testing.T) {
 						region = cfg.Region
 					}
 				}
-				actualClientCacheMissCount[*region]++
-				return s3Mock
+				if actualClientsCreated[*region] {
+					t.Fatalf("client created more than once for a region")
+				}
+				actualClientsCreated[*region] = true
+				return struct{ s3iface.S3API }{}
 			})
 			c = c.WithS3RegionGetter(func(ctx context.Context, sess *session.Session, bucket string) (string, error) {
-				actualRegionCacheMissCount[bucket]++
+				if actualRegionFetch[bucket] {
+					t.Fatalf("region fetched more than once for bucket")
+				}
+				actualRegionFetch[bucket] = true
 				if tst.regionErrorsIndexes[callIdx] {
 					return "", regionError
 				}
 				return tst.bucketToRegion[bucket], nil
 			})
-			bucketsInCache := make(map[string]bool)
-			for callIdx, call = range tst.calls {
-				expectedRegionCacheMissCount[call] = 1
-				if _, ok := bucketsInCache[call]; !ok {
+			alreadyCalled := make(map[string]bool)
+			for callIdx, bucket = range tst.bucketCalls {
+				expectedRegionFetch[bucket] = true // for every bucket, there should be exactly 1 region fetch
+				if _, ok := alreadyCalled[bucket]; !ok {
 					if tst.regionErrorsIndexes[callIdx] {
-						expectedClientCacheMissCount[defaultRegion] = 1
+						// if there's an error, a client should be created for the default region
+						expectedClientsCreated[defaultRegion] = true
 					} else {
-						expectedClientCacheMissCount[tst.bucketToRegion[call]] = 1
+						// for every region, a client is created exactly once
+						expectedClientsCreated[tst.bucketToRegion[bucket]] = true
 					}
 				}
-				bucketsInCache[call] = true
-				c.Get(context.Background(), call)
+				alreadyCalled[bucket] = true
+				c.Get(context.Background(), bucket)
 			}
-			if diff := deep.Equal(expectedClientCacheMissCount, actualClientCacheMissCount); diff != nil {
+			if diff := deep.Equal(expectedClientsCreated, actualClientsCreated); diff != nil {
 				t.Fatal("unexpected client cache miss count. diff: ", diff)
 			}
-			if diff := deep.Equal(expectedRegionCacheMissCount, actualRegionCacheMissCount); diff != nil {
+			if diff := deep.Equal(expectedRegionFetch, actualRegionFetch); diff != nil {
 				t.Fatal("unexpected region cache miss count. diff: ", diff)
 			}
 		})
