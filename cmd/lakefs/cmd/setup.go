5dcfa4fd108c61a5043288353c894982c757843a
Barak Amar <barak.amar@treeverse.io>
2021-08-15 17:06:39 +0300

    reduce db connect calls by reduce calls from migrate

--- cmd/lakefs/cmd/setup.go
+++ cmd/lakefs/cmd/setup.go
@@ -22,15 +22,16 @@ var setupCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := cmd.Context()
 
-		migrator := db.NewDatabaseMigrator(cfg.GetDatabaseParams())
+		dbParams := cfg.GetDatabaseParams()
+		dbPool := db.BuildDatabaseConnection(ctx, dbParams)
+		defer dbPool.Close()
+
+		migrator := db.NewDatabaseMigrator(dbParams)
 		err := migrator.Migrate(ctx)
 		if err != nil {
 			fmt.Printf("Failed to setup DB: %s\n", err)
 			os.Exit(1)
 		}
-
-		dbPool := db.BuildDatabaseConnection(ctx, cfg.GetDatabaseParams())
-		defer dbPool.Close()
 
 		userName, err := cmd.Flags().GetString("user-name")
 		if err != nil {
