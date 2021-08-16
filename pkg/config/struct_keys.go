a04aad8b1a09d0c40d139a7ddb480ad92fa2fcfb
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-08-16 14:22:34 +0300

    Add GetMissingRequiredKeys to detect missing config keys

--- pkg/config/struct_keys.go
+++ pkg/config/struct_keys.go
@@ -12,10 +12,10 @@ const sep = "."
 // on an embedded struct, name components for that embedded struct should not be included.  It
 // does not handle maps, does chase pointers, but does not check for loops in nesting.
 func GetStructKeys(typ reflect.Type, tag, squashValue string) []string {
-	return appendStructKeys(typ, tag, ","+squashValue, nil, nil)
+	return appendStructKeys(typ, tag, ","+squashValue, nil, nil, nil)
 }
 
-func appendStructKeys(typ reflect.Type, tag, squashValue string, prefix []string, keys []string) []string {
+func appendStructKeys(typ reflect.Type, tag, squashValue string, prefix []string, keys []string, tagPredicate func(t reflect.StructTag) bool) []string {
 	// finite loop: Go types are well-founded.
 	for ; typ.Kind() == reflect.Ptr; typ = typ.Elem() {
 	}
@@ -25,26 +25,107 @@ func appendStructKeys(typ reflect.Type, tag, squashValue string, prefix []string
 	}
 
 	for i := 0; i < typ.NumField(); i++ {
-		field := typ.Field(i)
+		fieldType := typ.Field(i)
 		var (
 			name   string
 			squash bool
 			ok     bool
 		)
-		if name, ok = field.Tag.Lookup(tag); ok {
+		if tagPredicate != nil && !tagPredicate(fieldType.Tag) {
+			continue
+		}
+		if name, ok = fieldType.Tag.Lookup(tag); ok {
 			if strings.HasSuffix(name, squashValue) {
 				squash = true
 				name = strings.TrimSuffix(name, squashValue)
 			}
 		} else {
-			name = field.Name
+			name = fieldType.Name
 		}
 		key := make([]string, len(prefix))
 		copy(key, prefix)
 		if !squash {
 			key = append(key, name)
 		}
-		keys = appendStructKeys(field.Type, tag, squashValue, key, keys)
+		keys = appendStructKeys(fieldType.Type, tag, squashValue, key, keys, tagPredicate)
+	}
+	return keys
+}
+
+// GetMissingRequiredKeys returns all keys of value in GetStructKeys format that have
+// an additional required tag set but are unset.
+func GetMissingRequiredKeys(value interface{}, tag, squashValue, requiredTag string) []string {
+	return appendStructKeysIfZero(reflect.ValueOf(value), reflect.TypeOf(value), tag, ","+squashValue, requiredTag, nil, nil)
+}
+
+// isScalar returns true if kind is "scalar", i.e. has no Elem().  This
+// recites the list from reflect/type.go and may start to give incorrect
+// results if new kinds are added to the language.
+func isScalar(kind reflect.Kind) bool {
+	switch kind {
+	case reflect.Array:
+	case reflect.Chan:
+	case reflect.Map:
+	case reflect.Ptr:
+	case reflect.Slice:
+		return false
+	}
+	return true
+}
+
+func appendStructKeysIfZero(value reflect.Value, typ reflect.Type, tag, squashValue, requiredTag string, prefix []string, keys []string) []string {
+	// finite loop: Go types are well-founded.
+	for ; typ.Kind() == reflect.Ptr; typ = typ.Elem() {
+		if !value.IsZero() {
+			// Keep going on zero values (nil pointers) so we
+			// can report that all their required sub-fields are
+			// missing.
+			value = value.Elem()
+		}
+	}
+
+	if !isScalar(typ.Kind()) {
+		if !isScalar(typ.Elem().Kind()) {
+			panic("No support for detecting required keys inside " + value.Kind().String() + " of structs")
+		}
+	}
+
+	if typ.Kind() != reflect.Struct {
+		return keys
+	}
+
+	for i := 0; i < typ.NumField(); i++ {
+		fieldType := typ.Field(i)
+		var fieldValue reflect.Value = value
+		if !value.IsZero() {
+			fieldValue = value.Field(i)
+		}
+
+		var (
+			name   string
+			squash bool
+			ok     bool
+		)
+		if name, ok = fieldType.Tag.Lookup(tag); ok {
+			if strings.HasSuffix(name, squashValue) {
+				squash = true
+				name = strings.TrimSuffix(name, squashValue)
+			}
+		} else {
+			name = fieldType.Name
+		}
+		if _, ok = fieldType.Tag.Lookup(requiredTag); ok {
+			if fieldValue.IsZero() {
+				keys = append(keys, strings.Join(append(prefix, name), sep))
+			}
+		}
+
+		key := make([]string, len(prefix))
+		copy(key, prefix)
+		if !squash {
+			key = append(key, name)
+		}
+		keys = appendStructKeysIfZero(fieldValue, fieldType.Type, tag, squashValue, requiredTag, key, keys)
 	}
 	return keys
 }
--- pkg/config/struct_keys_test.go
+++ pkg/config/struct_keys_test.go
@@ -9,8 +9,9 @@ import (
 )
 
 const (
-	tagName        = "test"
-	squashTagValue = "squash"
+	tagName         = "test"
+	squashTagValue  = "squash"
+	requiredTagName = "required"
 )
 
 func TestStructKeys_Simple(t *testing.T) {
@@ -102,6 +103,167 @@ func TestStructKeys_Squash(t *testing.T) {
 	}
 
 	keys := config.GetStructKeys(reflect.TypeOf(s{}), tagName, squashTagValue)
+	if diffs := deep.Equal(keys, []string{"A", "B", "C", "D"}); diffs != nil {
+		t.Error("wrong keys for struct: ", diffs)
+	}
+}
+
+func TestGetMissingRequired_SimpleRequired(t *testing.T) {
+	type s struct {
+		A  int `required:"yes"`
+		AA int
+		B  string `required:"no (is actually yes)"`
+		BB string
+		C  *float64 `required:"yup"`
+		CC *float64
+		D  []rune `required:""`
+		DD []rune
+	}
+
+	keys := config.GetMissingRequiredKeys(s{}, tagName, squashTagValue, requiredTagName)
+	if diffs := deep.Equal(keys, []string{"A", "B", "C", "D"}); diffs != nil {
+		t.Error("wrong keys for struct: ", diffs)
+	}
+	keys = config.GetMissingRequiredKeys(&s{}, tagName, squashTagValue, requiredTagName)
+	if diffs := deep.Equal(keys, []string{"A", "B", "C", "D"}); diffs != nil {
+		t.Error("wrong keys for pointer to struct: ", diffs)
+	}
+	ps := &s{}
+	keys = config.GetMissingRequiredKeys(&ps, tagName, squashTagValue, requiredTagName)
+	if diffs := deep.Equal(keys, []string{"A", "B", "C", "D"}); diffs != nil {
+		t.Error("wrong keys for pointer to pointer to struct: ", diffs)
+	}
+}
+
+func TestGetMissingRequired_SimpleNotMissing(t *testing.T) {
+	type s struct {
+		A  int `required:"yes"`
+		AA int
+		B  string `required:"no (is actually yes)"`
+		BB string
+		C  *float64 `required:"yup"`
+		CC *float64
+		D  []rune `required:""`
+		DD []rune
+	}
+
+	c := float64(12.34)
+	cases := []struct {
+		Value           s
+		MissingRequired []string
+	}{
+		{s{A: 2, C: &c}, []string{"B", "D"}},
+		{s{AA: 22, B: "foo", D: []rune{'a', 'b', 'c'}}, []string{"A", "C"}},
+	}
+
+	for _, c := range cases {
+		keys := config.GetMissingRequiredKeys(c.Value, tagName, squashTagValue, requiredTagName)
+		if diffs := deep.Equal(keys, c.MissingRequired); diffs != nil {
+			t.Errorf("wrong keys for %+v: %s", c.Value, diffs)
+		}
+	}
+}
+
+func TestGetMissingRequired_Nested(t *testing.T) {
+	type s struct {
+		A struct {
+			X string
+			Y int `required:"yes"`
+		}
+		B ***struct {
+			Z float32 `required:"yes"`
+			W float64
+		}
+	}
+
+	keys := config.GetMissingRequiredKeys(s{}, tagName, squashTagValue, requiredTagName)
+	if diffs := deep.Equal(keys, []string{"A.Y", "B.Z"}); diffs != nil {
+		t.Error("wrong keys for struct: ", diffs)
+	}
+}
+
+func TestGetMissingRequired_NestedNotMissing(t *testing.T) {
+	type A struct {
+		X string
+		Y int `required:"yes"`
+	}
+	type B struct {
+		Z float32 `required:"yes"`
+		W float64
+	}
+	type s struct {
+		A A
+		B ***B
+	}
+
+	ptr3 := func(b B) ***B { pb := &b; ppb := &pb; return &ppb }
+	ptr2 := func(b *B) ***B { pb := &b; return &pb }
+	ptr1 := func(b **B) ***B { return &b }
+
+	cases := []struct {
+		Value           s
+		MissingRequired []string
+	}{
+		{s{A: A{Y: 7}, B: ptr3(B{})}, []string{"B.Z"}},
+		{s{B: ptr3(B{Z: 1.23})}, []string{"A.Y"}},
+		{s{A: A{Y: 7}, B: ptr1(nil)}, []string{"B.Z"}},
+		{s{A: A{Y: 7}, B: ptr2(nil)}, []string{"B.Z"}},
+	}
+
+	for _, c := range cases {
+		keys := config.GetMissingRequiredKeys(c.Value, tagName, squashTagValue, requiredTagName)
+		if diffs := deep.Equal(keys, c.MissingRequired); diffs != nil {
+			t.Errorf("wrong keys for %+v: %s", c.Value, diffs)
+		}
+	}
+}
+
+func TestGetMissingRequired_SimpleTagged(t *testing.T) {
+	type s struct {
+		A int `test:"Aaa" required:"yes"`
+		B int `toast:"bee" required:"yes"`
+		c int `test:"ccc" toast:"sea" required:"yes"`
+	}
+
+	keys := config.GetMissingRequiredKeys(s{}, tagName, squashTagValue, requiredTagName)
+	if diffs := deep.Equal(keys, []string{"Aaa", "B", "ccc"}); diffs != nil {
+		t.Error("wrong keys for struct: ", diffs)
+	}
+}
+
+func TestGetMissingRequired_NestedTagged(t *testing.T) {
+	type s struct {
+		A struct {
+			X  int    `test:"eks" required:"yes"`
+			BE string `required:"yes"`
+		} `test:"aaa"`
+		B **struct {
+			Gamma int32 `required:"yes"`
+			Delta uint8 `test:"dee" required:"yes"`
+		}
+	}
+
+	keys := config.GetMissingRequiredKeys(s{}, tagName, squashTagValue, requiredTagName)
+	if diffs := deep.Equal(keys, []string{"aaa.eks", "aaa.BE", "B.Gamma", "B.dee"}); diffs != nil {
+		t.Error("wrong keys for struct: ", diffs)
+	}
+}
+
+func TestGetMissingRequired_Squash(t *testing.T) {
+	type I struct {
+		A int `required:"yes"`
+		B int `required:"yes"`
+	}
+	type J struct {
+		C uint `required:"yes"`
+		D uint `required:"yes"`
+	}
+	type s struct {
+		I `test:",squash"`
+		J `test:"ignore,squash"`
+	}
+
+	keys := config.GetMissingRequiredKeys(s{}, tagName, squashTagValue, requiredTagName)
 	if diffs := deep.Equal(keys, []string{"A", "B", "C", "D"}); diffs != nil {
 		t.Error("wrong keys for struct: ", diffs)
 	}
