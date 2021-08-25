884d3a93bb1738fc839290ee359da41a31c65ed7
Tal Sofer <tal.sofer@treeverse.io>
2021-08-25 11:00:31 +0300

    application writes the input json

--- clients/spark/core/src/test/resources/parser-test/sst_files_generator.go
+++ clients/spark/core/src/test/resources/parser-test/sst_files_generator.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"sort"
 
@@ -10,11 +12,17 @@ import (
 )
 
 const (
-	DefaultKeySizeBytes = 100
-	MbToBytes           = 1024 * 1024
+	DefaultKeySizeBytes   = 100
+	DefaultValueSizeBytes = 5
+	MbToBytes             = 1024 * 1024
 	// The max file size that can be written by graveler (pkg/config/config.go)
 	DefaultCommittedPermanentMaxRangeSizeMb = 20
 )
+
+type Entry struct {
+	Key   string
+	Value string
+}
 
 func main() {
 
@@ -32,31 +40,30 @@ func main() {
 
 func writeTwoLevelIdxSst(sizeBytes int) {
 	keys := generateSortedSlice(sizeBytes)
-	writePebbleSst(keys, sizeBytes, "2-level-idx.sst", true)
+	writePebbleSst(keys, sizeBytes, "two.level.idx", true)
 }
 
 func generateSortedSlice(size int) []string {
 	numLines := size / DefaultKeySizeBytes
 	slice := make([]string, 0, numLines)
 	for i := 0; i < numLines; i++ {
-		// Generate a name.
 		key, err := nanoid.New(DefaultKeySizeBytes)
 		if err != nil {
-			panic(err) //TODO: is this what I need to do here?
+			panic(err)
 		}
 		slice = append(slice, key)
 	}
-	//slice := generateStringsSlice(size, DefaultKeySizeBytes)
 	sort.Strings(slice)
 	return slice
 }
 
-func writePebbleSst(data []string, size int, name string, twoLevelIdx bool) {
+func writePebbleSst(keys []string, size int, name string, twoLevelIdx bool) {
 	fmt.Printf("Generate %s...\n", name)
-	file, err := os.Create(name)
+	sstFile, err := os.Create(name + ".sst")
 	if err != nil {
 		panic(err)
 	}
+	expectedContents := make([]Entry, 0, len(keys))
 
 	idxBlockSize := 4096 // Default index block size
 	// https://github.com/cockroachdb/pebble/blob/2aba043dd4a270dfdd7731fedf99817164476618/sstable/options.go#L196
@@ -64,7 +71,7 @@ func writePebbleSst(data []string, size int, name string, twoLevelIdx bool) {
 		// setting the index block size target to a small number to make 2-level index get enabled
 		idxBlockSize = 100
 	}
-	writer := sstable.NewWriter(file, sstable.WriterOptions{
+	writer := sstable.NewWriter(sstFile, sstable.WriterOptions{
 		Compression:    sstable.SnappyCompression,
 		IndexBlockSize: idxBlockSize,
 		//TODO: confirm that I don't need those properties
@@ -80,12 +87,23 @@ func writePebbleSst(data []string, size int, name string, twoLevelIdx bool) {
 		}
 	}()
 
-	for _, v := range data {
+	for _, k := range keys {
 		if writer.EstimatedSize() >= uint64(size) {
 			break
 		}
-		if err := writer.Set([]byte(v), []byte("some_value")); err != nil {
+		v, err := nanoid.New(DefaultValueSizeBytes)
+		if err != nil {
+			panic(err)
+		}
+		if err := writer.Set([]byte(k), []byte(v)); err != nil {
 			panic(fmt.Errorf("setting key and value: %w", err))
 		}
+		expectedContents = append(expectedContents, Entry{Key: k, Value: v})
 	}
+	saveExpectedContentsToDisk(expectedContents, name)
+}
+
+func saveExpectedContentsToDisk(contents []Entry, name string) {
+	j, _ := json.Marshal(contents)
+	ioutil.WriteFile(name+".json", j, os.ModePerm)
 }
