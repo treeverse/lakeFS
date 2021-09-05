a0713b384639d4a34628c40ca5bd9b5d8867b4e4
arielshaqed <ariels@treeverse.io>
2021-09-05 18:51:32 +0300

    [S3 gateway] Return HTTP 409 (Conflict) when creating existing repo (#2451)
    * [S3 gateway] Return HTTP 409 (Conflict) when creating existing repo
    
    When copying to a new key, Rclone likes to try to create _all_ the S3 path
    down (it seems to think it's on a POSIXy filesystem so it `mkdir -p`s).  A
    repository cannot be created by the gateway -- there is not enough info on
    the request -- but if we return a 409 (conflict) then Rclone continues and
    succeeds.
    
    There is no S3 gateway test, so tested manually using Rclone.
    
    Fixes #2447.
    
    * [CR] Require CreateRepositoryAction (not ReadRepositoryAction)

---
+++ pkg/gateway/operations/putbucket.go
@@ -0,0 +1,35 @@
+package operations
+
+import (
+	"net/http"
+
+	gatewayerrors "github.com/treeverse/lakefs/pkg/gateway/errors"
+	"github.com/treeverse/lakefs/pkg/permissions"
+)
+
+// PutBucket handles S3 Create Bucket operations.  It does *not* actually
+// create new repos (there is not enough information in the S3 request to
+// create a new repo), but *does* detect whether the repo already exists.
+type PutBucket struct{}
+
+func (controller *PutBucket) RequiredPermissions(_ *http.Request, repoID string) ([]permissions.Permission, error) {
+	return []permissions.Permission{
+		{
+			// Mimic S3, which requires s3:CreateBucket to call
+			// create-bucket, even if we only want to receive
+			// 409.
+			Action:   permissions.CreateRepositoryAction,
+			Resource: permissions.RepoArn(repoID),
+		},
+	}, nil
+}
+
+func (controller *PutBucket) Handle(w http.ResponseWriter, req *http.Request, o *RepoOperation) {
+	o.Incr("put_repo")
+	if o.Repository == nil {
+		// No repo, would have to create it, but not enough
+		// information -- so not supported.
+		o.EncodeError(w, req, gatewayerrors.ERRLakeFSNotSupported.ToAPIErr())
+	}
+	o.EncodeError(w, req, gatewayerrors.ErrBucketAlreadyExists.ToAPIErr())
+}
