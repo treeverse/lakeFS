739f33d77e0bcc2eb06c87c8dfb35cf3204413ca
johnnyaug <yoni.augarten@treeverse.io>
2021-09-05 14:42:25 +0300

    add comments

--- pkg/graveler/settings/manager.go
+++ pkg/graveler/settings/manager.go
@@ -17,6 +17,10 @@ const (
 	settingsSuffixTemplate = "/%s/settings/%s.json"
 )
 
+// Manager is a key-value store for Graveler repository-level settings.
+// Each setting is stored under a key, and it can be any proto.Message.
+// A setting is saved as an object in the backing storage, so the performance of save/get is bound by the
+// performance of a storage put/get respectively.
 type Manager struct {
 	refManager                  graveler.RefManager
 	branchLock                  graveler.BranchLocker
@@ -24,6 +28,7 @@ type Manager struct {
 	committedBlockStoragePrefix string
 }
 
+// Save persists the given proto.Message under the given repository and key.
 func (m *Manager) Save(ctx context.Context, repositoryID graveler.RepositoryID, key string, message proto.Message) error {
 	repo, err := m.refManager.GetRepository(ctx, repositoryID)
 	if err != nil {
@@ -40,6 +45,8 @@ func (m *Manager) Save(ctx context.Context, repositoryID graveler.RepositoryID, 
 	}, int64(len(messageBytes)), bytes.NewReader(messageBytes), block.PutOpts{})
 }
 
+// Get fetches the configuration under the given repository and key.
+// The result is placed in message.
 func (m *Manager) Get(ctx context.Context, repositoryID graveler.RepositoryID, key string, message proto.Message) error {
 	repo, err := m.refManager.GetRepository(ctx, repositoryID)
 	if err != nil {
@@ -67,6 +74,10 @@ func (m *Manager) Get(ctx context.Context, repositoryID graveler.RepositoryID, k
 	return proto.Unmarshal(messageBytes, message)
 }
 
+// UpdateWithLock atomically performs the following sequence:
+// 1. Fetch the configuration under the given repository and key, and place it in message.
+// 2. Call the given update function (which should presumably update the message).
+// 3. Persist message to the store.
 func (m *Manager) UpdateWithLock(ctx context.Context, repositoryID graveler.RepositoryID, key string, message proto.Message, update func()) error {
 	repo, err := m.refManager.GetRepository(ctx, repositoryID)
 	if err != nil {
