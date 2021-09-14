07974bc5c8dc7bdced6903dab680b86d117eaf60
Barak Amar <barak.amar@treeverse.io>
2021-09-14 16:44:52 +0300

    [s3 gateway] Handle all path characters in SigV2 and add Nessie S3-ish test (#2464)
    * Change AWS S3 sigV2 verification to use path as documented by AWS
    
    * Add Nessie S3-ish test
    
    Use the minIO client to test S3 uploads and downloads with both V2 and V4
    signatures.  (Unlike the AWS S3 client, the minIO client supports signing
    using SigV2.)
    
    * Parallelise S3 gateway test
    
    (Timed out on GCP)
    
    * Add S3 SigV2, SigV4 component tests
    
    Sign using minio, verify using gateway code.
    
    * SigV4: Test bothV4 path and host styles
    
    * Make S3 gateway test pathnames (just) shorter than 1023 chars
    
    * Close channel in Nessie S3 gateway test
    
    Also increase parallelism somewhat.
    
    * [CR] Use t.Errorf.  Kill test if minio client create fails.

---
+++ pkg/testutil/random.go
@@ -0,0 +1,35 @@
+package testutil
+
+import (
+	"math/rand"
+	"strings"
+	"unicode/utf8"
+)
+
+// RandomRune returns a random Unicode rune from rand, weighting at least
+// num out of den runes to be ASCII.
+func RandomRune(rand *rand.Rand, num, den int) rune {
+	if rand.Intn(den) < num {
+		return rune(rand.Intn(utf8.RuneSelf))
+	}
+	for {
+		r := rune(rand.Intn(utf8.MaxRune))
+		// Almost all chars are legal, so this usually
+		// returns immediately.
+		if utf8.ValidRune(r) {
+			return r
+		}
+	}
+}
+
+// RandomString returns a random UTF-8 string of size or almost size bytes
+// from rand.  It is weighted towards using many ASCII characters.
+func RandomString(rand *rand.Rand, size int) string {
+	sb := strings.Builder{}
+	for sb.Len() <= size {
+		sb.WriteRune(RandomRune(rand, 2, 5))
+	}
+	ret := sb.String()
+	_, lastRuneSize := utf8.DecodeLastRuneInString(ret)
+	return ret[0 : len(ret)-lastRuneSize]
+}
