33b4cd1e0fdca9ddb8d25ecd2956b4285eb19c01
guyhardonag <guy.hardonag@treeverse.io>
2021-10-10 12:20:50 +0300

    Code review changes

--- test/spark/run-client-test.sh
+++ test/spark/run-client-test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Run Export
 docker-compose run -v  $PWD/../../clients/spark/target/:/target/ -T --no-deps --rm spark-submit bash -c 'spark-submit  --master spark://spark:7077 --conf spark.hadoop.lakefs.api.url=http:/docker.lakefs.io:8000/api/v1   --conf spark.hadoop.lakefs.api.access_key=${TESTER_ACCESS_KEY_ID}   --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false   --conf spark.hadoop.lakefs.api.secret_key=${TESTER_SECRET_ACCESS_KEY}   --class io.treeverse.clients.Main  ${CLIENT_JAR} test-data ${EXPORT_LOCATION}   --branch=main'
@@ -12,7 +13,7 @@ docker-compose exec -T lakefs lakectl fs ls --recursive --no-color lakefs://test
 aws s3 ls --recursive ${EXPORT_LOCATION} | awk '{print $4}'| cut -d/ -f 2-  | grep -v EXPORT_ | sort > ${s3_out}
 
 if ! diff ${lakectl_out} ${s3_out}; then
-  echo "export location and lakefs should contain same f"
+  echo "export location and lakefs should contain same objects"
   exit 1
 fi
 # Run Garbage Collection 
@@ -20,30 +21,17 @@ docker-compose run  -v  $PWD/../../clients/spark/target/:/target/ -T --no-deps -
 
 
 # Validate Garbage Collection
-
-if ! docker-compose exec -T lakefs lakectl fs cat lakefs://test-data/697297df0c01d17/commits/commit-ten > /dev/null ; then
-  echo expected file commit-ten to exist
-  exit 1
-fi
-
-if ! docker-compose exec -T lakefs lakectl fs cat lakefs://test-data/651a755f7f5b1a4/commits/commit-three > /dev/null ; then
-  echo expected file commit-three to exist
-  exit 1
-fi
-if docker-compose exec -T lakefs lakectl fs cat lakefs://test-data/09d9016a12777fe/commits/commit-four > /dev/null 2>&1 ; then
-  echo expected file commit-four to be removed by garbage collection
-  exit 1
-fi
-if docker-compose exec -T lakefs lakectl fs cat lakefs://test-data/18c9a1fe4695109/commits/commit-two > /dev/null 2>&1 ; then
-  echo expected file commit-two to be removed by garbage collection
-  exit 1
-fi
-if docker-compose exec -T lakefs lakectl fs cat lakefs://test-data/efb14f67a2a3c38/commits/commit-one > /dev/null 2>&1 ; then
-  echo expected file commit-one to be removed by garbage collection
-  exit 1
-fi
-if docker-compose exec -T lakefs lakectl fs cat lakefs://test-data/dede737a85e27f4/commits/commit-five > /dev/null 2>&1 ; then
-  echo expected file commit-five to be removed by garbage collection
-  exit 1
-fi
+for f in \
+    lakefs://test-data/697297df0c01d17/commits/commit-ten \
+    lakefs://test-data/651a755f7f5b1a4/commits/commit-three \
+    lakefs://test-data/09d9016a12777fe/commits/commit-four \
+    lakefs://test-data/18c9a1fe4695109/commits/commit-two \
+    lakefs://test-data/efb14f67a2a3c38/commits/commit-one \
+    lakefs://test-data/dede737a85e27f4/commits/commit-five
+do
+    if docker-compose exec -T lakefs lakectl fs cat $f> /dev/null 2>&1 ; then
+      echo "Expected $f to be removed by garbage collection"
+      exit 1
+    fi
+done
 
