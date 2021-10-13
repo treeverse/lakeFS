8ac785e4fb213c5e4faed6cb383372fd8db5a04d
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-10-13 09:41:35 +0300

    Add friendly_name, source columns to auth_users

---
+++ pkg/ddl/000034_external_users.up.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+ALTER TABLE auth_users
+    ADD COLUMN IF NOT EXISTS friendly_name TEXT,
+    ADD COLUMN IF NOT EXISTS source TEXT;
+
+COMMENT ON COLUMN auth_users.friendly_name IS 'If set, a shorter name for user than "display_name"';
+
+END;
