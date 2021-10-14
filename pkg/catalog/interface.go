d70900ecd46145e27973bd762abde526744fd7da
Barak Amar <barak.amar@treeverse.io>
2021-10-15 00:13:09 +0300

    Remove unused CreateEntries from catalog (#2549)

--- pkg/catalog/interface.go
+++ pkg/catalog/interface.go
@@ -85,7 +85,6 @@ type Interface interface {
 	// the entry with ExpiredError if it has expired from underlying storage.
 	GetEntry(ctx context.Context, repository, reference string, path string, params GetEntryParams) (*DBEntry, error)
 	CreateEntry(ctx context.Context, repository, branch string, entry DBEntry, writeConditions ...graveler.WriteConditionOption) error
-	CreateEntries(ctx context.Context, repository, branch string, entries []DBEntry) error
 	DeleteEntry(ctx context.Context, repository, branch string, path string) error
 	ListEntries(ctx context.Context, repository, reference string, prefix, after string, delimiter string, limit int) ([]*DBEntry, bool, error)
 	ResetEntry(ctx context.Context, repository, branch string, path string) error
