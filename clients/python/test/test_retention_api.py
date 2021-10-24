fb5f04afb4fd7351e94de48b329cbdcbfa1add0f
Barak Amar <barak.amar@treeverse.io>
2021-10-24 11:36:11 +0300

    regenerate python client

--- clients/python/test/test_retention_api.py
+++ clients/python/test/test_retention_api.py
@@ -24,10 +24,22 @@ class TestRetentionApi(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_prepare_retention_commits(self):
-        """Test case for prepare_retention_commits
+    def test_get_garbage_collection_rules(self):
+        """Test case for get_garbage_collection_rules
+
+        """
+        pass
+
+    def test_prepare_garbage_collection_commits(self):
+        """Test case for prepare_garbage_collection_commits
 
         save lists of active and expired commits for garbage collection  # noqa: E501
+        """
+        pass
+
+    def test_set_garbage_collection_rules(self):
+        """Test case for set_garbage_collection_rules
+
         """
         pass
 
