4abf64c79eddabb8996812c0023e11124bd05f26
Tal Sofer <tal.sofer@treeverse.io>
2021-11-03 17:36:32 +0200

    Exclude team members name from log messages

--- clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
@@ -759,8 +759,9 @@ public class LakeFSFileSystem extends FileSystem {
 
     @Override
     public FileStatus[] globStatus(Path pathPattern) throws IOException {
+        // TODO: implement this method as part of https://github.com/treeverse/lakeFS/issues/2629
         FileStatus fStatus = new FileStatus(0, false, 1, 20, 1,
-                new Path("tal-test"));
+                new Path("globStatus-test-path"));
         return new FileStatus[]{fStatus};
     }
 
