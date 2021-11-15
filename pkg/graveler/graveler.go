b8c64e851f3042c19f05c2c76590278ba139a074
guyhardonag <guy.hardonag@treeverse.io>
2021-11-15 15:24:45 +0200

    Add merge log with metarange IDs

--- pkg/graveler/graveler.go
+++ pkg/graveler/graveler.go
@@ -1593,6 +1593,10 @@ func (g *Graveler) Merge(ctx context.Context, repositoryID RepositoryID, destina
 		if err != nil {
 			return nil, err
 		}
+		g.log.WithFields(logging.Fields{
+			"from_meta_range": fromCommit.MetaRangeID,
+			"to_meta_range":   toCommit.MetaRangeID,
+			"base_meta_range": baseCommit.MetaRangeID}).Trace("Merge")
 		metaRangeID, summary, err := g.CommittedManager.Merge(ctx, storageNamespace, toCommit.MetaRangeID, fromCommit.MetaRangeID, baseCommit.MetaRangeID)
 		if err != nil {
 			if !errors.Is(err, ErrUserVisible) {
