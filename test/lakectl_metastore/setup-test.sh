56d723472fa593d5eca41ade1c73566831150dd6
guyhardonag <guy.hardonag@treeverse.io>
2021-12-15 16:53:32 +0200

    Add test for metastore commands using DBT and Trino

---
+++ test/lakectl_metastore/setup-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+#
+# setup-test.sh - set up lakeFS to run Spark app tests.  Run once per lakeFS.
+
+docker-compose exec -T lakefs /app/wait-for localhost:8000
+
+docker-compose exec -T lakefs sh -c 'lakefs setup --user-name tester --access-key-id ${LAKECTL_CREDENTIALS_ACCESS_KEY_ID} --secret-access-key ${LAKECTL_CREDENTIALS_SECRET_ACCESS_KEY}'
+
+docker-compose exec -T lakefs sh -c "lakectl repo create \"lakefs://example\" ${STORAGE_NAMESPACE}_meta -d main"
+# create base schema
+ # another option would be to run as part of workflow
+docker-compose run --entrypoint '/usr/app/wait-for-trino.sh' dbt
+
+docker-compose exec -T trino sh -c "trino --catalog lakefs --execute \"create schema dbt_main with ( location = 's3://example/main/dbt/' )\" "
+
