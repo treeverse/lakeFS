69197115d4ef6d7ef5455f82154ba174d892a2d0
guy-har <60321938+guy-har@users.noreply.github.com>
2021-12-21 17:56:44 +0200

    Add Tests for metastore commands using Trino and DBT (#2813)

---
+++ test/lakectl_metastore/dbt/dbt-project/models/reviews/amazon_reviews.sql
@@ -0,0 +1,13 @@
+
+{{ config(materialized='table', properties={
+      "format": "'PARQUET'",
+        "partitioned_by":"ARRAY['year']",
+
+    }) }}
+
+with source_data as (
+    select marketplace , customer_id , review_id , product_id , product_parent , product_title , star_rating , helpful_votes , total_votes , vine , verified_purchase , review_headline , review_body , review_date ,product_category, "year"  from s3.nessie_system_testing.amazon_reviews_parquet_internal where year > 1980
+)
+
+select *
+from source_data
