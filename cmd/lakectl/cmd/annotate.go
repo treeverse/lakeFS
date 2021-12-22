243827229b369877d71f0eb461fb5dd9e759c7b7
shimi9276 <shimi9276@gmail.com>
2021-12-22 15:36:50 +0200

    Added pagination for all objects

--- cmd/lakectl/cmd/annotate.go
+++ cmd/lakectl/cmd/annotate.go
@@ -1,53 +1,77 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/treeverse/lakefs/pkg/api"
 )
 
-const annotateTemplate = `{{ range $val := .Commits}}
-{{ $.Object|ljust 10}} {{ $val.Committer }} {{ $val.Id | printf "%.16s"|ljust 20 }} {{ $val.Message |ljust 30 }} {{ $val.CreationDate|date }}{{ end -}}
+const annotateTemplate = `{{  $val := .Commit}}
+{{ $.Object|ljust 15}} {{ $val.Committer|ljust 20 }} {{ $val.Id | printf "%.16s"|ljust 20 }} {{ $val.CreationDate|date }}  {{ $.CommitMessage |ljust 30 }}
 `
 
 var annotateCmd = &cobra.Command{
 	Use:   "annotate <path uri>",
-	Short: "Show who created the latest commit",
-	Long:  "Show who created the latest commit to an object or objects in a give path",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, c_args []string) {
-		pathURI := MustParsePathURI("path", c_args[0])
+	Short: "List entries under a given path, annotating each with the latest modifying commit ",
+	// Long:    "Show who created the latest commit to an object or objects in a give path",
+	Aliases: []string{"blame"},
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		pathURI := MustParsePathURI("path", args[0])
 		client := getClient()
 		amount := 1
 		pfx := api.PaginationPrefix(*pathURI.Path)
 		res, err := client.ListObjectsWithResponse(cmd.Context(), pathURI.Repository, pathURI.Ref, &api.ListObjectsParams{Prefix: &pfx})
 		DieOnResponseError(res, err)
-		objList := res.JSON200.Results
-		logCommitsParams := &api.LogCommitsParams{
-			Amount: api.PaginationAmountPtr(amount),
-		}
-		pagination := api.Pagination{HasMore: true}
-		for _, obj := range objList {
-			res, err := client.LogCommitsWithResponse(cmd.Context(), pathURI.Repository, pathURI.Ref, logCommitsParams)
-			DieOnResponseError(res, err)
-			pagination = res.JSON200.Pagination
-			logCommitsParams.After = api.PaginationAfterPtr(pagination.NextOffset)
-			data := struct {
-				Commits    []api.Commit
-				Object     string
-				Pagination *Pagination
-			}{
-				Commits: res.JSON200.Results,
-				Object:  obj.Path,
-				Pagination: &Pagination{
-					Amount: amount,
-				},
+		var from string
+		for {
+			pfx := api.PaginationPrefix(*pathURI.Path)
+			params := &api.ListObjectsParams{
+				Prefix: &pfx,
+				After:  api.PaginationAfterPtr(from),
 			}
-			Write(annotateTemplate, data)
+			resp, err := client.ListObjectsWithResponse(cmd.Context(), pathURI.Repository, pathURI.Ref, params)
+			DieOnResponseError(resp, err)
+			for _, obj := range resp.JSON200.Results {
+
+				prfx := []string{obj.Path}
+				logCommitsParams := &api.LogCommitsParams{
+					Amount:  api.PaginationAmountPtr(amount),
+					Objects: &prfx,
+				}
+				res, err := client.LogCommitsWithResponse(cmd.Context(), pathURI.Repository, pathURI.Ref, logCommitsParams)
+				DieOnResponseError(res, err)
+				data := struct {
+					Commit        api.Commit
+					Object        string
+					CommitMessage string
+				}{
+					Commit:        res.JSON200.Results[0],
+					Object:        obj.Path,
+					CommitMessage: setMessageSize(100, string(res.JSON200.Results[0].Message)),
+				}
+				Write(annotateTemplate, data)
+			}
+			pagination := resp.JSON200.Pagination
+			if !pagination.HasMore {
+				break
+			}
+			from = pagination.NextOffset
 		}
 	},
+}
+
+func setMessageSize(size int, str string) string {
+	if len(str) > size {
+		str = str[:size] + "..."
+	}
+	str = strings.Split(str, "\\n")[0]
+	return str
 }
 
 //nolint:gochecknoinits
 func init() {
 	rootCmd.AddCommand(annotateCmd)
+
 }
