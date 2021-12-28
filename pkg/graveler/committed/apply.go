5bd89a9ed31905b6dca5ef974b46b4349f002479
Barak Amar <barak.amar@treeverse.io>
2021-12-28 14:01:01 +0200

    Fix naming in apply (#2837)

--- pkg/graveler/committed/apply.go
+++ pkg/graveler/committed/apply.go
@@ -23,11 +23,11 @@ type applier struct {
 	ctx                   context.Context
 	logger                logging.Logger
 	writer                MetaRangeWriter
-	source                Iterator
-	diffs                 Iterator
+	base                  Iterator
+	changes               Iterator
 	opts                  *ApplyOptions
 	summary               graveler.DiffSummary
-	haveDiffs, haveSource bool
+	haveChanges, haveBase bool
 }
 
 // applyAll applies all changes from Iterator to writer and returns the number of writes
@@ -118,47 +118,47 @@ func (a *applier) incrementDiffSummary(typ graveler.DiffType) {
 }
 
 func (a *applier) apply() error {
-	a.haveSource, a.haveDiffs = a.source.Next(), a.diffs.Next()
-	for a.haveSource && a.haveDiffs {
+	a.haveBase, a.haveChanges = a.base.Next(), a.changes.Next()
+	for a.haveBase && a.haveChanges {
 		select {
 		case <-a.ctx.Done():
 			return a.ctx.Err()
 		default:
 		}
-		sourceValue, sourceRange := a.source.Value()
-		diffValue, diffRange := a.diffs.Value()
+		baseValue, baseRange := a.base.Value()
+		changeValue, changeRange := a.changes.Value()
 		var err error
 		switch {
-		case diffValue == nil && sourceValue == nil:
-			err = a.applyBothRanges(diffRange, sourceRange)
-		case diffValue == nil && sourceValue != nil:
-			err = a.applyDiffRangeSourceKey(diffRange, sourceValue)
-		case sourceValue == nil && diffValue != nil:
-			err = a.applySourceRangeDiffKey(sourceRange, diffValue)
+		case changeValue == nil && baseValue == nil:
+			err = a.applyBothRanges(changeRange, baseRange)
+		case changeValue == nil && baseValue != nil:
+			err = a.applyChangeRangeBaseKey(changeRange, baseValue)
+		case baseValue == nil && changeValue != nil:
+			err = a.applyBaseRangeChangeKey(baseRange, changeValue)
 		default:
-			err = a.applyBothKeys(sourceValue, diffValue)
+			err = a.applyBothKeys(baseValue, changeValue)
 		}
 		if err != nil {
 			return err
 		}
 	}
-	if err := a.source.Err(); err != nil {
+	if err := a.base.Err(); err != nil {
 		return err
 	}
-	if err := a.diffs.Err(); err != nil {
+	if err := a.changes.Err(); err != nil {
 		if errors.Is(err, graveler.ErrConflictFound) {
 			a.incrementDiffSummary(graveler.DiffTypeConflict)
 		}
 		return err
 	}
-	if a.haveSource {
-		if _, err := a.applyAll(a.source); err != nil {
+	if a.haveBase {
+		if _, err := a.applyAll(a.base); err != nil {
 			return err
 		}
 	}
 
-	if a.haveDiffs {
-		numAdded, err := a.applyAll(a.diffs)
+	if a.haveChanges {
+		numAdded, err := a.applyAll(a.changes)
 		if err != nil {
 			return err
 		}
@@ -170,34 +170,34 @@ func (a *applier) apply() error {
 	if !a.opts.AllowEmpty && !a.hasChanges(a.summary) {
 		return graveler.ErrNoChanges
 	}
-	return a.diffs.Err()
+	return a.changes.Err()
 }
 
-func (a *applier) applyBothKeys(sourceValue *graveler.ValueRecord, diffValue *graveler.ValueRecord) error {
-	c := bytes.Compare(sourceValue.Key, diffValue.Key)
+func (a *applier) applyBothKeys(baseValue *graveler.ValueRecord, changeValue *graveler.ValueRecord) error {
+	c := bytes.Compare(baseValue.Key, changeValue.Key)
 	if c < 0 {
-		// select record from source
+		// select record from base
 		if a.logger.IsTracing() {
 			a.logger.WithFields(logging.Fields{
-				"key": string(sourceValue.Key),
-				"ID":  string(sourceValue.Identity),
-			}).Trace("write key from source")
+				"key": string(baseValue.Key),
+				"ID":  string(baseValue.Identity),
+			}).Trace("write key from base")
 		}
-		if err := a.writer.WriteRecord(*sourceValue); err != nil {
-			return fmt.Errorf("write source record: %w", err)
+		if err := a.writer.WriteRecord(*baseValue); err != nil {
+			return fmt.Errorf("write base record: %w", err)
 		}
 	} else {
-		// select record from diffs, possibly (c==0) overwriting source
+		// select record from changes, possibly (c==0) overwriting base
 		switch {
-		case !diffValue.IsTombstone():
+		case !changeValue.IsTombstone():
 			if a.logger.IsTracing() {
 				a.logger.WithFields(logging.Fields{
-					"key":       string(diffValue.Key),
-					"ID":        string(diffValue.Identity),
-					"tombstone": diffValue.IsTombstone(),
-				}).Trace("write key from diffs")
+					"key":       string(changeValue.Key),
+					"ID":        string(changeValue.Identity),
+					"tombstone": changeValue.IsTombstone(),
+				}).Trace("write key from changes")
 			}
-			if err := a.writer.WriteRecord(*diffValue); err != nil {
+			if err := a.writer.WriteRecord(*changeValue); err != nil {
 				return fmt.Errorf("write added record: %w", err)
 			}
 			diffType := graveler.DiffTypeAdded
@@ -208,77 +208,77 @@ func (a *applier) applyBothKeys(sourceValue *graveler.ValueRecord, diffValue *gr
 		case c > 0:
 			// internal error but no data lost: deletion requested of a
 			// file that was not there.
-			a.logger.WithField("id", string(diffValue.Identity)).Warn("[I] unmatched delete")
+			a.logger.WithField("id", string(changeValue.Identity)).Warn("[I] unmatched delete")
 		default:
 			// Delete: simply don't copy to output.
 			a.incrementDiffSummary(graveler.DiffTypeRemoved)
 		}
 	}
 	if c >= 0 {
-		// used up this record from diffs
-		a.haveDiffs = a.diffs.Next()
+		// used up this record from changes
+		a.haveChanges = a.changes.Next()
 	}
 	if c <= 0 {
-		// used up this record from source
-		a.haveSource = a.source.Next()
+		// used up this record from base
+		a.haveBase = a.base.Next()
 	}
 	return nil
 }
 
-func (a *applier) applySourceRangeDiffKey(sourceRange *Range, diffValue *graveler.ValueRecord) error {
-	if bytes.Compare(sourceRange.MaxKey, diffValue.Key) < 0 {
-		// Source at start of range which we do not need to scan --
+func (a *applier) applyBaseRangeChangeKey(baseRange *Range, changeValue *graveler.ValueRecord) error {
+	if bytes.Compare(baseRange.MaxKey, changeValue.Key) < 0 {
+		// Base at start of range which we do not need to scan --
 		// write and skip that entire range.
 		if a.logger.IsTracing() {
 			a.logger.WithFields(logging.Fields{
-				"from": string(sourceRange.MinKey),
-				"to":   string(sourceRange.MaxKey),
-				"ID":   sourceRange.ID,
-			}).Trace("copy entire source range")
+				"from": string(baseRange.MinKey),
+				"to":   string(baseRange.MaxKey),
+				"ID":   baseRange.ID,
+			}).Trace("copy entire base range")
 		}
 
-		if err := a.writer.WriteRange(*sourceRange); err != nil {
-			return fmt.Errorf("copy source range %s: %w", sourceRange.ID, err)
+		if err := a.writer.WriteRange(*baseRange); err != nil {
+			return fmt.Errorf("copy base range %s: %w", baseRange.ID, err)
 		}
-		a.haveSource = a.source.NextRange()
+		a.haveBase = a.base.NextRange()
 	} else {
-		// Source is at start of range which we need to scan, enter it.
-		a.haveSource = a.source.Next()
+		// Base is at start of range which we need to scan, enter it.
+		a.haveBase = a.base.Next()
 	}
 	return nil
 }
 
-func (a *applier) applyDiffRangeSourceKey(diffRange *Range, sourceValue *graveler.ValueRecord) error {
-	if bytes.Compare(diffRange.MinKey, sourceValue.Key) > 0 {
-		// source is before diff range
-		if err := a.writer.WriteRecord(*sourceValue); err != nil {
-			return fmt.Errorf("write source record: %w", err)
+func (a *applier) applyChangeRangeBaseKey(changeRange *Range, baseValue *graveler.ValueRecord) error {
+	if bytes.Compare(changeRange.MinKey, baseValue.Key) > 0 {
+		// base is before change range
+		if err := a.writer.WriteRecord(*baseValue); err != nil {
+			return fmt.Errorf("write base record: %w", err)
 		}
-		a.haveSource = a.source.Next()
+		a.haveBase = a.base.Next()
 		return nil
 	}
-	if bytes.Compare(diffRange.MaxKey, sourceValue.Key) >= 0 {
-		// diffs is at start of range which we need to scan, enter it.
-		a.haveDiffs = a.diffs.Next()
+	if bytes.Compare(changeRange.MaxKey, baseValue.Key) >= 0 {
+		// changes is at start of range which we need to scan, enter it.
+		a.haveChanges = a.changes.Next()
 		return nil
 	}
-	// diffs at start of range which was completely added or removed --
+	// changes at start of range which was completely added or removed --
 	// write and skip that entire range.
-	if diffRange.Tombstone {
-		a.addIntoDiffSummary(graveler.DiffTypeRemoved, int(diffRange.Count))
+	if changeRange.Tombstone {
+		a.addIntoDiffSummary(graveler.DiffTypeRemoved, int(changeRange.Count))
 	} else {
 		if a.logger.IsTracing() {
 			a.logger.WithFields(logging.Fields{
-				"from": string(diffRange.MinKey),
-				"to":   string(diffRange.MaxKey),
-				"ID":   diffRange.ID,
-			}).Trace("copy entire diff range")
+				"from": string(changeRange.MinKey),
+				"to":   string(changeRange.MaxKey),
+				"ID":   changeRange.ID,
+			}).Trace("copy entire change range")
 		}
-		if err := a.writer.WriteRange(*diffRange); err != nil {
-			return fmt.Errorf("copy diff range %s: %w", diffRange.ID, err)
+		if err := a.writer.WriteRange(*changeRange); err != nil {
+			return fmt.Errorf("copy change range %s: %w", changeRange.ID, err)
 		}
-		a.addIntoDiffSummary(graveler.DiffTypeAdded, int(diffRange.Count))
-		a.haveDiffs = a.diffs.NextRange()
+		a.addIntoDiffSummary(graveler.DiffTypeAdded, int(changeRange.Count))
+		a.haveChanges = a.changes.NextRange()
 	}
 	return nil
 }
@@ -308,65 +308,65 @@ func CompareRanges(rangeA, rangeB *Range) RangeCompareSate {
 	}
 }
 
-func (a *applier) applyBothRanges(diffRange *Range, sourceRange *Range) error {
-	comp := CompareRanges(sourceRange, diffRange)
+func (a *applier) applyBothRanges(changeRange *Range, baseRange *Range) error {
+	comp := CompareRanges(baseRange, changeRange)
 	switch comp {
 	case RangeCompareSameID:
-		if !diffRange.Tombstone {
-			return fmt.Errorf("%w - range already exists in apply source", ErrInvalidState)
+		if !changeRange.Tombstone {
+			return fmt.Errorf("%w - range already exists in apply base", ErrInvalidState)
 		}
-		a.addIntoDiffSummary(graveler.DiffTypeRemoved, int(diffRange.Count))
-		a.haveSource = a.source.NextRange()
-		a.haveDiffs = a.diffs.NextRange()
+		a.addIntoDiffSummary(graveler.DiffTypeRemoved, int(changeRange.Count))
+		a.haveBase = a.base.NextRange()
+		a.haveChanges = a.changes.NextRange()
 
 	case RangeCompareSameBounds:
-		// insert diff move both
-		if err := a.writer.WriteRange(*diffRange); err != nil {
-			return fmt.Errorf("copy diff range %s: %w", diffRange.ID, err)
+		// insert change move both
+		if err := a.writer.WriteRange(*changeRange); err != nil {
+			return fmt.Errorf("copy change range %s: %w", changeRange.ID, err)
 		}
-		// When this optimization (inserting the whole range in case base and source are identical) is used. we can't be sure of the diff summary. e.g 4 files added 2 removed or 3 files changed.
+		// When this optimization (inserting the whole range in case base and base are identical) is used. we can't be sure of the diff summary. e.g 4 files added 2 removed or 3 files changed.
 		a.setMissingInfo()
-		a.haveDiffs = a.diffs.NextRange()
-		a.haveSource = a.source.NextRange()
+		a.haveChanges = a.changes.NextRange()
+		a.haveBase = a.base.NextRange()
 
 	case RangeCompareAfter:
-		if diffRange.Tombstone {
+		if changeRange.Tombstone {
 			// internal error but no data lost: deletion requested of a
 			// range that was not there.
 			a.logger.WithFields(logging.Fields{
-				"from": string(diffRange.MinKey),
-				"to":   string(diffRange.MaxKey),
-				"ID":   string(diffRange.ID),
+				"from": string(changeRange.MinKey),
+				"to":   string(changeRange.MaxKey),
+				"ID":   string(changeRange.ID),
 			}).Warn("[I] unmatched delete")
 		} else {
-			// insert diff
-			if err := a.writer.WriteRange(*diffRange); err != nil {
-				return fmt.Errorf("copy diff range %s: %w", diffRange.ID, err)
+			// insert change
+			if err := a.writer.WriteRange(*changeRange); err != nil {
+				return fmt.Errorf("copy change range %s: %w", changeRange.ID, err)
 			}
-			a.addIntoDiffSummary(graveler.DiffTypeAdded, int(diffRange.Count))
+			a.addIntoDiffSummary(graveler.DiffTypeAdded, int(changeRange.Count))
 		}
-		a.haveDiffs = a.diffs.NextRange()
+		a.haveChanges = a.changes.NextRange()
 	case RangeCompareBefore:
-		// insert source
-		if err := a.writer.WriteRange(*sourceRange); err != nil {
-			return fmt.Errorf("copy source range %s: %w", sourceRange.ID, err)
+		// insert base
+		if err := a.writer.WriteRange(*baseRange); err != nil {
+			return fmt.Errorf("copy base range %s: %w", baseRange.ID, err)
 		}
-		a.haveSource = a.source.NextRange()
+		a.haveBase = a.base.NextRange()
 	case RangeCompareOverlapping:
-		a.haveSource = a.source.Next()
-		a.haveDiffs = a.diffs.Next()
+		a.haveBase = a.base.Next()
+		a.haveChanges = a.changes.Next()
 		// enter both
 	}
 	return nil
 }
 
-func Apply(ctx context.Context, writer MetaRangeWriter, source Iterator, diffs Iterator, opts *ApplyOptions) (graveler.DiffSummary, error) {
+func Apply(ctx context.Context, writer MetaRangeWriter, base Iterator, changes Iterator, opts *ApplyOptions) (graveler.DiffSummary, error) {
 	a := applier{
 		ctx:     ctx,
 		logger:  logging.FromContext(ctx),
 		writer:  writer,
-		source:  source,
-		diffs:   diffs,
+		base:    base,
+		changes: changes,
 		opts:    opts,
 		summary: graveler.DiffSummary{Count: make(map[graveler.DiffType]int)},
 	}
