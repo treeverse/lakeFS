983bb81e1da4ecd56942514e3390f8a0d8eeaa0b
johnnyaug <yoni.augarten@treeverse.io>
2021-12-29 17:54:35 +0200

    fix cmd

--- test/spark/run-test.sh
+++ test/spark/run-test.sh
@@ -22,28 +22,28 @@ docker-compose exec -T lakefs lakectl repo create "lakefs://${REPOSITORY}" ${STO
 docker-compose exec -T lakefs lakectl fs upload -s /local/app/data-sets/sonnets.txt "lakefs://${REPOSITORY}/main/sonnets.txt"
 
 if [ -v USE_DIRECT_ACCESS ]; then # Direct access using thick Spark client.
-    SUBMIT_CMD="spark-submit --master spark://spark:7077
-    --jars /target/client.jar
-    -c spark.hadoop.fs.lakefs.impl=io.lakefs.LakeFSFileSystem
-     --conf=spark.driver.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true
-     --conf=spark.executor.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true
-     -c spark.hadoop.fs.lakefs.endpoint=${TESTER_LAKEFS_URL:-http://localhost:8000}/api/v1
-     -c spark.hadoop.fs.lakefs.access.key=${TESTER_ACCESS_KEY_ID}
-     -c spark.hadoop.fs.lakefs.secret.key=${TESTER_SECRET_ACCESS_KEY}
-     -c spark.hadoop.fs.s3a.access.key=${AWS_ACCESS_KEY_ID}
-     -c spark.hadoop.fs.s3a.secret.key=${AWS_SECRET_ACCESS_KEY}
-     -c spark.hadoop.fs.s3a.region=${AWS_REGION}
+    SUBMIT_CMD="spark-submit --master spark://spark:7077\
+    --jars /target/client.jar\
+    -c spark.hadoop.fs.lakefs.impl=io.lakefs.LakeFSFileSystem\
+     --conf=spark.driver.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true\
+     --conf=spark.executor.extraJavaOptions=-Dcom.amazonaws.services.s3.enableV4=true\
+     -c spark.hadoop.fs.lakefs.endpoint=${TESTER_LAKEFS_URL:-http://localhost:8000}/api/v1\
+     -c spark.hadoop.fs.lakefs.access.key=${TESTER_ACCESS_KEY_ID}\
+     -c spark.hadoop.fs.lakefs.secret.key=${TESTER_SECRET_ACCESS_KEY}\
+     -c spark.hadoop.fs.s3a.access.key=${AWS_ACCESS_KEY_ID}\
+     -c spark.hadoop.fs.s3a.secret.key=${AWS_SECRET_ACCESS_KEY}\
+     -c spark.hadoop.fs.s3a.region=${AWS_REGION}\
      --class Sonnets /local/app/target/${SONNET_JAR} ${input} ${output}"
     echo $SUBMIT_CMD
     docker-compose run -v $PWD/../../clients/hadoopfs/target/:/target/ \
       -T --no-deps --rm spark-submit
       sh -c ''
 else				# Access via S3 gateway using regular Spark client.
-    SUBMIT_CMD="spark-submit --master spark://spark:7077
-    -c spark.hadoop.fs.s3a.access.key=${TESTER_ACCESS_KEY_ID}
-    -c spark.hadoop.fs.s3a.secret.key=${TESTER_SECRET_ACCESS_KEY}
-    -c spark.hadoop.fs.s3a.endpoint=${TESTER_LAKEFS_URL:-http://localhost:8000}
-    -c spark.hadoop.fs.s3a.connection.ssl.enabled=false
+    SUBMIT_CMD="spark-submit --master spark://spark:7077\
+    -c spark.hadoop.fs.s3a.access.key=${TESTER_ACCESS_KEY_ID}\
+    -c spark.hadoop.fs.s3a.secret.key=${TESTER_SECRET_ACCESS_KEY}\
+    -c spark.hadoop.fs.s3a.endpoint=${TESTER_LAKEFS_URL:-http://localhost:8000}\
+    -c spark.hadoop.fs.s3a.connection.ssl.enabled=false\
     --class Sonnets /local/app/target/${SONNET_JAR} ${s3input} ${s3output}"
     echo $SUBMIT_CMD
     export s3input="s3a://${REPOSITORY}/main/sonnets.txt"
