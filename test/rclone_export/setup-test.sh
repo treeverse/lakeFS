7d2fd816592c0c84a19fc652d05e890a810e0c7c
Lynn Rozen <lynn.rozen@treeverse.io>
2022-02-01 11:28:20 +0200

    change lakefs setup to be a step in nessie workflow

---
+++ test/rclone_export/setup-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+#
+# setup-test.sh - set up lakeFS to run rclone export tests.  Run once per lakeFS.
+
+docker-compose exec -T lakefs /app/wait-for localhost:8000
+
+docker-compose exec -T lakefs sh -c 'lakefs setup --user-name tester --access-key-id AKIAIOSFODNN7EXAMPLE --secret-access-key wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+
+docker-compose exec -T lakefs lakectl repo create lakefs://example $STORAGE_NAMESPACE
