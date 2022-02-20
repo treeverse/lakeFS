3d13630885703b598fcb174ebc39800a7610333c
Idan Novogroder <idan.novogroder@treeverse.io>
2022-02-20 11:31:30 +0200

    Changed errors structs names

--- cmd/lakectl/cmd/doctor.go
+++ cmd/lakectl/cmd/doctor.go
@@ -11,43 +11,59 @@ import (
 	"github.com/treeverse/lakefs/pkg/api"
 )
 
-type ErrCredential struct {
+type Detailed interface {
+	GetDetails() string
+}
+
+
+type CredentialsError struct {
 	Message string
 	Details string
 }
 
-func (e *ErrCredential) Error() string {
-	return (e.Message + "\n" + e.Details + "\n")
+func (e *CredentialsError) Error() string { return e.Message }
+
+func (e *CredentialsError) GetDetails() string {
+	return e.Message + "\n" + e.Details
 }
 
-type ErrWrongEndpointURI struct {
+type WrongEndpointURIError struct {
 	Message string
 	Details string
 }
 
-func (e *ErrWrongEndpointURI) Error() string {
-	return (e.Message + "\n" + e.Details + "\n")
+func (e *WrongEndpointURIError) Error() string { return e.Message }
+
+func (e *WrongEndpointURIError) GetDetails() string {
+	return e.Message + "\n" + e.Details
 }
 
-type ErrUnknownConfig struct {
+type UnknownConfigError struct {
 	Message string
 	Details string
 }
 
-func (e *ErrUnknownConfig) Error() string {
-	return (e.Message + "\n" + e.Details + "\n")
+func (e *UnknownConfigError) Error() string { return e.Message }
+
+func (e *UnknownConfigError) GetDetails() string {
+	return e.Message + "\n" + e.Details
 }
 
-type SuccessMessage struct {
+type UserMessage struct {
 	Message string
 }
 
-var configErrorTemplate = `{{ .Message |red }}
-{{  .Details | red }}
+var detailedErrorTemplate = `{{ .Message |red }}
+{{  .Details  }}
 `
+var errorTemplate = `{{ .Message |red }}
+`
+
 var successMessageTemplate = `{{ .Message | green}}
 `
 
+var analayzingMessageTemplate = `{{ .Message }}
+`
 // doctorCmd represents the doctor command
 var doctorCmd = &cobra.Command{
 	Use:   "doctor",
@@ -55,25 +71,29 @@ var doctorCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		err := ListRepositoriesAndAnalyze(cmd.Context())
 		if err == nil {
-			Write(successMessageTemplate, &SuccessMessage{"Valid configuration"})
+			Write(successMessageTemplate, &UserMessage{"Valid configuration"})
 			return
 		}
 
-		Write(configErrorTemplate, err)
+		if detailedErr, ok := err.(Detailed); ok {
+			Write(detailedErrorTemplate, detailedErr)
+		} else {
+			Write(errorTemplate, err)
+		}
 
 		accessKeyID := cfg.Values.Credentials.AccessKeyID
 		if !IsValidAccessKeyID(accessKeyID) {
-			Fmt("access_key_id value looks suspicious: %v\n", accessKeyID)
+			Write(analayzingMessageTemplate, &UserMessage{"access_key_id value looks suspicious: "+accessKeyID})
 		}
 
 		secretAccessKey := cfg.Values.Credentials.SecretAccessKey
 		if !IsValidSecretAccessKey(secretAccessKey) {
-			Fmt("secret_access_key value looks suspicious...\n")
+			Write(analayzingMessageTemplate, &UserMessage{"secret_access_key value looks suspicious..."})
 		}
 
 		serverEndpoint := cfg.Values.Server.EndpointURL
 		if !strings.HasSuffix(serverEndpoint, api.BaseURL) {
-			Fmt("Suspicious URI format for server.endpoint_url: %v, doesn't end with: `%v`.\n", serverEndpoint, api.BaseURL)
+			Write(analayzingMessageTemplate, &UserMessage{"Suspicious URI format for server.endpoint_url: "+serverEndpoint+" doesn't end with: `"+api.BaseURL+"`."})
 		}
 	},
 }
@@ -84,10 +104,11 @@ func ListRepositoriesAndAnalyze(ctx context.Context) error {
 	msgOnErrWrongEndpointURI := "It looks like endpoint url is wrong."
 	msgOnErrCredential := "It seems like the `access_key_id` or `secret_access_key` you supplied are wrong."
 
+	// getClient might die on url.Parse error, so check it first.
 	serverEndpoint := cfg.Values.Server.EndpointURL
 	_, err := url.Parse(serverEndpoint)
 	if err != nil {
-		return &ErrWrongEndpointURI{msgOnErrWrongEndpointURI, err.Error()}
+		return &WrongEndpointURIError{msgOnErrWrongEndpointURI, err.Error()}
 	}
 	client := getClient()
 	resp, err := client.ListRepositoriesWithResponse(ctx, &api.ListRepositoriesParams{})
@@ -96,22 +117,22 @@ func ListRepositoriesAndAnalyze(ctx context.Context) error {
 	case err != nil:
 		urlErr := &url.Error{}
 		if errors.As(err, &urlErr) {
-			return &ErrWrongEndpointURI{msgOnErrWrongEndpointURI, err.Error()}
+			return &WrongEndpointURIError{msgOnErrWrongEndpointURI, err.Error()}
 		}
-		return &ErrUnknownConfig{msgOnErrUnknownConfig, err.Error()}
+		return &UnknownConfigError{msgOnErrUnknownConfig, err.Error()}
 	case resp == nil:
 		break
 	case resp.JSON200 != nil:
 		return nil
 	case resp.JSON401 != nil:
-		return &ErrCredential{msgOnErrCredential, resp.JSON401.Message}
+		return &CredentialsError{msgOnErrCredential, resp.JSON401.Message}
 	// In case we get the "not found" HTML page (the status is 200 and not 404 in this case)
 	case resp.HTTPResponse != nil && resp.HTTPResponse.StatusCode == 200:
-		return &ErrWrongEndpointURI{msgOnErrWrongEndpointURI, ""}
+		return &WrongEndpointURIError{msgOnErrWrongEndpointURI, ""}
 	case resp.JSONDefault != nil:
-		return &ErrUnknownConfig{msgOnErrUnknownConfig, resp.JSONDefault.Message}
+		return &UnknownConfigError{msgOnErrUnknownConfig, resp.JSONDefault.Message}
 	}
-	return &ErrUnknownConfig{msgOnErrUnknownConfig, "An unknown error accourd while trying to analyzing LakeCtl configuration."}
+	return &UnknownConfigError{msgOnErrUnknownConfig, "An unknown error accourd while trying to analyzing LakeCtl configuration."}
 }
 
 //nolint:gochecknoinits
