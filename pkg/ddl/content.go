78c4493ef1bd187a3193fb4e227c6dcaf37ad62f
Barak Amar <barak.amar@treeverse.io>
2022-03-15 01:12:48 +0200

    comment exposed global content variables

--- pkg/ddl/content.go
+++ pkg/ddl/content.go
@@ -2,5 +2,6 @@ package ddl
 
 import "embed"
 
+// Content embeds database migration sql files
 //go:embed *.sql
 var Content embed.FS
--- webui/content.go
+++ webui/content.go
@@ -2,5 +2,6 @@ package webui
 
 import "embed"
 
+// Content embeds web ui dist folder
 //go:embed dist
 var Content embed.FS
