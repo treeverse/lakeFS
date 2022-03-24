f3b4e4576e0deefecc7ef2bf47428821125f17c8
Itai Admi <itaiad200@gmail.com>
2022-03-24 16:15:48 +0200

    undo nessie test

--- nessie/repository_test.go
+++ nessie/repository_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"github.com/treeverse/lakefs/pkg/api"
 )
 
 func TestRepositoryBasicOps(t *testing.T) {
@@ -25,39 +24,10 @@ func TestRepositoryBasicOps(t *testing.T) {
 		require.Contains(t, listedRepos, repo, "repository missing in listing")
 	}
 
-	// get repo and try creating another repo with the same storage namespace
-	testDupStorageNamespace(t, repos[0])
-
 	// delete repositories
 	for _, repo := range repos {
 		resp, err := client.DeleteRepositoryWithResponse(ctx, repo)
 		require.NoErrorf(t, err, "failed to delete repository %s, storage %s", repo)
 		require.Equal(t, http.StatusNoContent, resp.StatusCode())
 	}
-}
-
-func testDupStorageNamespace(t *testing.T, repo string) {
-	t.Helper()
-	ctx := context.Background()
-	respGetRepo, err := client.GetRepositoryWithResponse(ctx, repo)
-	require.NoError(t, err, "failed to get repo")
-	require.Equal(t, http.StatusOK, respGetRepo.StatusCode())
-
-	respUpload, err := uploadContent(ctx, repo, respGetRepo.JSON200.DefaultBranch, "path/to/file", "bla-bla")
-	require.NoError(t, err, "failed to upload file")
-	require.Equal(t, http.StatusCreated, respUpload.StatusCode())
-
-	respCommit, err := client.CommitWithResponse(ctx, repo, respGetRepo.JSON200.DefaultBranch, api.CommitJSONRequestBody{
-		Message: "nessie:repo test",
-	})
-	require.NoError(t, err, "failed to commit")
-	require.Equal(t, http.StatusCreated, respCommit.StatusCode())
-
-	respCreate, err := client.CreateRepositoryWithResponse(ctx, &api.CreateRepositoryParams{}, api.CreateRepositoryJSONRequestBody{
-		Name:             generateUniqueRepositoryName(),
-		StorageNamespace: respGetRepo.JSON200.StorageNamespace,
-	})
-	require.NoError(t, err)
-	require.Equal(t, http.StatusBadRequest, respCreate.StatusCode())
-	require.Contains(t, respCreate.JSON400.Message, "non-empty storage namespace")
 }
