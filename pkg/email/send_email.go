14a5e9ab4b6766cf263f59d1e065cf2f21239b3a
Shimi Wieder <shimi_wieder@Shimis-MacBook-Pro.local>
2022-03-27 14:37:40 +0300

    init commit for email functionality
    updated makefile to install required dependencies
    
    updated to add gomail pgk
    
    fixed SMTP to uppercase
    
    Update docs/reference/configuration.md
    
    Co-authored-by: Or Tzabary <or@tzabary.com>
    
    Update docs/reference/configuration.md
    
    Co-authored-by: Or Tzabary <or@tzabary.com>
    
    Update docs/reference/configuration.md
    
    Co-authored-by: Or Tzabary <or@tzabary.com>
    
    Update docs/reference/configuration.md
    
    Co-authored-by: Or Tzabary <or@tzabary.com>
    
    Update docs/reference/configuration.md
    
    Co-authored-by: Or Tzabary <or@tzabary.com>
    
    removed static and removed alias
    
    updated go.mod
    
    remove statik
    
    made sendemail a function on the email struct
    
    update location of email struct
    
    updated makefile to install required dependencies
    
    changed auther
    
    fixed go.mod after conflict

---
+++ pkg/email/send_email.go
@@ -0,0 +1,28 @@
+package email
+
+import (
+	"gopkg.in/gomail.v2"
+)
+
+type Email struct {
+	SMTPHost string
+	Port     int
+	Username string
+	Password string
+	Sender   string
+}
+
+func (e Email) SendEmail(receivers []string, subject string, body string, attachmentFilePath ...string) error {
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", e.Sender)
+	msg.SetHeader("To", receivers...)
+	msg.SetHeader("Subject", subject)
+	msg.SetBody("text/html", body)
+	if len(attachmentFilePath) > 0 {
+		for _, f := range attachmentFilePath {
+			msg.Attach(f)
+		}
+	}
+	d := gomail.NewDialer(e.SMTPHost, e.Port, e.Username, e.Password)
+	return d.DialAndSend(msg)
+}
