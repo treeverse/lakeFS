c6f81a03f96e1b149a7bdc7b147671f99871eee2
shimi9276 <shimi.wieder@treeverse.io>
2022-03-29 17:56:20 +0300

    moved dialer to struct

--- pkg/email/send_email.go
+++ pkg/email/send_email.go
@@ -10,6 +10,7 @@ type Emailer struct {
 	Username string
 	Password string
 	Sender   string
+	Dialer   *gomail.Dialer
 }
 
 type EmailParams struct {
@@ -21,10 +22,18 @@ type EmailParams struct {
 }
 
 func NewEmailer(e EmailParams) Emailer {
-	return Emailer(e)
+	d := gomail.NewDialer(e.SMTPHost, e.Port, e.Username, e.Password)
+	return Emailer{
+		SMTPHost: e.SMTPHost,
+		Port:     e.Port,
+		Username: e.Username,
+		Password: e.Password,
+		Sender:   e.Sender,
+		Dialer:   d,
+	}
 }
 
-func (e Emailer) SendEmail(receivers []string, subject string, body string, attachmentFilePath ...string) error {
+func (e Emailer) SendEmail(receivers []string, subject string, body string, attachmentFilePath []string) error {
 	msg := gomail.NewMessage()
 	msg.SetHeader("From", e.Sender)
 	msg.SetHeader("To", receivers...)
@@ -33,6 +42,6 @@ func (e Emailer) SendEmail(receivers []string, subject string, body string, atta
 	for _, f := range attachmentFilePath {
 		msg.Attach(f)
 	}
-	d := gomail.NewDialer(e.SMTPHost, e.Port, e.Username, e.Password)
-	return d.DialAndSend(msg)
+	// d := gomail.NewDialer(e.SMTPHost, e.Port, e.Username, e.Password)
+	return e.Dialer.DialAndSend(msg)
 }
