4cb12a946f0072540ad85180d0bd4ba35f330c3e
shimi9276 <shimi.wieder@treeverse.io>
2022-03-30 08:41:47 +0300

    removed duplicate struct

--- pkg/email/send_email.go
+++ pkg/email/send_email.go
@@ -5,12 +5,8 @@ import (
 )
 
 type Emailer struct {
-	SMTPHost string
-	Port     int
-	Username string
-	Password string
-	Sender   string
-	Dialer   *gomail.Dialer
+	Params EmailParams
+	Dialer *gomail.Dialer
 }
 
 type EmailParams struct {
@@ -24,24 +20,19 @@ type EmailParams struct {
 func NewEmailer(e EmailParams) Emailer {
 	d := gomail.NewDialer(e.SMTPHost, e.Port, e.Username, e.Password)
 	return Emailer{
-		SMTPHost: e.SMTPHost,
-		Port:     e.Port,
-		Username: e.Username,
-		Password: e.Password,
-		Sender:   e.Sender,
-		Dialer:   d,
+		Params: e,
+		Dialer: d,
 	}
 }
 
 func (e Emailer) SendEmail(receivers []string, subject string, body string, attachmentFilePath []string) error {
 	msg := gomail.NewMessage()
-	msg.SetHeader("From", e.Sender)
+	msg.SetHeader("From", e.Params.Sender)
 	msg.SetHeader("To", receivers...)
 	msg.SetHeader("Subject", subject)
 	msg.SetBody("text/html", body)
 	for _, f := range attachmentFilePath {
 		msg.Attach(f)
 	}
-	// d := gomail.NewDialer(e.SMTPHost, e.Port, e.Username, e.Password)
 	return e.Dialer.DialAndSend(msg)
 }
