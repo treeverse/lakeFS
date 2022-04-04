3ef95848180ce489bdb11c9bca9784225975a34e
Jonathan Rosenberg <jonathan.rosenberg@treeverse.io>
2022-04-04 19:56:59 +0300

    add spaces to spark-submit line

--- test/spark/run-exporter-test.sh
+++ test/spark/run-exporter-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -aux
 
 # Run Export
-docker-compose run -v  ${CLIENT_JAR}:/client/client.jar -T --no-deps --rm spark-submit bash -c 'spark-submit --master spark://spark:7077 --conf spark.hadoop.lakefs.api.url=http:/docker.lakefs.io:8000/api/v1 --conf spark.hadoop.lakefs.api.access_key=${TESTER_ACCESS_KEY_ID} --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false --conf spark.hadoop.lakefs.api.secret_key=${TESTER_SECRET_ACCESS_KEY} --class io.treeverse.clients.Main /client/client.jar ${REPOSITORY} ${EXPORT_LOCATION} --branch=main'
+docker-compose run -v  ${CLIENT_JAR}:/client/client.jar -T --no-deps --rm spark-submit bash -c 'spark-submit  --master spark://spark:7077 --conf spark.hadoop.lakefs.api.url=http:/docker.lakefs.io:8000/api/v1   --conf spark.hadoop.lakefs.api.access_key=${TESTER_ACCESS_KEY_ID}   --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false   --conf spark.hadoop.lakefs.api.secret_key=${TESTER_SECRET_ACCESS_KEY}   --class io.treeverse.clients.Main  /client/client.jar ${REPOSITORY} ${EXPORT_LOCATION}   --branch=main'
 
 # Validate export
 lakectl_out=$(mktemp)
