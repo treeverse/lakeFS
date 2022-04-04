2569d5fc112e1c303adfbb46af1784904187f16e
Barak Amar <barak.amar@treeverse.io>
2022-04-04 17:11:48 +0300

    Rename system tests from nessie to esti (#3147)
    * Rename nessie to esti - system tests
    
    * remove bootstrap shell script
    
    * fix lakectl golden
    
    * fix lakectl output with esti
    
    * lakectl util helpers
    
    * fix extra space
    
    * fix another space
    
    * missing newline
    
    * esti author
    
    * add missing newlines
    
    * remove new line
    
    * wip
    
    * fix azure test step
    
    * use azure access 2 for esti
    
    * remove esti reference from commit messages

---
+++ esti/repository_test.go
@@ -0,0 +1,33 @@
+package esti
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRepositoryBasicOps(t *testing.T) {
+	ctx := context.Background()
+	const numOfRepos = 5
+
+	// create repositories
+	repos := make([]string, numOfRepos)
+	for i := range repos {
+		repos[i] = createRepositoryUnique(ctx, t)
+	}
+
+	// list repositories - make sure we have the ones we created
+	listedRepos := listRepositoriesIDs(t, ctx)
+	for _, repo := range repos {
+		require.Contains(t, listedRepos, repo, "repository missing in listing")
+	}
+
+	// delete repositories
+	for _, repo := range repos {
+		resp, err := client.DeleteRepositoryWithResponse(ctx, repo)
+		require.NoErrorf(t, err, "failed to delete repository %s, storage %s", repo)
+		require.Equal(t, http.StatusNoContent, resp.StatusCode())
+	}
+}
