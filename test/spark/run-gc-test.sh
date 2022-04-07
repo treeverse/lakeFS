64b51453b511dba38056f9388b3ddff6680e40b7
Jonathan Rosenberg <jonathan.rosenberg@treeverse.io>
2022-04-07 14:43:11 +0300

    add test id to uploaded file

--- test/spark/run-gc-test.sh
+++ test/spark/run-gc-test.sh
@@ -29,8 +29,9 @@ clean_repo() {
 
 initialize_env() {
   local repo=$1
+  local test_id=$2
   run_lakectl branch create "lakefs://${repo}/a" -s "lakefs://${repo}/main"
-  run_lakectl fs upload "lakefs://${repo}/a/file" -s /local/gc-tests/sample_file
+  run_lakectl fs upload "lakefs://${repo}/a/file${test_id}" -s /local/gc-tests/sample_file
   run_lakectl commit "lakefs://${repo}/a" -m "uploaded file" --epoch-time-seconds 0
   run_lakectl branch create "lakefs://${repo}/b" -s "lakefs://${repo}/a"
 }
@@ -38,13 +39,14 @@ initialize_env() {
 delete_and_commit() {
   local test_case=$1
   local repo=$2
+  local test_id=$3
   for branch_props in $(echo ${test_case} | jq -r '.branches [] | @base64'); do
     branch_props=$(_jq ${branch_props})
     local branch_name=$(echo ${branch_props} | jq -r '.branch_name')
     local days_ago=$(echo ${branch_props} | jq -r '.delete_commit_days_ago')
     if [[ ${days_ago} -gt -1 ]]
     then
-      run_lakectl fs rm "lakefs://${repo}/${branch_name}/file"
+      run_lakectl fs rm "lakefs://${repo}/${branch_name}/file${test_id}"
       epoch_commit_date_in_seconds=$(( ${current_epoch_in_seconds} - ${day_in_seconds} * ${days_ago} ))
       run_lakectl commit "lakefs://${repo}/${branch_name}" --allow-empty-message --epoch-time-seconds ${epoch_commit_date_in_seconds}
     else   # This means that the branch should be deleted
@@ -63,11 +65,12 @@ validate_gc_job() {
   local test_case=$1
   local repo=$2
   local existing_ref=$3
+  local test_id=$4
   local file_should_be_deleted=$(echo ${test_case} | jq -r '.file_deleted')
-  if run_lakectl fs cat lakefs://${repo}/${existing_ref}/file > /dev/null 2>&1 && ${file_should_be_deleted} ; then
+  if run_lakectl fs cat "lakefs://${repo}/${existing_ref}/file${test_id}" > /dev/null 2>&1 && ${file_should_be_deleted} ; then
     echo "Expected the file to be removed by the garbage collector but it has remained in the repository"
     return 1
-  elif ! run_lakectl fs cat lakefs://${repo}/${existing_ref}/file > /dev/null && ! ${file_should_be_deleted} ; then
+  elif ! run_lakectl fs cat "lakefs://${repo}/${existing_ref}/file${test_id}" > /dev/null && ! ${file_should_be_deleted} ; then
     echo "Expected the file to remain in the repository but it was removed by the garbage collector"
     return 1
   fi
@@ -109,18 +112,20 @@ run_lakectl fs upload "lakefs://${REPOSITORY}/main/not_deleted_file2" -s /local/
 run_lakectl fs upload "lakefs://${REPOSITORY}/main/not_deleted_file3" -s /local/gc-tests/sample_file
 run_lakectl commit "lakefs://${REPOSITORY}/main" -m "add three files not to be deleted" --epoch-time-seconds 0
 
+test_id=0
 failed_tests=()
 for test_case in $(jq -r '.[] | @base64' gc-tests/test_scenarios.json); do
   test_case=$(_jq ${test_case})
+  ((test_id++))
   test_description=$(echo "${test_case}" | jq -r '.description')
   echo "Test: ${test_description}"
-  initialize_env ${REPOSITORY}
+  initialize_env ${REPOSITORY} ${test_id}
   file_existing_ref=$(last_commit_ref ${REPOSITORY} a)
   echo "${test_case}" | jq --raw-output '.policy' > policy.json
   run_lakectl gc set-config lakefs://${REPOSITORY} -f /local/policy.json
-  delete_and_commit "${test_case}" ${REPOSITORY}
+  delete_and_commit "${test_case}" ${REPOSITORY} ${test_id}
   run_gc ${REPOSITORY}
-  if ! validate_gc_job "${test_case}" ${REPOSITORY} ${file_existing_ref}; then
+  if ! validate_gc_job "${test_case}" ${REPOSITORY} ${file_existing_ref} ${test_id}; then
     failed_tests+=("${test_description}")
   else
     echo "Test: ${test_description} - SUCCEEDED"
