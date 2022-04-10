95b900d2dbb5290c63509a22be6a2d98c573dd62
shimi9276 <shimi.wieder@treeverse.io>
2022-04-10 19:30:30 +0300

    renamed token

--- pkg/email/template/template.go
+++ pkg/email/template/template.go
@@ -10,13 +10,9 @@ const (
 	ResetEmailTemplate = `Hello, <br>
 	A request has been received to change the password to your account,  <br>
 	Click on this link to reset your password <br>
-
-	{{.Host}}{{.ResetPasswordPath}}{{.Token}} <br>
-
+	{{.Host}}{{.ResetPasswordPath}}{{.Tkn}} <br>
 	If you did not initiate this request you can please diregard this email. <br>
-
 	Thanks  <br>
-
 	The LakeFS team <br>
 	`
 	ResetPasswordEmailSubject = "Reset Password Request for your Lakefs account"
@@ -25,14 +21,14 @@ const (
 type Link struct {
 	Host              string
 	ResetPasswordPath string
-	Token             string
+	Tkn               string
 }
 
 func BuildResetPasswordEmailTemplate(tpl string, host string, token string) (string, error) {
 	templ := template.New("resetPasswordtemplate")
 	t := template.Must(templ.Parse(tpl))
 	builder := &strings.Builder{}
-	l := Link{Host: host, ResetPasswordPath: ResetPasswordPath, Token: token}
+	l := Link{Host: host, ResetPasswordPath: ResetPasswordPath, Tkn: token}
 	err := t.Execute(builder, l)
 	return builder.String(), err
 }
