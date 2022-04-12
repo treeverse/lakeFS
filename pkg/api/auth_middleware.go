f1b0fbeac9a0b9366737dbb88caea1a120d3c85a
Barak Amar <barak.amar@treeverse.io>
2022-04-12 08:21:16 +0300

    auth: generate jwt token ID (#3179)
    * populate jwt token id using uuid
    
    * log token id on error

--- pkg/api/auth_middleware.go
+++ pkg/api/auth_middleware.go
@@ -115,11 +115,7 @@ func verifyToken(authService auth.Service, tokenString string) (*jwt.StandardCla
 		}
 		return authService.SecretStore().SharedSecret(), nil
 	})
-	if err != nil {
-		return nil, ErrAuthenticatingRequest
-	}
-	claims, ok := token.Claims.(*jwt.StandardClaims)
-	if !ok || !token.Valid {
+	if err != nil || !token.Valid {
 		return nil, ErrAuthenticatingRequest
 	}
 	return claims, nil
@@ -131,18 +127,23 @@ func userByToken(ctx context.Context, logger logging.Logger, authService auth.Se
 	if err != nil || !claims.VerifyAudience(LoginAudience, false) {
 		return nil, ErrAuthenticatingRequest
 	}
+
 	const base = 10
 	const bitSize = 32
 	id, err := strconv.ParseInt(claims.Subject, base, bitSize)
 	if err != nil {
-		logger.WithField("subject", claims.Subject).Info("could not parse user ID on token")
+		logger.WithFields(logging.Fields{
+			"subject":  claims.Subject,
+			"token_id": claims.Id,
+		}).Info("could not parse user ID on token")
 		return nil, ErrAuthenticatingRequest
 	}
 	userData, err := authService.GetUserByID(ctx, int(id))
 	if err != nil {
 		logger.WithFields(logging.Fields{
-			"user_id": id,
-			"subject": claims.Subject,
+			"token_id": claims.Id,
+			"user_id":  id,
+			"subject":  claims.Subject,
 		}).Debug("could not find user id by credentials")
 		return nil, ErrAuthenticatingRequest
 	}
