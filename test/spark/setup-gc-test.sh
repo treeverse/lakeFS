cd3d26497e4755dbb082a6a65f4ece7efe2b8843
johnnyaug <yoni.augarten@treeverse.io>
2022-05-03 13:08:58 -0400

    Esti: performance improvment

--- test/spark/setup-gc-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-#
-# setup-gc-test.sh - set up lakeFS to run Spark GC tests.
-
-docker-compose exec -T lakefs /app/wait-for localhost:8000
-
-
-docker-compose exec -T lakefs lakectl repo create lakefs://${REPOSITORY//./-} ${STORAGE_NAMESPACE}
