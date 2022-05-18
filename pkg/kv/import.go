004c62e66d898d076fdeb3e73a03882dc3a3257a
N-o-Z <ozery.nir@gmail.com>
2022-05-18 18:13:25 +0300

    KV Store: Migration and multipart (#3340)
    * KV Store: Migration and multipart
    
    * Update pkg/kv/export/export.go
    
    Co-authored-by: itai-david <90712874+itai-david@users.noreply.github.com>
    
    * Update pkg/kv/export/export.go
    
    Co-authored-by: itai-david <90712874+itai-david@users.noreply.github.com>
    
    * Update pkg/db/migration.go
    
    Co-authored-by: itai-david <90712874+itai-david@users.noreply.github.com>
    
    * Fixes 1
    
    * Fixes 2
    
    * Fixes 3
    
    * Use real tag for lakeFS migration testing
    
    * Update pkg/kv/store_message_test.go
    
    Co-authored-by: Barak Amar <barak.amar@treeverse.io>
    
    * Fixes 4
    
    * Apply suggestions from code review
    
    Co-authored-by: Barak Amar <barak.amar@treeverse.io>
    
    * Final fix
    
    Co-authored-by: itai-david <90712874+itai-david@users.noreply.github.com>
    Co-authored-by: Itai Admi <itaiad200@gmail.com>
    Co-authored-by: Barak Amar <barak.amar@treeverse.io>

---
+++ pkg/kv/import.go
@@ -0,0 +1,70 @@
+package kv
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/treeverse/lakefs/pkg/logging"
+)
+
+var ErrInvalidFormat = errors.New("invalid format")
+
+// Header contains metadata information for import / export file
+type Header struct {
+	LakeFSVersion   string
+	PackageName     string
+	DBSchemaVersion int
+	CreatedAt       time.Time
+}
+
+func Import(ctx context.Context, reader io.Reader, store Store) error {
+	jd := json.NewDecoder(reader)
+	// Read header
+	var header Header
+	if err := jd.Decode(&header); err != nil {
+		if errors.Is(err, io.EOF) {
+			return fmt.Errorf("empty file: %w", ErrInvalidFormat)
+		} else {
+			return fmt.Errorf("decoding header: %w", err)
+		}
+	}
+	// Decode does not return error onm missing data / incompatible format
+	if header == (Header{}) {
+		return fmt.Errorf("bad header format: %w", ErrInvalidFormat)
+	}
+
+	// TODO(niro): Add validation the header in the future
+	logging.Default().WithFields(logging.Fields{
+		"package_name":      header.PackageName,
+		"lakefs_version":    header.LakeFSVersion,
+		"db_schema_version": header.DBSchemaVersion,
+		"created_at":        header.CreatedAt,
+	}).Info("Processing file")
+
+	var entry Entry
+	for {
+		err := jd.Decode(&entry)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		// Decode does not return error onm missing data / incompatible format
+		if err != nil {
+			return fmt.Errorf("decoding entry: %w", err)
+		}
+		if len(entry.Key) == 0 {
+			return fmt.Errorf("bad entry key: %w", ErrInvalidFormat)
+		}
+		if entry.Value == nil {
+			return fmt.Errorf("bad entry value: %w", ErrInvalidFormat)
+		}
+		err = store.SetIf(ctx, entry.Key, entry.Value, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
