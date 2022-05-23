f8ae56abfaf570c69f3702597c0dc6a16e41ca7f
Barak Amar <barak.amar@treeverse.io>
2022-05-23 21:29:45 +0300

    KV Store: Message Store Scan (#3412)

--- pkg/kv/store_message.go
+++ pkg/kv/store_message.go
@@ -64,8 +64,66 @@ func (s *StoreMessage) Delete(ctx context.Context, path string) error {
 	return err
 }
 
-// TODO(niro): implement Scan when required
+// Scan returns a prefix iterator which returns entries in deserialized format. Scan in store message implementation is msg specific
+// Therefore, the given prefix should bound the range of the data only to that which can be deserialized by the given proto message type
+func (s *StoreMessage) Scan(ctx context.Context, msg protoreflect.Message, prefix string) (*MessageIterator, error) {
+	return NewMessageIterator(ctx, s.Store, msg, prefix)
+}
 
 func (s *StoreMessage) Close() {
 	s.Store.Close()
+}
+
+type MessageIterator struct {
+	itr EntriesIterator
+	msg protoreflect.Message
+	err error
+}
+
+type MessageEntry struct {
+	Key   string
+	Value protoreflect.ProtoMessage
+}
+
+func NewMessageIterator(ctx context.Context, store Store, msg protoreflect.Message, prefix string) (*MessageIterator, error) {
+	itr, err := ScanPrefix(ctx, store, []byte(prefix))
+	if err != nil {
+		return nil, fmt.Errorf("create prefix iterator: %w", err)
+	}
+	return &MessageIterator{itr: itr, msg: msg}, nil
+}
+
+func (m *MessageIterator) Next() bool {
+	if m.err != nil {
+		return false
+	}
+	return m.itr.Next()
+}
+
+func (m *MessageIterator) Entry() *MessageEntry {
+	entry := m.itr.Entry()
+	if entry == nil {
+		return nil
+	}
+	msg := m.msg.New().Interface()
+	err := proto.Unmarshal(entry.Value, msg)
+	if err != nil {
+		m.err = fmt.Errorf("unmarshal proto data for key %s: %w", string(entry.Key), err)
+		return nil
+	}
+	return &MessageEntry{
+		Key:   string(entry.Key),
+		Value: msg,
+	}
+}
+
+func (m *MessageIterator) Err() error {
+	if m.err != nil {
+		return m.err
+	}
+	return m.itr.Err()
+}
+
+func (m *MessageIterator) Close() {
+	m.itr.Close()
 }
