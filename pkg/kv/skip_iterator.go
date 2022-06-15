c9ac540513e68839cf649cd1e0767339ae890524
Itai Admi <itaiad200@gmail.com>
2022-06-15 15:15:38 -0400

    rename

--- pkg/kv/skip_iterator.go
+++ pkg/kv/skip_iterator.go
@@ -2,13 +2,13 @@ package kv
 
 import "bytes"
 
-type SkipIterator struct {
+type SkipFirstIterator struct {
 	it         EntriesIterator
 	after      []byte
 	nextCalled bool
 }
 
-func (si *SkipIterator) Next() bool {
+func (si *SkipFirstIterator) Next() bool {
 	if !si.nextCalled {
 		si.nextCalled = true
 		if !si.it.Next() {
@@ -21,18 +21,18 @@ func (si *SkipIterator) Next() bool {
 	return si.it.Next()
 }
 
-func (si *SkipIterator) Entry() *Entry {
+func (si *SkipFirstIterator) Entry() *Entry {
 	return si.it.Entry()
 }
 
-func (si *SkipIterator) Err() error {
+func (si *SkipFirstIterator) Err() error {
 	return si.it.Err()
 }
 
-func (si *SkipIterator) Close() {
+func (si *SkipFirstIterator) Close() {
 	si.it.Close()
 }
 
 func NewSkipIterator(it EntriesIterator, after []byte) EntriesIterator {
-	return &SkipIterator{it: it, after: after}
+	return &SkipFirstIterator{it: it, after: after}
 }
