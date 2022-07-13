84017c082cd615154e5d331e233085222ad70092
Lynn Rozen <lynn.rozen@treeverse.io>
2022-07-13 14:20:18 +0300

    added check of storage type and schema convertion in case of storage type azure to abfs

--- clients/spark/core/src/main/scala/io/treeverse/clients/ApiClient.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/ApiClient.scala
@@ -3,6 +3,7 @@ package io.treeverse.clients
 import com.google.common.cache.CacheBuilder
 import io.lakefs.clients.api
 import io.lakefs.clients.api.RetentionApi
+import io.lakefs.clients.api.ConfigApi
 import io.lakefs.clients.api.model.{
   GarbageCollectionPrepareRequest,
   GarbageCollectionPrepareResponse
@@ -11,24 +12,41 @@ import io.lakefs.clients.api.model.{
 import java.net.URI
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.Callable
+import java.util.regex.Pattern
 
 private object ApiClient {
+  val StorageTypeS3 = "s3"
+  val StorageTypeAzure = "azure"
 
   /** Translate the protocol of uri from "standard"-ish "s3" to "s3a", to
    *  trigger processing by S3AFileSystem.
    */
-  def translateS3(uri: URI): URI =
-    if (uri.getScheme == "s3")
-      new URI("s3a",
-              uri.getUserInfo,
-              uri.getHost,
-              uri.getPort,
-              uri.getPath,
-              uri.getQuery,
-              uri.getFragment
-             )
-    else
+  def translateURI(uri: URI, storageType: String): URI = {
+    if ((storageType == StorageTypeS3) && (uri.getScheme == "s3")) {
+      return new URI("s3a",
+                     uri.getUserInfo,
+                     uri.getHost,
+                     uri.getPort,
+                     uri.getPath,
+                     uri.getQuery,
+                     uri.getFragment
+                    )
+    } else if (storageType == StorageTypeAzure) {
+
+      /** regex to catch url of type: https://StorageAccountName.blob.core.windows.net/Container[/BlobName] and convert it to abfs url */
+      val regex = "https?://([^.]+)(\\.[^/]+)(?:/([^/]+)(.+))?"
+      val pattern = Pattern.compile(regex)
+      val matcher = pattern.matcher(uri.toString())
+      if (matcher.find()) {
+        return new URI(
+          s"abfs://${matcher.group(3)}@${matcher.group(1)}.dfs.core.windows.net${matcher.group(4)}"
+        )
+      }
       uri
+    } else {
+      uri
+    }
+  }
 }
 
 class ApiClient(apiUrl: String, accessKey: String, secretKey: String) {
@@ -41,6 +59,7 @@ class ApiClient(apiUrl: String, accessKey: String, secretKey: String) {
   private val metadataApi = new api.MetadataApi(client)
   private val branchesApi = new api.BranchesApi(client)
   private val retentionApi = new RetentionApi(client)
+  private val configApi = new ConfigApi(client)
 
   private val storageNamespaceCache =
     CacheBuilder.newBuilder().expireAfterWrite(2, TimeUnit.MINUTES).build[String, String]()
@@ -55,7 +74,10 @@ class ApiClient(apiUrl: String, accessKey: String, secretKey: String) {
       new CallableFn(() => {
         val repo = repositoriesApi.getRepository(repoName)
 
-        ApiClient.translateS3(URI.create(repo.getStorageNamespace)).normalize().toString
+        ApiClient
+          .translateURI(URI.create(repo.getStorageNamespace), getBlockstoreType())
+          .normalize()
+          .toString
       })
     )
   }
@@ -73,6 +95,11 @@ class ApiClient(apiUrl: String, accessKey: String, secretKey: String) {
   def getGarbageCollectionRules(repoName: String): String = {
     val gcRules = retentionApi.getGarbageCollectionRules(repoName)
     gcRules.toString()
+  }
+
+  def getBlockstoreType(): String = {
+    val storageConfig = configApi.getStorageConfig()
+    storageConfig.getBlockstoreType()
   }
 
   /** Query lakeFS for a URL to the metarange of commitID of repoName and
