5c285c3a589959e38ff52ba37217eaa2c1ea255c
Barak Amar <barak.amar@treeverse.io>
2022-07-17 16:28:11 +0300

    TTV Wizard: Import data step (#3650)
    * extract the import functionality and main components to a separate file
    
    * remove renamed file (importUtils.js)
    
    * add an "import data" step
    
    * insert the "import data" step to the wizard
    
    * use the import_data module with the regular import
    
    * format files
    
    * remove incorrect default assignment to `commitMsgRef`
    
    * import RepositoryPage properly
    
    * Revert "import RepositoryPage properly"
    
    This reverts commit c7f1a3a779b4bade52262b3b091aa792c432c028.
    
    * PR fixes
    
    * replace `=== false` with !. add Failed import phase
    
    * format code, change error text for non valid sources, change source validity to be a state
    
    * change the usage of ImportFormat to support the validity changes.
    
    * present -> content
    
    * fix URL source validation. show error within the form.
    
    * make the import button disabled only if it was explicitly disabled or if the import phase is neither NotStarted nor Failed
    
    * disable skip button in the wizard, send the correct state data

---
+++ webui/src/pages/repositories/wizard/import_data_wizard_step.jsx
@@ -0,0 +1,86 @@
+import React, {useRef, useState} from "react";
+import {
+    ExecuteImportButton,
+    ImportDone,
+    ImportForm,
+    ImportPhase,
+    ImportProgress,
+    runImport
+} from "../services/import_data";
+import {Error, Loading} from "../../../lib/components/controls";
+import {useAPI} from "../../../lib/hooks/api";
+import {config} from "../../../lib/api";
+
+const ImportDataStep = ({repoId, branchName, onComplete, prependPath = ''}) => {
+    const [importPhase, setImportPhase] = useState(ImportPhase.NotStarted);
+    const [numberOfImportedObjects, setNumberOfImportedObjects] = useState(0);
+    const [isImportEnabled, setIsImportEnabled] = useState(false);
+    const [importError, setImportError] = useState(null);
+    const sourceRef = useRef(null);
+    const commitMsgRef = useRef(null);
+    const {response, error, loading} = useAPI(() => config.getStorageConfig());
+
+    if (loading) {
+        return <Loading/>
+    }
+
+    const showError = importError ? importError : error;
+
+    const doImport = async () => {
+        setImportPhase(ImportPhase.InProgress);
+        const updateStateFromImport = ({importPhase, numObj}) => {
+            setImportPhase(importPhase);
+            setNumberOfImportedObjects(numObj)
+        }
+        try {
+            await runImport(
+                updateStateFromImport,
+                prependPath,
+                commitMsgRef.current.value,
+                sourceRef.current.value,
+                branchName,
+                repoId
+            );
+            onComplete();
+        } catch (error) {
+            setImportError(error);
+            setImportPhase(ImportPhase.Failed);
+            setIsImportEnabled(false);
+        }
+    }
+
+    return (
+        <>
+            <h2 className={"wizard-step-header"}>Import Data</h2>
+            {
+                (importPhase === ImportPhase.NotStarted || importPhase === ImportPhase.Failed) &&
+                <ImportForm
+                    config={response}
+                    pathStyle={{'minWidth': '25%'}}
+                    sourceRef={sourceRef}
+                    updateSrcValidity={(isValid) => setIsImportEnabled(isValid)}
+                    repoId={repoId}
+                    importBranch={branchName}
+                    commitMsgRef={commitMsgRef}
+                    shouldAddPath={false}/>
+            }
+            {
+                importPhase === ImportPhase.InProgress &&
+                <ImportProgress numObjects={numberOfImportedObjects}/>
+            }
+            {
+                importPhase === ImportPhase.Completed &&
+                <ImportDone currBranch={branchName} importBranch={branchName} numObjects={numberOfImportedObjects}/>
+            }
+            {
+                showError && <Error error={showError}/>
+            }
+            {
+                <ExecuteImportButton importPhase={importPhase} importFunc={doImport} isEnabled={isImportEnabled}/>
+            }
+
+        </>
+    );
+}
+
+export default ImportDataStep
