a78164023b1d09f03c20b54578180692ae941c05
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2022-07-18 19:10:48 +0300

    initial spark config step

---
+++ webui/src/lib/components/tab_utils.jsx
@@ -0,0 +1,23 @@
+import {Box, Typography} from "@mui/material";
+import React from "react";
+
+export function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
