b34cf464b7a772c36b02c1bbd6222aef66bd34f8
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2022-07-21 11:45:48 +0300

    add TabsWrapper and CodeTabPanel components

--- webui/src/lib/components/nav.jsx
+++ webui/src/lib/components/nav.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
+import {Link as RouterLink} from "react-router-dom";
 
 import Nav from "react-bootstrap/Nav";
 
 import {buildURL} from "../hooks/router";
+import {Box, Tabs} from "@mui/material";
+import {ClipboardButton} from "./controls";
+import {FaRegCopy} from "react-icons/fa";
 
 
 function isModifiedEvent(event) {
@@ -11,8 +14,8 @@ function isModifiedEvent(event) {
 }
 
 const wrapComponent = (component) => {
-    const linkWrapper = React.forwardRef(({ navigate, onClick, ...rest}, forwardedRef) => {
-        const { target } = rest;
+    const linkWrapper = React.forwardRef(({navigate, onClick, ...rest}, forwardedRef) => {
+        const {target} = rest;
         const props = {
             ...rest,
             ref: forwardedRef,
@@ -53,7 +56,7 @@ export const Link = (props) => {
     return React.createElement(RouterLink, linkProps, props.children);
 }
 
-export const NavItem = ({ href, active, children }) => {
+export const NavItem = ({href, active, children}) => {
     return (
         <Nav.Item>
             <Link href={href} component={Nav.Link} active={active}>
@@ -62,3 +65,51 @@ export const NavItem = ({ href, active, children }) => {
         </Nav.Item>
     );
 };
+
+export function CodeTabPanel({children, value, index, ...other}) {
+    return (
+        <div
+            role="code-tabpanel"
+            hidden={value !== index}
+            id={`code-tabpanel-${index}`}
+            aria-labelledby={`code-tabpanel-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', py: 1}}
+                     className={'code-container text-secondary'}>
+                    <Box sx={{ml: 2}}>
+                        {children}
+                    </Box>
+                    <Box sx={{mr: 2}}>
+                        <ClipboardButton icon={<FaRegCopy size={16}/>} variant="link" tooltip="Copy to clipboard"
+                                         text={children} size={'sm'}/>
+                    </Box>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+export const TabsWrapper = ({
+                                isCentered,
+                                children,
+                                defaultTabIndex,
+                                handleTabChange,
+                                ariaLabel = '',
+                                textColor = 'primary',
+                                indicatorColor = 'primary'
+                            }) => {
+    return (
+        <Tabs
+            value={defaultTabIndex}
+            onChange={handleTabChange}
+            textColor={textColor}
+            indicatorColor={indicatorColor}
+            aria-label={ariaLabel}
+            centered={isCentered}
+        >
+            {children}
+        </Tabs>
+    );
+}
