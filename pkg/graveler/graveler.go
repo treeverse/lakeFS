d6c0688e83e92c55dec742508e74413175dd24ac
eden-ohana <edenohana20@gmail.com>
2022-07-24 10:00:24 +0300

    graveler migrate + tests

--- pkg/graveler/graveler.go
+++ pkg/graveler/graveler.go
@@ -303,7 +303,7 @@ type Branch struct {
 
 // BranchRecord holds BranchID with the associated Branch data
 type BranchRecord struct {
-	BranchID BranchID
+	BranchID BranchID `db:"id"`
 	*Branch
 }
 
@@ -315,7 +315,7 @@ type ValueUpdateFunc func(*Value) (*Value, error)
 
 // TagRecord holds TagID with the associated Tag data
 type TagRecord struct {
-	TagID    TagID
+	TagID    TagID `db:"id"`
 	CommitID CommitID
 }
 
