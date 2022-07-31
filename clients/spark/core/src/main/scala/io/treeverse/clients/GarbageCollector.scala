3b418ab2fa518d157ca6c025b634f87f20cce008
Barak Amar <barak.amar@treeverse.io>
2022-07-31 15:18:39 +0300

    exit GC in case that no GC rules are configured for a repo (#3779)
    * exit GC in case that no GC rules are configured for a repo
    
    * scalafix errors
    
    * fix cr comments

--- clients/spark/core/src/main/scala/io/treeverse/clients/GarbageCollector.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/GarbageCollector.scala
@@ -334,7 +334,16 @@ object GarbageCollector {
     // needed FileSystems.
     val hcValues = spark.sparkContext.broadcast(getHadoopConfigurationValues(hc, "fs."))
 
-    val gcRules = apiClient.getGarbageCollectionRules(repo)
+    var gcRules: String = ""
+    try {
+      gcRules = apiClient.getGarbageCollectionRules(repo)
+    } catch {
+      case e: Throwable =>
+        e.printStackTrace()
+        println("No GC rules found for repository: " + repo)
+        // Exiting with a failure status code because users should not really run gc on repos without GC rules.
+        System.exit(2)
+    }
 
     val res = apiClient.prepareGarbageCollectionCommits(repo, previousRunID)
     val runID = res.getRunId
