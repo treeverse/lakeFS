f8785aafa6690514a78863a8d2d2fa3fecd9e1a2
Idan Novogroder <43949240+idanovo@users.noreply.github.com>
2022-08-28 18:42:32 +0300

    Add an options to config AdminAccessKeyID and AdminSecretAccessKey in Esti tests (#4014)
    * Add an options to config AdminAccessKeyID and AdminSecretAccessKey in Esti tests

--- esti/main_test.go
+++ esti/main_test.go
@@ -42,6 +42,11 @@ func (bs *Booleans) Parse(value string) error {
 	return nil
 }
 
+const (
+	DefaultAdminAccessKeyId     = "AKIAIOSFDNN7EXAMPLEQ"
+	DefaultAdminSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+)
+
 var (
 	logger logging.Logger
 	client api.ClientWithResponsesInterface
@@ -54,7 +59,8 @@ var (
 func TestMain(m *testing.M) {
 	systemTests := flag.Bool("system-tests", false, "Run system tests")
 	useLocalCredentials := flag.Bool("use-local-credentials", false, "Generate local API key during `lakefs setup'")
-
+	adminAccessKeyID := flag.String("admin-access-key-id", DefaultAdminAccessKeyId, "lakeFS Admin access key ID")
+	adminSecretAccessKey := flag.String("admin-secret-access-key", DefaultAdminSecretAccessKey, "lakeFS Admin secret access key")
 	if directs, ok := os.LookupEnv("ESTI_TEST_DATA_ACCESS"); ok {
 		if err := testDirectDataAccess.Parse(directs); err != nil {
 			logger.Fatalf("ESTI_TEST_DATA_ACCESS=\"%s\": %s", directs, err)
@@ -70,11 +76,11 @@ func TestMain(m *testing.M) {
 		Name:      "esti",
 		StorageNS: "esti-system-testing",
 	}
-	if *useLocalCredentials {
-		params.AdminAccessKeyID = "AKIAIOSFDNN7EXAMPLEQ"
-		params.AdminSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-	}
 
+	if *useLocalCredentials {
+		params.AdminAccessKeyID = *adminAccessKeyID
+		params.AdminSecretAccessKey = *adminSecretAccessKey
+	}
 	viper.SetDefault("post_migrate", false)
 
 	logger, client, svc = testutil.SetupTestingEnv(&params)
