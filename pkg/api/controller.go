d34a7e4fa1847c40294af066370dac2138eb176d
itai-david <90712874+itai-david@users.noreply.github.com>
2022-09-01 12:28:39 -0500

    Concurrent merge improvements (#4071)
    * using retryBranchUpdate for merge, with StatusLocked on retries exhaustion
    
    * Rephrasing message on retries exhaustion
    
    * Changing tries const to be public and use it in exhaustion test
    
    * Review comments
    
    * Setting retryBrnachUpdate to 10 retries
    
    * Fix TestGravelerCommit to use BranchUpdateMaxTries

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -1743,6 +1743,9 @@ func (c *Controller) handleAPIError(ctx context.Context, w http.ResponseWriter, 
 	case errors.Is(err, db.ErrAlreadyExists):
 		writeError(w, http.StatusBadRequest, "Already exists")
 
+	case errors.Is(err, graveler.ErrTooManyTries):
+		writeError(w, http.StatusLocked, "Too many attempts, try again later")
+
 	case err != nil:
 		c.Logger.WithContext(ctx).WithError(err).Error("API call returned status internal server error")
 		writeError(w, http.StatusInternalServerError, err)
