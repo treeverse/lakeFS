e44ade59dc3aa0b000c6303196145e6949f99a1d
Idan Novogroder <43949240+idanovo@users.noreply.github.com>
2022-09-04 18:31:28 +0300

    Update esti/main_test.go
    Co-authored-by: N-o-Z <ozery.nir@gmail.com>

--- esti/main_test.go
+++ esti/main_test.go
@@ -261,7 +261,7 @@ func TestMain(m *testing.M) {
 	useLocalCredentials := flag.Bool("use-local-credentials", false, "Generate local API key during `lakefs setup'")
 	adminAccessKeyID := flag.String("admin-access-key-id", DefaultAdminAccessKeyId, "lakeFS Admin access key ID")
 	adminSecretAccessKey := flag.String("admin-secret-access-key", DefaultAdminSecretAccessKey, "lakeFS Admin secret access key")
-	cleanupEnv := flag.Bool("cleanup-env-pre-run", false, "Clean repositories, groups, users and polices before sunning esti tests")
+	cleanupEnv := flag.Bool("cleanup-env-pre-run", false, "Clean repositories, groups, users and polices before running esti tests")
 	flag.Var(&repositoriesToKeep, "repository-to-keep", "Repositories to keep in case of pre-run cleanup")
 	flag.Var(&groupsToKeep, "group-to-keep", "Groups to keep in case of pre-run cleanup")
 	flag.Var(&usersToKeep, "user-to-keep", "Users to keep in case of pre-run cleanup")
