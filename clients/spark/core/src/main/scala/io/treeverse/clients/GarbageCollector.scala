3386f9c467199609eb07a47a3e51aaddd0ae3352
johnnyaug <yoni.augarten@treeverse.io>
2022-09-07 17:25:26 +0300

    remove unused import

--- clients/spark/core/src/main/scala/io/treeverse/clients/GarbageCollector.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/GarbageCollector.scala
@@ -18,7 +18,6 @@ import org.json4s.native.JsonMethods._
 import org.json4s._
 import org.json4s.JsonDSL._
 import java.time.LocalDateTime
-import java.time.ZoneId
 import java.time.ZoneOffset
 
 /** Interface to build an S3 client.  The object
