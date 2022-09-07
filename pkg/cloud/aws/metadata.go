55effdaf584d8e789c6bb769e8cbd4d728e22da4
itaiad200 <itaiad200@gmail.com>
2022-09-07 17:46:20 +0300

    Retry cloud id fetch (#4129)

--- pkg/cloud/aws/metadata.go
+++ pkg/cloud/aws/metadata.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/cenkalti/backoff/v4"
 	"github.com/treeverse/lakefs/pkg/cloud"
 	"github.com/treeverse/lakefs/pkg/logging"
 )
@@ -42,11 +43,28 @@ func (m *MetadataProvider) GetMetadata() map[string]string {
 	sess.ClientConfig(s3.ServiceName)
 
 	stsClient := sts.New(sess)
-	identity, err := stsClient.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+
+	const (
+		maxInterval    = 200 * time.Millisecond
+		maxElapsedTime = 3 * time.Second
+	)
+	bo := backoff.NewExponentialBackOff()
+	bo.MaxInterval = maxInterval
+	bo.MaxElapsedTime = maxElapsedTime
+	var identity *sts.GetCallerIdentityOutput
+	err = backoff.Retry(func() error {
+		identity, err = stsClient.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+		if err != nil {
+			m.logger.WithError(err).Warn("Tried to to get AWS account ID for BI")
+			return err
+		}
+		return nil
+	}, bo)
 	if err != nil {
-		m.logger.WithError(err).Warn("Failed to get AWS account ID for BI")
+		m.logger.WithError(err).Warn("Failed to to get AWS account ID for BI")
 		return nil
 	}
+
 	return map[string]string{
 		cloud.IDKey:     fmt.Sprintf("%x", md5.Sum([]byte(*identity.Account))), //nolint:gosec
 		cloud.IDTypeKey: "aws_account_id",
