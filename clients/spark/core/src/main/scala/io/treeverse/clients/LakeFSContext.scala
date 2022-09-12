58b3d336dbbedd80da2e0e71c90cde35c1d141cc
Yoni <yoni.augarten@treeverse.io>
2022-09-12 18:26:57 +0300

    Metadata Client: DataFrame out of entire storage namespace (#4125)

--- clients/spark/core/src/main/scala/io/treeverse/clients/LakeFSContext.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/LakeFSContext.scala
@@ -18,20 +18,24 @@ object LakeFSContext {
   val LAKEFS_CONF_API_CONNECTION_TIMEOUT_SEC_KEY = "lakefs.api.connection.timeout_seconds"
   val LAKEFS_CONF_API_READ_TIMEOUT_SEC_KEY = "lakefs.api.read.timeout_seconds"
   val LAKEFS_CONF_JOB_REPO_NAME_KEY = "lakefs.job.repo_name"
+  val LAKEFS_CONF_JOB_STORAGE_NAMESPACE_KEY = "lakefs.job.storage_namespace"
   val LAKEFS_CONF_JOB_COMMIT_ID_KEY = "lakefs.job.commit_id"
   val LAKEFS_CONF_DEBUG_GC_MAX_COMMIT_ISO_DATETIME_KEY = "lakefs.debug.gc.max_commit_iso_datetime"
   val LAKEFS_CONF_DEBUG_GC_MAX_COMMIT_EPOCH_SECONDS_KEY = "lakefs.debug.gc.max_commit_epoch_seconds"
   val LAKEFS_CONF_DEBUG_GC_NO_DELETE_KEY = "lakefs.debug.gc.no_delete"
   val LAKEFS_CONF_DEBUG_GC_REPRODUCE_RUN_ID_KEY = "lakefs.debug.gc.reproduce_run_id"
 
-  def newRDD(
+  private def newRDD(
       sc: SparkContext,
       repoName: String,
-      commitID: String
+      storageNamespace: String,
+      commitID: String,
+      inputFormatClass: Class[_ <: LakeFSBaseInputFormat]
   ): RDD[(Array[Byte], WithIdentifier[Entry])] = {
     val conf = new Configuration(sc.hadoopConfiguration)
     conf.set(LAKEFS_CONF_JOB_REPO_NAME_KEY, repoName)
     conf.set(LAKEFS_CONF_JOB_COMMIT_ID_KEY, commitID)
+    conf.set(LAKEFS_CONF_JOB_STORAGE_NAMESPACE_KEY, storageNamespace)
     if (StringUtils.isBlank(conf.get(LAKEFS_CONF_API_URL_KEY))) {
       throw new InvalidJobConfException(s"$LAKEFS_CONF_API_URL_KEY must not be empty")
     }
@@ -43,29 +47,82 @@ object LakeFSContext {
     }
     sc.newAPIHadoopRDD(
       conf,
-      classOf[LakeFSInputFormat],
+      inputFormatClass,
       classOf[Array[Byte]],
       classOf[WithIdentifier[Entry]]
     )
   }
 
-  def newDF(spark: SparkSession, repoName: String, commitID: String): DataFrame = {
-    val rdd = newRDD(spark.sparkContext, repoName, commitID).map { case (k, v) =>
-      val entry = v.message
-      Row(
-        new String(k),
-        entry.address,
-        entry.eTag,
-        new java.sql.Timestamp(TimeUnit.SECONDS.toMillis(entry.getLastModified.seconds)),
-        entry.size
-      )
-    }
+  /** Returns all entries in all ranges of the given commit, as an RDD.
+   *  If no commit is given, returns all entries in all ranges of the entire repository.
+   *  The same entry may be found in multiple ranges.
+   */
+  def newRDD(
+      sc: SparkContext,
+      repoName: String,
+      commitID: String = ""
+  ): RDD[(Array[Byte], WithIdentifier[Entry])] = {
+    val inputFormatClass =
+      if (StringUtils.isNotBlank(commitID)) classOf[LakeFSCommitInputFormat]
+      else classOf[LakeFSAllRangesInputFormat]
+
+    newRDD(sc, repoName, "", commitID, inputFormatClass)
+  }
+
+  private def newDF(
+      spark: SparkSession,
+      repoName: String,
+      storageNamespace: String,
+      commitID: String,
+      inputFormatClass: Class[_ <: LakeFSBaseInputFormat]
+  ): DataFrame = {
+    val rdd =
+      newRDD(spark.sparkContext, repoName, storageNamespace, commitID, inputFormatClass).map {
+        case (k, v) =>
+          val entry = v.message
+          Row(
+            new String(k),
+            entry.address,
+            entry.eTag,
+            new java.sql.Timestamp(TimeUnit.SECONDS.toMillis(entry.getLastModified.seconds)),
+            entry.size,
+            new String(v.rangeID)
+          )
+      }
     val schema = new StructType()
       .add(StructField("key", StringType))
       .add(StructField("address", StringType))
       .add(StructField("etag", StringType))
       .add(StructField("last_modified", TimestampType))
       .add(StructField("size", LongType))
+      .add(StructField("range_id", StringType))
     spark.createDataFrame(rdd, schema)
+  }
+
+  /** Returns all entries in all ranges found in this storage namespace.
+   *  The same entry may be found in multiple ranges.
+   *
+   *  The storage namespace is expected to be a URI accessible by Hadoop.
+   */
+  def newDF(
+      spark: SparkSession,
+      storageNamespace: String
+  ): DataFrame = {
+    newDF(spark, "", storageNamespace, "", classOf[LakeFSAllRangesInputFormat])
+  }
+
+  /** Returns all entries in all ranges of the given commit, as a DataFrame.
+   *  If no commit is given, returns all entries in all ranges of the entire repository.
+   *  The same entry may be found in multiple ranges.
+   */
+  def newDF(
+      spark: SparkSession,
+      repoName: String,
+      commitID: String = ""
+  ): DataFrame = {
+    val inputFormatClass =
+      if (StringUtils.isNotBlank(commitID)) classOf[LakeFSCommitInputFormat]
+      else classOf[LakeFSAllRangesInputFormat]
+    newDF(spark, repoName, "", commitID, inputFormatClass)
   }
 }
