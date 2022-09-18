7b4c52a411fc2072298b002dbd9079a7c1fb6825
Barak Amar <barak.amar@treeverse.io>
2022-09-18 13:27:16 +0300

    KV: local driver documentation and docker compose configuration (#4185)
    Co-authored-by: Itai Admi <itaiad200@gmail.com>

--- esti/scripts/set_env_vars.sh
+++ esti/scripts/set_env_vars.sh
@@ -7,4 +7,3 @@ export ESTI_STORAGE_NAMESPACE="local://system-testing"
 
 # Lakefs env vars for test
 export ACTIONS_VAR="this_is_actions_var"
-export LAKEFS_DATABASE_POSTGRES_CONNECTION_STRING="postgres://lakefs:lakefs@localhost:5433/postgres?sslmode=disable"
