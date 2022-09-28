3d11d44a30333ecf0561323e9af5b7e9a5bdbb63
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2022-09-28 20:12:10 +0300

    put back the LAKEFS_CONF_DEBUG_GC_NO_DELETE_KEY flag and deprecate its usage

--- clients/spark/core/src/main/scala/io/treeverse/clients/LakeFSContext.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/LakeFSContext.scala
@@ -28,6 +28,7 @@ object LakeFSContext {
   val LAKEFS_CONF_GC_DO_MARK = "lakefs.gc.do_mark"
   val LAKEFS_CONF_GC_DO_SWEEP = "lakefs.gc.do_sweep"
   val LAKEFS_CONF_GC_MARK_ID = "lakefs.gc.mark_id"
+  val LAKEFS_CONF_DEBUG_GC_NO_DELETE_KEY = "lakefs.debug.gc.no_delete"
 
   val MARK_ID_KEY = "mark_id"
   val DEFAULT_LAKEFS_CONF_GC_NUM_RANGE_PARTITIONS = 50
