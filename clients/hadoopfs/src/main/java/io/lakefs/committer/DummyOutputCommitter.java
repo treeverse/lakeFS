75d48c1c4220c296f12177f06484ff0724d45246
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2022-10-31 15:22:07 +0200

    Make a working OutputCommitter (#4513)
    * Add a merge strategy
    
    This is a bit more like SaveMode.OVERWRITE.
    
    * Add client timeouts
    
    *NOT* configurable yet, that's a separate bug.
    
    * Clean up logging, don't commit empty job branch
    
    "%s" doesn't work in LOG.
    
    * Only commit or merge when there are diffs
    
    Otherwise lakeFS errors out and the job fails.
    
    * Use branch name based on outputPath not job name
    
    This will not work for the multi-writer case.
    
    * Add prefix to OC paths
    
    * Fix typo :-/

--- clients/hadoopfs/src/main/java/io/lakefs/committer/DummyOutputCommitter.java
+++ clients/hadoopfs/src/main/java/io/lakefs/committer/DummyOutputCommitter.java
@@ -10,6 +10,7 @@ import io.lakefs.clients.api.RefsApi;
 import io.lakefs.clients.api.CommitsApi;
 import io.lakefs.clients.api.model.BranchCreation;
 import io.lakefs.clients.api.model.CommitCreation;
+import io.lakefs.clients.api.model.DiffList;
 import io.lakefs.clients.api.model.Merge;
 
 import org.apache.commons.lang.exception.ExceptionUtils; // (for debug prints ONLY)
@@ -25,16 +26,20 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter;
 import org.apache.http.HttpStatus;
 
 import com.google.common.base.Preconditions;
-
+import com.google.common.hash.Hashing;
+import com.google.common.hash.HashFunction;
+    
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 // TODO(ariels): For Hadoop 3, it is enough (and better!) to extend PathOutputCommitter.
 public class DummyOutputCommitter extends FileOutputCommitter {
     private static final Logger LOG = LoggerFactory.getLogger(DummyOutputCommitter.class);
 
+    private static final String branchNamePrefix = "lakeFS-OC-";
 
     /**
      * API client connected to the lakeFS server used on the filesystem.
@@ -78,17 +83,32 @@ public class DummyOutputCommitter extends FileOutputCommitter {
      */
     protected Path workPath = null;
 
+    private static HashFunction hash = Hashing.sha256();
+    private static Charset utf8 = Charset.forName("utf8");
+
+    /*
+     * BUG(ariels): This is based on the outputPath, the only thing that
+     *     works.  It will need to be changed for multiwriter -- need to
+     *     depend on a configured algorithm here!
+     */
+    protected String pathToBranch(Path p) {
+        String path = p.toString();
+        // TODO(ariels): Use a more compact encoding (base-36?)
+        String digest = hash.hashString(path, utf8).toString();
+        String pathPrefix = path.length() > 128 ? path.substring(0, 128) : path;
+        pathPrefix = pathPrefix.replaceAll("[^-_a-zA-Z0-9]", "-");
+        return String.format("%s-%s-%s", branchNamePrefix, digest, pathPrefix);
+    }
+
     public DummyOutputCommitter(Path outputPath, JobContext context) throws IOException {
         super(outputPath, context);
         // TODO(lynn): Create lakeFS API client.
 
         JobID id = context.getJobID();
-        // BUG(ariels): Clean up job name.  It is selected by user and might
-        // contain illegal characters.
-        this.jobBranch = context.getJobName().isEmpty() ?
-            id.toString() :
-            String.format("%s-%s", context.getJobName(), id.toString());
-        System.out.printf("TODO: Job branch: %s\n", jobBranch);
+
+        this.jobBranch = pathToBranch(outputPath);
+
+        LOG.info("Construct OC: Job branch: {}", jobBranch);
 
         if (outputPath != null) {
             Configuration conf = context.getConfiguration();
@@ -116,19 +136,19 @@ public class DummyOutputCommitter extends FileOutputCommitter {
             ObjectLocation loc = ObjectLocation.pathToObjectLocation(null, outputPath);
             loc.setRef(this.taskBranch);
             this.workPath = loc.toFSPath();
-            System.out.printf("TODO: Working path: %s\n", workPath);
+            LOG.trace("Working path: {}", workPath);
         }
     }
 
     private boolean createBranch(String branch, String base) throws IOException {
-        System.out.printf("TODO: Create branch %s from %s\n", branch, base);
+        LOG.info("Create branch {} from {}", branch, base);
         try {
             BranchesApi branches = lakeFSClient.getBranches();
             branches.createBranch(repository, new BranchCreation().name(branch).source(base));
             return true;
         } catch (ApiException e) {
             if (e.getCode() == HttpStatus.SC_CONFLICT) {
-                LOG.debug("branch {} already exists", branch);
+                LOG.info("branch {} already exists", branch);
                 return false;
                 // TODO(ariels): Consider checking the branch originates
                 //     from the expected location.  That will improve error
@@ -144,8 +164,6 @@ public class DummyOutputCommitter extends FileOutputCommitter {
 
     @Override
     public Path getWorkPath() {
-        System.out.printf("Get working path for %s -> %s\n%s\n", taskBranch, workPath,
-                          ExceptionUtils.getStackTrace(new Throwable()));
         return workPath;
     }
 
@@ -153,20 +171,42 @@ public class DummyOutputCommitter extends FileOutputCommitter {
     public void setupJob(JobContext context) throws IOException {
         if (outputPath == null)
             return;
-        LOG.debug("Setup job for %s on %s\n", outputBranch, jobBranch);
+        LOG.info("Setup job for {} on {}", outputBranch, jobBranch);
         createBranch(jobBranch, outputBranch);
+    }
+
+    protected boolean hasChanges(String branch) throws IOException {
+        try {
+            BranchesApi branches = lakeFSClient.getBranches();
+            DiffList res = branches.diffBranch(repository, branch, null, 1, null, null);
+            return res.getResults().size() > 0;
+        } catch (ApiException e) {
+            throw new IOException(String.format("hasChanges(%s, %s): diffBranch failed", repository, branch), e);
+        }
+    }
+
+    protected boolean hasDiffs(String repository, String from, String to) throws IOException {
+        try {
+            RefsApi refs = lakeFSClient.getRefs();
+            DiffList res = refs.diffRefs(repository, from, to, null, 1, null, null, "two_dot", "two_dot");
+            return res.getResults().size() > 0;
+        } catch (ApiException e) {
+            throw new IOException(String.format("hasDiffs(%s, %s => %s): diff failed", repository, from, to), e);
+        }
     }
 
     @Override
     public void commitJob(JobContext jobContext) throws IOException {
         if (outputPath == null)
             return;
-        LOG.debug("Commit job branch %s to %s\n", jobBranch, outputBranch);
         try {
-            CommitsApi commits = lakeFSClient.getCommits();
-            commits.commit(repository, jobBranch, new CommitCreation().message(String.format("commiting Job %s", jobContext.getJobID())), null);
+            if (!hasDiffs(repository, jobBranch, outputBranch)) {
+                LOG.debug("No differences from {} to {}, nothing to merge", jobBranch, outputBranch);
+                return;
+            }
+            LOG.info("Commit job branch {} to {}", jobBranch, outputBranch);
             RefsApi refs = lakeFSClient.getRefs();
-            refs.mergeIntoBranch(repository, jobBranch, outputBranch, new Merge().message(""));
+            refs.mergeIntoBranch(repository, jobBranch, outputBranch, new Merge().message("").strategy("source-wins"));
         } catch (ApiException e) {
             throw new IOException(String.format("commitJob %s failed", jobContext.getJobID()), e);
         }
@@ -176,7 +216,7 @@ public class DummyOutputCommitter extends FileOutputCommitter {
     public void abortJob(JobContext jobContext, JobStatus.State status) throws IOException { // TODO(lynn)
         if (outputPath == null)
             return;
-        System.out.printf("TODO: Delete(?) job branch %s\n", jobBranch);
+        LOG.info("TODO: Delete(?) job branch {}", jobBranch);
     }
 
     @Override
@@ -190,19 +230,30 @@ public class DummyOutputCommitter extends FileOutputCommitter {
         throws IOException {
         if (outputPath == null)
             return;
-        LOG.debug("Setup task for %s on %s\n", taskBranch, jobBranch);
+        LOG.info("Setup task for {} on {}", taskBranch, jobBranch);
+        createBranch(jobBranch, outputBranch);
         createBranch(taskBranch, jobBranch);
     }
 
     @Override
     public void commitTask(TaskAttemptContext taskContext)
-        throws IOException {    // TODO(lynn)
+        throws IOException {
         if (outputPath == null)
             return;
-        LOG.debug("Commit task branch %s to %s\n", taskBranch, jobBranch);
         try {
+            if (!hasChanges(taskBranch)) {
+                LOG.debug("Nothing to commit into {} for {}", taskBranch, jobBranch);
+                return;
+            }
+
+            LOG.info("Commit task branch {} and merge to {}", taskBranch, jobBranch);
             CommitsApi commits = lakeFSClient.getCommits();
             commits.commit(repository, taskBranch, new CommitCreation().message(String.format("committing Task %s", taskContext.getTaskAttemptID())), null);
+
+            if (!hasDiffs(repository, taskBranch, jobBranch)) {
+                LOG.info("Strange, after committing no differences from {} to {}, nothing to merge", taskBranch, jobBranch);
+                return;
+            }
             RefsApi refs = lakeFSClient.getRefs();
             refs.mergeIntoBranch(repository, taskBranch, jobBranch, new Merge().message(""));
         } catch (ApiException e) {
@@ -214,7 +265,7 @@ public class DummyOutputCommitter extends FileOutputCommitter {
         throws IOException {    // TODO(lynn)
         if (outputPath == null)
             return;
-        System.out.printf("TODO: Delete task branch %s\n", taskBranch);
+        LOG.info("TODO: Delete task branch %s", taskBranch);
     }
 
     // TODO(lynn): More methods: isRecoverySupported, isCommitJobRepeatable, recoverTask.
