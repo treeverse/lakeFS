33ec2a9134251042c60d9e05a0e9490fe75abe88
Barak Amar <barak.amar@treeverse.io>
2022-11-02 10:54:32 +0200

    Improve physical address regexp

--- esti/lakectl_util.go
+++ esti/lakectl_util.go
@@ -31,7 +31,7 @@ var (
 	reShortCommitID   = regexp.MustCompile(`[\d|a-f]{16}`)
 	reChecksum        = regexp.MustCompile(`[\d|a-f]{32}`)
 	reEndpoint        = regexp.MustCompile(`https?://\w+(:\d+)?/api/v\d+/`)
-	rePhysicalAddress = regexp.MustCompile(`/data/\w+/\w+`)
+	rePhysicalAddress = regexp.MustCompile(`/data/[0-9a-v]{20}/[0-9a-v]{20}`)
 	reVariable        = regexp.MustCompile(`\$\{([^${}]+)}`)
 )
 
