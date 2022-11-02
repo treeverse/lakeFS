7d430b5c78dd5f076e0a0d0991387307a0f4074d
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2022-11-02 13:05:14 +0200

    [CR] Call config variables "delete_xxx_branch"
    This imperative form is more clearly boolean than "delete.xxx_branch".
    
    Comment from @johnnyaug:
    > These configurations can both pass as a branch name configuration.
    > Would go with something as committer.cleanup.delete_job_branch - to clarify it's a boolean.

--- clients/hadoopfs/src/main/java/io/lakefs/Constants.java
+++ clients/hadoopfs/src/main/java/io/lakefs/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
     public static final String SECRET_KEY_KEY_SUFFIX = "secret.key";
     public static final String ENDPOINT_KEY_SUFFIX = "endpoint";
     public static final String LIST_AMOUNT_KEY_SUFFIX = "list.amount";
-    public static final String OC_DELETE_JOB_BRANCH = "committer.delete.job_branch";
-    public static final String OC_DELETE_TASK_BRANCH = "committer.delete.task_branch";
+    public static final String OC_DELETE_JOB_BRANCH = "committer.delete_job_branch";
+    public static final String OC_DELETE_TASK_BRANCH = "committer.delete_task_branch";
 
 
     public static final int DEFAULT_LIST_AMOUNT = 1000;
