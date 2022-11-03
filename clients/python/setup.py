e5dd02371a2c8a559f9cc15fb282024b199ce640
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2022-11-03 11:39:28 +0200

    Add Apache 2 license to Swagger API and generated clients
    Fixes #4452.

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -40,6 +40,7 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
+    license="Apache 2.0",
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
