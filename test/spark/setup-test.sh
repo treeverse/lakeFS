b707b5a7e505a6d1ef8762bbceb84e16b12a0068
johnnyaug <yoni.augarten@treeverse.io>
2022-11-06 13:49:13 +0200

    compose

--- test/spark/setup-test.sh
+++ test/spark/setup-test.sh
@@ -2,7 +2,7 @@
 #
 # setup-test.sh - set up lakeFS to run Spark app tests.  Run once per lakeFS.
 
-docker-compose exec -T lakefs /app/wait-for localhost:8000
+docker compose exec -T lakefs /app/wait-for localhost:8000
 
-docker-compose exec -T lakefs sh -c 'lakefs setup --user-name tester --access-key-id ${TESTER_ACCESS_KEY_ID} --secret-access-key ${TESTER_SECRET_ACCESS_KEY}'
+docker compose exec -T lakefs sh -c 'lakefs setup --user-name tester --access-key-id ${TESTER_ACCESS_KEY_ID} --secret-access-key ${TESTER_SECRET_ACCESS_KEY}'
 
