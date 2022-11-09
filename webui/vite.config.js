a377abc9ca7bc4e0a440e22a6e52216f7d652aec
Elad Lachmi <elad.lachmi@treeverse.io>
2022-11-09 14:54:50 +0200

    CR Fixes

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -1,9 +1,6 @@
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 import replace from '@rollup/plugin-replace';
-import dns from "dns";
-
-dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default ({ command }) => {
