de5c524ccb6f27ac48d248d7d3707447c6e9f658
eladlachmi <110764839+eladlachmi@users.noreply.github.com>
2022-11-10 17:36:27 +0200

    Add TS support with generics to router hooks (#4574)
    * Add TS support with generics to router hooks

---
+++ webui/src/lib/hooks/router.ts
@@ -0,0 +1,53 @@
+import {
+  useHistory,
+  useLocation,
+  useParams,
+  generatePath,
+} from "react-router-dom";
+
+export const useQuery = <T>(): Partial<T> => {
+  const location = useLocation();
+  const result: Record<string, string> = {};
+  for (const [key, value] of new URLSearchParams(location.search).entries()) {
+    result[key] = value;
+  }
+  return result as Partial<T>;
+};
+
+interface URLDetails {
+  pathname: string;
+  params: Record<string, string>;
+  query: Record<string, string>;
+}
+
+type URLBuilderInput = URLDetails | string;
+
+// TODO(elad): Return URL
+export const buildURL = (url: URLBuilderInput): string => {
+  if (typeof url === "string") return url;
+  // otherwise, assume query, params and pathname
+  const path = generatePath(url.pathname, url.params ? url.params : {});
+  if (!url.query) return path;
+  const query = new URLSearchParams(url.query).toString();
+  return `${path}?${query}`;
+};
+
+export const useRouter = <
+  Q = Record<string, string | undefined>,
+  T extends { [K in keyof T]?: string | undefined } = Record<
+    string,
+    string | undefined
+  >
+>() => {
+  const location = useLocation();
+  const history = useHistory();
+  const query = useQuery<Q>();
+  const params = useParams<T>();
+  return {
+    query,
+    params,
+    route: location.pathname,
+    history: history,
+    push: (url: URLBuilderInput) => history.push(buildURL(url)),
+  };
+};
