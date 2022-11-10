e0ca5d17a6a9b3a5a0d73cb72be26090fab98a79
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2022-11-10 13:08:25 +0200

    fix DRY

--- clients/spark/core/src/main/hadoop3/scala/io/treeverse/clients/conditional/S3ClientBuilder.scala
+++ clients/spark/core/src/main/hadoop3/scala/io/treeverse/clients/conditional/S3ClientBuilder.scala
@@ -1,11 +1,11 @@
 package io.treeverse.clients.conditional
 
 import com.amazonaws.ClientConfiguration
-import com.amazonaws.auth.AWSCredentialsProvider
-import com.amazonaws.services.s3.model.{HeadBucketRequest, Region}
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import io.treeverse.clients.StorageUtils
+import com.amazonaws.services.s3.AmazonS3
 import org.apache.hadoop.conf.Configuration
 import org.slf4j.{Logger, LoggerFactory}
+import com.amazonaws.auth.AWSCredentialsProvider
 
 object S3ClientBuilder extends io.treeverse.clients.S3ClientBuilder {
   val logger: Logger = LoggerFactory.getLogger(getClass.toString + "[hadoop3]")
@@ -38,50 +38,6 @@ object S3ClientBuilder extends io.treeverse.clients.S3ClientBuilder {
         )
       } else None
 
-    initializeClient(configuration, credentialsProvider, region, bucket)
-  }
-
-  private def initializeClient(
-      config: ClientConfiguration,
-      credentialsProvider: Option[AWSCredentialsProvider],
-      region: String,
-      bucket: String
-  ): AmazonS3 = {
-    val builder = AmazonS3ClientBuilder
-      .standard()
-      .withClientConfiguration(config)
-      .withRegion(region)
-    val builderWithCredentials = credentialsProvider match {
-      case Some(cp) => builder.withCredentials(cp)
-      case None     => builder
-    }
-
-    val client = builderWithCredentials.build
-
-    if (validateClientCorrectness(client, bucket)) {
-      return client
-    }
-    val bucketRegion = getAWSS3Region(client, bucket)
-    builder.withRegion(bucketRegion).build
-  }
-
-  private def validateClientCorrectness(client: AmazonS3, bucket: String): Boolean = {
-    try {
-      client.headBucket(new HeadBucketRequest(bucket))
-      true
-    } catch {
-      case _: Exception => false
-    }
-  }
-
-  private def getAWSS3Region(client: AmazonS3, bucket: String): String = {
-    val bucketRegion = client.getBucketLocation(bucket)
-    val region = Region.fromValue(bucketRegion)
-    // The comparison `region.equals(Region.US_Standard))` is required due to AWS's backward compatibility:
-    // https://github.com/aws/aws-sdk-java/issues/1470.
-    // "us-east-1" was previously called "US Standard". This resulted in a return value of "US" when
-    // calling `client.getBucketLocation(bucket)`.
-    if (region.equals(Region.US_Standard)) "us-east-1"
-    else region.toString
+    StorageUtils.S3.initializeS3Client(configuration, credentialsProvider, region, bucket)
   }
 }
