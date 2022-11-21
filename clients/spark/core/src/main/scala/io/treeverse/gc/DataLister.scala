f7470a89282c294653b60d2af2d0da1381455ce2
Barak Amar <barak.amar@treeverse.io>
2022-11-21 13:57:44 +0200

    Parallelize listing of data in GC+ client code (#4610)

--- clients/spark/core/src/main/scala/io/treeverse/gc/DataLister.scala
+++ clients/spark/core/src/main/scala/io/treeverse/gc/DataLister.scala
@@ -3,19 +3,24 @@ package io.treeverse.gc
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 import scala.collection.mutable.ListBuffer
+import io.treeverse.clients.ConfigMapper
+import org.apache.hadoop.fs.FileStatus
 
 /** List all the files under a given path.
  */
 trait DataLister {
-  def listData(spark: SparkSession, path: Path): DataFrame
+  @transient lazy val spark = SparkSession.active
+
+  def listData(configMapper: ConfigMapper, path: Path): DataFrame
 }
 
 class NaiveDataLister extends DataLister {
-  override def listData(spark: SparkSession, path: Path): DataFrame = {
+  override def listData(configMapper: ConfigMapper, path: Path): DataFrame = {
     import spark.implicits._
-    val fs = path.getFileSystem(spark.sparkContext.hadoopConfiguration)
+    val fs = path.getFileSystem(configMapper.configuration)
     val dataIt = fs.listFiles(path, false)
     val dataList = new ListBuffer[(String, Long)]()
     while (dataIt.hasNext) {
@@ -23,5 +28,42 @@ class NaiveDataLister extends DataLister {
       dataList += ((fileStatus.getPath.toString, fileStatus.getModificationTime()))
     }
     dataList.toDF("address", "last_modified")
+  }
+}
+
+class FileDescriptor(val path: String, val lastModified: Long) extends Serializable
+
+class ParallelDataLister extends DataLister with Serializable {
+  def listPath(
+      configMapper: ConfigMapper,
+      path: String
+  ): List[FileDescriptor] = {
+    val p = new Path(path)
+    val fs = p.getFileSystem(configMapper.configuration)
+    fs.listStatus(p)
+      .map((status: FileStatus) =>
+        new FileDescriptor(status.getPath.getName, status.getModificationTime)
+      )
+      .toList
+  }
+
+  override def listData(configMapper: ConfigMapper, path: Path): DataFrame = {
+    import spark.implicits._
+    val fs = path.getFileSystem(spark.sparkContext.hadoopConfiguration)
+    val slices = listPath(configMapper, path.toString)
+    val pathStr = path.toString
+    val objectsUDF = udf((sliceId: String) => {
+      listPath(configMapper, pathStr + sliceId)
+        .map(s => (s.path, s.lastModified))
+    })
+    val objectsDF = slices
+      .map(_.path)
+      .toDF("slice_id")
+      .withColumn("udf", explode(objectsUDF(col("slice_id"))))
+      .withColumn("address", concat(col("slice_id"), lit("/"), col("udf._1")))
+      .withColumn("last_modified", col("udf._2"))
+      .select("address", "last_modified")
+      .toDF("address", "last_modified")
+    objectsDF
   }
 }
