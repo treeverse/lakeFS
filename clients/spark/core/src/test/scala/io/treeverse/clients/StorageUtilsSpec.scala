ca74e5ba46fc8f6b1fa6d9115d0a11bdec51a68d
Barak Amar <barak.amar@treeverse.io>
2022-11-29 12:45:40 +0200

    [GC] Validate bucket's correct region in AWS S3 (#4579)
    * validate correct region in GC
    
    * scalafmt
    
    * validate bucket location in case of failed head bucket
    
    * move region fetching to each S#ClientBuilder
    
    * fix DRY
    
    * extract client initializtion to StorageUtils
    
    * remove unnecessary import
    
    * add logger and catch explicit error after the 'headBucket' request
    
    * scalafmt
    
    * clean StorageUtils class and use it in S3ClientBuilder
    
    * add unit tests to StorageUtils class, upgrade Mockito
    
    * change mock server package and complete tests + scalafmt
    
    * spliced -> split
    
    * Test -> "test"
    
    * remove unnecessary print
    
    * add conditional request split in test and move the AmazonS3ClientBuilder creation out of the before clause
    
    * add xml parser to tests
    
    * add log to indicate a mismatch between bucket and region
    
    * fix escaping quotes
    
    * scalafmt

---
+++ clients/spark/core/src/test/scala/io/treeverse/clients/StorageUtilsSpec.scala
@@ -0,0 +1,147 @@
+package io.treeverse.clients
+
+import com.amazonaws.auth.{
+  AWSCredentialsProvider,
+  AWSStaticCredentialsProvider,
+  BasicAWSCredentials
+}
+import com.amazonaws.services.s3.model.Region
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import com.amazonaws.thirdparty.apache.http.HttpStatus
+import com.amazonaws.{ClientConfiguration, HttpMethod, Protocol, SdkClientException}
+import okhttp3.HttpUrl
+import okhttp3.mockwebserver.{MockResponse, MockWebServer, RecordedRequest}
+import org.scalatest.BeforeAndAfter
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+class StorageUtilsSpec extends AnyFunSpec with BeforeAndAfter with MockitoSugar with Matchers {
+  private val credentialsProvider: AWSCredentialsProvider = new AWSStaticCredentialsProvider(
+    new BasicAWSCredentials("ACCESS_KEY", "SECRET_KEY")
+  )
+
+  private val awsS3ClientBuilder: AmazonS3ClientBuilder =
+    AmazonS3ClientBuilder.standard().withPathStyleAccessEnabled(true)
+  private var server: MockWebServer = null
+  private var clientConfiguration: ClientConfiguration = null
+
+  private val US_STANDARD = "US"
+  private val US_WEST_2 = "us-west-2"
+  private val BUCKET_NAME = "bucket"
+
+  before {
+    server = new MockWebServer
+    server.start()
+    clientConfiguration = generateS3ClientConfigurations(server.url("/"))
+  }
+
+  after {
+    if (server != null) {
+      server.shutdown()
+    }
+  }
+
+  describe("createAndValidateS3Client") {
+    it("should create a client after a successful validation") {
+      server.enqueue(new MockResponse().setResponseCode(200))
+      val initializedClient: AmazonS3 = StorageUtils.S3.createAndValidateS3Client(
+        clientConfiguration,
+        Some(credentialsProvider),
+        awsS3ClientBuilder,
+        US_WEST_2,
+        BUCKET_NAME
+      )
+
+      server.getRequestCount should equal(1)
+      val request: RecordedRequest = server.takeRequest()
+      initializedClient should not be null
+      initializedClient.getRegion.toString should equal(US_WEST_2)
+      extractBucketFromRecordedRequest(request) should equal(BUCKET_NAME)
+      request.getMethod should equal(HttpMethod.HEAD.toString)
+    }
+
+    it("should create the S3 client successfully with US STANDARD region validation") {
+      server.enqueue(new MockResponse().setBody("successful validation response"))
+      val initializedClient: AmazonS3 = initializeClient()
+
+      server.getRequestCount should equal(1)
+
+      val request: RecordedRequest = server.takeRequest()
+      initializedClient should not be null
+      initializedClient.getRegion.name should equal(Region.US_Standard.name())
+      extractBucketFromRecordedRequest(request) should equal(BUCKET_NAME)
+      request.getMethod should equal(HttpMethod.HEAD.toString)
+    }
+
+    it("should get the correct region for the given bucket and create the S3 client successfully") {
+      server.enqueue(new MockResponse().setResponseCode(HttpStatus.SC_FORBIDDEN))
+      server.enqueue(
+        new MockResponse()
+          .setBody(generateGetBucketLocationResponseWithRegion(US_WEST_2))
+          .setResponseCode(HttpStatus.SC_OK)
+      )
+      val initializedClient: AmazonS3 = initializeClient()
+
+      server.getRequestCount should equal(2)
+      val headBucketRequest: RecordedRequest = server.takeRequest()
+      val getLocationRequest: RecordedRequest = server.takeRequest()
+      initializedClient should not be null
+      initializedClient.getRegion.toString should equal(US_WEST_2)
+      extractBucketFromRecordedRequest(headBucketRequest) should equal(BUCKET_NAME)
+      headBucketRequest.getMethod should equal(HttpMethod.HEAD.toString)
+      getLocationRequest.getMethod should equal(HttpMethod.GET.toString)
+    }
+
+    it("should fail creating a client due to failed 'getBucketLocation' request") {
+      server.enqueue(new MockResponse().setResponseCode(HttpStatus.SC_FORBIDDEN))
+      server.enqueue(
+        new MockResponse()
+          .setResponseCode(HttpStatus.SC_NOT_FOUND)
+      )
+
+      assertThrows[SdkClientException] {
+        initializeClient()
+      }
+
+      server.getRequestCount should equal(2)
+      val headBucketRequest: RecordedRequest = server.takeRequest()
+      val getLocationRequest: RecordedRequest = server.takeRequest()
+      extractBucketFromRecordedRequest(headBucketRequest) should equal(BUCKET_NAME)
+      headBucketRequest.getMethod should equal(HttpMethod.HEAD.toString)
+      getLocationRequest.getMethod should equal(HttpMethod.GET.toString)
+    }
+  }
+
+  private def extractBucketFromRecordedRequest(request: RecordedRequest): String = {
+    val splitRequestLine = request.getRequestLine.split('/')
+    if (splitRequestLine.length < 3) {
+      return ""
+    }
+    splitRequestLine(splitRequestLine.length - 3)
+  }
+
+  private def generateGetBucketLocationResponseWithRegion(region: String): String = {
+    s"""<?xml version="1.0" encoding="UTF-8"?>\n<LocationConstraint xmlns="http://s3.amazonaws.com/doc/2006-03-01/">$region</LocationConstraint>"""
+  }
+
+  private def generateS3ClientConfigurations(baseUrl: HttpUrl): ClientConfiguration = {
+    new ClientConfiguration()
+      .withProxyHost(baseUrl.host())
+      .withProxyPort(baseUrl.port())
+      .withProtocol(Protocol.HTTP)
+      .withMaxErrorRetry(0)
+      .withSocketTimeout(15000)
+      .withConnectionTimeout(15000)
+  }
+
+  private def initializeClient(): AmazonS3 = {
+    StorageUtils.S3.createAndValidateS3Client(
+      clientConfiguration,
+      Some(credentialsProvider),
+      awsS3ClientBuilder,
+      US_STANDARD,
+      BUCKET_NAME
+    )
+  }
+}
