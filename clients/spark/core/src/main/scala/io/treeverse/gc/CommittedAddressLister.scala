1feee378580128296f01cd33faa13c83dfafc881
Barak Amar <barak.amar@treeverse.io>
2022-12-08 19:19:28 +0200

    Uncommitted GC - collect full path if part of repo storage namespace (#4754)

--- clients/spark/core/src/main/scala/io/treeverse/gc/CommittedAddressLister.scala
+++ clients/spark/core/src/main/scala/io/treeverse/gc/CommittedAddressLister.scala
@@ -1,35 +1,78 @@
 package io.treeverse.gc
 
-import org.apache.spark.sql.DataFrame
-import io.treeverse.clients.LakeFSContext
-import org.apache.spark.sql.SparkSession
-import io.treeverse.clients.LakeFSJobParams
+import io.treeverse.clients.{LakeFSContext, LakeFSJobParams}
 import io.treeverse.lakefs.catalog.Entry.AddressType
-import org.apache.spark.sql.functions.col
+import org.apache.hadoop.fs.Path
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 trait CommittedAddressLister {
-  def listCommittedAddresses(spark: SparkSession, storageNamespace: String): DataFrame
+  def listCommittedAddresses(
+      spark: SparkSession,
+      storageNamespace: String,
+      clientStorageNamespace: String
+  ): DataFrame
 }
 
 class NaiveCommittedAddressLister extends CommittedAddressLister {
-  override def listCommittedAddresses(spark: SparkSession, storageNamespace: String): DataFrame = {
+
+  override def listCommittedAddresses(
+      spark: SparkSession,
+      storageNamespace: String,
+      clientStorageNamespace: String
+  ): DataFrame = {
     val normalizedStorageNamespace =
       if (storageNamespace.endsWith("/")) storageNamespace else storageNamespace + "/"
     val params = LakeFSJobParams.forStorageNamespace(
       normalizedStorageNamespace,
       UncommittedGarbageCollector.UNCOMMITTED_GC_SOURCE_NAME
     )
-    var df = LakeFSContext.newDF(spark, params)
-    df = df
-      // TODO (optional): push down a filter to the input format, to filter out absolute addresses!
-      .filter(
-        (col("address_type") === AddressType.RELATIVE.name) ||
-          // Backwards compatability with entries prior to address_type
-          (col("address_type") === AddressType.BY_PREFIX_DEPRECATED.name &&
-            !col("address_type").contains("://"))
+    val df = LakeFSContext.newDF(spark, params)
+
+    val normalizedClientStorageNamespace =
+      if (clientStorageNamespace.endsWith("/")) clientStorageNamespace
+      else clientStorageNamespace + "/"
+
+    filterAddresses(spark, df, normalizedClientStorageNamespace)
+  }
+
+  def filterAddresses(
+      spark: SparkSession,
+      df: DataFrame,
+      normalizedClientStorageNamespace: String
+  ): DataFrame = {
+    // extract the schema
+    var storageScheme = new Path(normalizedClientStorageNamespace).toUri.getScheme
+    if (storageScheme.isEmpty) {
+      throw new IllegalArgumentException(
+        s"Invalid storage namespace - missing scheme $normalizedClientStorageNamespace"
       )
-      .select("address")
-      .distinct
+    }
+    storageScheme += ":"
+
+    import spark.implicits._
     df
+      .select("address_type", "address")
+      .filter(row => {
+        val (addrType, addr) = (row.getString(0), row.getString(1))
+        // allow:
+        // - type relative
+        // - non relative to repo storage namespace
+        // - non relative with no schema (relative)
+        addrType.equals(AddressType.RELATIVE.name) ||
+        addr.startsWith(normalizedClientStorageNamespace) ||
+        !addr.startsWith(storageScheme)
+      })
+      .map(row => {
+        val (addrType, addr) = (row.getString(0), row.getString(1))
+        // if not relative and starts with storage namespace - trim to relative
+        val relativeType = addrType.equals(AddressType.RELATIVE.name)
+        if (!relativeType && addr.startsWith(normalizedClientStorageNamespace)) {
+          addr.substring(normalizedClientStorageNamespace.length)
+        } else {
+          addr
+        }
+      })
+      .distinct()
+      .toDF("address")
   }
 }
