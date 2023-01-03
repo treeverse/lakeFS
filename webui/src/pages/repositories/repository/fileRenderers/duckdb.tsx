d59b65788172583e1e53dea807d2b99012ffc7f1
Barak Amar <barak.amar@treeverse.io>
2023-01-03 20:38:23 +0200

    fix: loading of duckdb every query (#4903)

--- webui/src/pages/repositories/repository/fileRenderers/duckdb.tsx
+++ webui/src/pages/repositories/repository/fileRenderers/duckdb.tsx
@@ -5,6 +5,54 @@ import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
 import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 
 
+// based on the replacement rules on the percent-encoding MDN page:
+// https://developer.mozilla.org/en-US/docs/Glossary/Percent-encoding
+// also, I tried doing something nicer with list comprehensions and printf('%x') to convert
+//  from unicode code point to hex - DuckDB didn't seem to evaluate lambdas and list comprehensions
+// Issue: https://github.com/duckdb/duckdb/issues/5821
+// when padding a macro to a table function such as read_parquet() or read_csv().
+// so - string replacements it is.
+const URL_ENCODE_MACRO_SQL = `
+CREATE MACRO p_encode(s) AS
+    replace(
+        replace(
+            replace(
+                replace(
+                    replace(
+                        replace(
+                            replace(
+                                replace(
+                                    replace(
+                                        replace(
+                                            replace(
+                                                replace(
+                                                    replace(
+                                                        replace(
+                                                            replace(
+                                                                replace(
+                                                                    replace(
+                                                                        replace(
+                                                                            replace(s, '%', '%25'),
+                                                                            '/', '%2F'),
+                                                                        '?', '%3F'),
+                                                                    '#', '%23'),
+                                                                '[', '%5B'),
+                                                            ']', '%5D'),
+                                                        '@', '%40'),
+                                                    '!', '%21'),
+                                                '$', '%24'),
+                                            '&', '%26'),
+                                        '''', '%27'),
+                                    '(', '%28'),
+                                ')', '%29'),
+                            '+', '%2B'),
+                        ',', '%2C'),
+                    ';', '%3B'),
+                '=','%3D'),
+            ' ', '%20'),
+        ':', '%3A');
+`
+
 
 const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
     mvp: {
@@ -16,33 +64,36 @@ const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
         mainWorker: eh_worker,
     },
 };
-// Select a bundle based on browser checks
-let _bundle: duckdb.DuckDBBundle;
-let bundlePromise: Promise<duckdb.DuckDBBundle>;
 
-function getBundle(): Promise<duckdb.DuckDBBundle> {
-    if (_bundle) return new Promise(() => _bundle)
-    if (!bundlePromise) {
-        bundlePromise = duckdb.selectBundle(MANUAL_BUNDLES)
+
+let _db: duckdb.AsyncDuckDB | null
+let _worker: Worker | null
+
+async function getDB(): Promise<duckdb.AsyncDuckDB> {
+    if (!_db) {
+        const bundle = await duckdb.selectBundle(MANUAL_BUNDLES)
+        if (!bundle.mainWorker) {
+            throw Error("could not initialize DuckDB")
+        }
+        _worker = new Worker(bundle.mainWorker);
+        const db = new duckdb.AsyncDuckDB(new duckdb.VoidLogger(), _worker);
+        await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+        const conn = await db.connect()
+        // await conn.query(`SET access_mode = READ_ONLY`)
+        await conn.query(URL_ENCODE_MACRO_SQL)
+        await conn.query(`
+            CREATE MACRO lakefs_object(repoId, refId, path) AS
+                '${document.location.protocol}//${document.location.host}/api/v1/repositories/' ||
+                p_encode(repoId) || '/refs/' || p_encode(refId) || '/objects?path=' || p_encode(path);
+        `)
+        await conn.close()
+        _db = db
     }
-    return bundlePromise
+    return _db
 }
 
-export async function withConnection(cb: (conn: duckdb.AsyncDuckDBConnection) => void) {
+export async function getConnection(): Promise<duckdb.AsyncDuckDBConnection> {
     // Instantiate the async version of DuckDB-wasm
-    const bundle = await getBundle();
-    if (!bundle.mainWorker) {
-        throw Error("could not initialize DuckDB")
-    }
-    const worker = new Worker(bundle.mainWorker);
-    const logger = new duckdb.VoidLogger();
-    const db = new duckdb.AsyncDuckDB(logger, worker);
-    await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-    const conn = await db.connect()
-    await conn.query(`CREATE MACRO lakefs_object(repoId, refId, path) AS '${document.location.protocol}//${document.location.host}/api/v1/repositories/' || repoId || '/refs/' || refId || '/objects?path=' || replace(path, '/', '%2F');`)
-    const results = await cb(conn)
-    await conn.close()
-    await db.terminate()
-    await worker.terminate()
-    return results
+    const db = await getDB()
+    return await db.connect()
 }
