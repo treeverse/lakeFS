326943177881fe27f77550bcc5e74867060512c5
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-01-11 20:28:22 +0200

    rename

--- pkg/plugins/plugins_manager.go
+++ pkg/plugins/plugins_manager.go
@@ -10,9 +10,9 @@ import (
 	"github.com/treeverse/lakefs/pkg/logging"
 )
 
-// PluginGroupType specifies the group name of related plugins
+// PluginGroup specifies the group name of related plugins
 // For example, "diff" is a group of plugins that know how to perform a diff.
-type PluginGroupType string
+type PluginGroup string
 
 // PluginType specifies a type of plugin
 // For example, "delta" is a plugin.
@@ -91,33 +91,33 @@ type PluginTypeConfigMap map[PluginType]plugin.ClientConfig
 	},
 }
 */
-type PluginGroupTypeMap map[PluginGroupType]PluginTypeConfigMap
+type PluginGroupTypeMap map[PluginGroup]PluginTypeConfigMap
 
 // The Manager holds the different types of plugins that can be used in the plugin system
 type Manager struct {
-	pluginTypes PluginGroupTypeMap
+	pluginGroups PluginGroupTypeMap
 }
 
-func (m *Manager) AddPluginType(pluginGroupType PluginGroupType, pluginType PluginType, pluginTypeConfig plugin.ClientConfig) {
-	if m.pluginTypes == nil {
-		m.pluginTypes = make(PluginGroupTypeMap)
+func (m *Manager) AddPluginType(pluginGroup PluginGroup, pluginType PluginType, clientConfig plugin.ClientConfig) {
+	if m.pluginGroups == nil {
+		m.pluginGroups = make(PluginGroupTypeMap)
 	}
-	ptcm, ok := m.pluginTypes[pluginGroupType]
+	ptcm, ok := m.pluginGroups[pluginGroup]
 	if !ok {
 		ptcm := make(PluginTypeConfigMap)
-		m.pluginTypes[pluginGroupType] = ptcm
+		m.pluginGroups[pluginGroup] = ptcm
 	}
-	ptcm[pluginType] = pluginTypeConfig
+	ptcm[pluginType] = clientConfig
 }
 
 // WrapPlugin generates a PluginWrapper that wraps the go-plugin client.
 //
-// It accepts two parameters: the top PluginGroupType and the required PluginType under it.
-// For example, PluginGroupType = "diff", PluginType = "delta" will generate a PluginWrapper with a client that performs
+// It accepts two parameters: the top PluginGroup and the required PluginType under it.
+// For example, PluginGroup = "diff", PluginType = "delta" will generate a PluginWrapper with a client that performs
 // diffs over Delta Lake tables.
 // It also returns a DestroyClientFunc
-func (m *Manager) WrapPlugin(pluginGroupType PluginGroupType, pluginType PluginType) (*PluginWrapper, error) {
-	ptpp, ok := m.pluginTypes[pluginGroupType]
+func (m *Manager) WrapPlugin(pluginGroupType PluginGroup, pluginType PluginType) (*PluginWrapper, error) {
+	ptpp, ok := m.pluginGroups[pluginGroupType]
 	if !ok {
 		return nil, fmt.Errorf("unknown plugin group %s", pluginGroupType)
 	}
