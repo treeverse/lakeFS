0c3aac6c0abb307f6b65d57137600d4c8ccde6b5
johnnyaug <yoni.augarten@treeverse.io>
2023-01-11 11:58:50 +0200

    gc+ tests: unified golang flow

--- esti/gc_utils_test.go
+++ esti/gc_utils_test.go
@@ -4,25 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 	"strings"
 )
 
-func getSparkSubmitArgs() []string {
+func getSparkSubmitArgs(entryPoint string) []string {
 	return []string{
 		"--master", "spark://localhost:7077",
 		"--conf", "spark.driver.extraJavaOptions=-Divy.cache.dir=/tmp -Divy.home=/tmp",
 		"--conf", "spark.hadoop.lakefs.api.url=http://lakefs:8000/api/v1",
 		"--conf", "spark.hadoop.lakefs.api.access_key=AKIAIOSFDNN7EXAMPLEQ",
 		"--conf", "spark.hadoop.lakefs.api.secret_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-		"--class", "io.treeverse.clients.GarbageCollector",
+		"--class", entryPoint,
 	}
 }
 
-func getDockerArgs(workingDirectory string) []string {
+func getDockerArgs(workingDirectory string, localJar string) []string {
 	return []string{"run", "--network", "host", "--add-host", "lakefs:127.0.0.1",
 		"-v", fmt.Sprintf("%s/ivy:/opt/bitnami/spark/.ivy2", workingDirectory),
-		"-v", fmt.Sprintf("%s:/opt/metaclient/client.jar", metaclientJarPath),
+		"-v", fmt.Sprintf("%s:/opt/metaclient/client.jar", localJar),
 		"--rm",
 		"-e", "AWS_ACCESS_KEY_ID",
 		"-e", "AWS_SECRET_ACCESS_KEY",
@@ -65,4 +66,32 @@ func runCommand(cmdName string, cmd *exec.Cmd) error {
 		return err
 	}
 	return cmd.Wait()
+}
+
+type sparkSubmitConfig struct {
+	sparkVersion string
+	// localJar is a local path to a jar that contains the main class.
+	localJar string
+	// entryPoint is the class name to run
+	entryPoint      string
+	extraSubmitArgs []string
+	programArgs     []string
+	logSource       string
+}
+
+func runSparkSubmit(config *sparkSubmitConfig) error {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("getting working directory: %w", err)
+	}
+	workingDirectory = strings.TrimSuffix(workingDirectory, "/")
+	dockerArgs := getDockerArgs(workingDirectory, config.localJar)
+	dockerArgs = append(dockerArgs, fmt.Sprintf("docker.io/bitnami/spark:%s", config.sparkVersion), "spark-submit")
+	sparkSubmitArgs := getSparkSubmitArgs(config.entryPoint)
+	sparkSubmitArgs = append(sparkSubmitArgs, config.extraSubmitArgs...)
+	args := append(dockerArgs, sparkSubmitArgs...)
+	args = append(args, "/opt/metaclient/client.jar")
+	args = append(args, config.programArgs...)
+	cmd := exec.Command("docker", args...)
+	return runCommand(config.logSource, cmd)
 }
