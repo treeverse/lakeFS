90bff05c8e07afa18748fbceccd3feb613ada173
Nir Ozery <nir.ozery@treeverse.io>
2023-01-11 18:44:26 +0200

    Fix duplicate case

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -1835,7 +1835,6 @@ func (c *Controller) handleAPIErrorCallback(ctx context.Context, w http.Response
 		errors.Is(err, permissions.ErrInvalidAction),
 		errors.Is(err, model.ErrValidationError),
 		errors.Is(err, graveler.ErrInvalidRef),
-		errors.Is(err, graveler.ErrInvalidValue),
 		errors.Is(err, actions.ErrParamConflict),
 		errors.Is(err, graveler.ErrDereferenceCommitWithStaging):
 		cb(w, r, http.StatusBadRequest, err)
