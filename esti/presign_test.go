aefdea1aeb6d4874e3af30381297af24b013707e
Oz Katz <oz.katz@treeverse.io>
2023-01-11 10:27:10 +0200

    added presigned tests for all object stores

--- esti/presign_test.go
+++ esti/presign_test.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/spf13/viper"
+	"github.com/treeverse/lakefs/pkg/block"
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/treeverse/lakefs/pkg/api"
@@ -11,6 +14,19 @@ import (
 
 func TestPreSign(t *testing.T) {
 	SkipTestIfAskedTo(t)
+	blockStoreType := viper.GetViper().GetString("blockstore_type")
+	expected := ""
+	switch blockStoreType {
+	case block.BlockstoreTypeS3:
+		expected = "X-Amz-Signature="
+	case block.BlockstoreTypeGS:
+		expected = "X-Goog-Signature="
+	case block.BlockstoreTypeAzure:
+		expected = "sv="
+	default:
+		t.Skip("Only GS, S3 and Azure Blob supported for pre-signed urls")
+	}
+
 	ctx, _, repo := setupTest(t)
 	defer tearDownTest(repo)
 	_, _ = uploadFileRandomData(ctx, t, repo, mainBranch, "foo/bar", true)
@@ -23,8 +39,11 @@ func TestPreSign(t *testing.T) {
 	})
 	require.NoError(t, err, "failed to stat object with presign=true")
 	signedUrl := response.JSON200.PhysicalAddress
+	if !strings.HasPrefix(signedUrl, "http") {
+		t.Errorf("exected an HTTP(s) URL instead of an object store URI, got %s", signedUrl)
+	}
 
-	if !strings.Contains(signedUrl, "X-Amz-Signature=") {
+	if !strings.Contains(signedUrl, expected) {
 		t.Errorf("expected a signed URL, got %s", signedUrl)
 	}
 }
