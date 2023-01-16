c26833431c8eec86015687d4265328315b61b625
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-01-16 17:38:46 +0200

    implement GRPCPlugin and GRPCClient for delta

--- pkg/plugins/diff/diff_grpc_client.go
+++ pkg/plugins/diff/diff_grpc_client.go
@@ -2,15 +2,16 @@ package diff
 
 import (
 	"context"
+
 	"github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
 )
 
-type GrpcClient struct {
+type DeltaDiffGRPCClient struct {
 	client DifferClient // This is the underlying GRPC API client
 }
 
-func (d *GrpcClient) Diff(ctx context.Context, paths TablePaths, s3Creds S3Creds) ([]*Diff, error) {
+func (d *DeltaDiffGRPCClient) Diff(ctx context.Context, paths TablePaths, s3Creds S3Creds) ([]*Diff, error) {
 	dr, err := d.client.Diff(ctx, &DiffRequest{
 		Paths: &DiffPaths{
 			LeftPath:  paths.LeftTablePath,
@@ -28,20 +29,21 @@ func (d *GrpcClient) Diff(ctx context.Context, paths TablePaths, s3Creds S3Creds
 	return dr.Diffs, nil
 }
 
-type GRPCPlugin struct {
-	// GRPCPlugin must implement the Plugin interface
+// DeltaDiffGRPCPlugin is responsible for generating a client and a server for the Delta Diff plugin implementation.
+// If the plugin server is not implemented in Go, the GRPCServer method is useless.
+type DeltaDiffGRPCPlugin struct {
+	// DeltaDiffGRPCPlugin must implement the Plugin interface
 	plugin.Plugin
-	Impl Differ
 }
 
 // GRPCServer must be implemented even though we won't use it
-func (p *GRPCPlugin) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
+func (p *DeltaDiffGRPCPlugin) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
 	return nil
 }
 
 // GRPCClient will return the Delta diff GRPC custom client
-func (p *GRPCPlugin) GRPCClient(ctx context.Context, broker *plugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
-	return &GrpcClient{
+func (p *DeltaDiffGRPCPlugin) GRPCClient(ctx context.Context, broker *plugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
+	return &DeltaDiffGRPCClient{
 		client: NewDifferClient(c), // This is the underlying GRPC client
 	}, nil
 }
