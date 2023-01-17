4a3c115dcc2a5f239e2933c1ddff2333bb57829b
Tal Sofer <tal.sofer@treeverse.io>
2023-01-17 17:03:18 +0200

    improve performance of checking if prefix is a delta table root

--- webui/src/util/tablesUtil.jsx
+++ webui/src/util/tablesUtil.jsx
@@ -3,10 +3,6 @@ import {objects} from "../lib/api";
 import {useAPI} from "../lib/hooks/api";
 import {TableType, TreeItemType} from "../constants";
 
-function isDeltaLogPrefix(obj) {
-    return obj.path.includes("_delta_log/");
-}
-
 /**
  * Checks whether a path is a table root and calculates its type.
  *
@@ -16,18 +12,9 @@ function isDeltaLogPrefix(obj) {
  * @return the type of table if the path is a table root, and null otherwise (for paths to objects or prefixes).
  */
 export function getTableType(path, repo, ref) {
-    let listedItems = null;
-    const listResponse = useAPI(() => objects.list(repo.id, ref, path), []);
+    const listResponse = useAPI(() => objects.list(repo.id, ref, path + "_delta_log/"), []);
     if (listResponse.response !== null && listResponse.response.results.length !== 0) {
-            listedItems = listResponse.response.results;
-    }
-
-    if (listedItems !== null) {
-        for (const item of listedItems) {
-            if (isDeltaLogPrefix(item)) {
-                return TableType.DeltaLake;
-            }
-        }
+        return TableType.DeltaLake;
     }
     return null;
 }
