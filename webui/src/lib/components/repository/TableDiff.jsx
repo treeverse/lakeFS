3f0ad1602db29db16c4af1bc16347dbd71a0c678
Tal Sofer <tal.sofer@treeverse.io>
2023-01-22 16:24:42 +0200

    classify delta operations to diff type and color them

--- webui/src/lib/components/repository/TableDiff.jsx
+++ webui/src/lib/components/repository/TableDiff.jsx
@@ -3,6 +3,26 @@ import Table from "react-bootstrap/Table";
 import {ChevronDownIcon, ChevronRightIcon} from "@primer/octicons-react";
 import {OverlayTrigger} from "react-bootstrap";
 import Tooltip from "react-bootstrap/Tooltip";
+import {DiffType} from "../../../constants";
+
+// The list of available operations is based on: https://docs.databricks.com/delta/history.html#operation-metrics-keys
+const deltaLakeOperationToDiffType = new Map([
+    ["WRITE" , DiffType.Added],
+    ["INSERT" , DiffType.Added], // We see this returning from describe history operation although not listed.
+    ["DELETE" , DiffType.Removed],
+    ["CREATE TABLE AS SELECT" , DiffType.Added],
+    ["REPLACE TABLE AS SELECT" , DiffType.Changed],
+    ["COPY INTO" , DiffType.Changed],
+    ["STREAMING UPDATE", DiffType.Changed],
+    ["TRUNCATE" , DiffType.Removed] ,
+    ["MERGE" , DiffType.Changed] ,
+    ["UPDATE" , DiffType.Changed] ,
+    ["FSCK" , DiffType.Removed] ,
+    ["CONVERT" , DiffType.Added] ,
+    ["OPTIMIZE" , DiffType.Changed],
+    ["RESTORE" , DiffType.Changed] ,
+    ["VACUUM" , DiffType.Removed],
+]);
 
 export const TableDiff = () => {
     // TODO: use otfDiffAPiEndpoint to get otfdiffList and populate results
@@ -22,16 +42,17 @@ export const TableDiff = () => {
 
 const OtfDiffRow = ({otfDiff}) => {
     const [rowExpanded, setRowExpanded] = useState(false);
+    const rowClass = "otf-diff-" + operationToDiffType(otfDiff.operation);
     return <>
-            <OperationMetadataRow otfDiff={otfDiff} operationExpanded={rowExpanded} onExpand={() => setRowExpanded(!rowExpanded)}/>
+            <OperationMetadataRow className={rowClass} otfDiff={otfDiff} operationExpanded={rowExpanded} onExpand={() => setRowExpanded(!rowExpanded)}/>
             {rowExpanded
                 ? <OperationDetailsRow otfDiff={otfDiff}/>
             : ""}
     </>
 }
 
-const OperationMetadataRow = ({otfDiff, operationExpanded, onExpand}) => {
-    return <tr>
+const OperationMetadataRow = ({otfDiff, operationExpanded, onExpand, ...rest}) => {
+    return <tr {...rest}>
         <td className="col-10 table-operation-type">{otfDiff.operation}</td>
         <td className="col-1 table-version">Version = {otfDiff.version}</td>
         <td className="col-1 operation-expansion">
@@ -44,7 +65,7 @@ const OperationMetadataRow = ({otfDiff, operationExpanded, onExpand}) => {
 const OperationDetailsRow = ({otfDiff}) => {
     const operationTimestamp = otfDiff.timestamp;
     const operationContent = JSON.stringify(otfDiff.operationContent);
-    return <tr>
+    return <tr className="otf-diff-operation-details">
         <div>
             Timestamp: {operationTimestamp}
         </div>
@@ -60,4 +81,9 @@ const OperationExpansionSection = ({operationExpanded, onExpand}) => {
     return <OverlayTrigger placement="bottom" overlay={<Tooltip>{operationExpanded ? "Hide operation info" : "Show operation info"}</Tooltip>}>
                 <span onClick={onExpand}>{operationExpanded ? <ChevronDownIcon/> : <ChevronRightIcon/>}</span>
            </OverlayTrigger>
+}
+
+function operationToDiffType(operation) {
+    const diffType = deltaLakeOperationToDiffType.get(operation);
+    return diffType !== undefined ? diffType : DiffType.Changed;
 }
