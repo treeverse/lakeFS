953763309e0ce676fb4ada0a19db2f8711419466
Tal Sofer <tal.sofer@treeverse.io>
2023-01-22 13:26:34 +0200

    mock data and add diff rows

--- webui/src/lib/components/repository/TableDiff.jsx
+++ webui/src/lib/components/repository/TableDiff.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import Table from "react-bootstrap/Table";
+import {ChevronDownIcon, ChevronRightIcon} from "@primer/octicons-react";
+
 
 export const TableDiff = () => {
-    return <div>
-         This will be a table diff
-    </div>
+    // TODO: use otfDiffAPiEndpoint to get otfdiffList and populate results
+    const mockRes = '{"results": [{"version": "1", "timestamp": 1515491537026, "operation": "INSERT", "operationContent": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}, {"version": "2", "timestamp": 1515491537026, "operation": "DELETE", "operationContent": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}]}'
+
+    let response = JSON.parse(mockRes)
+    return <Table borderless size="sm">
+            <tbody>
+            {
+                response.results.map(otfDiff => {
+                    return <OtfDiffRow key={otfDiff.timeStamp + "-diff-row"} otfDiff={otfDiff}/>;
+                })
+            }
+            </tbody>
+        </Table>
 }
+
+const OtfDiffRow = ({otfDiff}) => {
+    const [rowExpanded, setRowExpanded] = useState(false)
+    return (<tr>
+            <td className="col-10 table-operation-type">{otfDiff.operation}</td>
+            <td className="col-1 table-version">Version = {otfDiff.version}</td>
+            <td className="col-1 operation-expansion">
+                <OperationExpansionSection operationExpanded={rowExpanded} onClick={() => setRowExpanded(!rowExpanded)}/>
+            </td>
+        </tr>
+    )
+}
+
+// const TableOperationContent = ({content, onExpand}) => {
+//     return <span>
+//         operation content
+//     </span>
+// }
+
+const OperationExpansionSection = ({operationExpanded, onClick}) => {
+    return <span onClick={onClick}>{operationExpanded ? <ChevronDownIcon/> : <ChevronRightIcon/>}</span>
+}
+
