ee71542aa507790d685a70e15c7f2c3e1bbe28a6
Tal Sofer <tal.sofer@treeverse.io>
2023-01-22 15:01:24 +0200

    display operation details on row expansion

--- webui/src/lib/components/repository/TableDiff.jsx
+++ webui/src/lib/components/repository/TableDiff.jsx
@@ -6,9 +6,9 @@ import Tooltip from "react-bootstrap/Tooltip";
 
 export const TableDiff = () => {
     // TODO: use otfDiffAPiEndpoint to get otfdiffList and populate results
-    const mockRes = '{"results": [{"version": "1", "timestamp": 1515491537026, "operation": "INSERT", "operationContent": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}, {"version": "2", "timestamp": 1515491537026, "operation": "DELETE", "operationContent": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}]}'
+    const mockRes = '{"results": [{"version": "1", "timestamp": 1515491537026, "operation": "INSERT", "operationContent": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}, {"version": "2", "timestamp": 1515491537346, "operation": "DELETE", "operationContent": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}]}'
 
-    let response = JSON.parse(mockRes)
+    let response = JSON.parse(mockRes);
     return <Table borderless size="sm">
             <tbody>
             {
@@ -21,26 +21,43 @@ export const TableDiff = () => {
 }
 
 const OtfDiffRow = ({otfDiff}) => {
-    const [rowExpanded, setRowExpanded] = useState(false)
-    return (<tr>
-            <td className="col-10 table-operation-type">{otfDiff.operation}</td>
-            <td className="col-1 table-version">Version = {otfDiff.version}</td>
-            <td className="col-1 operation-expansion">
-                <OperationExpansionSection operationExpanded={rowExpanded} onClick={() => setRowExpanded(!rowExpanded)}/>
-            </td>
-        </tr>
-    )
+    const [rowExpanded, setRowExpanded] = useState(false);
+    return <>
+            <OperationMetadataRow otfDiff={otfDiff} operationExpanded={rowExpanded} onExpand={() => setRowExpanded(!rowExpanded)}/>
+            {rowExpanded
+                ? <OperationDetailsRow otfDiff={otfDiff}/>
+            : ""}
+    </>
 }
 
-// const TableOperationContent = ({content, onExpand}) => {
-//     return <span>
-//         operation content
-//     </span>
-// }
+const OperationMetadataRow = ({otfDiff, operationExpanded, onExpand}) => {
+    return <tr>
+        <td className="col-10 table-operation-type">{otfDiff.operation}</td>
+        <td className="col-1 table-version">Version = {otfDiff.version}</td>
+        <td className="col-1 operation-expansion">
+            <OperationExpansionSection operationExpanded={operationExpanded} onExpand={onExpand}/>
+        </td>
+    </tr>
 
-const OperationExpansionSection = ({operationExpanded, onClick}) => {
+}
+
+const OperationDetailsRow = ({otfDiff}) => {
+    const operationTimestamp = otfDiff.timestamp;
+    const operationContent = JSON.stringify(otfDiff.operationContent);
+    return <tr>
+        <div>
+            Timestamp: {operationTimestamp}
+        </div>
+        <div>
+            Operation Content:
+            <br/>
+            {operationContent}
+        </div>
+    </tr>
+}
+
+const OperationExpansionSection = ({operationExpanded, onExpand}) => {
     return <OverlayTrigger placement="bottom" overlay={<Tooltip>{operationExpanded ? "Hide operation info" : "Show operation info"}</Tooltip>}>
-                <span onClick={onClick}>{operationExpanded ? <ChevronDownIcon/> : <ChevronRightIcon/>}</span>
+                <span onClick={onExpand}>{operationExpanded ? <ChevronDownIcon/> : <ChevronRightIcon/>}</span>
            </OverlayTrigger>
 }
-
