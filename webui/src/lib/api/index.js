ad07cc606cb8b98dfc2d292bb5106a7204c2954a
talSofer <tal.sofer@treeverse.io>
2023-01-23 17:18:48 +0200

    Delta diff prototype: Expanded delta table diff  (#5081)
    * mock data and add diff rows
    
    * add operation info tooltip
    
    * support table diff for all tree levels
    
    * display operation details on row expansion
    
    * classify delta operations to diff type and color them
    
    * connect to API endpoint
    
    * prettify operation content
    
    * use actual API endpoint
    
    * handle no changes case
    
    * fix tooltip placement

--- webui/src/lib/api/index.js
+++ webui/src/lib/api/index.js
@@ -476,6 +476,18 @@ class Repositories {
             throw new Error(await extractError(response));
         }
     }
+
+    async otfDiff(repoId, leftRef, rightRef, table_path = "", type) {
+        const query = qs({table_path, type});
+        const response = await apiRequest(`/repositories/${encodeURIComponent(repoId)}/otf/refs/${encodeURIComponent(leftRef)}/diff/${encodeURIComponent(rightRef)}?` + query);
+        if (response.status !== 200) {
+            throw new Error(await extractError(response));
+        }
+        return response.json();
+        // const mockRes = '{"results": [{"version": "1", "timestamp": 1515491537026, "operation": "INSERT", "operation_content": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}, {"version": "2", "timestamp": 1515491537346, "operation": "DELETE", "operation_content": {"operationParameters": {"mode": "Append","partitionBy": "[]"}}}]}'
+        // const mockRes = '{"results": []}'
+        // return JSON.parse(mockRes);
+    }
 }
 
 class Branches {
--- webui/src/lib/components/repository/TableDiff.jsx
+++ webui/src/lib/components/repository/TableDiff.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import React, {useState} from "react";
+import Table from "react-bootstrap/Table";
+import {ChevronDownIcon, ChevronRightIcon} from "@primer/octicons-react";
+import {OverlayTrigger} from "react-bootstrap";
+import Tooltip from "react-bootstrap/Tooltip";
+import {DiffType, OtfType} from "../../../constants";
+import {useAPI} from "../../hooks/api";
+import {repositories} from "../../api";
+import {Error, Loading} from "../controls";
+import Alert from "react-bootstrap/Alert";
 
-export const TableDiff = () => {
-    return <div>
-         This will be a table diff
-    </div>
+// The list of available operations is based on: https://docs.databricks.com/delta/history.html#operation-metrics-keys
+const deltaLakeOperationToDiffType = new Map([
+    ["WRITE" , DiffType.Added],
+    ["INSERT" , DiffType.Added], // We see this returning from describe history operation although not listed.
+    ["DELETE" , DiffType.Removed],
+    ["CREATE TABLE AS SELECT" , DiffType.Added],
+    ["REPLACE TABLE AS SELECT" , DiffType.Changed],
+    ["COPY INTO" , DiffType.Changed],
+    ["STREAMING UPDATE", DiffType.Changed],
+    ["TRUNCATE" , DiffType.Removed] ,
+    ["MERGE" , DiffType.Changed] ,
+    ["UPDATE" , DiffType.Changed] ,
+    ["FSCK" , DiffType.Removed] ,
+    ["CONVERT" , DiffType.Added] ,
+    ["OPTIMIZE" , DiffType.Changed],
+    ["RESTORE" , DiffType.Changed] ,
+    ["VACUUM" , DiffType.Removed],
+]);
+
+export const TableDiff = ({repo, leftRef, rightRef, tablePath}) => {
+    let response = useAPI(() => repositories.otfDiff(repo.id, leftRef.id, rightRef.id, tablePath, OtfType.Delta), [])
+    if (response && response.loading) return <Loading/>;
+    const err = response && response.error;
+    if (err) return <Error error={err}/>;
+
+    const otfDiffs = response.response.results;
+    return <>
+        {(otfDiffs.length === 0) ?  <Alert variant="info">No changes</Alert> :
+                <Table borderless size="md">
+                    <tbody>
+                    {
+                        response.response.results.map(otfDiff => {
+                            return <OtfDiffRow key={otfDiff.timestamp + "-diff-row"} otfDiff={otfDiff}/>;
+                        })
+                    }
+                    </tbody>
+                </Table>
+        }
+    </>
+}
+
+const OtfDiffRow = ({otfDiff}) => {
+    const [rowExpanded, setRowExpanded] = useState(false);
+    const rowClass = "otf-diff-" + operationToDiffType(otfDiff.operation);
+    return <>
+            <OperationMetadataRow className={rowClass} otfDiff={otfDiff} operationExpanded={rowExpanded} onExpand={() => setRowExpanded(!rowExpanded)}/>
+            {rowExpanded
+                ? <OperationDetailsRow otfDiff={otfDiff}/>
+            : ""}
+    </>
+}
+
+const OperationMetadataRow = ({otfDiff, operationExpanded, onExpand, ...rest}) => {
+    return <tr {...rest}>
+        <td className="pl-lg-10 col-10 table-operation-type">{otfDiff.operation}</td>
+        <td className="col-sm-1 table-version">Version = {otfDiff.version}</td>
+        <td className="col-sm-auto operation-expansion">
+            <OperationExpansionSection operationExpanded={operationExpanded} onExpand={onExpand}/>
+        </td>
+    </tr>
+
+}
+
+const OperationDetailsRow = ({otfDiff}) => {
+    const operationTimestamp = otfDiff.timestamp;
+    const operationContent = JSON.stringify(otfDiff.operation_content, null, 2);
+    return <tr className="otf-diff-operation-details">
+        <td className="pl-lg-10 col-10 operation-details">
+            <div>
+                <strong>Timestamp:</strong> {operationTimestamp}
+            </div>
+            <div>
+                <strong>Commit Info:</strong>
+                <pre>{operationContent}</pre>
+            </div>
+        </td>
+    </tr>
+}
+
+const OperationExpansionSection = ({operationExpanded, onExpand}) => {
+    return <OverlayTrigger placement="bottom" overlay={<Tooltip>{operationExpanded ? "Hide operation info" : "Show operation info"}</Tooltip>}>
+                <span onClick={onExpand}>{operationExpanded ? <ChevronDownIcon/> : <ChevronRightIcon/>}</span>
+           </OverlayTrigger>
+}
+
+function operationToDiffType(operation) {
+    const diffType = deltaLakeOperationToDiffType.get(operation);
+    return diffType !== undefined ? diffType : DiffType.Changed;
 }
