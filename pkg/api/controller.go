1583a9aa3720ca21d59d4726583a05558c9fda0c
Elad Lachmi <elad.lachmi@treeverse.io>
2023-01-31 14:56:36 +0200

    CR fixes

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -3512,12 +3512,12 @@ func (c *Controller) GetSetupState(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if savedState == auth.SetupStateNotInitialized {
+	if !c.Config.IsAuthTypeAPI() && savedState == auth.SetupStateNotInitialized {
 		c.Collector.CollectEvent(stats.Event{Class: "global", Name: "preinit", Client: httputil.GetRequestLakeFSClient(r)})
 	}
 	state := string(savedState)
 	// no need to create an admin user if users are managed externally
-	if c.Config.IsAuthTypeAPI() && savedState == auth.SetupStateCommPrefsDone {
+	if c.Config.IsAuthTypeAPI() {
 		state = string(auth.SetupStateInitialized)
 	}
 	response := SetupState{
