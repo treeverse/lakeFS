c353794b802f885489cfe5a6aa4ca921e958b391
N-o-Z <ozery.nir@gmail.com>
2023-02-01 17:35:27 +0200

    Update pkg/block/errors.go
    Co-authored-by: Barak Amar <barak.amar@treeverse.io>

--- pkg/block/errors.go
+++ pkg/block/errors.go
@@ -5,5 +5,5 @@ import "github.com/pkg/errors"
 var (
 	ErrDataNotFound          = errors.New("not found")
 	ErrOperationNotSupported = errors.New("operation not supported")
-	ErrAsyncCopy             = errors.New("async copy failure")
+	ErrAsyncCopyFailed             = errors.New("async copy failed")
 )
