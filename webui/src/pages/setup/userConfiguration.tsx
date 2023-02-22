3d845ff789022e6d71106eb2d8175d29cf319f40
Robin Moffatt <robin.moffatt@treeverse.io>
2023-02-22 14:24:03 +0000

    [DX] WebUI Setup: Replace first two screens with one (#5289)
    * - Set security optin by default
    - Set admin username default
    - Simplify wording and text usage
    
    * fix admin default value attribute
    
    * (WIP) Combine first two screens of setup flow
    
    * Make the error a bit friendlier
    
    * tidyup
    
    * Address review comment https://github.com/treeverse/lakeFS/pull/5289\#discussion_r1112662233
    
    * rename function to be more accurate
    
    * remove unused var
    
    * tidyup
    
    * keep Security optin as false

--- webui/src/pages/setup/userConfiguration.tsx
+++ webui/src/pages/setup/userConfiguration.tsx
@@ -6,29 +6,34 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import {Error} from "../../lib/components/controls";
 
-interface CommunicationPreferencesSetupProps {
-    onSubmit: (email: string, updatesCheck: boolean, securityCheck: boolean) => Promise<void>;
+interface UserConfigurationProps {
+    onSubmit: (email: string, admin: string, updatesCheck: boolean, securityCheck: boolean) => Promise<void>;
     setupError: Error;
     disabled: boolean;
 }
 
-export const CommunicationPreferencesSetup: FC<CommunicationPreferencesSetupProps> = ({
+export const UserConfiguration: FC<UserConfigurationProps> = ({
     onSubmit,
     setupError,
     disabled,
 }) => {
     const [userEmail, setUserEmail] = useState<string>("");
+    const [adminUser, setAdminUser] = useState<string>("admin");
     const [updatesCheck, setUpdatesCheck] = useState<boolean>(false);
     const [securityCheck, setSecurityCheck] = useState<boolean>(false);
 
     const submitHandler = useCallback((e: FormEvent) => {
-        onSubmit(userEmail, updatesCheck, securityCheck);
+        onSubmit(adminUser, userEmail, updatesCheck, securityCheck);
         e.preventDefault();
-    }, [onSubmit, userEmail, updatesCheck, securityCheck]);
+    }, [onSubmit, adminUser, userEmail, updatesCheck, securityCheck]);
 
     const handleEmailChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         setUserEmail(e.target.value);
     }, [setUserEmail]);
+
+    const handleAdminUserChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setAdminUser(e.target.value);
+    }, [setAdminUser]);
 
     const handleUpdatesChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         setUpdatesCheck(e.target.checked);
@@ -42,31 +47,36 @@ export const CommunicationPreferencesSetup: FC<CommunicationPreferencesSetupProp
         <Row>
             <Col md={{offset: 2, span: 8}}>
                 <Card className="setup-widget">
-                    <Card.Header>Communication Preferences</Card.Header>
+                    <Card.Header>User Configuration</Card.Header>
                     <Card.Body>
                         <Card.Text>
-                            Please provide your email below. <br />
-                            We recommend at least signing up for security updates. <br />
-                            You may unsubscribe from either or both at any time.
+                            Please specify the name of the first admin account to create, or leave it as the default.
                         </Card.Text>
+
                         <Form onSubmit={submitHandler}>
+
+                        <Form.Group controlId="user-name" className="mb-3">
+                            <Form.Control type="text" value={adminUser}  onChange={handleAdminUserChange} placeholder="Admin Username" autoFocus/>
+                        </Form.Group>
+
+                            <Card.Text>
+                            Please provide your email address, and indicate what optional communications you&apos;d like to receive:
+                            </Card.Text>
                             <Form.Group controlId="user-email" className="mt-4">
                                 <Form.Label>Email <span className="required-field-label">*</span></Form.Label>
-                                <Form.Control type="email" placeholder="name@company.com" value={userEmail}  onChange={handleEmailChange} autoFocus/>
+                                <Form.Control type="email" placeholder="name@company.com" value={userEmail}  onChange={handleEmailChange} />
                             </Form.Group>
 
                             <Form.Group controlId="updates-check" className="mt-4">
-                                <Form.Label>News & feature updates</Form.Label>
                                 <Form.Check type="checkbox" checked={updatesCheck} onChange={handleUpdatesChange} label="I'd like to receive news and feature updates" />
                             </Form.Group>
 
                             <Form.Group controlId="security-check" className="mt-4 mb-3">
-                                <Form.Label>Security updates</Form.Label>
-                                <Form.Check type="checkbox" checked={securityCheck} onChange={handleSecurityChange} label="I'd like to receive security updates" />
+                                <Form.Check type="checkbox" checked={securityCheck} onChange={handleSecurityChange} label="I'd like to receive security updates (recommended)" />
                             </Form.Group>
 
                             {!!setupError && <Error error={setupError}/>}
-                            <Button variant="primary" disabled={disabled} type="submit">Continue</Button>
+                            <Button variant="primary" disabled={disabled} type="submit">Setup</Button>
                         </Form>
                     </Card.Body>
                 </Card>
