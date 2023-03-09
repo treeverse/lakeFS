c2ab0b42a2119d81264ec42f9aab00bb6fbd6f72
johnnyaug <yoni.augarten@treeverse.io>
2023-03-09 14:55:49 +0700

    cleanup

--- clients/hadoopfs/src/main/java/io/lakefs/storage/PresignedStorageAccessStrategy.java
+++ clients/hadoopfs/src/main/java/io/lakefs/storage/PresignedStorageAccessStrategy.java
@@ -39,7 +39,7 @@ public class PresignedStorageAccessStrategy implements StorageAccessStrategy {
         System.out.println("presignedUrl: " + presignedUrl); // TODO(johnnyaug) remove this print
         HttpURLConnection connection = (HttpURLConnection) presignedUrl.openConnection();
         connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/octet-stream"); // TODO be better than this
+        connection.setRequestProperty("Content-Type", "application/octet-stream");
         connection.setRequestMethod("PUT");
         OutputStream out = new LakeFSFileSystemOutputStream(lakeFSFileSystem, lfsClient, connection, objectLocation,
                 stagingLocation);
--- clients/hadoopfs/src/main/java/io/lakefs/storage/StorageAccessStrategy.java
+++ clients/hadoopfs/src/main/java/io/lakefs/storage/StorageAccessStrategy.java
@@ -12,19 +12,6 @@ import io.lakefs.clients.api.ApiException;
 import io.lakefs.utils.ObjectLocation;
 
 public interface StorageAccessStrategy {
-
-    static URI translateUri(String blockstoreType, URI uri) throws java.net.URISyntaxException {
-        switch (blockstoreType) {
-            case "s3":
-                return new URI("s3a", uri.getUserInfo(), uri.getHost(), uri.getPort(), uri.getPath(), uri.getQuery(),
-                        uri.getFragment());
-            case "azure":
-                // TODO(johnnyaug) - translate https:// style to abfs://
-            default:
-                throw new RuntimeException(String.format("lakeFS has unsupported blockstore type %s", blockstoreType));
-        }
-    }
-
     public FSDataOutputStream createDataOutputStream(ObjectLocation objectLocation, CreateOutputStreamParams params)
             throws ApiException, IOException;
 
