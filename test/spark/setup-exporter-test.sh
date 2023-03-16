ae3d1526ca83dc7324bc1a709a1958c145b13d52
Yoni <yoni.augarten@treeverse.io>
2023-03-16 18:35:20 +0200

    Esti: Avoid using bash (#5481)
    * python from bash
    
    * setup python
    
    * skipimg
    
    * fix yaml
    
    * delete non esti
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * add req.txt
    
    * add req.txt
    
    * add req.txt
    
    * typo
    
    * thick client
    
    * stream output
    
    * maven skip tests
    
    * reorg
    
    * errrr
    
    * try split spark app
    
    * fix typo
    
    * fix path
    
    * fix path
    
    * one app
    
    * typo
    
    * --
    
    * typo
    
    * python more
    
    * re-re
    
    * don't remove?
    
    * cleaning
    
    * typo
    
    * try again
    
    * try again
    
    * fix typo
    
    * fix commprefs
    
    * setup in compose
    
    * revert mess
    
    * revert
    
    * matrixify spark tests
    
    * typos
    
    * fix structure
    
    * add spark 2 to matrix
    
    * fix filename
    
    * add wait for setup
    
    * add wait for setup
    
    * compose new syntax?

--- test/spark/setup-exporter-test.sh
+++ test/spark/setup-exporter-test.sh
@@ -4,6 +4,5 @@
 
 docker-compose exec -T lakefs /app/wait-for localhost:8000
 
-
 docker-compose exec -T lakefs lakectl repo create-bare lakefs://${REPOSITORY//./-} ${STORAGE_NAMESPACE}
 docker-compose exec -T lakefs lakectl refs-restore lakefs://${REPOSITORY//./-} --manifest /local/refs.json
