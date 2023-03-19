2436c582a00a0bc44afa2fa22825d2facb1489b6
johnnyaug <yoni.augarten@treeverse.io>
2023-03-19 14:16:22 +0200

    cleanup

--- clients/hadoopfs/src/main/java/io/lakefs/storage/PresignedStorageAccessStrategy.java
+++ clients/hadoopfs/src/main/java/io/lakefs/storage/PresignedStorageAccessStrategy.java
@@ -38,7 +38,6 @@ public class PresignedStorageAccessStrategy implements StorageAccessStrategy {
                 stagingApi.getPhysicalAddress(objectLocation.getRepository(),
                         objectLocation.getRef(), objectLocation.getPath(), true);
         URL presignedUrl = new URL(stagingLocation.getPresignedUrl());
-        System.out.println("presignedUrl: " + presignedUrl); // TODO(johnnyaug) remove this print
         HttpURLConnection connection = (HttpURLConnection) presignedUrl.openConnection();
         connection.setDoOutput(true);
         connection.setRequestProperty("Content-Type", "application/octet-stream");
