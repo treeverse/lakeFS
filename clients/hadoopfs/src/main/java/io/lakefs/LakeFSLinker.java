af7e9eb515267c0da63bbdbe9e7192c317a719b7
johnnyaug <yoni.augarten@treeverse.io>
2023-03-29 18:38:16 +0300

    HadoopFS defensive linking

--- clients/hadoopfs/src/main/java/io/lakefs/LakeFSLinker.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LakeFSLinker.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import org.apache.hadoop.fs.Path;
 import io.lakefs.clients.api.ApiException;
 import io.lakefs.clients.api.StagingApi;
+import io.lakefs.clients.api.model.ObjectStats;
 import io.lakefs.clients.api.model.StagingLocation;
 import io.lakefs.clients.api.model.StagingMetadata;
 import io.lakefs.utils.ObjectLocation;
@@ -30,7 +31,15 @@ public class LakeFSLinker {
             staging.linkPhysicalAddress(objectLoc.getRepository(), objectLoc.getRef(),
                     objectLoc.getPath(), stagingMetadata);
         } catch (ApiException e) {
-            throw new IOException("link lakeFS path to physical address", e);
+            try {
+                ObjectStats stats = lakeFSClient.getObjectsApi().statObject(objectLoc.getRepository(),
+                        objectLoc.getRef(), objectLoc.getPath(), false, false);
+                if (!stats.getChecksum().equals(eTag)) {
+                    throw e;
+                }
+            } catch (ApiException e1) {
+                throw new IOException("link lakeFS path to physical address", e);
+            }
         }
         lfs.deleteEmptyDirectoryMarkers(new Path(objectLoc.toString()).getParent());
     }
