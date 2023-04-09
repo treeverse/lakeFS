2a215280ef7937f011c5b5195fefc733359b26c5
guyhardonag <guy.hardonag@treeverse.io>
2023-04-09 16:50:31 +0300

    Revert "Remove simplified configuration and conclude from auth api"
    This reverts commit 13ea9fef1f537d7d1d62a9197b8d9e0d92728f7e.

--- pkg/auth/setup/setup.go
+++ pkg/auth/setup/setup.go
@@ -261,7 +261,7 @@ func CreateInitialAdminUserWithKeys(ctx context.Context, authService auth.Servic
 }
 
 func SetupBaseGroups(ctx context.Context, authService auth.Service, cfg *config.Config, ts time.Time) error {
-	if !cfg.IsAuthTypeAPI() {
+	if cfg.IsAuthUISimplified() {
 		return SetupACLBaseGroups(ctx, authService, ts)
 	}
 	return SetupRBACBaseGroups(ctx, authService, ts)
