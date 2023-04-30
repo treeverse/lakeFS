698d4c0991d69c539c07631d1e7b8fdf4ffc280a
johnnyaug <yoni.augarten@treeverse.io>
2023-04-30 18:22:32 +0300

    explore

--- clients/spark/core/src/main/scala/io/treeverse/clients/GarbageCollector.scala
+++ clients/spark/core/src/main/scala/io/treeverse/clients/GarbageCollector.scala
@@ -191,10 +191,10 @@ class GarbageCollector(val rangeGetter: RangeGetter) extends Serializable {
     val partitioner = new HashPartitioner(numAddressPartitions)
     val addresses = readRanges(dedupedRangeIDs, "ranges")
       .aggregateByKey(true, partitioner)(shouldExpire, shouldExpire)
-      .persist(StorageLevel.MEMORY_AND_DISK_SER)
+      .persist(StorageLevel.MEMORY_ONLY_SER)
 
     // Report some rows on expiry proportions
-    addresses.sample(true, 0.01).toDS.show()
+//    addresses.sample(true, 0.01).toDS.show()
 
     addresses
       .filter({ case (_, expire) => expire })
@@ -216,7 +216,7 @@ class GarbageCollector(val rangeGetter: RangeGetter) extends Serializable {
     val commitsDS = getCommitsDF(commitDFLocation)
       .as[(String, Boolean)]
       .repartition(numRangePartitions)
-      .persist(StorageLevel.MEMORY_AND_DISK_SER)
+      .persist(StorageLevel.MEMORY_ONLY_SER)
     val rangeIDs = getRangeIDsForCommits(commitsDS, repo)
 
     try {
@@ -499,7 +499,7 @@ object GarbageCollector {
                           )
       .toDF("address")
       .withColumn(MARK_ID_KEY, lit(markID))
-      .persist(StorageLevel.MEMORY_AND_DISK_SER)
+      .persist(StorageLevel.MEMORY_ONLY_SER)
 
     spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
     expiredAddresses.write
@@ -509,7 +509,7 @@ object GarbageCollector {
 
     println(f"Total expired addresses: ${expiredAddresses.count()}")
     println("Expired addresses:")
-    expiredAddresses.show()
+//    expiredAddresses.show()
 
     if (hc.getBoolean(LAKEFS_CONF_GC_WRITE_EXPIRED_AS_TEXT, true)) {
       // Enable source for rclone backup and resource
