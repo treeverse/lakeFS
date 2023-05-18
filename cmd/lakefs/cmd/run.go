71683ebfecdfd706e8776d85d2a49955b2e8fea0
rmoff <robin.moffatt@treeverse.io>
2023-05-18 10:39:15 +0100

    Update quickstart URL

--- cmd/lakefs/cmd/run.go
+++ cmd/lakefs/cmd/run.go
@@ -440,7 +440,7 @@ const runBanner = `
 
 │
 │ For more information on how to use lakeFS,
-│     check out the docs at https://docs.lakefs.io/quickstart/repository
+│     check out the docs at https://docs.lakefs.io/quickstart/
 │
 
 │
