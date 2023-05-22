32058c41fc8de08a0c601e77a1abb8cdafc7e016
Elad Lachmi <elad.lachmi@treeverse.io>
2023-05-22 20:39:16 +0300

    Review fixes

---
+++ webui/src/lib/api/apiUtils.tsx
@@ -0,0 +1,27 @@
+import { branches } from "./index";
+
+export const findBranchRegexMatch = async (
+  repoId: string,
+  prefix: string,
+  regex: string | RegExp
+): Promise<string | null> => {
+  let hasMore = true;
+  let after: string | undefined;
+  while (hasMore) {
+    const { results: branchList, pagination } = await branches.list(
+      repoId,
+      prefix,
+      after,
+      100
+    );
+    // Once we type the API we can remove the eslint-disable
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const branch = branchList.find((branch: any) => branch.id.match(regex));
+    if (branch) {
+      return branch.id;
+    }
+    hasMore = pagination.has_more;
+    after = pagination.next_offset;
+  }
+  return null;
+};
--- webui/src/lib/onboarding/repoOnboardingService.tsx
+++ webui/src/lib/onboarding/repoOnboardingService.tsx
@@ -2,21 +2,21 @@
 import React from "react";
 import { OnboardingStep } from "./types";
 import { NavigateFunction } from "react-router-dom";
-import { branches, retention, branchProtectionRules, objects, NotFoundError } from "../api";
+import {
+  retention,
+  branchProtectionRules,
+  objects,
+  NotFoundError,
+} from "../api";
+import { findBranchRegexMatch } from "../api/apiUtils";
 
 const IMPORT_BRANCH_REGEX = /^_(.*)_imported$/;
 const HOOKS_PATH = "_lakefs_actions/";
 const SAMPLE_REPO_OBJECT_PATH = "lakes.parquet";
 
-// generate a random file name to avoid collisions for the upload preflight check
-const generateRandomFileName = (): string => {
-  const randomString = Math.random().toString(36).substring(2, 15);
-  return `sample_${randomString}.yaml`;
-};
-
 export const isSampleRepo = async (
   repo: string,
-  defaultBranch: string,
+  defaultBranch: string
 ): Promise<boolean> => {
   try {
     await objects.head(repo, defaultBranch, SAMPLE_REPO_OBJECT_PATH);
@@ -24,23 +24,18 @@ export const isSampleRepo = async (
   } catch (e) {
     return false;
   }
-  
 };
 
 export const canUseRepoOnboarding = async (
   repoId: string,
-  defaultBranch: string,
+  defaultBranch: string
 ): Promise<boolean> => {
   const promises = [
     retention.setGCPolicyPreflight(repoId),
     branchProtectionRules.createRulePreflight(repoId),
-    objects.uploadPreflight(
-      repoId,
-      defaultBranch,
-      `${HOOKS_PATH}${generateRandomFileName()}`
-    ),
+    objects.uploadPreflight(repoId, defaultBranch, `${HOOKS_PATH}sample_hook`),
   ];
-  
+
   try {
     await Promise.all(promises);
     return true;
@@ -60,21 +55,50 @@ export const getRepoOnboardingSteps = (
     title: "Import your existing data",
     description: (
       <>
-        <p>Import your existing data from ${objectStoreName} to your repository by creating pointers to it (no copying of data involved).</p>
-        <p>Please see our documentation for information on the <a href="https://docs.lakefs.io/howto/import.html#limitations" target="_blank">limitations of the import process</a> and <a href="https://docs.lakefs.io/howto/import.html#working-with-imported-data" target="_blank">working with imported data</a></p>,
+        <p>
+          Import your existing data from ${objectStoreName} to your repository
+          by creating pointers to it (no copying of data involved).
+        </p>
+        <p>
+          Please see our documentation for information on the{" "}
+          <a
+            href="https://docs.lakefs.io/howto/import.html#limitations"
+            target="_blank"
+          >
+            limitations of the import process
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://docs.lakefs.io/howto/import.html#working-with-imported-data"
+            target="_blank"
+          >
+            working with imported data
+          </a>
+        </p>
+        ,
       </>
     ),
     cta: "Run import",
     onClick: () =>
       navigate(`/repositories/${currentRepo}/objects?importDialog=true`),
-    showStep: () => objectStoreName !== "local",
+    showStep: () => true, // objectStoreName !== "local",
     isCompleted: async () => {
-      const repoBranches = await branches.list(currentRepo, "_");
-      // when the API client is typed, we can remove the eslint-disable
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return repoBranches.results.some((branch: any) =>
-        IMPORT_BRANCH_REGEX.test(branch.id)
-      );
+      try {
+        const branch = await findBranchRegexMatch(
+          currentRepo,
+          "_",
+          IMPORT_BRANCH_REGEX
+        );
+        if (branch) {
+          return true;
+        }
+        return false;
+      } catch (e) {
+        if (e instanceof NotFoundError) {
+          return false;
+        }
+        return true;
+      }
     },
   },
   {
@@ -82,8 +106,20 @@ export const getRepoOnboardingSteps = (
     title: "Setup garbage collection",
     description: (
       <>
-        <p>Set up rules to automatically manage when data can be deleted from your repository</p>
-        <p>For more information about setting up and running garbage collection on your lakeFS repository, see the <a href="https://docs.lakefs.io/howto/garbage-collection-committed.html" target="_blank">documentation</a></p>
+        <p>
+          Set up rules to automatically manage when data can be deleted from
+          your repository
+        </p>
+        <p>
+          For more information about setting up and running garbage collection
+          on your lakeFS repository, see the{" "}
+          <a
+            href="https://docs.lakefs.io/howto/garbage-collection-committed.html"
+            target="_blank"
+          >
+            documentation
+          </a>
+        </p>
       </>
     ),
     cta: "Configure",
@@ -107,8 +143,11 @@ export const getRepoOnboardingSteps = (
     description: (
       <>
         <p>You can protect important branches with branch protection rules.</p>
-        <p>Protected branches cannot be changed directly. Any changes are applied only via merging from other branches. This helps protect those branches from bugs in your code as well as human error.</p>
-
+        <p>
+          Protected branches cannot be changed directly. Any changes are applied
+          only via merging from other branches. This helps protect those
+          branches from bugs in your code as well as human error.
+        </p>
       </>
     ),
     cta: "Configure",
@@ -137,7 +176,14 @@ export const getRepoOnboardingSteps = (
     showStep: () => true,
     isCompleted: async () => {
       try {
-        const objectList = await objects.list(currentRepo, defaultBranch, HOOKS_PATH, undefined, undefined, 1);
+        const objectList = await objects.list(
+          currentRepo,
+          defaultBranch,
+          HOOKS_PATH,
+          undefined,
+          undefined,
+          1
+        );
         return objectList?.results?.length > 0;
       } catch (e) {
         if (e instanceof NotFoundError) {
