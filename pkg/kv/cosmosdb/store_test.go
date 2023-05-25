732ed6fbe1985974fe91b043cfd77d55b68f8e62
Itai Admi <itaiad200@gmail.com>
2023-05-25 14:49:21 +0300

    Add cosmosdb esti test

--- pkg/kv/cosmosdb/store_test.go
+++ pkg/kv/cosmosdb/store_test.go
@@ -9,5 +9,7 @@ import (
 )
 
 func TestCosmosDB(t *testing.T) {
+	t.Skip("CosmosDB tests are flaky due to the emulator. If you plan on running those, make sure to assign at least 3CPUs and" +
+		" 4GB of memory to the container running the emulator.")
 	kvtest.DriverTest(t, cosmosdb.DriverName, kvparams.Config{CosmosDB: testParams})
 }
