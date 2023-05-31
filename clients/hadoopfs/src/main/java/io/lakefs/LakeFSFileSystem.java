27d99d0dcb9f838afe1bc53255d8b64bc7dfa529
johnnyaug <yoni.augarten@treeverse.io>
2023-05-31 12:03:23 +0200

    catch all exception when trying fsforconfig

--- clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
@@ -162,7 +162,7 @@ public class LakeFSFileSystem extends FileSystem {
             URI storageURI = URI.create(storageNamespace);
             Path physicalPath = new Path(physicalAddressTranslator.translate(storageURI));
             fsForConfig = physicalPath.getFileSystem(conf);
-        } catch (ApiException | URISyntaxException | IOException e) {
+        } catch (Exception e) {
             failedFSForConfig = true;
             LOG.warn("get underlying filesystem for {}: {} (use default values)", path, e);
         }
