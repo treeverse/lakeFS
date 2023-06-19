dc5d233f92af0b9d594261f77d0577320a64114c
Barak Amar <barak.amar@treeverse.io>
2023-06-19 10:59:22 +0300

    Fix: Create repo with invalid namespace (#6084)
    * Fix: Create repo with invalid namespace
    
    * CR Fixes
    
    * Fix validation before create bare repo
    
    * CR Fixes 2

--- pkg/block/azure/adapter_test.go
+++ pkg/block/azure/adapter_test.go
@@ -2,6 +2,7 @@ package azure_test
 
 import (
 	"net/url"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -24,6 +25,58 @@ func TestAzureAdapter(t *testing.T) {
 		TestEndpointURL:  blockURL,
 	})
 	require.NoError(t, err, "create new adapter")
-
 	blocktest.AdapterTest(t, adapter, localPath, externalPath)
+}
+
+func TestAdapterNamespace(t *testing.T) {
+	adapter, err := azure.NewAdapter(params.Azure{
+		StorageAccount:   accountName,
+		StorageAccessKey: accountKey,
+		TestEndpointURL:  blockURL,
+	})
+
+	expr, err := regexp.Compile(adapter.GetStorageNamespaceInfo().ValidityRegex)
+	require.NoError(t, err)
+
+	tests := []struct {
+		Name      string
+		Namespace string
+		Success   bool
+	}{
+		{
+			Name:      "valid_https",
+			Namespace: "https://test.blob.core.windows.net/container1/repo1",
+			Success:   true,
+		},
+		{
+			Name:      "valid_http",
+			Namespace: "http://test.blob.core.windows.net/container1/repo1",
+			Success:   true,
+		},
+		{
+			Name:      "invalid_subdomain",
+			Namespace: "https://test.adls.core.windows.net/container1/repo1",
+			Success:   false,
+		},
+		{
+			Name:      "partial",
+			Namespace: "https://test.adls.core.windows.n",
+			Success:   false,
+		},
+		{
+			Name:      "s3",
+			Namespace: "s3://test/adls/core/windows/net",
+			Success:   false,
+		},
+		{
+			Name:      "invalid_string",
+			Namespace: "this is a bad string",
+			Success:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			require.Equal(t, tt.Success, expr.MatchString(tt.Namespace))
+		})
+	}
 }
