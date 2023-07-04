a977804edc5e52fad63ea2d456a51b88c92acb95
Barak Amar <barak.amar@treeverse.io>
2023-07-04 11:10:51 +0300

    test: refactor esti env cleanup (#6159)
    * test: refactor esti env cleanup
    
    - use multi error
    - loop with single call for pagination
    - fix: use single logger
    - fix: don't use response when err is not nil
    - fix: don't use logger before initialized
    
    * Fix webhook server

--- esti/main_test.go
+++ esti/main_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"regexp"
@@ -13,15 +12,46 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/hashicorp/go-multierror"
 	"github.com/spf13/viper"
 	"github.com/treeverse/lakefs/pkg/api"
 	"github.com/treeverse/lakefs/pkg/logging"
 	"github.com/treeverse/lakefs/pkg/testutil"
+	"golang.org/x/exp/slices"
+)
+
+const (
+	DefaultAdminAccessKeyID     = "AKIAIOSFDNN7EXAMPLEQ"
+	DefaultAdminSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 )
 
 type (
 	Booleans   []bool
 	arrayFlags []string
+)
+
+var (
+	logger      logging.Logger
+	client      api.ClientWithResponsesInterface
+	endpointURL string
+	svc         *s3.S3
+	server      *webhookServer
+
+	testDirectDataAccess = Booleans{false}
+
+	metaClientJarPath  string
+	sparkImageTag      string
+	repositoriesToKeep arrayFlags
+	groupsToKeep       arrayFlags
+	usersToKeep        arrayFlags
+	policiesToKeep     arrayFlags
+
+	testsToSkipRegex *regexp.Regexp
+)
+
+var (
+	azureStorageAccount   string
+	azureStorageAccessKey string
 )
 
 func (bs *Booleans) String() string {
@@ -58,220 +88,172 @@ func (i *arrayFlags) Set(value string) error {
 	return nil
 }
 
-const (
-	DefaultAdminAccessKeyId     = "AKIAIOSFDNN7EXAMPLEQ"
-	DefaultAdminSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-)
-
-var (
-	logger      logging.Logger
-	client      api.ClientWithResponsesInterface
-	endpointURL string
-	svc         *s3.S3
-	server      *webhookServer
-
-	testDirectDataAccess = Booleans{false}
-
-	metaclientJarPath  string
-	sparkImageTag      string
-	repositoriesToKeep arrayFlags
-	groupsToKeep       arrayFlags
-	usersToKeep        arrayFlags
-	policiesToKeep     arrayFlags
-
-	testsToSkipRegex *regexp.Regexp
-)
-
-var (
-	azureStorageAccount   string
-	azureStorageAccessKey string
-)
-
-func arrayFlagsToMap(arr arrayFlags) map[interface{}]bool {
-	retMap := map[interface{}]bool{}
-	for _, element := range arr {
-		retMap[element] = true
-	}
-	return retMap
-}
-
-func envCleanup(client api.ClientWithResponsesInterface, repositoriesToKeep, groupsToKeep, usersToKeep, policiesToKeep arrayFlags, logger logging.Logger) error {
+func envCleanup(client api.ClientWithResponsesInterface, repositoriesToKeep, groupsToKeep, usersToKeep, policiesToKeep arrayFlags) error {
 	ctx := context.Background()
-	err := deleteAllRepositories(ctx, client, repositoriesToKeep, logger)
-	if err != nil {
-		return err
-	}
-
-	err = deleteAllGroups(ctx, client, groupsToKeep, logger)
-	if err != nil {
-		return err
-	}
-
-	err = deleteAllPolicies(ctx, client, policiesToKeep, logger)
-	if err != nil {
-		return err
-	}
-
-	err = deleteAllUsers(ctx, client, usersToKeep, logger)
-	return err
+	errRepos := deleteAllRepositories(ctx, client, repositoriesToKeep)
+	errGroups := deleteAllGroups(ctx, client, groupsToKeep)
+	errPolicies := deleteAllPolicies(ctx, client, policiesToKeep)
+	errUsers := deleteAllUsers(ctx, client, usersToKeep)
+	return multierror.Append(errRepos, errGroups, errPolicies, errUsers).ErrorOrNil()
 }
 
-func deleteAllRepositories(ctx context.Context, client api.ClientWithResponsesInterface, repositoriesToKeep arrayFlags, logger logging.Logger) error {
-	repositoriesToNotDelete := arrayFlagsToMap(repositoriesToKeep)
-	listRepositoriesResponse, err := client.ListRepositoriesWithResponse(ctx, &api.ListRepositoriesParams{})
-	if err != nil {
-		return err
-	}
-	if listRepositoriesResponse.StatusCode() != http.StatusOK {
-		return fmt.Errorf("unexpected status code for list repositories request: %d", listRepositoriesResponse.StatusCode())
-	}
-	hasMore := true
-	for hasMore {
-		for _, repo := range listRepositoriesResponse.JSON200.Results {
-			if !repositoriesToNotDelete[repo.Id] {
-				deleteRepositoryResponse, err := client.DeleteRepositoryWithResponse(ctx, repo.Id)
-				if err != nil {
-					logger.Warnf("failed to delete repository: %s. err: %s", repo.Id, err.Error())
-					continue
-				}
-				if deleteRepositoryResponse.StatusCode() != http.StatusNoContent {
-					logger.Warnf("unexpected status code when trying to delete repository: %s, status code: %s", repo.Id, deleteRepositoryResponse.Status())
-				}
+func deleteAllRepositories(ctx context.Context, client api.ClientWithResponsesInterface, repositoriesToKeep arrayFlags) error {
+	// collect repositories to delete
+	var (
+		repositoriesToDelete []string
+		nextOffset           string
+	)
+
+	for {
+		resp, err := client.ListRepositoriesWithResponse(ctx, &api.ListRepositoriesParams{After: api.PaginationAfterPtr(nextOffset)})
+		if err != nil {
+			return fmt.Errorf("list repositories: %w", err)
+		}
+		if resp.StatusCode() != http.StatusOK {
+			return fmt.Errorf("list repositories: status: %s", resp.Status())
+		}
+		for _, repo := range resp.JSON200.Results {
+			if !slices.Contains(repositoriesToKeep, repo.Id) {
+				repositoriesToDelete = append(repositoriesToDelete, repo.Id)
 			}
 		}
-		hasMore = listRepositoriesResponse.JSON200.Pagination.HasMore
-		if hasMore {
-			nextOffset := api.PaginationAfter(listRepositoriesResponse.JSON200.Pagination.NextOffset)
-			listRepositoriesResponse, err = client.ListRepositoriesWithResponse(ctx, &api.ListRepositoriesParams{After: &nextOffset})
-			if err != nil {
-				return err
-			}
-			if listRepositoriesResponse.StatusCode() != http.StatusOK {
-				return fmt.Errorf("unexpected status code for list repositories request: %d", listRepositoriesResponse.StatusCode())
-			}
+		if !resp.JSON200.Pagination.HasMore {
+			break
 		}
+		nextOffset = resp.JSON200.Pagination.NextOffset
 	}
-	return err
+
+	var errs *multierror.Error
+	for _, id := range repositoriesToDelete {
+		resp, err := client.DeleteRepositoryWithResponse(ctx, id)
+		if err != nil {
+			errs = multierror.Append(errs, fmt.Errorf("delete repository: %s, err: %w", id, err))
+		} else if resp.StatusCode() != http.StatusNoContent {
+			errs = multierror.Append(errs, fmt.Errorf("delete repository: %s, status: %s", id, resp.Status()))
+		}
+
+	}
+	return errs.ErrorOrNil()
 }
 
-func deleteAllGroups(ctx context.Context, client api.ClientWithResponsesInterface, groupsToKeep arrayFlags, logger logging.Logger) error {
-	groupsToNotDelete := arrayFlagsToMap(groupsToKeep)
+func deleteAllGroups(ctx context.Context, client api.ClientWithResponsesInterface, groupsToKeep arrayFlags) error {
+	// list groups to delete
+	var (
+		groupsToDelete []string
+		nextOffset     string
+	)
+	for {
+		resp, err := client.ListGroupsWithResponse(ctx, &api.ListGroupsParams{After: api.PaginationAfterPtr(nextOffset)})
+		if err != nil {
+			return fmt.Errorf("list groups: %w", err)
+		}
+		if resp.StatusCode() != http.StatusOK {
+			return fmt.Errorf("list groups: status: %s", resp.Status())
+		}
+		for _, group := range resp.JSON200.Results {
+			if !slices.Contains(groupsToKeep, group.Id) {
+				groupsToDelete = append(groupsToDelete, group.Id)
+			}
+		}
+		if !resp.JSON200.Pagination.HasMore {
+			break
+		}
+		nextOffset = resp.JSON200.Pagination.NextOffset
+	}
 
-	listGroupsResponse, err := client.ListGroupsWithResponse(ctx, &api.ListGroupsParams{})
-	if err != nil {
-		return err
-	}
-	if listGroupsResponse.StatusCode() != http.StatusOK {
-		return fmt.Errorf("unexpected status code for list groups request: %d", listGroupsResponse.StatusCode())
-	}
-	hasMore := true
-	for hasMore {
-		for _, group := range listGroupsResponse.JSON200.Results {
-			if !groupsToNotDelete[group.Id] {
-				deleteGroupResponse, err := client.DeleteGroupWithResponse(ctx, group.Id)
-				if err != nil {
-					logger.Warnf("failed to delete group: %s. err: %s", group.Id, err.Error())
-				}
-				if deleteGroupResponse.StatusCode() != http.StatusNoContent {
-					logger.Warnf("unexpected status code when trying to delete group: %s, status code: %d", group.Id, deleteGroupResponse.StatusCode())
-				}
-			}
-		}
-		hasMore = listGroupsResponse.JSON200.Pagination.HasMore
-		if hasMore {
-			nextOffset := api.PaginationAfter(listGroupsResponse.JSON200.Pagination.NextOffset)
-			listGroupsResponse, err = client.ListGroupsWithResponse(ctx, &api.ListGroupsParams{After: &nextOffset})
-			if err != nil {
-				return err
-			}
-			if listGroupsResponse.StatusCode() != http.StatusOK {
-				return fmt.Errorf("unexpected status code for list groups request: %d", listGroupsResponse.StatusCode())
-			}
+	// delete groups
+	var errs *multierror.Error
+	for _, id := range groupsToDelete {
+		resp, err := client.DeleteGroupWithResponse(ctx, id)
+		if err != nil {
+			errs = multierror.Append(errs, fmt.Errorf("delete group: %s, err: %w", id, err))
+		} else if resp.StatusCode() != http.StatusNoContent {
+			errs = multierror.Append(errs, fmt.Errorf("delete group: %s, status: %s", id, resp.Status()))
 		}
 	}
-	return err
+	return errs.ErrorOrNil()
 }
 
-func deleteAllUsers(ctx context.Context, client api.ClientWithResponsesInterface, usersToKeep arrayFlags, logger logging.Logger) error {
-	usersToNotDelete := arrayFlagsToMap(usersToKeep)
+func deleteAllUsers(ctx context.Context, client api.ClientWithResponsesInterface, usersToKeep arrayFlags) error {
+	// collect users to delete
+	var (
+		usersToDelete []string
+		nextOffset    string
+	)
+	for {
+		resp, err := client.ListUsersWithResponse(ctx, &api.ListUsersParams{After: api.PaginationAfterPtr(nextOffset)})
+		if err != nil {
+			return fmt.Errorf("list users: %s", err)
+		}
+		if resp.JSON200 == nil {
+			return fmt.Errorf("list users, status: %s", resp.Status())
+		}
+		for _, user := range resp.JSON200.Results {
+			if !slices.Contains(usersToKeep, user.Id) {
+				usersToKeep = append(usersToDelete, user.Id)
+			}
+		}
+		if !resp.JSON200.Pagination.HasMore {
+			break
+		}
+		nextOffset = resp.JSON200.Pagination.NextOffset
+	}
 
-	listUsersResponse, err := client.ListUsersWithResponse(ctx, &api.ListUsersParams{})
-	if err != nil {
-		return err
-	}
-	if listUsersResponse.StatusCode() != http.StatusOK {
-		return fmt.Errorf("unexpected status code for list users request: %d", listUsersResponse.StatusCode())
-	}
-	hasMore := true
-	for hasMore {
-		for _, user := range listUsersResponse.JSON200.Results {
-			if !usersToNotDelete[user.Id] {
-				deleteUserResponse, err := client.DeleteUserWithResponse(ctx, user.Id)
-				if err != nil {
-					logger.Warnf("failed to delete user: %s. err: %s", user.Id, err.Error())
-				}
-				if deleteUserResponse.StatusCode() != http.StatusNoContent {
-					logger.Warnf("unexpected status code when trying to delete user: %s, status code: %d", user.Id, deleteUserResponse.StatusCode())
-				}
-			}
-		}
-		hasMore = listUsersResponse.JSON200.Pagination.HasMore
-		if hasMore {
-			nextOffset := api.PaginationAfter(listUsersResponse.JSON200.Pagination.NextOffset)
-			listUsersResponse, err = client.ListUsersWithResponse(ctx, &api.ListUsersParams{After: &nextOffset})
-			if err != nil {
-				return err
-			}
-			if listUsersResponse.StatusCode() != http.StatusOK {
-				return fmt.Errorf("unexpected status code for list users request: %d", listUsersResponse.StatusCode())
-			}
+	// delete users
+	var errs *multierror.Error
+	for _, id := range usersToDelete {
+		resp, err := client.DeleteUserWithResponse(ctx, id)
+		if err != nil {
+			errs = multierror.Append(errs, fmt.Errorf("delete user %s: %w", id, err))
+		} else if resp.StatusCode() != http.StatusNoContent {
+			errs = multierror.Append(errs, fmt.Errorf("delete user %s, status: %s", id, resp.Status()))
 		}
 	}
-	return err
+	return errs.ErrorOrNil()
 }
 
-func deleteAllPolicies(ctx context.Context, client api.ClientWithResponsesInterface, policiesToKeep arrayFlags, logger logging.Logger) error {
-	policiesToNotDelete := arrayFlagsToMap(policiesToKeep)
+func deleteAllPolicies(ctx context.Context, client api.ClientWithResponsesInterface, policiesToKeep arrayFlags) error {
+	// list policies to delete
+	var (
+		policiesToDelete []string
+		nextOffset       string
+	)
+	for {
+		resp, err := client.ListPoliciesWithResponse(ctx, &api.ListPoliciesParams{After: api.PaginationAfterPtr(nextOffset)})
+		if err != nil {
+			return fmt.Errorf("list policies: %w", err)
+		}
+		if resp.JSON200 == nil {
+			return fmt.Errorf("list policies, status: %s", resp.Status())
+		}
+		for _, policy := range resp.JSON200.Results {
+			if !slices.Contains(policiesToKeep, policy.Id) {
+				policiesToDelete = append(policiesToDelete, policy.Id)
+			}
+		}
+		if !resp.JSON200.Pagination.HasMore {
+			break
+		}
+		nextOffset = resp.JSON200.Pagination.NextOffset
+	}
 
-	listPoliciesResponse, err := client.ListPoliciesWithResponse(ctx, &api.ListPoliciesParams{})
-	if err != nil {
-		return err
-	}
-	if listPoliciesResponse.StatusCode() != http.StatusOK {
-		return fmt.Errorf("unexpected status code for list policies request: %d", listPoliciesResponse.StatusCode())
-	}
-	hasMore := true
-	for hasMore {
-		for _, policy := range listPoliciesResponse.JSON200.Results {
-			if !policiesToNotDelete[policy.Id] {
-				deletePolicyResponse, err := client.DeletePolicyWithResponse(ctx, policy.Id)
-				if err != nil {
-					logger.Warnf("failed to delete policy: %s. err: %s", policy.Id, err.Error())
-				}
-				if deletePolicyResponse.StatusCode() != http.StatusNoContent {
-					logger.Warnf("unexpected status code when trying to delete policy: %s, status code: %d", policy.Id, deletePolicyResponse.StatusCode())
-				}
-			}
-		}
-		hasMore = listPoliciesResponse.JSON200.Pagination.HasMore
-		if hasMore {
-			nextOffset := api.PaginationAfter(listPoliciesResponse.JSON200.Pagination.NextOffset)
-			listPoliciesResponse, err = client.ListPoliciesWithResponse(ctx, &api.ListPoliciesParams{After: &nextOffset})
-			if err != nil {
-				return err
-			}
-			if listPoliciesResponse.StatusCode() != http.StatusOK {
-				return fmt.Errorf("unexpected status code for list policies request: %d", listPoliciesResponse.StatusCode())
-			}
+	// delete policies
+	var errs *multierror.Error
+	for _, id := range policiesToDelete {
+		resp, err := client.DeletePolicyWithResponse(ctx, id)
+		if err != nil {
+			errs = multierror.Append(errs, fmt.Errorf("delete policy %s: %w", id, err))
+		} else if resp.StatusCode() != http.StatusNoContent {
+			errs = multierror.Append(errs, fmt.Errorf("delete policy %s, status: %s", id, resp.Status()))
 		}
 	}
-	return err
+	return errs.ErrorOrNil()
 }
 
 func TestMain(m *testing.M) {
 	systemTests := flag.Bool("system-tests", false, "Run system tests")
 	useLocalCredentials := flag.Bool("use-local-credentials", false, "Generate local API key during `lakefs setup'")
-	adminAccessKeyID := flag.String("admin-access-key-id", DefaultAdminAccessKeyId, "lakeFS Admin access key ID")
+	adminAccessKeyID := flag.String("admin-access-key-id", DefaultAdminAccessKeyID, "lakeFS Admin access key ID")
 	adminSecretAccessKey := flag.String("admin-secret-access-key", DefaultAdminSecretAccessKey, "lakeFS Admin secret access key")
 	cleanupEnv := flag.Bool("cleanup-env-pre-run", false, "Clean repositories, groups, users and polices before running esti tests")
 	testsToSkip := flag.String("skip", "", "Tests to skip in a regex format")
@@ -279,18 +261,14 @@ func TestMain(m *testing.M) {
 	flag.Var(&groupsToKeep, "group-to-keep", "Groups to keep in case of pre-run cleanup")
 	flag.Var(&usersToKeep, "user-to-keep", "Users to keep in case of pre-run cleanup")
 	flag.Var(&policiesToKeep, "policy-to-keep", "Policies to keep in case of pre-run cleanup")
-	flag.StringVar(&metaclientJarPath, "metaclient-jar", "", "Location of the lakeFS metadata client jar")
+	flag.StringVar(&metaClientJarPath, "metaclient-jar", "", "Location of the lakeFS metadata client jar")
 	flag.StringVar(&sparkImageTag, "spark-image-tag", "", "Tag of Bitnami Spark image")
-	if directs, ok := os.LookupEnv("ESTI_TEST_DATA_ACCESS"); ok {
-		if err := testDirectDataAccess.Parse(directs); err != nil {
-			logger.Fatalf("ESTI_TEST_DATA_ACCESS=\"%s\": %s", directs, err)
-		}
-	}
-
 	flag.Parse()
+
 	if !*systemTests {
 		os.Exit(0)
 	}
+
 	params := testutil.SetupTestingEnvParams{
 		Name:      "esti",
 		StorageNS: "esti-system-testing",
@@ -302,33 +280,40 @@ func TestMain(m *testing.M) {
 	}
 
 	logger, client, svc, endpointURL = testutil.SetupTestingEnv(&params)
+
+	if directs, ok := os.LookupEnv("ESTI_TEST_DATA_ACCESS"); ok {
+		if err := testDirectDataAccess.Parse(directs); err != nil {
+			logger.Fatalf("ESTI_TEST_DATA_ACCESS=\"%s\": %s", directs, err)
+		}
+	}
 	azureStorageAccount = viper.GetString("azure_storage_account")
 	azureStorageAccessKey = viper.GetString("azure_storage_access_key")
 
-	var err error
+	if *testsToSkip != "" {
+		testsToSkipRegex = regexp.MustCompile(*testsToSkip)
+	}
+
 	setupLakeFS := viper.GetBool("setup_lakefs")
 	if !setupLakeFS && *cleanupEnv {
-		logger.Infof("Deleting Repositories, groups, users and policies before Esti run. Repositories to keep: %s, groups to keep: %s, users to keep: %s, policies to keep: %s", repositoriesToKeep, groupsToKeep, usersToKeep, policiesToKeep)
-
-		err = envCleanup(client, repositoriesToKeep, groupsToKeep, usersToKeep, policiesToKeep, logger)
+		logger.
+			WithFields(logging.Fields{
+				"repositories": repositoriesToKeep,
+				"groups":       groupsToKeep,
+				"users":        usersToKeep,
+				"policies":     policiesToKeep,
+			}).Info("Deleting repositories, groups, users and policies before Esti run")
+		err := envCleanup(client, repositoriesToKeep, groupsToKeep, usersToKeep, policiesToKeep)
 		if err != nil {
-			log.Fatal(err)
+			logger.WithError(err).Fatal("env cleanup")
 		}
 	}
 
+	var err error
 	server, err = startWebhookServer()
 	if err != nil {
-		log.Fatal(err)
+		logger.WithError(err).Fatal("start webhook server")
 	}
-
 	defer func() { _ = server.s.Close() }()
-
-	if *testsToSkip != "" {
-		testsToSkipRegex, err = regexp.Compile(*testsToSkip)
-		if err != nil {
-			log.Fatalf("Skip pattern '%s' failed to compile: %s", *testsToSkip, err)
-		}
-	}
 
 	logger.Info("Setup succeeded, running the tests")
 	os.Exit(m.Run())
