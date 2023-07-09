da4ba841d2b8e5d4091265ef85a3fde0e4c2b278
johnnyaug <yoni.augarten@treeverse.io>
2023-07-09 18:46:14 +0300

    compil247

--- clients/spark/core/src/test/scala/io/treeverse/clients/LakeFSInputFormatSpec.scala
+++ clients/spark/core/src/test/scala/io/treeverse/clients/LakeFSInputFormatSpec.scala
@@ -52,14 +52,17 @@ class LakeFSInputFormatSpec
     })
     Mockito
       .when(mockClient.getRangeURL(mockito.Matchers.eq(repoName), mockito.Matchers.anyString()))
-      .thenAnswer((i: mockito.invocation.InvocationOnMock) =>
-        "s3a://bucket/" + i.getArguments()(1).asInstanceOf[String]
-      )
+      .thenAnswer(new mockito.stubbing.Answer[String] {
+        override def answer(i: mockito.invocation.InvocationOnMock): String =
+          "s3a://bucket/" + i.getArguments()(1).asInstanceOf[String]
+
+      })
     Mockito
       .when(mockClient.getMetaRangeURL(mockito.Matchers.eq(repoName), mockito.Matchers.anyString))
-      .thenAnswer((i: mockito.invocation.InvocationOnMock) =>
-        i.getArguments()(1).asInstanceOf[String].replace("c", "mr")
-      )
+      .thenAnswer(new mockito.stubbing.Answer[String] {
+        override def answer(i: mockito.invocation.InvocationOnMock): String =
+          i.getArguments()(1).asInstanceOf[String].replace("c", "mr")
+      })
     val inputFormat = new LakeFSCommitInputFormat()
     inputFormat.apiClientGetter = (APIConfigurations) => mockClient
     inputFormat.metarangeReaderGetter = (Configuration, metaRangeURL: String, Boolean) => {
