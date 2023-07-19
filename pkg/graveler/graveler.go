d1bf6534afa56bfc28f52b9e91994ac8a5c28c10
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-07-19 16:09:06 +0300

    change committedManager's Import method's documentation

--- pkg/graveler/graveler.go
+++ pkg/graveler/graveler.go
@@ -917,9 +917,7 @@ type CommittedManager interface {
 	// GetRangeIDByKey returns the RangeID that contains the given key.
 	GetRangeIDByKey(ctx context.Context, ns StorageNamespace, id MetaRangeID, key Key) (RangeID, error)
 
-	// Import applies changes from 'source' to 'destination' and returns the ID of the new metarange.
-	// This is similar to the Merge operation, but it will override existing prefixes in the destination
-	// if they also exist in the provided import paths.
+	// Import sync changes from 'source' to 'destination' and returns the ID of the new metarange.
 	Import(ctx context.Context, ns StorageNamespace, destination, source MetaRangeID, importPaths []ImportPath) (MetaRangeID, error)
 }
 
