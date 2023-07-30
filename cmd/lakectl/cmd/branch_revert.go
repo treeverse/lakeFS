00d1f0150547de215a49a51a52257e6d9346d205
Barak Amar <barak.amar@treeverse.io>
2023-07-30 14:07:07 +0300

    lakectl cmd split part 3

---
+++ cmd/lakectl/cmd/branch_revert.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/treeverse/lakefs/pkg/api"
+)
+
+const (
+	branchRevertCmdArgs  = 2
+	ParentNumberFlagName = "parent-number"
+)
+
+// lakectl branch revert lakefs://myrepo/main commitId
+var branchRevertCmd = &cobra.Command{
+	Use:   "revert <branch uri> <commit ref to revert> [<more commits>...]",
+	Short: "Given a commit, record a new commit to reverse the effect of this commit",
+	Long:  "The commits will be reverted in left-to-right order",
+	Example: `lakectl branch revert lakefs://example-repo/example-branch commitA
+	          Revert the changes done by commitA in example-branch
+		      branch revert lakefs://example-repo/example-branch HEAD~1 HEAD~2 HEAD~3
+		      Revert the changes done by the second last commit to the fourth last commit in example-branch`,
+	Args: cobra.MinimumNArgs(branchRevertCmdArgs),
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return validRepositoryToComplete(cmd.Context(), toComplete)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		u := MustParseBranchURI("branch", args[0])
+		fmt.Printf("Branch: %s\n", u)
+		hasParentNumber := cmd.Flags().Changed(ParentNumberFlagName)
+		parentNumber := Must(cmd.Flags().GetInt(ParentNumberFlagName))
+		if hasParentNumber && parentNumber <= 0 {
+			Die("parent number must be non-negative, if specified", 1)
+		}
+		commits := strings.Join(args[1:], " ")
+		confirmation, err := Confirm(cmd.Flags(), fmt.Sprintf("Are you sure you want to revert the effect of commits %s", commits))
+		if err != nil || !confirmation {
+			Die("Revert aborted", 1)
+		}
+		clt := getClient()
+		for i := 1; i < len(args); i++ {
+			commitRef := args[i]
+			resp, err := clt.RevertBranchWithResponse(cmd.Context(), u.Repository, u.Ref, api.RevertBranchJSONRequestBody{
+				ParentNumber: parentNumber,
+				Ref:          commitRef,
+			})
+			DieOnErrorOrUnexpectedStatusCode(resp, err, http.StatusNoContent)
+			fmt.Printf("commit %s successfully reverted\n", commitRef)
+		}
+	},
+}
+
+//nolint:gochecknoinits
+func init() {
+	AssignAutoConfirmFlag(branchRevertCmd.Flags())
+
+	branchRevertCmd.Flags().IntP(ParentNumberFlagName, "m", 0, "the parent number (starting from 1) of the mainline. The revert will reverse the change relative to the specified parent.")
+
+	branchCmd.AddCommand(branchRevertCmd)
+}
---
+++ cmd/lakectl/cmd/repo_create_bare.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+	"github.com/treeverse/lakefs/pkg/api"
+)
+
+// repoCreateBareCmd represents the create repo command
+// lakectl create-bare lakefs://myrepo s3://my-bucket/
+var repoCreateBareCmd = &cobra.Command{
+	Use:               "create-bare <repository uri> <storage namespace>",
+	Short:             "Create a new repository with no initial branch or commit",
+	Example:           "lakectl create-bare lakefs://some-repo-name s3://some-bucket-name",
+	Hidden:            true,
+	Args:              cobra.ExactArgs(repoCreateCmdArgs),
+	ValidArgsFunction: ValidArgsRepository,
+	Run: func(cmd *cobra.Command, args []string) {
+		clt := getClient()
+		u := MustParseRepoURI("repository", args[0])
+		fmt.Printf("Repository: %s\n", u)
+		defaultBranch, err := cmd.Flags().GetString("default-branch")
+		if err != nil {
+			DieErr(err)
+		}
+		bareRepo := true
+		resp, err := clt.CreateRepositoryWithResponse(cmd.Context(), &api.CreateRepositoryParams{
+			Bare: &bareRepo,
+		}, api.CreateRepositoryJSONRequestBody{
+			DefaultBranch:    &defaultBranch,
+			Name:             u.Repository,
+			StorageNamespace: args[1],
+		})
+		DieOnErrorOrUnexpectedStatusCode(resp, err, http.StatusCreated)
+		if resp.JSON201 == nil {
+			Die("Bad response from server", 1)
+		}
+		repo := resp.JSON201
+		fmt.Printf("Repository '%s' created:\nstorage namespace: %s\ndefault branch: %s\ntimestamp: %d\n", repo.Id, repo.StorageNamespace, repo.DefaultBranch, repo.CreationDate)
+	},
+}
+
+//nolint:gochecknoinits
+func init() {
+	repoCreateBareCmd.Flags().StringP("default-branch", "d", DefaultBranch, "the default branch name of this repository (will not be created)")
+
+	repoCmd.AddCommand(repoCreateBareCmd)
+}
