e8f5b145b737ce3195565dbd48ab22be150a459a
Barak Amar <barak.amar@treeverse.io>
2023-08-04 09:32:44 +0300

    test: fix git test temporary directory for macOS as symlink (#6317)

--- pkg/git/git_test.go
+++ pkg/git/git_test.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -36,7 +35,9 @@ func TestIsRepository(t *testing.T) {
 }
 
 func TestGetRepositoryPath(t *testing.T) {
+	var err error
 	tmpdir := t.TempDir()
+	tmpdir, err = filepath.EvalSymlinks(tmpdir) // on macOS tmpdir is a symlink
 	tmpSubdir, err := os.MkdirTemp(tmpdir, "")
 	require.NoError(t, err)
 	defer os.Remove(tmpSubdir)
@@ -70,7 +71,10 @@ func TestIgnore(t *testing.T) {
 		trackedFile  = "file1"
 		marker       = "Test Marker"
 	)
+	var err error
 	tmpdir := t.TempDir()
+	tmpdir, err = filepath.EvalSymlinks(tmpdir) // on macOS tmpdir is a symlink
+	require.NoError(t, err)
 	tmpSubdir, err := os.MkdirTemp(tmpdir, "")
 	require.NoError(t, err)
 	defer os.Remove(tmpSubdir)
@@ -103,7 +107,7 @@ func TestIgnore(t *testing.T) {
 	fd.Close()
 
 	// Changing the working directory
-	require.NoError(t, syscall.Chdir(tmpdir))
+	require.NoError(t, os.Chdir(tmpdir))
 	verifyPathTracked(t, []string{filepath.Base(tmpSubdir), trackedFile})
 
 	_, err = git.Ignore(tmpFile.Name(), []string{}, []string{excludedPath}, marker)
