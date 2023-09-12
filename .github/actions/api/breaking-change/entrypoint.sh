b40e18c6346faa5636d677f3ebbf6e0fc57dca66
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-09-12 15:25:36 +0300

    add api breaking change github action and workflow

---
+++ .github/actions/api/breaking-change/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+readonly base="$1"
+readonly revision="$2"
+readonly fail_on_diff="$3"
+readonly include_checks="$4"
+echo "running oasdiff breaking base: $base, revision: $revision, fail_on_diff: $fail_on_diff, include_checks: $include_checks"
+
+go install github.com/treeverse/oasdiff@v1.8.1
+# Build flags to pass in command
+flags=""
+if [ "$fail_on_diff" = "true" ]; then
+    flags="${flags} --fail-on WARN"
+fi
+if [ -n "$include_checks" ]; then
+    flags="${flags} --include-checks $include_checks"
+fi
+echo "flags: $flags"
+
+if [ -n "$flags" ]; then
+    oasdiff breaking "$base" "$revision" $flags
+else
+    oasdiff breaking "$base" "$revision"
+fi
