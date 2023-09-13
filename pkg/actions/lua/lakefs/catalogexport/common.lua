98926489e9976ecf6383279faf626ee2046a0e6d
Isan Rivkin <isanrivkin@gmail.com>
2023-09-13 19:11:49 +0300

    clean

--- pkg/actions/lua/lakefs/catalogexport/common.lua
+++ pkg/actions/lua/lakefs/catalogexport/common.lua
@@ -27,24 +27,6 @@ function lakefs_object_pager(lakefs_client, repo_id, commit_id, after, prefix, p
     end, after)
 end 
 
--- function lakefs_object_pager_old(lakefs_client, repo_id, commit_id, after, prefix, page_size, delimiter)
---     local next_offset = after
---     local has_more = true
---     return function()
---         if not has_more then
---             return nil
---         end
---         local code, resp = lakefs_client.list_objects(repo_id, commit_id, next_offset, prefix, delimiter, page_size)
---         if code ~= 200 then
---             error("lakeFS: could not list objects in: " .. prefix .. ", error: " .. resp.message)
---         end
---         local objects = resp.results
---         has_more = resp.pagination.has_more
---         next_offset = resp.pagination.next_offset
---         return objects
---     end
--- end
-
 return {
     lakefs_object_pager=lakefs_object_pager,
     short_digest=short_digest,
