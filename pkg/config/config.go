6e4a28976a041116ab06bf5265b430960a693953
N-o-Z <ozery.nir@gmail.com>
2023-10-04 16:51:03 +0300

    CosmosDB: Add throughput configuration (#6693)
    * CosmosDB: Add throughput configuration
    
    * CR Fixes
    
    * CR Fixes

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -172,10 +172,12 @@ type Config struct {
 		} `mapstructure:"dynamodb"`
 
 		CosmosDB *struct {
-			Key       SecureString `mapstructure:"key"`
-			Endpoint  string       `mapstructure:"endpoint"`
-			Database  string       `mapstructure:"database"`
-			Container string       `mapstructure:"container"`
+			Key        SecureString `mapstructure:"key"`
+			Endpoint   string       `mapstructure:"endpoint"`
+			Database   string       `mapstructure:"database"`
+			Container  string       `mapstructure:"container"`
+			Throughput int32        `mapstructure:"throughput"`
+			Autoscale  bool         `mapstructure:"autoscale"`
 		} `mapstructure:"cosmosdb"`
 	}
 
