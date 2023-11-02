19712f081fc5191794a5131163bebe339f4419e0
Barak Amar <barak.amar@treeverse.io>
2023-11-02 10:17:07 +0200

    Remove unused code - reset password token (#6925)
    * remove unsued reset password token
    
    * Update go mod

--- pkg/auth/token.go
+++ pkg/auth/token.go
@@ -2,34 +2,9 @@ package auth
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/golang-jwt/jwt"
-	"github.com/rs/xid"
 )
-
-const (
-	ResetPasswordAudience = "reset_password"
-)
-
-func generateJWT(claims *jwt.StandardClaims, secret []byte) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(secret)
-}
-
-// GenerateJWTResetPassword creates a jwt token with the field subject set the email passed.
-func GenerateJWTResetPassword(secret []byte, email string, issuedAt, expiresAt time.Time) (string, error) {
-	claims := &jwt.StandardClaims{
-		// xid is k-sorted which is important functionality to ensure
-		// easy deletion of tokens from the kv-store.
-		Id:        xid.New().String(),
-		Audience:  ResetPasswordAudience,
-		Subject:   email,
-		IssuedAt:  issuedAt.Unix(),
-		ExpiresAt: expiresAt.Unix(),
-	}
-	return generateJWT(claims, secret)
-}
 
 func VerifyToken(secret []byte, tokenString string) (*jwt.StandardClaims, error) {
 	claims := &jwt.StandardClaims{}
@@ -40,17 +15,6 @@ func VerifyToken(secret []byte, tokenString string) (*jwt.StandardClaims, error)
 		return secret, nil
 	})
 	if err != nil || !token.Valid {
-		return nil, ErrInvalidToken
-	}
-	return claims, nil
-}
-
-func VerifyTokenWithAudience(secret []byte, token, audience string) (*jwt.StandardClaims, error) {
-	claims, err := VerifyToken(secret, token)
-	if err != nil {
-		return nil, err
-	}
-	if !claims.VerifyAudience(audience, true) {
 		return nil, ErrInvalidToken
 	}
 	return claims, nil
