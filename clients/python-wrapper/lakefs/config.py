1a1f52d36591b7e61fa3f1a8c1a5552e17f621a1
N-o-Z <ozery.nir@gmail.com>
2023-11-08 17:05:28 +0000

    Python Wrapper Part1: Client and Object IO (#6945)
    * Python Wrapper: Client and IO
    
    * Fixes
    
    * Add ServerConfig
    
    * Fix tests
    
    * Fix tests2
    
    * Mor fixes
    
    * CR Fixes
    
    * CR Fixes 2
    
    * fix requirements
    
    * fix requirements2
    
    * close instead of __del__
    
    * fix default client
    
    * CR Fixes 3
    
    * CR Fixes 4
    
    * CR Fixes 5

---
+++ clients/python-wrapper/lakefs/config.py
@@ -0,0 +1,70 @@
+import os
+import yaml
+from pathlib import Path
+from typing import NamedTuple
+
+from lakefs_sdk import (
+    Configuration,
+)
+from lakefs.exceptions import NoAuthenticationFound
+
+_LAKECTL_YAML_PATH = os.path.join(Path.home(), ".lakectl.yaml")
+_LAKECTL_ENDPOINT_ENV = "LAKECTL_SERVER_ENDPOINT_URL"
+_LAKECTL_ACCESS_KEY_ID_ENV = "LAKECTL_CREDENTIALS_ACCESS_KEY_ID"
+_LAKECTL_SECRET_ACCESS_KEY_ENV = "LAKECTL_CREDENTIALS_SECRET_ACCESS_KEY"
+
+
+class ClientConfig:
+    """
+    Configuration class for the SDK wrapper Client.
+    Instantiation will try to get authentication methods using the following chain:
+    1. Provided kwargs to __init__ func (should contain necessary credentials as defined in lakefs_sdk.Configuration)
+    2. Use LAKECTL_SERVER_ENDPOINT_URL, LAKECTL_ACCESS_KEY_ID and LAKECTL_ACCESS_SECRET_KEY if set
+    3. Try to read ~/.lakectl.yaml if exists
+    4. TBD: try and use IAM role from current machine (using AWS IAM role will work only with enterprise/cloud)
+
+    This class also encapsulates the required lakectl configuration for authentication and used to unmarshall the
+    lakectl yaml file.
+    """
+
+    class Server(NamedTuple):
+        endpoint_url: str = ""
+
+    class Credentials(NamedTuple):
+        access_key_id: str = ""
+        secret_access_key: str = ""
+
+    configuration: Configuration
+    server: Server = Server()
+    credentials: Credentials = Credentials()
+
+    def __init__(self, **kwargs):
+        self.configuration = Configuration(**kwargs)
+        if kwargs:
+            return
+
+        found = False
+
+        # Get credentials from lakectl
+        try:
+            with open(_LAKECTL_YAML_PATH) as fd:
+                data = yaml.load(fd, Loader=yaml.Loader)
+                self.server = ClientConfig.Server(**data["server"])
+                self.credentials = ClientConfig.Credentials(**data["credentials"])
+            found = True
+        except FileNotFoundError:  # File not found, fallback to env variables
+            pass
+
+        endpoint_env = os.getenv(_LAKECTL_ENDPOINT_ENV)
+        key_env = os.getenv(_LAKECTL_ACCESS_KEY_ID_ENV)
+        secret_env = os.getenv(_LAKECTL_SECRET_ACCESS_KEY_ENV)
+
+        self.configuration.host = endpoint_env if endpoint_env is not None else self.server.endpoint_url
+        self.configuration.username = key_env if key_env is not None else self.credentials.access_key_id
+        self.configuration.password = secret_env if secret_env is not None else self.credentials.secret_access_key
+        if len(self.configuration.username) > 0 and len(self.configuration.password) > 0:
+            found = True
+
+        # TODO: authentication via IAM Role
+        if not found:
+            raise NoAuthenticationFound
---
+++ clients/python-wrapper/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+NAME = "lakefs"
+VERSION = "0.1.0-alpha"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+PYTHON_REQUIRES = ">=3.9"
+REQUIRES = [
+    "python-dateutil",
+    "pydantic >= 1.10.5, < 2",
+]
+
+# TODO: autogenerate docs and grab long description from docs
+long_description = "Some long description"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="lakeFS Python SDK Wrapper",
+    author="Treeverse",
+    author_email="services@treeverse.io",
+    url="https://github.com/treeverse/lakeFS/tree/master/clients/python-wrapper",
+    keywords=["OpenAPI", "OpenAPI-Generator", "lakeFS API", "Python Wrapper"],
+    python_requires=">=3.9",
+    install_requires=REQUIRES,
+    tests_require={
+        "dev": ["pytest ~= 7.4.3", "pytest-mock ~= 3.12.0"]},
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    license="Apache 2.0",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
