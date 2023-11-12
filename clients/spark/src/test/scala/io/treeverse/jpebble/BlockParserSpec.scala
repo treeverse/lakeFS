0d16d1efc4f8183976827408dc3b9076727038cc
Barak Amar <barak.amar@treeverse.io>
2023-11-12 11:56:13 +0200

    Upgrade to use Go v1.21.3 (#6979)

--- clients/spark/src/test/scala/io/treeverse/jpebble/BlockParserSpec.scala
+++ clients/spark/src/test/scala/io/treeverse/jpebble/BlockParserSpec.scala
@@ -304,7 +304,7 @@ class CountedIteratorSpec extends AnyFunSpec with Matchers {
 
 class GolangContainerSpec extends AnyFunSpec with ForAllTestContainer {
   override val container: GenericContainer = GenericContainer(
-    "golang:1.20.6-alpine",
+    "golang:1.21.3-alpine",
     classpathResourceMapping = Seq(
       FileSystemBind("parser-test/sst_files_generator.go",
                      "/local/sst_files_generator.go",
