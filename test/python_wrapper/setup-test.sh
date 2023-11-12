f232808e359b8e4a84a9a989ebdd47f81448b87d
Nir Ozery <nir.ozery@treeverse.io>
2023-11-12 11:33:40 +0000

    Fixes

--- test/python_wrapper/setup-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-#
-# setup-test.sh - set up lakeFS to run Python Wrapper tests.  Run once per lakeFS.
-
-docker-compose exec -T lakefs /app/wait-for localhost:8000
-docker-compose exec -T lakefs sh -c 'lakefs setup --user-name tester --access-key-id ${LAKECTL_CREDENTIALS_ACCESS_KEY_ID} --secret-access-key ${LAKECTL_CREDENTIALS_SECRET_ACCESS_KEY}'
-docker-compose exec -T lakefs sh -c 'lakectl repo create lakefs://example ${STORAGE_NAMESPACE} -d main'
