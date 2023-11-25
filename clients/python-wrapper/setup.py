8c8b615a0fff9389b6f7fbed9422b8ba7eb82d69
Nir Ozery <nir.ozery@treeverse.io>
2023-11-25 09:57:56 +0000

    CR Fixes

--- clients/python-wrapper/setup.py
+++ clients/python-wrapper/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = "lakefs"
-VERSION = "0.1.0-alpha.3"
+VERSION = "0.1.0-alpha.4"
 # To install the library, run the following
 #
 # python setup.py install
