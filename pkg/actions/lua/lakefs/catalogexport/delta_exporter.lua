855a4432080422f23a6a1125cd1526b58a6010ba
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-11-30 12:01:16 +0200

    remove storage_utils.lua

--- pkg/actions/lua/lakefs/catalogexport/delta_exporter.lua
+++ pkg/actions/lua/lakefs/catalogexport/delta_exporter.lua
@@ -2,7 +2,6 @@ local lakefs = require("lakefs")
 local formats = require("formats")
 local pathlib = require("path")
 local json = require("encoding/json")
-local storage_utils = require("lakefs/catalogexport/storage_utils")
 local utils = require("lakefs/catalogexport/internal")
 
 --[[
@@ -39,9 +38,12 @@ local function export_delta_log(action, hook_args, storage_client)
         local sortedKeys = utils.sortedKeys(t, nil)
         --[[ Pairs of (version, map of json content):
                 (1,
-                {"commitInfo":{"timestamp":1699276565259,"operation":"WRITE","operationParameters":{"mode":"Overwrite","partitionBy":"[]"},"readVersion":9,"isolationLevel":"Serializable","isBlindAppend":false,"operationMetrics":{"numFiles":"1","numOutputRows":"4","numOutputBytes":"1353"},"engineInfo":"Apache-Spark/3.3.2 Delta-Lake/2.3.0","txnId":"eb6816ae-404f-4338-9e1a-2cb0a4626ab3"}}
-                {"add":{"path":"part-00000-a5a20e52-2b3d-440b-97a8-829fbc4a2804-c000.snappy.parquet","partitionValues":{},"size":1353,"modificationTime":1699276565000,"dataChange":true,"stats":"{\"numRecords\":4,\"minValues\":{\"Hylak_id\":18,\"Lake_name\":\" \",\"Country\":\"Malawi\",\"Depth_m\":3.0},\"maxValues\":{\"Hylak_id\":16138,\"Lake_name\":\"Malombe\",\"Country\":\"Malawi\",\"Depth_m\":706.0},\"nullCount\":{\"Hylak_id\":0,\"Lake_name\":0,\"Country\":0,\"Depth_m\":0}}"}}
-                {"remove":{"path":"part-00000-d660b401-ceec-415a-a791-e8d1c7599e3d-c000.snappy.parquet","deletionTimestamp":1699276565259,"dataChange":true,"extendedFileMetadata":true,"partitionValues":{},"size":82491}})
+                {
+                    {"commitInfo":{"timestamp":1699276565259,"operation":"WRITE","operationParameters":{"mode":"Overwrite","partitionBy":"[]"},"readVersion":9,"isolationLevel":"Serializable","isBlindAppend":false,"operationMetrics":{"numFiles":"1","numOutputRows":"4","numOutputBytes":"1353"},"engineInfo":"Apache-Spark/3.3.2 Delta-Lake/2.3.0","txnId":"eb6816ae-404f-4338-9e1a-2cb0a4626ab3"}}
+                    {"add":{"path":"part-00000-a5a20e52-2b3d-440b-97a8-829fbc4a2804-c000.snappy.parquet","partitionValues":{},"size":1353,"modificationTime":1699276565000,"dataChange":true,"stats":"{\"numRecords\":4,\"minValues\":{\"Hylak_id\":18,\"Lake_name\":\" \",\"Country\":\"Malawi\",\"Depth_m\":3.0},\"maxValues\":{\"Hylak_id\":16138,\"Lake_name\":\"Malombe\",\"Country\":\"Malawi\",\"Depth_m\":706.0},\"nullCount\":{\"Hylak_id\":0,\"Lake_name\":0,\"Country\":0,\"Depth_m\":0}}"}}
+                    {"remove":{"path":"part-00000-d660b401-ceec-415a-a791-e8d1c7599e3d-c000.snappy.parquet","deletionTimestamp":1699276565259,"dataChange":true,"extendedFileMetadata":true,"partitionValues":{},"size":82491}}
+                }
+                )
         ]]
         local table_log = {}
         local keyGenerator = delta_log_entry_key_generator()
@@ -82,12 +84,12 @@ local function export_delta_log(action, hook_args, storage_client)
 
         -- Get the table delta log physical location
         local t_name = pathlib.parse(path)["base_name"]
-        local table_export_prefix = storage_utils.get_storage_uri_prefix(ns, commit_id, action)
+        local table_export_prefix = utils.get_storage_uri_prefix(ns, commit_id, action)
         local table_physical_path = pathlib.join("/", table_export_prefix, t_name)
         local table_log_physical_path = pathlib.join("/", table_physical_path, "_delta_log")
 
         -- Upload the log to this physical_address
-        local storage_props = storage_utils.parse_storage_uri(table_log_physical_path)
+        local storage_props = utils.parse_storage_uri(table_log_physical_path)
         --[[
             table_log:
                 {
