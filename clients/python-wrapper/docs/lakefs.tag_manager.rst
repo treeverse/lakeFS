b7fc084cde553d94b976312726dbdb97e044a9f3
N-o-Z <ozery.nir@gmail.com>
2023-11-30 10:29:16 +0000

    Python Wrapper: Branch and Tag Managers (#7062)
    * Python Wrapper: Branch and Tag Managers
    
    * CR Fixes
    
    * Refactor
    
    * Refactor
    
    * CR Fixes
    
    * CR Fixes
    
    * merge fixes

--- clients/python-wrapper/docs/lakefs.tag_manager.rst
+++
@@ -1,7 +0,0 @@
-lakefs.tag\_manager module
-==========================
-
-.. automodule:: lakefs.tag_manager
-   :members:
-   :undoc-members:
-   :show-inheritance:
--- clients/python-wrapper/lakefs/__init__.py
+++ clients/python-wrapper/lakefs/__init__.py
@@ -7,6 +7,4 @@ from lakefs.reference import Reference, Commit, Change
 from lakefs.tag import Tag
 from lakefs.branch import Branch
 from lakefs.object import StoredObject, WriteableObject, ObjectReader
-from lakefs.branch_manager import BranchManager
 from lakefs.object_manager import ObjectManager
-from lakefs.tag_manager import TagManager
