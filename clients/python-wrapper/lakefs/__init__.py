6972ce5573463f3cdd6213a6de0007336678eaf5
N-o-Z <ozery.nir@gmail.com>
2023-12-05 21:09:10 +0000

    Python Wrapper: Objects generator (#7110)
    * Python Wrapper: Objects generator
    
    * fix test
    
    * fix test 2
    
    * CF Fixes

--- clients/python-wrapper/lakefs/__init__.py
+++ clients/python-wrapper/lakefs/__init__.py
@@ -4,8 +4,15 @@ Allow importing of models from package root
 
 from lakefs.repository import Repository
 from lakefs.reference import Reference
-from lakefs.models import Commit, Change, RepositoryProperties
+from lakefs.models import (
+    Commit,
+    Change,
+    ImportStatus,
+    ServerStorageConfiguration,
+    ObjectInfo,
+    CommonPrefix,
+    RepositoryProperties
+)
 from lakefs.tag import Tag
 from lakefs.branch import Branch
 from lakefs.object import StoredObject, WriteableObject, ObjectReader
-from lakefs.object_manager import ObjectManager
