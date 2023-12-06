7a59eaa03c3850bccd886b2694cae5f089805267
N-o-Z <ozery.nir@gmail.com>
2023-12-06 13:57:52 +0000

    Python Wrapper: Implement repository list (#7119)

--- clients/python-wrapper/lakefs/repository.py
+++ clients/python-wrapper/lakefs/repository.py
@@ -12,7 +12,7 @@ from lakefs.models import RepositoryProperties
 from lakefs.tag import Tag
 from lakefs.branch import Branch
 from lakefs.client import Client, DEFAULT_CLIENT
-from lakefs.exceptions import api_exception_handler, ConflictException, LakeFSException
+from lakefs.exceptions import api_exception_handler, ConflictException, LakeFSException, NoAuthenticationFound
 from lakefs.reference import Reference, generate_listing
 
 
@@ -161,3 +161,25 @@ class Repository:
                 return self._properties
 
         return self._properties
+
+
+def repositories(client: Client = DEFAULT_CLIENT,
+                 prefix: Optional[str] = None,
+                 after: Optional[str] = None,
+                 **kwargs) -> Generator[Repository]:
+    """
+    Creates a repositories object generator listing lakeFS repositories
+
+    :param client: The lakeFS client to use, if None, tries to use the default client
+    :param prefix: Return items prefixed with this value
+    :param after: Return items after this value
+    :return: A generator listing lakeFS repositories
+    """
+    if client is None:
+        raise NoAuthenticationFound("Explicitly provide a client or invoke client module's init method")
+
+    for res in generate_listing(client.sdk_client.repositories_api.list_repositories,
+                                prefix=prefix,
+                                after=after,
+                                **kwargs):
+        yield Repository(res.id, client)
