f15cf9097372b28ff827845828b2ed8a41bdd402
dongzf <dongzf@meixing.com>
2023-12-06 08:03:31 +0800

    modify pylint error

--- clients/python-wrapper/lakefs/reference.py
+++ clients/python-wrapper/lakefs/reference.py
@@ -148,7 +148,7 @@ class Reference:
 
         :param path: The object's path
         """
-        return StoredObject(self._repo_id, self._id, path,self._client)
+        return StoredObject(self._repo_id, self._id, path, self._client)
 
     def __str__(self) -> str:
         return self._id
