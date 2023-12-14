04466af600c30fe34c031abc80e0415dc9818420
Jonathan Rosenberg <96974219+Jonathan-Rosenberg@users.noreply.github.com>
2023-12-14 14:29:25 +0200

    Change the delta lake catalog export behavior to read a table descriptor (#7166)
    * change the delta lake catalog export behavior to read a table descriptor
    
    * remove comment
    
    * fix delta export test

--- examples/hooks/delta_lake_S3_export.lua
+++ examples/hooks/delta_lake_S3_export.lua
@@ -12,10 +12,11 @@ local aws = require("aws")
 local formats = require("formats")
 local delta_export = require("lakefs/catalogexport/delta_exporter")
 
+local table_descriptors_path = "_lakefs_tables"
 local sc = aws.s3_client(args.aws.access_key_id, args.aws.secret_access_key, args.aws.region)
 
 local delta_client = formats.delta_client(args.lakefs.access_key_id, args.lakefs.secret_access_key, args.aws.region)
-local delta_table_locations = delta_export.export_delta_log(action, args.table_paths, sc.put_object, delta_client)
+local delta_table_locations = delta_export.export_delta_log(action, args.table_paths, sc.put_object, delta_client, table_descriptors_path)
 for t, loc in pairs(delta_table_locations) do
     print("Delta Lake exported table \"" .. t .. "\"'s location: " .. loc .. "\n")
 end
