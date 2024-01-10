96d3c1ac0d3372e97f733d3fb69ea114dd0c1c7d
Barak Amar <barak.amar@treeverse.io>
2024-01-10 22:33:51 +0200

    logging

--- pkg/api/helpers/download.go
+++ pkg/api/helpers/download.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"sync/atomic"
+	"time"
 
 	"github.com/go-openapi/swag"
 	"github.com/treeverse/lakefs/pkg/api/apigen"
@@ -15,7 +16,16 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+const (
+	DefaultDownloadPartSize    int64 = 1024 * 1024 * 8 // 8MB
+	DefaultDownloadConcurrency       = 10
+)
+
 func Download(ctx context.Context, client *apigen.ClientWithResponses, preSign bool, src uri.URI, dst string) error {
+	start := time.Now()
+	defer func() {
+		fmt.Printf("Downloaded %s in %s\n", src.String(), time.Since(start))
+	}()
 	// create destination dir if needed
 	dir := filepath.Dir(dst)
 	_ = os.MkdirAll(dir, os.ModePerm)
@@ -37,11 +47,11 @@ func downloadPresigned(ctx context.Context, client *apigen.ClientWithResponses, 
 		return err
 	}
 	if statResp.JSON200 == nil || statResp.JSON200.SizeBytes == nil {
-		return fmt.Errorf("bad response from server")
+		return fmt.Errorf("%w: missing object size (%s)", ErrRequestFailed, statResp.Status())
 	}
 
 	// check if the object is small enough to download in one request
-	if *statResp.JSON200.SizeBytes < DefaultUploadPartSize {
+	if *statResp.JSON200.SizeBytes < DefaultDownloadPartSize {
 		return downloadObject(ctx, client, true, src, dst)
 	}
 
@@ -56,25 +66,32 @@ func downloadPresigned(ctx context.Context, client *apigen.ClientWithResponses, 
 	defer func() { _ = f.Close() }()
 
 	// download the file using ranges and concurrency
-	httpClient := &http.Client{}
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.MaxIdleConnsPerHost = 10
+	httpClient := &http.Client{
+		Transport: transport,
+	}
+
 	physicalAddress := statResp.JSON200.PhysicalAddress
 	size := swag.Int64Value(statResp.JSON200.SizeBytes)
+
 	var nextPart atomic.Int64
 	g, grpCtx := errgroup.WithContext(context.Background())
-	for i := 0; i < DefaultUploadConcurrency; i++ {
+	for i := 0; i < DefaultDownloadConcurrency; i++ {
 		g.Go(func() error {
 			for {
 				partNumber := nextPart.Add(1)
-				start := (partNumber - 1) * DefaultUploadPartSize
-				if start >= size {
+				rangeStart := (partNumber - 1) * DefaultDownloadPartSize
+				if rangeStart >= size {
 					return nil
 				}
-				partSize := DefaultUploadPartSize
-				if start+partSize > size {
-					partSize = size - start
+				partSize := DefaultDownloadPartSize
+				if rangeStart+partSize > size {
+					partSize = size - rangeStart
 				}
 
-				if err := downloadPresignedPart(grpCtx, httpClient, physicalAddress, start, size, partSize, partNumber, f); err != nil {
+				err := downloadPresignedPart(grpCtx, httpClient, physicalAddress, rangeStart, partSize, partNumber, f)
+				if err != nil {
 					return err
 				}
 			}
@@ -83,9 +100,15 @@ func downloadPresigned(ctx context.Context, client *apigen.ClientWithResponses, 
 	return g.Wait()
 }
 
-func downloadPresignedPart(ctx context.Context, httpClient *http.Client, physicalAddress string, start int64, size int64, partSize int64, partNumber int64, f *os.File) error {
-	end := start + partSize - 1
-	rangeHeader := fmt.Sprintf("bytes=%d-%d", start, end)
+func downloadPresignedPart(ctx context.Context, httpClient *http.Client, physicalAddress string, rangeStart int64, partSize int64, partNumber int64, f *os.File) error {
+	fmt.Printf("Downloading part %d (%d)...\n", partNumber, partSize)
+	start := time.Now()
+	defer func() {
+		fmt.Printf("Finished part %d (%d), took %s.\n", partNumber, partSize, time.Since(start))
+	}()
+
+	rangeEnd := rangeStart + partSize - 1
+	rangeHeader := fmt.Sprintf("bytes=%d-%d", rangeStart, rangeEnd)
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, physicalAddress, nil)
 	if err != nil {
 		return err
@@ -95,13 +118,15 @@ func downloadPresignedPart(ctx context.Context, httpClient *http.Client, physica
 	if err != nil {
 		return err
 	}
+	defer func() { _ = resp.Body.Close() }()
+
 	if resp.StatusCode != http.StatusPartialContent {
 		return fmt.Errorf("%w: %s", ErrRequestFailed, resp.Status)
 	}
 	if resp.ContentLength != partSize {
 		return fmt.Errorf("part %d expected %d bytes, got %d", partNumber, partSize, resp.ContentLength)
 	}
-	writer := io.NewOffsetWriter(f, start)
+	writer := io.NewOffsetWriter(f, rangeStart)
 	_, err = io.Copy(writer, resp.Body)
 	return err
 }
