8ee18f186bc6f5bc32ea98f82d6809ca4766989a
Barak Amar <barak.amar@treeverse.io>
2024-01-22 11:25:14 +0200

    Return conflict for creating already existing commit (#7321)

--- pkg/graveler/ref/manager.go
+++ pkg/graveler/ref/manager.go
@@ -578,7 +578,7 @@ func (m *Manager) CreateCommitRecord(ctx context.Context, repository *graveler.R
 	commitKey := graveler.CommitPath(commitID)
 	err := kv.SetMsgIf(ctx, m.kvStore, graveler.RepoPartition(repository), []byte(commitKey), c, nil)
 	if errors.Is(err, kv.ErrPredicateFailed) {
-		logging.FromContext(ctx).WithField("ID", commitID).Info("Same commit already exists, ignore")
+		return graveler.ErrCommitAlreadyExists
 	} else if err != nil {
 		return err
 	}
