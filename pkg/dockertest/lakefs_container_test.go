e8bd65b176512adaaaa9e6374055a1488c5a8bd1
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2024-01-22 16:07:18 +0200

    Add a lakeFS container wrapper
    It runs a lakeFS in a container and provides a client wrapper to access it.

---
+++ pkg/dockertest/lakefs_container_test.go
@@ -0,0 +1,38 @@
+package dockertest_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/treeverse/lakefs/pkg/api/apigen"
+	"github.com/treeverse/lakefs/pkg/api/helpers"
+	lakefs_ci "github.com/treeverse/lakefs/pkg/dockertest"
+
+	"github.com/ory/dockertest/v3"
+)
+
+func TestLakeFSContainer(t *testing.T) {
+	const version = "1.9.1"
+
+	ctx := context.TODO()
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	container := lakefs_ci.New().WithTag(version).New(ctx, t, pool)
+
+	res, err := container.Client.ListRepositoriesWithResponse(ctx, &apigen.ListRepositoriesParams{})
+	if err != nil {
+		t.Error(err)
+	}
+	if err = helpers.ResponseAsError(res); err != nil {
+		t.Errorf("Received error: %s", err)
+	}
+	if len(res.JSON200.Results) != 0 {
+		t.Errorf("Got repositories %+v", res.JSON200.Results)
+	}
+
+	container.Close()
+}
