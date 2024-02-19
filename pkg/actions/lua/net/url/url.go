a60974d557dd51fa1663c71e273a15dbfac2c01f
Barak Amar <barak.amar@treeverse.io>
2024-02-19 16:37:39 +0200

    Fix: Delta export path unescape (#7473)
    * Fix: Delta export path unescape
    
    * CR Fixes

--- pkg/actions/lua/net/url/url.go
+++ pkg/actions/lua/net/url/url.go
@@ -19,6 +19,8 @@ func Open(l *lua.State) {
 var library = []lua.RegistryFunction{
 	{Name: "parse", Function: parse},
 	{Name: "query_unescape", Function: queryUnescape},
+	{Name: "path_escape", Function: pathEscape},
+	{Name: "build_url", Function: build},
 }
 
 func parse(l *lua.State) int {
@@ -45,5 +47,32 @@ func queryUnescape(l *lua.State) int {
 		panic("unreachable")
 	}
 	l.PushString(qu)
+	return 1
+}
+
+func pathEscape(l *lua.State) int {
+	path := lua.CheckString(l, 1)
+	ep := neturl.PathEscape(path)
+	l.PushString(ep)
+	return 1
+}
+
+func build(l *lua.State) int {
+	scheme := lua.CheckString(l, 1)
+	host := lua.CheckString(l, 2)
+	u := neturl.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	if !l.IsNone(3) {
+		u.Path = lua.CheckString(l, 3)
+	}
+	if !l.IsNone(4) {
+		u.RawQuery = lua.CheckString(l, 4)
+	}
+	if !l.IsNone(5) {
+		u.Fragment = lua.CheckString(l, 3)
+	}
+	l.PushString(u.String())
 	return 1
 }
