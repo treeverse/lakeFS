391c9eab1881da912b4614adcef4783b20db5f1c
guy-har <60321938+guy-har@users.noreply.github.com>
2024-04-04 11:18:57 +0300

    Fix github actions (#7623)
    * Bump ubuntu version for actions
    
    * Fix changes
    
    * test
    
    * test2
    
    * test3
    
    * test4
    
    * Good for now
    
    ---------
    
    Co-authored-by: Nir Ozery <nir.ozery@treeverse.io>

--- test/rclone_export/run-test.sh
+++ test/rclone_export/run-test.sh
@@ -3,17 +3,23 @@
 REPOSITORY=${REPOSITORY:-example}
 
 # Run Export without previous commit
+echo "Current working directory: ${WORKING_DIRECTORY}"
+echo "upload file_one"
+docker compose exec -T lakefs lakectl fs upload lakefs://${REPOSITORY}/main/a/file_one.txt --source /local/file_one.txt
+echo "res $?"
 
-docker-compose exec -T lakefs lakectl fs upload lakefs://${REPOSITORY}/main/a/file_one.txt --source /local/file_one.txt
-
-docker-compose run --rm lakefs-export ${REPOSITORY} ${EXPORT_LOCATION} --branch="main"
+echo "export no previous commit"
+docker compose --project-directory ${WORKING_DIRECTORY} run --rm lakefs-export ${REPOSITORY} ${EXPORT_LOCATION} --branch="main"
+echo "res $?"
 
 # Validate export
 lakectl_out=$(mktemp)
 s3_out=$(mktemp)
 trap 'rm -f -- $s3_out $lakectl_out' INT TERM EXIT
 
-docker-compose exec -T lakefs lakectl fs ls --recursive --no-color lakefs://${REPOSITORY}/main/ | awk '{print $8}' | sort > ${lakectl_out}
+echo "ls"
+docker compose exec -T lakefs lakectl fs ls --recursive --no-color lakefs://${REPOSITORY}/main/ | awk '{print $8}' | sort > ${lakectl_out}
+echo "res $?"
 
 n=$(grep -o "/" <<< ${EXPORT_LOCATION} | wc -l)
 
@@ -24,7 +30,9 @@ else
   n=$((n-1))
 fi
 
+echo "aws ls"
 aws s3 ls --recursive ${EXPORT_LOCATION} | awk '{print $4}'| cut -d/ -f ${n}-  | grep -v EXPORT_ | sort > ${s3_out}
+echo "res $?"
 
 if ! diff ${lakectl_out} ${s3_out}; then
   echo "export location and lakefs should contain same objects"
@@ -32,19 +40,27 @@ if ! diff ${lakectl_out} ${s3_out}; then
 fi
 
 # Run Export with previous commit - add file and also delete an existing file
-docker-compose exec -T lakefs lakectl fs upload lakefs://${REPOSITORY}/main/a/file_two.txt --source /local/file_two.txt
-docker-compose exec -T lakefs lakectl fs rm lakefs://${REPOSITORY}/main/a/file_one.txt
-docker-compose exec -T lakefs lakectl commit lakefs://${REPOSITORY}/main --message="removed file_one and added file_two"
+echo "export previous commit"
+docker compose exec -T lakefs lakectl fs upload lakefs://${REPOSITORY}/main/a/file_two.txt --source /local/file_two.txt
+echo "res $?"
+docker compose exec -T lakefs lakectl fs rm lakefs://${REPOSITORY}/main/a/file_one.txt
+echo "res $?"
+docker compose exec -T lakefs lakectl commit lakefs://${REPOSITORY}/main --message="removed file_one and added file_two"
+echo "res $?"
 
-docker-compose run --rm lakefs-export ${REPOSITORY} ${EXPORT_LOCATION} --branch="main" --prev_commit_id="some_commit"
+docker compose --project-directory ${WORKING_DIRECTORY} run --rm lakefs-export ${REPOSITORY} ${EXPORT_LOCATION} --branch="main" --prev_commit_id="some_commit"
+echo "res $?"
 
 # Validate sync
 lakectl_out=$(mktemp)
 s3_out=$(mktemp)
 trap 'rm -f -- $s3_out $lakectl_out' INT TERM EXIT
 
-docker-compose exec -T lakefs lakectl fs ls --recursive --no-color lakefs://${REPOSITORY}/main/ | awk '{print $8}' | sort > ${lakectl_out}
+echo "ls"
+docker compose exec -T lakefs lakectl fs ls --recursive --no-color lakefs://${REPOSITORY}/main/ | awk '{print $8}' | sort > ${lakectl_out}
+echo "res $?"
 
+echo "aws ls"
 aws s3 ls --recursive ${EXPORT_LOCATION} | awk '{print $4}'| cut -d/ -f ${n}-  | grep -v EXPORT_ | sort > ${s3_out}
 
 if ! diff ${lakectl_out} ${s3_out}; then
@@ -53,11 +69,13 @@ if ! diff ${lakectl_out} ${s3_out}; then
 fi
 
 # Run Export with commit_id reference
+echo "export with commit_id"
+docker compose exec -T lakefs lakectl fs upload lakefs://${REPOSITORY}/main/a/file_three.txt --source /local/file_three.txt
+echo "res $?"
+commit_id=$(docker compose exec -T lakefs lakectl commit lakefs://${REPOSITORY}/main --message="added file_three" | sed -n 4p | awk '{print $2}')
 
-docker-compose exec -T lakefs lakectl fs upload lakefs://${REPOSITORY}/main/a/file_three.txt --source /local/file_three.txt
-commit_id=$(docker-compose exec -T lakefs lakectl commit lakefs://${REPOSITORY}/main --message="added file_three" | sed -n 4p | awk '{print $2}')
-
-docker-compose run --rm lakefs-export ${REPOSITORY} ${EXPORT_LOCATION} --commit_id="$commit_id"
+docker compose --project-directory ${WORKING_DIRECTORY} run --rm lakefs-export ${REPOSITORY} ${EXPORT_LOCATION} --commit_id="$commit_id"
+echo "res $?"
 
 echo "commit_id $commit_id"
 
@@ -66,10 +84,13 @@ lakectl_out=$(mktemp)
 s3_out=$(mktemp)
 trap 'rm -f -- $s3_out $lakectl_out' INT TERM EXIT
 
-docker-compose exec -T lakefs lakectl fs ls --recursive --no-color lakefs://${REPOSITORY}/main/ | awk '{print $8}' | sort > ${lakectl_out}
+echo "ls"
+docker compose exec -T lakefs lakectl fs ls --recursive --no-color lakefs://${REPOSITORY}/main/ | awk '{print $8}' | sort > ${lakectl_out}
+echo "res $?"
 
+echo "aws ls"
 aws s3 ls --recursive ${EXPORT_LOCATION} | awk '{print $4}'| cut -d/ -f ${n}-  | grep -v EXPORT_ | sort > ${s3_out}
-
+echo "res $?"
 
 if ! diff ${lakectl_out} ${s3_out}; then
   echo "export location and lakefs should contain same objects"
@@ -78,3 +99,4 @@ fi
 
 # Delete files at destination in case of multiple runs, each one produce output under different folder
 aws s3 rm ${EXPORT_LOCATION} --recursive
+echo "res $?"
--- test/rclone_export/setup-test.sh
+++ test/rclone_export/setup-test.sh
@@ -2,8 +2,8 @@
 #
 # setup-test.sh - set up lakeFS to run rclone export tests.  Run once per lakeFS.
 
-docker-compose exec -T lakefs /app/wait-for localhost:8000
+docker compose exec -T lakefs /app/wait-for localhost:8000
 
-docker-compose exec -T lakefs sh -c 'lakefs setup --user-name tester --access-key-id AKIAIOSFODNN7EXAMPLE --secret-access-key wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+docker compose exec -T lakefs sh -c 'lakefs setup --user-name tester --access-key-id AKIAIOSFODNN7EXAMPLE --secret-access-key wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
 
-docker-compose exec -T lakefs lakectl repo create lakefs://example $STORAGE_NAMESPACE
+docker compose exec -T lakefs lakectl repo create lakefs://example $STORAGE_NAMESPACE
