cb0339ab45a75fc5bbdaf1e78a9e8cc81cad3a9d
Nir Ozery <nir.ozery@treeverse.io>
2024-04-10 01:13:25 +0300

    Fix workflow 3

--- test/spark/run-test.py
+++ test/spark/run-test.py
@@ -90,6 +90,7 @@ def main():
                          "spark.hadoop.fs.s3a.endpoint": "s3.docker.lakefs.io:8000",
                          "spark.hadoop.fs.s3a.connection.ssl.enabled": "false"}
         if args.redirect:
+            spark_configs["spark.hadoop.fs.s3a.path.style.access"] = "true"
             spark_configs[f"spark.hadoop.fs.s3a.signing-algorithm"] = "QueryStringSignerType"
             spark_configs[f"spark.hadoop.fs.s3a.user.agent.prefix"] = "s3RedirectionSupport"
 
