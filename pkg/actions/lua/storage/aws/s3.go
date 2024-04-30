1aac24e00b9b11a2d585672d94b892410d85383b
Barak Amar <barak.amar@treeverse.io>
2024-04-30 15:48:24 +0800

    fix: close resp body (#7703)

--- pkg/actions/lua/storage/aws/s3.go
+++ pkg/actions/lua/storage/aws/s3.go
@@ -150,6 +150,7 @@ func (c *S3Client) GetObject(l *lua.State) int {
 		lua.Errorf(l, "%s", err.Error())
 		panic("unreachable")
 	}
+	defer resp.Body.Close()
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		lua.Errorf(l, "%s", err.Error())
