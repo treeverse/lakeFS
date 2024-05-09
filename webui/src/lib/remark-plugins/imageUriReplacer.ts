a2e0128c0b8e9a54f59aa6ffd3b4722c3d9b587c
eladlachmi <110764839+eladlachmi@users.noreply.github.com>
2024-05-09 17:24:57 +0300

    Support presigned image URLs in markdown (#7745)
    * Support presigned image URLs in markdown
    * Migrate from react-markdown to rehype-react + remark-rehype

--- webui/src/lib/remark-plugins/imageUriReplacer.ts
+++ webui/src/lib/remark-plugins/imageUriReplacer.ts
@@ -1,12 +1,12 @@
 import { visit } from "unist-util-visit";
 import type { Node } from "unist";
-import type { Root } from "mdast";
-import type { Plugin } from "unified";
+import {objects} from "../api";
 
 type ImageUriReplacerOptions = {
   repo: string;
   ref: string;
   path: string;
+  presign: boolean;
 };
 
 const ABSOLUTE_URL_REGEX = /^(https?):\/\/.*/;
@@ -14,28 +14,45 @@ const qs = (queryParts: { [key: string]: string }) => {
   const parts = Object.keys(queryParts).map((key) => [key, queryParts[key]]);
   return new URLSearchParams(parts).toString();
 };
-export const getImageUrl = (
+export const getImageUrl = async (
   repo: string,
   ref: string,
-  path: string
-): string => {
+  path: string,
+  presign: boolean,
+): Promise<string> => {
+  if (presign) {
+    try {
+      const obj = await objects.getStat(repo, ref, path, true);
+      return obj.physical_address;
+    } catch(e) {
+      console.error("failed to fetch presigned URL", e);
+      return ""
+    }
+  }
+
   const query = qs({ path });
   return `/api/v1/repositories/${encodeURIComponent(
     repo
   )}/refs/${encodeURIComponent(ref)}/objects?${query}`;
 };
 
-const imageUriReplacer: Plugin<[ImageUriReplacerOptions], Root> =
-  (options) => (tree) => {
-    visit(tree, "image", (node: Node & { url: string }) => {
+const imageUriReplacer =
+  (options: ImageUriReplacerOptions) => async (tree: Node) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const promises: any[] = [];
+    visit(tree, "image", visitor);
+    await Promise.all(promises);
+
+    function visitor(node: Node & { url: string }) {
       if (node.url.startsWith("lakefs://")) {
         const [repo, ref, ...imgPath] = node.url.split("/").slice(2);
-        node.url = getImageUrl(repo, ref, imgPath.join("/"));
+        const p = getImageUrl(repo, ref, imgPath.join("/"), options.presign).then((url) => node.url = url);
+        promises.push(p);
       } else if (!node.url.match(ABSOLUTE_URL_REGEX)) {
         // If the image is not an absolute URL, we assume it's a relative path
         // relative to repo and ref
         if (node.url.startsWith("/")) {
-          node.url = node.url.slice(1);
+            node.url = node.url.slice(1);
         }
         // relative to MD file location
         if (node.url.startsWith("./")) {
@@ -43,9 +60,10 @@ const imageUriReplacer: Plugin<[ImageUriReplacerOptions], Root> =
             2
           )}`;
         }
-        node.url = getImageUrl(options.repo, options.ref, node.url);
+        const p = getImageUrl(options.repo, options.ref, node.url, options.presign).then((url) => node.url = url);
+        promises.push(p);
       }
-    });
+    }
   };
 
 export default imageUriReplacer;
