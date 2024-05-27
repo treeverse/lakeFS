1a67e1484a3a890794ed48c6b4cf1b60dbe799f7
Barak Amar <barak.amar@treeverse.io>
2024-05-27 13:57:14 +0300

    Make RBAC discoverable on lakeFS (#7806)
    * make rbac discoverable on lakeFS oss
    
    * make alert dismissable
    
    * missing whitespace
    
    * added whitespace
    
    * Update webui/src/lib/components/auth/layout.tsx
    
    Co-authored-by: talSofer <tal.sofer@treeverse.io>
    
    * fix pr comments
    
    ---------
    
    Co-authored-by: Oz Katz <oz.katz@treeverse.io>

--- webui/src/lib/components/auth/layout.tsx
+++ webui/src/lib/components/auth/layout.tsx
@@ -9,11 +9,15 @@ import Card from "react-bootstrap/Card";
 import {Link} from "../nav";
 import {useLoginConfigContext} from "../../hooks/conf";
 import {useLayoutOutletContext} from "../layout";
+import Alert from "react-bootstrap/Alert";
+import {InfoIcon} from "@primer/octicons-react";
 
 type AuthOutletContext = [(tab: string) => void];
 
 
+const rbacDismissedKey = "lakefs:ui:acl:dismissRBACAlert";
 export const AuthLayout = () => {
+    const [showRBACAlert, setShowRBACAlert] = useState(!window.localStorage.getItem(rbacDismissedKey));
     const [activeTab, setActiveTab] = useState("credentials");
     const {RBAC: rbac} = useLoginConfigContext();
     const [setIsLogged] = useLayoutOutletContext();
@@ -23,6 +27,15 @@ export const AuthLayout = () => {
     return (
         <Container fluid="xl">
             <Row className="mt-5">
+                <div>
+                    {rbac === 'simplified' &&  showRBACAlert &&
+                    <Alert severity="info" title="rbac CTA" dismissible onClose={() => {
+                        window.localStorage.setItem(rbacDismissedKey, "true");
+                        setShowRBACAlert(false);
+                    }}><InfoIcon/>{" "}Enhance Your Security with {" "}<Alert.Link href={"https://docs.lakefs.io/reference/security/rbac.html"}>Role-Based Access Control</Alert.Link>{" "}
+                        – Available on <Alert.Link href={"https://lakefs.cloud/register"}>lakeFS Cloud</Alert.Link> and <Alert.Link href={"https://docs.lakefs.io/understand/enterprise/"}>lakeFS Enterprise</Alert.Link>!</Alert>
+                    }
+                </div>
                 <Col md={{span: 3}}>
                     <Card>
                         <Card.Header>
