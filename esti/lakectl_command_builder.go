0aab9ba5b4a6e3a316fb26b58dcd3755bebfedca
yoni <yoni.peleg1@gmail.com>
2024-06-06 14:37:19 +0300

    suggest builder pattern for lakectl command in tests

---
+++ esti/lakectl_command_builder.go
@@ -0,0 +1,32 @@
+package esti
+
+import "strings"
+
+type LakeCtl struct {
+	rawCmd string
+}
+
+func NewLakeCtl() *LakeCtl {
+	return &LakeCtl{
+		rawCmd: Lakectl(),
+	}
+}
+
+func (l *LakeCtl) Arg(arg string) *LakeCtl {
+	l.rawCmd += " " + arg
+	return l
+}
+
+func (l *LakeCtl) PathArg(components ...string) *LakeCtl {
+	l.rawCmd += " " + strings.Join(components, "/")
+	return l
+}
+
+func (l *LakeCtl) UrlArg(schemaPrefix string, components ...string) *LakeCtl {
+	l.rawCmd += " " + schemaPrefix + strings.Join(components, "/")
+	return l
+}
+
+func (l *LakeCtl) Get() string {
+	return l.rawCmd
+}
