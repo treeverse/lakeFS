af3a510356920e64835c3a5d1cb19744be952064
Barak Amar <barak.amar@treeverse.io>
2024-06-18 15:28:13 +0300

    Fix: lakectl local diff slowness (#7842)
    * Fix: lakectl local diff slowness
    
    * Fix upload
    
    * Update pkg/local/diff.go
    
    Co-authored-by: Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
    
    * Add unit tests
    
    * Add a fuzzer for walking POSIX directories in S3 order
    
    Looks like it found somethine almost immediately :-/
    
    AFAIU tests will run _existing_ failures in testdata/fuzz.  You can fuzz
    more by running something like:
    
    ```sh
    $ go test -v ./pkg/local/ -fuzz FuzzWalkS3 --fuzztime 5m
    === RUN   TestDiffLocal
    === RUN   TestDiffLocal/t1_no_diff
    === RUN   TestDiffLocal/t1_modified
    === RUN   TestDiffLocal/t1_local_before
    === RUN   TestDiffLocal/t1_local_after
    === RUN   TestDiffLocal/t1_hidden_changed
    --- PASS: TestDiffLocal (0.00s)
        --- PASS: TestDiffLocal/t1_no_diff (0.00s)
        --- PASS: TestDiffLocal/t1_modified (0.00s)
        --- PASS: TestDiffLocal/t1_local_before (0.00s)
        --- PASS: TestDiffLocal/t1_local_after (0.00s)
        --- PASS: TestDiffLocal/t1_hidden_changed (0.00s)
    === RUN   TestWalkS3
    === RUN   TestWalkS3/reverse_order
    === RUN   TestWalkS3/file_in_the_middle
    === RUN   TestWalkS3/dirs_at_the_end
    === RUN   TestWalkS3/files_mixed_with_directories
    --- PASS: TestWalkS3 (0.00s)
        --- PASS: TestWalkS3/reverse_order (0.00s)
        --- PASS: TestWalkS3/file_in_the_middle (0.00s)
        --- PASS: TestWalkS3/dirs_at_the_end (0.00s)
        --- PASS: TestWalkS3/files_mixed_with_directories (0.00s)
    === RUN   TestWriteIndex
    --- PASS: TestWriteIndex (0.00s)
    === RUN   TestReadIndex
    --- PASS: TestReadIndex (0.00s)
    === RUN   TestFindIndices
    --- PASS: TestFindIndices (0.00s)
    === RUN   FuzzWalkS3
    fuzz: elapsed: 0s, gathering baseline coverage: 0/47 completed
    failure while testing seed corpus entry: FuzzWalkS3/0345f16af6907ab1
    fuzz: elapsed: 0s, gathering baseline coverage: 0/47 completed
    --- FAIL: FuzzWalkS3 (0.03s)
        --- FAIL: FuzzWalkS3 (0.00s)
            diff_test.go:323:
                    Error Trace:    /home/ariels/dev/lakeFS/pkg/local/diff_test.go:323
                                                            /home/ariels/sdk/go1.21.3/src/reflect/value.go:596
                                                            /home/ariels/sdk/go1.21.3/src/reflect/value.go:380
                                                            /home/ariels/sdk/go1.21.3/src/testing/fuzz.go:335
                    Error:          Not equal:
                                    expected: []string{"imported 0", "imported/0"}
                                    actual  : []string{"imported/0", "imported 0"}
    
                                    Diff:
                                    --- Expected
                                    +++ Actual
                                    @@ -1,4 +1,4 @@
                                     ([]string) (len=2) {
                                    - (string) (len=10) "imported 0",
                                    - (string) (len=10) "imported/0"
                                    + (string) (len=10) "imported/0",
                                    + (string) (len=10) "imported 0"
                                     }
                    Test:           FuzzWalkS3
    
    === NAME
    FAIL
    exit status 1
    FAIL    github.com/treeverse/lakefs/pkg/local   0.038s
    ```
    
    Note that Go only finds a _first_ bug - but at least it tries to minimize it.
    
    * Add sorted queue
    
    * CR fixes
    
    * CR Fixes last
    
    ---------
    
    Co-authored-by: Ariel Shaqed (Scolnicov) <ariels@treeverse.io>

--- pkg/local/diff_test.go
+++ pkg/local/diff_test.go
@@ -4,7 +4,9 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"slices"
 	"sort"
+	"strings"
 	"testing"
 	"time"
 
@@ -188,4 +190,140 @@ func fixTime(t *testing.T, localPath string) {
 		return nil
 	})
 	require.NoError(t, err)
+}
+
+// hasPrefix returns true if slice starts with prefix.
+func hasPrefix(slice, prefix []string) bool {
+	if len(slice) < len(prefix) {
+		return false
+	}
+	return slices.Equal(slice[:len(prefix)], prefix)
+}
+
+// cleanPrefixes removes all paths from fileList which are prefixes of
+// another path.  The result is a list of paths which can be created as
+// files in a POSIX directory structure.  As a special case, an empty string
+// is considered a self-prefix: it will be a directory, which already
+// exists.
+func cleanPrefixes(t testing.TB, fileList []string) []string {
+	t.Helper()
+	for i, file := range fileList {
+		if file != "" && file[0] == '/' {
+			file = "." + file
+		}
+		fileList[i] = filepath.Clean(file)
+	}
+	sort.Strings(fileList)
+
+	ret := make([]string, 0, len(fileList))
+	var cur []string
+
+	for _, file := range fileList {
+		// Split on "/" explicitly: this is how we construct our
+		// test data.
+		//
+		// TODO(ariels): Fuzzing on Windows will probably fail
+		//     because of this, and we will need to translate
+		//     backslashes to slashes above.
+		next := strings.Split(file, "/")
+		if file == "." || next[len(next)-1] == "" {
+			continue
+		}
+		if cur != nil && hasPrefix(next, cur) {
+			continue
+		}
+		cur = next
+		ret = append(ret, file)
+	}
+	return ret
+}
+
+// setupFiles creates a directory structure containing all files in
+// fileList.  fileList should already be cleaned of prefixes by
+// cleanPrefixes.
+func setupFiles(t testing.TB, fileList []string) string {
+	t.Helper()
+	dir := t.TempDir() + string(filepath.Separator)
+	for _, file := range fileList {
+		p := filepath.Join(dir, file)
+		require.NoError(t, os.MkdirAll(filepath.Dir(p), os.ModePerm))
+		fd, err := os.Create(p)
+		require.NoError(t, err)
+		_ = fd.Close()
+	}
+	return dir
+}
+
+func TestWalkS3(t *testing.T) {
+	cases := []struct {
+		Name     string
+		FileList []string
+	}{
+		{
+			Name:     "reverse order",
+			FileList: []string{"imported/new-prefix/prefix-1/file000001", "imported./new-prefix/prefix-1/file002100"},
+		},
+		{
+			Name:     "file in the middle",
+			FileList: []string{"imported/file000001", "imported/new-prefix/prefix-1/file000001", "imported./new-prefix/prefix-1/file002100"},
+		},
+		{
+			Name:     "dirs at the end",
+			FileList: []string{"imported/new-prefix/prefix-1/file000001", "imported./new-prefix/prefix-1/file002100", "file000001"},
+		},
+		{
+			Name:     "files mixed with directories",
+			FileList: []string{"imported/0000/1", "imported/00000", "imported/00010/1"},
+		},
+		{
+			Name:     "file in between",
+			FileList: []string{"imported,/0000/1", "imported.", "imported/00010/1"},
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			dir := setupFiles(t, tt.FileList)
+			var walkOrder []string
+			err := local.WalkS3(dir, func(p string, info fs.FileInfo, err error) error {
+				walkOrder = append(walkOrder, strings.TrimPrefix(p, dir))
+				return nil
+			})
+			require.NoError(t, err)
+			sort.Strings(tt.FileList)
+			require.Equal(t, tt.FileList, walkOrder)
+		})
+	}
+}
+
+func FuzzWalkS3(f *testing.F) {
+	testcases := [][]string{
+		{"imported/file000001", "imported/new-prefix/prefix-1/file000001", "imported./new-prefix/prefix-1/file002100"},
+		{"imported/new-prefix/prefix-1/file000001", "imported./new-prefix/prefix-1/file002100", "file000001"},
+		{"imported/0000/1", "imported/00000", "imported/00010/1"},
+	}
+
+	// Go fuzzing only supports string test cases.  Since \0 is not
+	// valid in POSIX filenames, decode that as a separator.
+	for _, tc := range testcases {
+		f.Add(strings.Join(tc, "\x00"))
+	}
+	f.Fuzz(func(t *testing.T, tc string) {
+		tcf := strings.Split(tc, "\x00")
+		files := cleanPrefixes(t, tcf)
+
+		dir := setupFiles(t, files)
+		var walkOrder []string
+		err := local.WalkS3(dir, func(p string, info fs.FileInfo, err error) error {
+			walkOrder = append(walkOrder, strings.TrimPrefix(p, dir))
+			return nil
+		})
+		require.NoError(t, err)
+		sort.Strings(files)
+		if len(files) == 0 {
+			// require.Equal doesn't understand empty slices;
+			// our code represents empty slices as nil.
+			files = nil
+		}
+		require.Equal(t, files, walkOrder)
+	})
 }
