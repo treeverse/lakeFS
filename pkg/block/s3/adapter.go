3435d66078ecc61333e578ad5bc815027ee3bd84
Itai Gilo <itai.gilo@treeverse.io>
2024-06-20 09:05:14 +0300

    Fix S3 bucket validation for inter-region storage

--- pkg/block/s3/adapter.go
+++ pkg/block/s3/adapter.go
@@ -870,10 +870,12 @@ func (a *Adapter) ResolveNamespace(storageNamespace, key string, identifierType 
 }
 
 func (a *Adapter) GetRegion(ctx context.Context, storageNamespace string) (string, error) {
-	bucket, found := strings.CutPrefix(storageNamespace, fmt.Sprintf("%s://", block.BlockstoreTypeS3))
+	namespace, found := strings.CutPrefix(storageNamespace, fmt.Sprintf("%s://", block.BlockstoreTypeS3))
 	if !found {
 		return "", fmt.Errorf(`%w: "s3://" prefix not found %s`, block.ErrInvalidNamespace, storageNamespace)
 	}
+
+	bucket, _, _ := strings.Cut(namespace, "/")
 
 	return a.clients.GetBucketRegionFromAWS(ctx, bucket)
 }
