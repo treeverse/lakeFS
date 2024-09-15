291e7b8e9ba72d17faa890ad22793fcebf434cfb
Itai Gilo <itai.gilo@treeverse.io>
2024-09-15 13:32:24 +0300

    Add border to Dark NavBar

--- webui/src/lib/components/navbar.jsx
+++ webui/src/lib/components/navbar.jsx
@@ -77,7 +77,7 @@ const TopNav = ({logged = true}) => {
         );
     }
     return (
-        <Navbar variant="dark" bg="dark" expand="md">
+        <Navbar variant="dark" bg="dark" expand="md" className="border-bottom">
             <Container fluid={true}>
                 <Link component={Navbar.Brand} href="/">
                     <img src="/logo.png" alt="lakeFS" className="logo"/>
