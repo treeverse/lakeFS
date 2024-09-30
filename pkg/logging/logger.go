e21565d498fd4388cf97334df649ce3d9d52bfd7
Idan Novogroder <idan.novogroder@treeverse.io>
2024-09-30 12:57:37 +0300

    Changed syslogs function to build only for non-windows OS

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log/syslog"
 	"os"
 	"reflect"
 	"runtime"
@@ -14,7 +13,6 @@ import (
 	"time"
 
 	"github.com/sirupsen/logrus"
-	lSyslog "github.com/sirupsen/logrus/hooks/syslog"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
@@ -391,40 +389,6 @@ func ContextUnavailable() Logger {
 		defaultLogger.SetReportCaller(true)
 		defaultLogger.SetNoLock()
 		defaultLogger.Formatter = logrusCallerFormatter{defaultLogger.Formatter}
-	})
-	return &logrusEntryWrapper{
-		e: logrus.NewEntry(defaultLogger),
-	}
-}
-
-func ContextUnavailableWithSysLogs(sysLogsLevel string) Logger {
-	// wrap formatter with our own formatter that overrides caller
-	formatterInitOnce.Do(func() {
-		defaultLogger.SetReportCaller(true)
-		defaultLogger.SetNoLock()
-		defaultLogger.Formatter = logrusCallerFormatter{defaultLogger.Formatter}
-	})
-	syslogOnce.Do(func() {
-		var hook *lSyslog.SyslogHook
-		var err error
-		switch strings.ToLower(sysLogsLevel) {
-		// There's no syslog level for trace, using debug instead.
-		case "trace", "debug":
-			hook, err = lSyslog.NewSyslogHook("", "", syslog.LOG_DEBUG, "")
-		case "info":
-			hook, err = lSyslog.NewSyslogHook("", "", syslog.LOG_INFO, "")
-		case "warn", "warning":
-			hook, err = lSyslog.NewSyslogHook("", "", syslog.LOG_WARNING, "")
-		case "error":
-			hook, err = lSyslog.NewSyslogHook("", "", syslog.LOG_ERR, "")
-		case "panic", "null", "none":
-			hook, err = lSyslog.NewSyslogHook("", "", syslog.LOG_CRIT, "")
-		}
-		if err != nil {
-			defaultLogger.WithError(err).Error("failed to set syslog hook")
-		} else {
-			defaultLogger.AddHook(hook)
-		}
 	})
 	return &logrusEntryWrapper{
 		e: logrus.NewEntry(defaultLogger),
