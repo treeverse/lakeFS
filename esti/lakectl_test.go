267cfed155f73f6a9ac0c6518df9031586d44d69
Itamar Yuran <itamaryuran@gmail.com>
2025-01-15 15:06:27 +0100

    check upload before rm

--- esti/lakectl_test.go
+++ esti/lakectl_test.go
@@ -747,11 +747,13 @@ func TestLakectlFsRm_protectedBranch(t *testing.T) {
 	repoName := generateUniqueRepositoryName()
 	storage := generateUniqueStorageNamespace(repoName)
 	file := "data/ro_1k.0"
+	file2 := "data/ro_2k.0"
 	vars := map[string]string{
-		"REPO":      repoName,
-		"STORAGE":   storage,
-		"BRANCH":    mainBranch,
-		"FILE_PATH": file,
+		"REPO":       repoName,
+		"STORAGE":    storage,
+		"BRANCH":     mainBranch,
+		"FILE_PATH":  file,
+		"FILE_PATH2": file2,
 	}
 
 	RunCmdAndVerifySuccessWithFile(t, Lakectl()+" repo create lakefs://"+vars["REPO"]+" "+vars["STORAGE"], false, "lakectl_repo_create", vars)
@@ -759,6 +761,7 @@ func TestLakectlFsRm_protectedBranch(t *testing.T) {
 	runCmd(t, Lakectl()+" commit lakefs://"+vars["REPO"]+"/"+vars["BRANCH"]+" --allow-empty-message -m \" \"", false, false, vars)
 	runCmd(t, Lakectl()+" branch-protect add lakefs://"+vars["REPO"]+"/  '*'", false, false, vars)
 	RunCmdAndVerifyContainsText(t, Lakectl()+" branch-protect list lakefs://"+vars["REPO"]+"/ ", false, "*", vars)
+	RunCmdAndVerifyFailure(t, Lakectl()+" fs upload lakefs://"+vars["REPO"]+"/"+vars["BRANCH"]+"/"+vars["FILE_PATH2"]+" -s files/ro_1k", false, "cannot write to protected branch\n403 Forbidden\n", vars)
 	RunCmdAndVerifyFailure(t, Lakectl()+" fs rm lakefs://"+vars["REPO"]+"/"+vars["BRANCH"]+"/"+vars["FILE_PATH"], false, "cannot write to protected branch\n403 Forbidden\n", vars)
 }
 
