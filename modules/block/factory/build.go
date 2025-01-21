d7ced51e24a446b84cb4f4f6d37b2c3260911bd3
Nir Ozery <nir.ozery@treeverse.io>
2025-01-21 18:59:57 -0500

    Fix: Block factory config interface

--- modules/block/factory/build.go
+++ modules/block/factory/build.go
@@ -2,15 +2,15 @@ package factory
 
 import (
 	"context"
+	"github.com/treeverse/lakefs/pkg/config"
 
 	"github.com/treeverse/lakefs/pkg/block"
 	"github.com/treeverse/lakefs/pkg/block/factory"
-	"github.com/treeverse/lakefs/pkg/block/params"
 	"github.com/treeverse/lakefs/pkg/stats"
 )
 
-func BuildBlockAdapter(ctx context.Context, statsCollector stats.Collector, c params.AdapterConfig) (block.Adapter, error) {
-	adapter, err := factory.BuildBlockAdapter(ctx, statsCollector, c)
+func BuildBlockAdapter(ctx context.Context, statsCollector stats.Collector, c config.Config) (block.Adapter, error) {
+	adapter, err := factory.BuildBlockAdapter(ctx, statsCollector, c.GetBaseConfig())
 	if err != nil {
 		return nil, err
 	}
