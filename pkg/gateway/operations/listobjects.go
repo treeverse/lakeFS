22c17568fbdce06edba4e8b12acf229fc9438d04
Itamar Yuran <itamaryuran@gmail.com>
2025-01-23 17:03:43 +0200

    check

--- pkg/gateway/operations/listobjects.go
+++ pkg/gateway/operations/listobjects.go
@@ -410,6 +410,9 @@ func handleListMultipartUploads(w http.ResponseWriter, req *http.Request, o *Rep
 		StorageNamespace: o.Repository.StorageNamespace,
 		IdentifierType:   block.IdentifierTypeRelative,
 	})
+	for _, up := range mpuResp.Uploads {
+		fmt.Println(*up.UploadId)
+	}
 	if err != nil {
 		o.Log(req).WithError(err).Error("list multipart uploads failed")
 		_ = o.EncodeError(w, req, err, gatewayerrors.Codes.ToAPIErr(gatewayerrors.ErrInternalError))
