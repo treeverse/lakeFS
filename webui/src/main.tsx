b03c3935b0820d4353fa22d148e4e0dc10216225
Itai Gilo <itai.gilo@treeverse.io>
2025-02-03 13:48:16 +0200

    Add WebUI plugins infra

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 // styles
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/globals.css';
 
-// main page
-import {IndexPage} from './pages';
+// app and plugins system
+import LakeFSApp from "./extendable/lakefsApp";
+import { PluginManager } from "./extendable/plugins/pluginManager";
+
+const pluginManager = new PluginManager();
 
 const container = document.getElementById('root');
 if (!container) throw new Error("Failed to find root element!");
 
 const root = createRoot(container);
-root.render(<IndexPage />);
+root.render(<LakeFSApp pluginManager={pluginManager} />);
