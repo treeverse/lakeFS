bd38527dd3ef16e40f8b4083bd0f25afa63e9216
nadavsteindler <nadavsteindler@gmail.com>
2025-02-09 17:28:48 +0200

    revert mem adapter change

--- pkg/block/mem/adapter.go
+++ pkg/block/mem/adapter.go
@@ -74,11 +74,7 @@ func getKey(obj block.ObjectPointer) string {
 	if obj.IdentifierType == block.IdentifierTypeFull {
 		return obj.Identifier
 	}
-	if obj.StorageID == "" {
-		return fmt.Sprintf("%s:%s", obj.StorageNamespace, obj.Identifier)
-	} else {
-		return fmt.Sprintf("%s:%s:%s", obj.StorageID, obj.StorageNamespace, obj.Identifier)
-	}
+	return fmt.Sprintf("%s:%s", obj.StorageNamespace, obj.Identifier)
 }
 
 func (a *Adapter) Put(_ context.Context, obj block.ObjectPointer, _ int64, reader io.Reader, opts block.PutOpts) (*block.PutResponse, error) {
