bb26f64563d55ae232a48d653b314be889f9ca72
Barak Amar <barak.amar@treeverse.io>
2025-02-16 11:28:01 +0200

    Update test config to reflect the new generated code

--- clients/python/test/test_config.py
+++ clients/python/test/test_config.py
@@ -53,11 +53,7 @@ class TestConfig(unittest.TestCase):
                     import_validity_regex = '', 
                     pre_sign_multipart_upload = True, 
                     blockstore_id = '', 
-                    blockstore_description = '', 
-                    backward_compatible = True, 
-                    blockstore_extras = {
-                        'key' : ''
-                        }, ),
+                    blockstore_description = '', ),
                 storage_config_list = [
                     lakefs_sdk.models.storage_config.StorageConfig(
                         blockstore_type = '', 
