e6d44992a59ebd3c33e8cf5d2383e3f17c79e3ac
Annaseli <anna.seliverstov@treeverse.io>
2025-02-16 19:40:39 +0200

    Removed spaces so it will be formatted as in the master branch 3

--- webui/src/lib/components/auth/forms.jsx
+++ webui/src/lib/components/auth/forms.jsx
@@ -11,21 +11,21 @@ import {Checkbox, DataTable, DebouncedFormControl, AlertError, Loading} from "..
 
 
 export const AttachModal = ({
-                                show, searchFn, resolveEntityFn = (ent => ent.id), onAttach, onHide, addText = "Add",
-                                emptyState = 'No matches', modalTitle = 'Add', headers = ['Select', 'ID'],
-                                filterPlaceholder = 'Filter...'
+                              show, searchFn, resolveEntityFn = (ent => ent.id), onAttach, onHide, addText = "Add",
+                              emptyState = 'No matches', modalTitle = 'Add', headers = ['Select', 'ID'],
+                              filterPlaceholder = 'Filter...'
                             }) => {
   const search = useRef(null);
   const [searchPrefix, setSearchPrefix] = useState("");
   const [selected, setSelected] = useState([]);
 
   useEffect(() => {
-      if (!!search.current && search.current.value === "")
-          search.current.focus();
+    if (!!search.current && search.current.value === "")
+      search.current.focus();
   });
 
   const {response, error, loading} = useAPI(() => {
-      return searchFn(searchPrefix);
+    return searchFn(searchPrefix);
   }, [searchPrefix]);
 
   let content;
@@ -34,83 +34,83 @@ export const AttachModal = ({
   else content = (
       <>
         <DataTable
-            headers={headers}
-            keyFn={ent => ent.id}
-            emptyState={emptyState}
-            results={response}
-            rowFn={ent => [
-                <Checkbox
-                    defaultChecked={selected.some(selectedEnt => selectedEnt.id === ent.id)}
-                    onAdd={() => setSelected([...selected, ent])}
-                    onRemove={() => setSelected(selected.filter(selectedEnt => selectedEnt.id !== ent.id))}
-                    name={'selected'}/>,
-                <strong>{resolveEntityFn(ent)}</strong>
-            ]}/>
+          headers={headers}
+          keyFn={ent => ent.id}
+          emptyState={emptyState}
+          results={response}
+          rowFn={ent => [
+            <Checkbox
+              defaultChecked={selected.some(selectedEnt => selectedEnt.id === ent.id)}
+              onAdd={() => setSelected([...selected, ent])}
+              onRemove={() => setSelected(selected.filter(selectedEnt => selectedEnt.id !== ent.id))}
+              name={'selected'}/>,
+            <strong>{resolveEntityFn(ent)}</strong>
+        ]}/>
 
         <div className="mt-3">
-            {(selected.length > 0) &&
-                <p>
-                    <strong>Selected: </strong>
-                    {(selected.map(item => (
-                        <Badge
-                            key={item.id}
-                            pill
-                            variant="primary"
-                            className="
-                                me-1
-                                d-inline-block
-                                w-25
-                                text-nowrap
-                                overflow-hidden
-                                text-truncate
-                                align-middle
-                            "
-                            title={resolveEntityFn(item)}
-                        >
-                        {resolveEntityFn(item)}
-                        </Badge>
-                    )))}
-                </p>
-            }
+        {(selected.length > 0) &&
+          <p>
+            <strong>Selected: </strong>
+            {(selected.map(item => (
+              <Badge
+                key={item.id}
+                pill
+                variant="primary"
+                className="
+                    me-1
+                    d-inline-block
+                    w-25
+                    text-nowrap
+                    overflow-hidden
+                    text-truncate
+                    align-middle
+                "
+                title={resolveEntityFn(item)}
+              >
+                {resolveEntityFn(item)}
+              </Badge>
+            )))}
+          </p>
+        }
         </div>
-        </>
+      </>
     );
 
-    return (
-      <Modal show={show} onHide={onHide}>
-        <Modal.Header closeButton>
-          <Modal.Title>{modalTitle}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={e => {
-            e.preventDefault()
-          }}>
-            <InputGroup>
-                <InputGroup.Text>
-                    <SearchIcon/>
-                </InputGroup.Text>
-                <DebouncedFormControl
-                    ref={search}
-                    placeholder={filterPlaceholder}
-                    onChange={() => {
-                        setSearchPrefix(search.current.value)
-                    }}/>
-            </InputGroup>
-          </Form>
-          <div className="mt-2">
-            {content}
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="success" disabled={selected.length === 0} onClick={() => {
-            onAttach(selected)
-          }}>
-            {addText}
-          </Button>
-          <Button variant="secondary" onClick={onHide}>Cancel</Button>
-        </Modal.Footer>
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{modalTitle}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={e => {
+          e.preventDefault()
+        }}>
+          <InputGroup>
+            <InputGroup.Text>
+              <SearchIcon/>
+            </InputGroup.Text>
+            <DebouncedFormControl
+              ref={search}
+              placeholder={filterPlaceholder}
+              onChange={() => {
+                setSearchPrefix(search.current.value)
+              }}/>
+          </InputGroup>
+        </Form>
+        <div className="mt-2">
+          {content}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="success" disabled={selected.length === 0} onClick={() => {
+          onAttach(selected)
+        }}>
+          {addText}
+        </Button>
+        <Button variant="secondary" onClick={onHide}>Cancel</Button>
+      </Modal.Footer>
     </Modal>
-    );
+  );
 };
 
 export const EntityActionModal = ({
@@ -131,26 +131,26 @@ export const EntityActionModal = ({
 
   useEffect(() => {
     if (!!idField.current && idField.current.value === "") {
-        idField.current.focus();
+      idField.current.focus();
     }
   });
 
   const onSubmit = () => {
     if (validationFunction) {
-        const validationResult = validationFunction(idField.current.value);
+      const validationResult = validationFunction(idField.current.value);
+      if (!validationResult.isValid) {
+        setError(validationResult.errorMessage);
+        return;
+      }
+    }
+    if (showExtraField) {
+      if (extraValidationFunction) {
+        const validationResult = extraValidationFunction(extraField.current.value);
         if (!validationResult.isValid) {
           setError(validationResult.errorMessage);
           return;
         }
-    }
-    if (showExtraField) {
-        if (extraValidationFunction) {
-            const validationResult = extraValidationFunction(extraField.current.value);
-            if (!validationResult.isValid) {
-              setError(validationResult.errorMessage);
-              return;
-            }
-        }
+     }
       onAction(idField.current.value, extraField.current.value).catch(err => setError(err));
     } else {
       onAction(idField.current.value).catch(err => setError(err));
@@ -163,20 +163,20 @@ export const EntityActionModal = ({
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
 
-    <Modal.Body>
-      <Form onSubmit={e => {
-        e.preventDefault()
-        onSubmit()
-      }}>
-        <FormControl ref={idField} autoFocus placeholder={placeholder} type="text"/>
-        {showExtraField &&
-          <FormControl ref={extraField} placeholder={extraPlaceholder} type="text" className="mt-3"/>
-        }
-    </Form>
+      <Modal.Body>
+        <Form onSubmit={e => {
+          e.preventDefault()
+          onSubmit()
+        }}>
+          <FormControl ref={idField} autoFocus placeholder={placeholder} type="text"/>
+          {showExtraField &&
+            <FormControl ref={extraField} placeholder={extraPlaceholder} type="text" className="mt-3"/>
+          }
+      </Form>
 
-      {(!!error) && <AlertError className="mt-3" error={error}/>}
+        {(!!error) && <AlertError className="mt-3" error={error}/>}
 
-    </Modal.Body>
+      </Modal.Body>
 
       <Modal.Footer>
         <Button onClick={onSubmit} variant="success">{actionName}</Button>
