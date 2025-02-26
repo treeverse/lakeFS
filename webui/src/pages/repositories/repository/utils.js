bd63ddd9b820a128d01ce5801bba518b2a88185b
Barak Amar <barak.amar@treeverse.io>
2025-02-26 17:17:04 +0000

    Adjust WebUI config handling (#8715)

--- webui/src/pages/repositories/repository/utils.js
+++ webui/src/pages/repositories/repository/utils.js
@@ -7,24 +7,16 @@ function getRepoStorageConfig(configs, repo) {
     if (storageID) {
         // find the storage config that matches the repo
         const storageConfig = configs.find(c => c['blockstore_id'] === storageID);
-        if (storageConfig) {
-            return {storageConfig, error: null};
-        } else {
-            return {storageConfig: null, error: new Error('No storage config found for repo')};
+        if (!storageConfig) {
+            return {storageConfig: null, error: new Error('No storage config found for repo. StorageID: ' + storageID)};
         }
+        return {storageConfig, error: null};
     } else {
-        if (configs.length === 1) {
-            // single blockstore config
-            return {storageConfig: configs[0], error: null};
-        } else {
-            // fallback to backward compatible config
-            const storageConfig = configs.find(c => c['backward_compatible']);
-            if (storageConfig) {
-                return {storageConfig, error: null};
-            } else {
-                return {storageConfig: null, error: new Error('No backward compatible storage config found')};
-            }
+        if (configs.length > 1) {
+            return {storageConfig: null, error: new Error('Repo with no StorageID, cannot match storage config')};
         }
+        // single blockstore config
+        return {storageConfig: configs[0], error: null};
     }
 }
 
