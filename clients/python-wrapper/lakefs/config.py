40e08d3dc1605f24548a47a353fc83067ab8cb63
Itamar Yuran <itamaryuran@gmail.com>
2025-02-27 18:27:31 +0200

    starters

--- clients/python-wrapper/lakefs/config.py
+++ clients/python-wrapper/lakefs/config.py
@@ -17,7 +17,7 @@ _LAKECTL_YAML_PATH = os.path.join(Path.home(), ".lakectl.yaml")
 _LAKECTL_ENDPOINT_ENV = "LAKECTL_SERVER_ENDPOINT_URL"
 _LAKECTL_ACCESS_KEY_ID_ENV = "LAKECTL_CREDENTIALS_ACCESS_KEY_ID"
 _LAKECTL_SECRET_ACCESS_KEY_ENV = "LAKECTL_CREDENTIALS_SECRET_ACCESS_KEY"
-
+_LAKECTL_CREDENTIALS_ACCESS_TOKEN = "LAKECTL_CREDENTIALS_ACCESS_TOKEN"
 
 class ClientConfig(Configuration):
     """
@@ -75,13 +75,19 @@ class ClientConfig(Configuration):
         endpoint_env = os.getenv(_LAKECTL_ENDPOINT_ENV)
         key_env = os.getenv(_LAKECTL_ACCESS_KEY_ID_ENV)
         secret_env = os.getenv(_LAKECTL_SECRET_ACCESS_KEY_ENV)
+        access_token_env = os.getenv(_LAKECTL_CREDENTIALS_ACCESS_TOKEN)
+
 
         self.host = endpoint_env if endpoint_env is not None else self.server.endpoint_url
         self.username = key_env if key_env is not None else self.credentials.access_key_id
         self.password = secret_env if secret_env is not None else self.credentials.secret_access_key
         if len(self.username) > 0 and len(self.password) > 0:
             found = True
-
-        # TODO: authentication via IAM Role
+        print("access ",access_token_env)
+        print("endpint ",self.host)
+        self.access_token = access_token_env if access_token_env is not None  else  ""  
+        if len(self.access_token) > 0:
+            found = True
+            
         if not found:
             raise NoAuthenticationFound
