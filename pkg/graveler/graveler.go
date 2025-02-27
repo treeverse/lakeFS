85b91390ad51b11cfd9dc0a0539f03120ee68e71
Barak Amar <barak.amar@treeverse.io>
2025-02-27 14:25:44 +0200

    add storageID to gc manager (#8684)
    * add storageID to gc manager
    
    * gen mock
    
    * fix test
    
    * fix test
    
    * fix test
    
    * more storageIDs
    
    * gen mocks
    
    * moar tests
    
    * test failure

--- pkg/graveler/graveler.go
+++ pkg/graveler/graveler.go
@@ -1640,7 +1640,7 @@ func (g *Graveler) GetStagingToken(ctx context.Context, repository *RepositoryRe
 }
 
 func (g *Graveler) getGarbageCollectionRules(ctx context.Context, repository *RepositoryRecord) (*GarbageCollectionRules, error) {
-	return g.garbageCollectionManager.GetRules(ctx, repository.StorageNamespace)
+	return g.garbageCollectionManager.GetRules(ctx, repository.StorageID, repository.StorageNamespace)
 }
 
 func (g *Graveler) GetGarbageCollectionRules(ctx context.Context, repository *RepositoryRecord) (*GarbageCollectionRules, error) {
@@ -1648,7 +1648,7 @@ func (g *Graveler) GetGarbageCollectionRules(ctx context.Context, repository *Re
 }
 
 func (g *Graveler) SetGarbageCollectionRules(ctx context.Context, repository *RepositoryRecord, rules *GarbageCollectionRules) error {
-	return g.garbageCollectionManager.SaveRules(ctx, repository.StorageNamespace, rules)
+	return g.garbageCollectionManager.SaveRules(ctx, repository.StorageID, repository.StorageNamespace, rules)
 }
 
 func (g *Graveler) SaveGarbageCollectionCommits(ctx context.Context, repository *RepositoryRecord) (*GarbageCollectionRunMetadata, error) {
@@ -1661,11 +1661,11 @@ func (g *Graveler) SaveGarbageCollectionCommits(ctx context.Context, repository 
 	if err != nil {
 		return nil, fmt.Errorf("save garbage collection commits: %w", err)
 	}
-	commitsLocation, err := g.garbageCollectionManager.GetCommitsCSVLocation(runID, repository.StorageNamespace)
+	commitsLocation, err := g.garbageCollectionManager.GetCommitsCSVLocation(runID, repository.StorageID, repository.StorageNamespace)
 	if err != nil {
 		return nil, err
 	}
-	addressLocation, err := g.garbageCollectionManager.GetAddressesLocation(repository.StorageNamespace)
+	addressLocation, err := g.garbageCollectionManager.GetAddressesLocation(repository.StorageID, repository.StorageNamespace)
 	if err != nil {
 		return nil, err
 	}
@@ -1678,7 +1678,7 @@ func (g *Graveler) SaveGarbageCollectionCommits(ctx context.Context, repository 
 }
 
 func (g *Graveler) GCGetUncommittedLocation(repository *RepositoryRecord, runID string) (string, error) {
-	return g.garbageCollectionManager.GetUncommittedLocation(runID, repository.StorageNamespace)
+	return g.garbageCollectionManager.GetUncommittedLocation(runID, repository.StorageID, repository.StorageNamespace)
 }
 
 func (g *Graveler) GCNewRunID() string {
@@ -3725,14 +3725,14 @@ func (c *commitValueIterator) Close() {
 }
 
 type GarbageCollectionManager interface {
-	GetRules(ctx context.Context, storageNamespace StorageNamespace) (*GarbageCollectionRules, error)
-	SaveRules(ctx context.Context, storageNamespace StorageNamespace, rules *GarbageCollectionRules) error
+	GetRules(ctx context.Context, storageID StorageID, storageNamespace StorageNamespace) (*GarbageCollectionRules, error)
+	SaveRules(ctx context.Context, storageID StorageID, storageNamespace StorageNamespace, rules *GarbageCollectionRules) error
 
 	SaveGarbageCollectionCommits(ctx context.Context, repository *RepositoryRecord, rules *GarbageCollectionRules) (string, error)
-	GetCommitsCSVLocation(runID string, sn StorageNamespace) (string, error)
+	GetCommitsCSVLocation(runID string, storageID StorageID, sn StorageNamespace) (string, error)
 	SaveGarbageCollectionUncommitted(ctx context.Context, repository *RepositoryRecord, filename, runID string) error
-	GetUncommittedLocation(runID string, sn StorageNamespace) (string, error)
-	GetAddressesLocation(sn StorageNamespace) (string, error)
+	GetUncommittedLocation(runID string, storageID StorageID, sn StorageNamespace) (string, error)
+	GetAddressesLocation(storageID StorageID, sn StorageNamespace) (string, error)
 	NewID() string
 }
 
