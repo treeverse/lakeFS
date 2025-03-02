a4cac4b090412bd22d82c02da994a4c7140d8325
Itamar Yuran <itamaryuran@gmail.com>
2025-03-02 18:22:40 +0200

    maybe now

--- clients/python-wrapper/lakefs/client.py
+++ clients/python-wrapper/lakefs/client.py
@@ -18,7 +18,6 @@ import lakefs_sdk
 from lakefs_sdk import ExternalLoginInformation
 from lakefs_sdk.client import LakeFSClient
 
-from botocore.exceptions import NoCredentialsError
 from lakefs.config import ClientConfig
 from lakefs.exceptions import NotAuthorizedException, ServerException, NoAuthenticationFound, api_exception_handler
 from lakefs.models import ServerStorageConfiguration
