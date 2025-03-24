4cc164f7f072c83cf891cf6becba49fd8bbc172d
Isan Rivkin <isanrivkin@gmail.com>
2025-03-24 14:03:25 +0200

    update auth factory

--- modules/auth/factory/build.go
+++ modules/auth/factory/build.go
@@ -3,13 +3,13 @@ package factory
 import (
 	"context"
 
-	authhelper "github.com/treeverse/lakefs/modules/auth/helper"
 	"github.com/treeverse/lakefs/pkg/auth"
+	authfactoryhelper "github.com/treeverse/lakefs/pkg/auth/factory"
 	"github.com/treeverse/lakefs/pkg/config"
 	"github.com/treeverse/lakefs/pkg/kv"
 	"github.com/treeverse/lakefs/pkg/logging"
 )
 
 func NewAuthService(ctx context.Context, cfg config.Config, logger logging.Logger, kvStore kv.Store, metadataManager *auth.KVMetadataManager) auth.Service {
-	return authhelper.NewAuthService(ctx, cfg, logger, kvStore, metadataManager)
+	return authfactoryhelper.NewAuthService(ctx, cfg, logger, kvStore, metadataManager)
 }
