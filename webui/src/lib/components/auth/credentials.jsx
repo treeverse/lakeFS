a205cddb5d76a20e555a30bceade08d23e3c2485
Barak Amar <barak.amar@treeverse.io>
2025-03-26 07:44:22 +0200

    fix code style

--- webui/src/lib/components/auth/credentials.jsx
+++ webui/src/lib/components/auth/credentials.jsx
@@ -1,18 +1,18 @@
-import React, { useContext, useState } from "react";
+import React, {useContext, useState} from "react";
 
 import Modal from "react-bootstrap/Modal";
 import Table from "react-bootstrap/Table";
 import Alert from "react-bootstrap/Alert";
-import { DownloadIcon } from "@primer/octicons-react";
+import {DownloadIcon} from "@primer/octicons-react";
 
-import { auth } from "../../api";
-import { useAPIWithPagination } from "../../hooks/api";
-import { ClipboardButton, DataTable, AlertError, FormattedDate, Loading } from "../controls";
-import { ConfirmationButton } from "../modals";
-import { Paginator } from "../pagination";
-import { AppContext } from "../../hooks/appContext";
+import {auth} from "../../api";
+import {useAPIWithPagination} from "../../hooks/api";
+import {ClipboardButton, DataTable, AlertError, FormattedDate, Loading} from "../controls";
+import {ConfirmationButton} from "../modals";
+import {Paginator} from "../pagination";
+import {AppContext} from "../../hooks/appContext";
 
-export const DownloadCredentialsButton = ({ accessKeyId, secretAccessKey, apiEndpoint = '/api/v1' }) => {
+export const DownloadCredentialsButton = ({accessKeyId, secretAccessKey, apiEndpoint = '/api/v1'}) => {
     const downloadContent = 'data:application/octet-stream,' + encodeURIComponent(`# lakectl command line configuration - save under the filename $HOME/.lakectl.yaml
 credentials:
     access_key_id: ${accessKeyId}
@@ -23,25 +23,25 @@ server:
 
     return (
         <a className="btn p-2 pl-3 pr-3"
-            style={{ backgroundColor: '#808080', color: 'white' }}
-            href={downloadContent}
-            target="_blank" rel="noreferrer" download="lakectl.yaml">
-            <DownloadIcon /> Download Credentials
+           style={{backgroundColor: '#808080', color: 'white'}}
+           href={downloadContent}
+           target="_blank" rel="noreferrer" download="lakectl.yaml">
+            <DownloadIcon/> Download Credentials
         </a>
     );
 };
 
-export const CredentialsTable = ({ userId, currentAccessKey, refresh, after, onPaginate }) => {
+export const CredentialsTable = ({userId, currentAccessKey, refresh, after, onPaginate}) => {
     const [internalRefresh, setInternalRefresh] = useState(false);
     const [revokeError, setRevokeError] = useState(null);
 
-    const { results, error, loading, nextPage } = useAPIWithPagination(() => {
+    const {results, error, loading, nextPage} = useAPIWithPagination(() => {
         return auth.listCredentials(userId, after);
     }, [refresh, internalRefresh, userId, after]);
 
-    if (error) return <AlertError error={error} />;
-    if (revokeError) return <AlertError error={revokeError} />;
-    if (loading) return <Loading />;
+    if (error) return <AlertError error={error}/>;
+    if (revokeError) return <AlertError error={revokeError}/>;
+    if (loading) return <Loading/>;
 
     return (
         <>
@@ -55,13 +55,14 @@ export const CredentialsTable = ({ userId, currentAccessKey, refresh, after, onP
                         <code>{row.access_key_id}</code>
                         {(currentAccessKey === row.access_key_id) && <strong>{' '}(current)</strong>}
                     </>,
-                    <FormattedDate dateValue={row.creation_date} />,
+                    <FormattedDate dateValue={row.creation_date}/>,
                     <span className="row-hover">
                         {(currentAccessKey !== row.access_key_id) &&
                             <ConfirmationButton
                                 variant="outline-danger"
                                 size="sm"
-                                msg={<span>Are you sure you{'\''}d like to delete access key <code>{row.access_key_id}</code>?</span>}
+                                msg={
+                                    <span>Are you sure you{'\''}d like to delete access key <code>{row.access_key_id}</code>?</span>}
                                 onConfirm={() => {
                                     auth.deleteCredentials(userId, row.access_key_id)
                                         .catch(err => setRevokeError(err))
@@ -73,16 +74,16 @@ export const CredentialsTable = ({ userId, currentAccessKey, refresh, after, onP
                     </span>
                 ]}
             />
-            <Paginator onPaginate={onPaginate} after={after} nextPage={nextPage} />
+            <Paginator onPaginate={onPaginate} after={after} nextPage={nextPage}/>
         </>
     );
 };
 
 
-export const CredentialsShowModal = ({ credentials, show, onHide }) => {
+export const CredentialsShowModal = ({credentials, show, onHide}) => {
     if (!credentials) return <></>;
 
-    const { state } = useContext(AppContext);
+    const {state} = useContext(AppContext);
     const buttonVariant = state.settings.darkMode ? "outline-light" : "outline-dark";
 
     return (
@@ -94,20 +95,22 @@ export const CredentialsShowModal = ({ credentials, show, onHide }) => {
             <Modal.Body>
                 <Table>
                     <tbody>
-                        <tr>
-                            <td><strong>Access Key ID</strong></td>
-                            <td><code>{credentials.access_key_id}</code></td>
-                            <td>
-                                <ClipboardButton variant={buttonVariant} tooltip="Copy to clipboard" text={credentials.access_key_id} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><strong>Secret Access Key</strong></td>
-                            <td><code>{credentials.secret_access_key}</code></td>
-                            <td>
-                                <ClipboardButton variant={buttonVariant} tooltip="Copy to clipboard" text={credentials.secret_access_key} />
-                            </td>
-                        </tr>
+                    <tr>
+                        <td><strong>Access Key ID</strong></td>
+                        <td><code>{credentials.access_key_id}</code></td>
+                        <td>
+                            <ClipboardButton variant={buttonVariant} tooltip="Copy to clipboard"
+                                             text={credentials.access_key_id}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>Secret Access Key</strong></td>
+                        <td><code>{credentials.secret_access_key}</code></td>
+                        <td>
+                            <ClipboardButton variant={buttonVariant} tooltip="Copy to clipboard"
+                                             text={credentials.secret_access_key}/>
+                        </td>
+                    </tr>
                     </tbody>
                 </Table>
 
