23d99163d937757ccdbd7d05da79bdc04ff29c25
nadavsteindler <nadavsteindler@gmail.com>
2025-04-10 11:05:32 +0300

    review fixes

--- pkg/license/license.go
+++ pkg/license/license.go
@@ -15,6 +15,7 @@ type Manager interface {
 
 var errNotImplemented = errors.New("not implemented")
 
+// NopLicenseManager is a No-Operation implementation of license manager i.e. does not actually check license restrictions
 type NopLicenseManager struct {
 }
 
