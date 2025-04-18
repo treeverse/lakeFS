470374b541a6e1a90c51f24d5ffc5f5fe8ad0406
Oz Katz <oz.katz@treeverse.io>
2025-04-18 20:31:48 +0300

    execute module and explicit download

--- clients/python-wrapper/lakefs/__init__.py
+++ clients/python-wrapper/lakefs/__init__.py
@@ -18,8 +18,6 @@ from lakefs.branch import Branch
 from lakefs.object import StoredObject, WriteableObject, ObjectReader
 from lakefs.branch import LakeFSDeprecationWarning
 
-from lakefs.cmd import find_or_download_binary, run_binary
-
 
 def repository(repository_id: str, *args, **kwargs) -> Repository:
     """
@@ -30,19 +28,3 @@ def repository(repository_id: str, *args, **kwargs) -> Repository:
     :return: Repository object representing a lakeFS repository with the give repository_id
     """
     return Repository(repository_id, *args, **kwargs)
-
-
-def lakefs_cli() -> int:
-    '''
-    Wrapper around the lakefs cli that checks for the lakefs binary
-    in $PATH or ~/.lakefs/bin/lakefs, downloads it if not found, and executes it.
-    '''
-    return run_binary(find_or_download_binary('lakefs'))
-
-
-def lakectl_cli() -> int:
-    '''
-    Wrapper around the lakectl cli that checks for the lakectl binary
-    in $PATH or ~/.lakefs/bin/lakectl, downloads it if not found, and executes it.
-    '''
-    return run_binary(find_or_download_binary('lakectl'))
