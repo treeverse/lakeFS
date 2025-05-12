6e9701f567a065480b6b7e1a7054c0cef184a7d5
guyhardonag <guy.hardonag@treeverse.io>
2025-05-12 11:50:45 +0300

    Use MD5 as ETag in gs adapter

--- pkg/block/gs/adapter_test.go
+++ pkg/block/gs/adapter_test.go
@@ -1,11 +1,17 @@
 package gs_test
 
 import (
+	"bytes"
+	"context"
+	"crypto/md5"
+	"io"
 	"net/url"
 	"regexp"
 	"testing"
 
+	"encoding/hex"
 	"github.com/stretchr/testify/require"
+	"github.com/treeverse/lakefs/pkg/block"
 	"github.com/treeverse/lakefs/pkg/block/blocktest"
 	"github.com/treeverse/lakefs/pkg/block/gs"
 	"github.com/treeverse/lakefs/pkg/config"
@@ -76,4 +82,56 @@ func TestAdapterNamespace(t *testing.T) {
 			require.Equal(t, tt.Success, expr.MatchString(tt.Namespace))
 		})
 	}
+}
+func TestMultipartUploadWithMD5(t *testing.T) {
+	ctx := context.Background()
+	adapter := newAdapter()
+	defer require.NoError(t, adapter.Close())
+
+	content := []byte("test content for multipart upload")
+	obj := block.ObjectPointer{
+		StorageNamespace: "gs://" + bucketName,
+		Identifier:       "test-object",
+		IdentifierType:   block.IdentifierTypeRelative,
+	}
+
+	// Create multipart upload
+	createResp, err := adapter.CreateMultiPartUpload(ctx, obj, nil, block.CreateMultiPartUploadOpts{})
+	require.NoError(t, err)
+	uploadID := createResp.UploadID
+	// Upload first part
+	partResp1, err := adapter.UploadPart(ctx, obj, int64(len(content[:10])), bytes.NewReader(content[:10]), uploadID, 1)
+	require.NoError(t, err)
+	require.Equal(t, calculateMD5(content[:10]), partResp1.ETag)
+
+	// Upload second part
+	partResp2, err := adapter.UploadPart(ctx, obj, int64(len(content[10:])), bytes.NewReader(content[10:]), uploadID, 2)
+	require.NoError(t, err)
+	require.Equal(t, calculateMD5(content[10:]), partResp2.ETag)
+
+	// Complete multipart upload
+	completion := &block.MultipartUploadCompletion{
+		Part: []block.MultipartPart{
+			{PartNumber: 1, ETag: partResp1.ETag},
+			{PartNumber: 2, ETag: partResp2.ETag},
+		},
+	}
+	completeResp, err := adapter.CompleteMultiPartUpload(ctx, obj, uploadID, completion)
+	require.NoError(t, err)
+	require.NotEmpty(t, completeResp.ETag)
+
+	// Verify uploaded content
+	reader, err := adapter.Get(ctx, obj)
+	require.NoError(t, err)
+	defer reader.Close()
+
+	readContent, err := io.ReadAll(reader)
+	require.NoError(t, err)
+	require.Equal(t, content, readContent)
+}
+
+func calculateMD5(data []byte) string {
+	hash := md5.New()
+	hash.Write(data)
+	return hex.EncodeToString(hash.Sum(nil))
 }
