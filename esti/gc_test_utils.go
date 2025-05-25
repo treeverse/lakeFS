13737235153825a3f39b24fcb6663fa6142aafda
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-05-25 20:30:09 +0300

    Refactor Spark integration and simplify GC testing.
    Replaced Dockerized Spark setup with direct Spark binary integration for improved execution. Consolidated and updated `docker-compose` files for Spark and other services. Removed deprecated GC test workflow, shifting Spark-specific configurations to a common setup.

--- esti/gc_test_utils.go
+++ esti/gc_test_utils.go
@@ -2,11 +2,10 @@ package esti
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"os"
 	"os/exec"
-	"strings"
+	"path/filepath"
 
 	"github.com/treeverse/lakefs/pkg/api/apiutil"
 	"github.com/treeverse/lakefs/pkg/logging"
@@ -14,7 +13,7 @@ import (
 
 func getSparkSubmitArgs(entryPoint string) []string {
 	return []string{
-		"--master", "spark://localhost:7077",
+		"--master", "spark://spark-master:7077",
 		"--conf", "spark.driver.extraJavaOptions=-Divy.cache.dir=/tmp -Divy.home=/tmp",
 		"--conf", "spark.hadoop.lakefs.api.url=http://lakefs:8000" + apiutil.BaseURL,
 		"--conf", "spark.hadoop.lakefs.api.access_key=AKIAIOSFDNN7EXAMPLEQ",
@@ -23,81 +22,35 @@ func getSparkSubmitArgs(entryPoint string) []string {
 	}
 }
 
-func getDockerArgs(workingDirectory string, localJar string) []string {
-	return []string{
-		"run", "--network", "host", "--add-host", "lakefs:127.0.0.1",
-		"-v", fmt.Sprintf("%s/ivy:/opt/bitnami/spark/.ivy2", workingDirectory),
-		"-v", fmt.Sprintf("%s:/opt/metaclient/client.jar", localJar),
-		"--rm",
-		"-e", "AWS_ACCESS_KEY_ID",
-		"-e", "AWS_SECRET_ACCESS_KEY",
-	}
-}
+func runCommand(cmdName string, args []string) error {
+	cmd := exec.Command(
+		filepath.Join(os.Getenv("SPARK_HOME"), "bin", "spark-submit"),
+		args...,
+	)
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+	errs := make(chan error, 2)
 
-// handlePipe calls log on each line of pipe, and writes nil or an error to
-// ch when done.
-func handlePipe(pipe io.ReadCloser, log func(messages ...interface{}), ch chan<- error) {
-	reader := bufio.NewReader(pipe)
-	go func() {
-		var err error
-		for {
-			str, err := reader.ReadString('\n')
-			if err != nil {
-				break
-			}
-			log(strings.TrimSuffix(str, "\n"))
-		}
-		if err == io.EOF {
-			err = nil
-		}
-		ch <- err
-	}()
-}
+	go streamLog(stdout, cmdName, "out", errs)
+	go streamLog(stderr, cmdName, "err", errs)
 
-// runCommand runs cmd. It logs the outputs of cmd with an appropriate field
-// to distinguish stdout from stderr.
-func runCommand(cmdName string, cmd *exec.Cmd) error {
-	stdoutPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("failed to get stdout from command: %w", err)
-	}
-	stderrPipe, err := cmd.StderrPipe()
-	if err != nil {
-		return fmt.Errorf("failed to get stderr from command: %w", err)
-	}
-
-	const channelSize = 2
-	cmdErrs := make(chan error, channelSize)
-	handlePipe(stdoutPipe, logger.WithFields(logging.Fields{
-		"source": cmdName,
-		"std":    "out",
-	}).Info, cmdErrs)
-	handlePipe(stderrPipe, logger.WithFields(logging.Fields{
-		"source": cmdName,
-		"std":    "err",
-	}).Info, cmdErrs)
-
-	err = cmd.Start()
-	if err != nil {
+	if err := cmd.Start(); err != nil {
 		return err
 	}
-
-	err = <-cmdErrs
-	if err != nil {
-		logger.WithFields(logging.Fields{"source": cmdName, "component": "handlePipe"}).WithError(err).Error("Error reading command pipe")
-	}
-	err = <-cmdErrs
-	if err != nil {
-		logger.WithFields(logging.Fields{"source": cmdName, "component": "handlePipe"}).WithError(err).Error("Error reading command pipe")
-	}
+	<-errs
+	<-errs
 	return cmd.Wait()
 }
 
+func streamLog(r io.Reader, src, std string, ch chan<- error) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		logger.WithFields(logging.Fields{"source": src, "std": std}).Info(scanner.Text())
+	}
+	ch <- scanner.Err()
+}
+
 type SparkSubmitConfig struct {
-	SparkVersion string
-	// LocalJar is a local path to a jar that contains the main class.
-	LocalJar string
-	// EntryPoint is the class name to run
 	EntryPoint      string
 	ExtraSubmitArgs []string
 	ProgramArgs     []string
@@ -105,20 +58,9 @@ type SparkSubmitConfig struct {
 }
 
 func RunSparkSubmit(config *SparkSubmitConfig) error {
-	workingDirectory, err := os.Getwd()
-	if err != nil {
-		return fmt.Errorf("getting working directory: %w", err)
-	}
-	workingDirectory = strings.TrimSuffix(workingDirectory, "/")
-	dockerArgs := getDockerArgs(workingDirectory, config.LocalJar)
-	dockerArgs = append(dockerArgs, fmt.Sprintf("docker.io/bitnami/spark:%s", config.SparkVersion), "spark-submit")
-	sparkSubmitArgs := getSparkSubmitArgs(config.EntryPoint)
-	sparkSubmitArgs = append(sparkSubmitArgs, config.ExtraSubmitArgs...)
-	args := dockerArgs
-	args = append(args, sparkSubmitArgs...)
-	args = append(args, "/opt/metaclient/client.jar")
+	args := getSparkSubmitArgs(config.EntryPoint)
+	args = append(args, config.ExtraSubmitArgs...)
+	args = append(args, "/lakefs/test/spark/metaclient/spark-assembly.jar")
 	args = append(args, config.ProgramArgs...)
-	cmd := exec.Command("docker", args...)
-	logger.Infof("Running command: %s", cmd.String())
-	return runCommand(config.LogSource, cmd)
+	return runCommand(config.LogSource, args)
 }
