d2fd073c4a0eb45966ca572da1914a600c50d91c
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-06-10 12:19:04 +0300

    Set default marker icons for Leaflet in GeoJSON preview
    Configured Leaflet to use default marker icons and shadow images by importing assets and merging options. This ensures consistent marker display and fixes potential issues with missing icons in the GeoJSON preview component.

--- webui/src/lib/components/repository/GeoJSONPreview.jsx
+++ webui/src/lib/components/repository/GeoJSONPreview.jsx
@@ -3,6 +3,17 @@ import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon   from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl:       markerIcon,
+    shadowUrl:     markerShadow,
+});
+
 const parseGeoJSON = data => {
     try {
         return JSON.parse(data);
