3c4917be1be9a4495199a67efc7e180ccf458a6f
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-06-10 14:33:51 +0300

    Add leaflet-defaulticon-compatibility to simplify map icons
    Replaced manual marker icon configuration with the leaflet-defaulticon-compatibility package for better maintainability and cleaner code. Updated dependencies in package.json and adjusted GeoJSONPreview.jsx imports accordingly. This change improves code readability and ensures seamless icon handling in leaflet maps.

--- webui/src/lib/components/repository/GeoJSONPreview.jsx
+++ webui/src/lib/components/repository/GeoJSONPreview.jsx
@@ -2,17 +2,8 @@ import React from "react";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
-
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon   from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: markerIcon2x,
-    iconUrl:       markerIcon,
-    shadowUrl:     markerShadow,
-});
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
 
 const parseGeoJSON = data => {
     try {
