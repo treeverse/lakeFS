14e2501a7d9e998423948904c3cbf3e8d574a47e
Barak Amar <barak.amar@treeverse.io>
2025-06-12 09:09:37 +0300

    fix swagger ui yml location

--- docs/src/assets/js/swagger-ui-lakefs.js
+++ docs/src/assets/js/swagger-ui-lakefs.js
@@ -1,27 +1,28 @@
+window.onload = function () {
+  // Begin Swagger UI call region
+  const ui = SwaggerUIBundle({
+    url: "swagger.yml",
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    validatorUrl: null,
+    supportedSubmitMethods: [],
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [],
+    layout: "BaseLayout",
+    onComplete: () => {
+      const operationId = window.location.hash
+        .replace(/\//g, "-")
+        .replace("#", "");
+      const elem =
+        operationId && document.getElementById("operations" + operationId);
+      if (elem) {
+        setTimeout(function () {
+          elem.scrollIntoView();
+        }, 100);
+      }
+    },
+  });
+  // End Swagger UI call region
 
-window.onload = function() {
-    // Begin Swagger UI call region
-    const ui = SwaggerUIBundle({
-        url: "/assets/js/swagger.yml",
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        validatorUrl: null,
-        supportedSubmitMethods: [],
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIStandalonePreset
-        ],
-        plugins: [],
-        layout: "BaseLayout",
-        onComplete: () => {
-            const operationId = window.location.hash.replace(/\//g, '-').replace('#', '');
-            const elem = operationId && document.getElementById('operations' + operationId);
-            if (elem) {
-                setTimeout(function () { elem.scrollIntoView(); }, 100);
-            }
-        }
-    })
-    // End Swagger UI call region
-
-    window.ui = ui
-}
+  window.ui = ui;
+};
