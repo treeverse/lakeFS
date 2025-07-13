af0b5b61e69ebab07ffea1d395a34539e2639256
Barak Amar <barak.amar@treeverse.io>
2025-07-13 12:16:02 +0300

    WebUI: replace the theme toggle button implementation

--- webui/src/lib/components/darkModeToggle.tsx
+++ webui/src/lib/components/darkModeToggle.tsx
@@ -1,24 +1,32 @@
-import React, {FC, useContext} from "react";
-import {DarkModeSwitch} from "react-toggle-dark-mode";
-import {AppActionType, AppContext} from "../hooks/appContext";
+import React, { FC, useContext } from "react";
+import "@theme-toggles/react/css/Classic.css";
+import { Classic } from "@theme-toggles/react";
+import { AppActionType, AppContext } from "../hooks/appContext";
 
 const DarkModeToggle: FC = () => {
-    const {state, dispatch} = useContext(AppContext);
+    const { state, dispatch } = useContext(AppContext);
 
-    const toggleDarkMode = (isOn: boolean) => {
+    const toggleDarkMode = (toggled: boolean) => {
         dispatch({
             type: AppActionType.setDarkMode,
-            value: isOn,
+            value: toggled,
         });
     };
 
     return (
-        <DarkModeSwitch
-            style={{marginRight: '2rem'}}
-            checked={state.settings.darkMode}
-            onChange={toggleDarkMode}
-            size={28}
-            sunColor={"white"}
+        <Classic
+            toggled={state.settings.darkMode}
+            onToggle={toggleDarkMode}
+            duration={750}
+            placeholder=""
+            style={{
+                color: 'white',
+                fontSize: 'var(--spacing-xl)',
+                marginRight: 'var(--spacing-md)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
         />
     );
 };
