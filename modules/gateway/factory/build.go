9acbfae9afc14887ea4cc31b533e90498b4d0c30
Idan Novogroder <idan.novogroder@treeverse.io>
2025-08-06 13:01:01 +0300

    Fix comment

--- modules/gateway/factory/build.go
+++ modules/gateway/factory/build.go
@@ -11,13 +11,13 @@ import (
 // MiddlewareFactory creates a new middleware that will be applied to s3Gateway handler
 // the middleware will be applied right after the LoggingMiddleware,
 // so the order of the middlewares is (from innermost to outermost):
-// 1. LoggingMiddleware - logs request and response details
-// 2. --> THE CREATED GATEWAY MIDDLEWARE APPLIED HERE <--
-// 3. EnrichWithRepositoryOrFallback - resolves repository context
+// 1. EnrichWithOperation - populates operation context
+// 2. MetricsMiddleware - request metrics collection
+// 3. AuthenticationHandler - validates credentials and user
 // 4. EnrichWithParts - adds multipart upload context
-// 5. AuthenticationHandler - validates credentials and user
-// 6. MetricsMiddleware - request metrics collection
-// 7. EnrichWithOperation - populates operation context
+// 5. EnrichWithRepositoryOrFallback - resolves repository context
+// 6. --> THE CREATED GATEWAY MIDDLEWARE APPLIED HERE <--
+// 7. LoggingMiddleware - logs request and response details
 
 type MiddlewareFactory interface {
 	Build() func(http.Handler) http.Handler
