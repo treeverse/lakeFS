ba3751697136661984f2243968d8c59c00a1ae67
Itamar Yuran <itamaryuran@gmail.com>
2025-08-17 10:27:34 +0300

    caching

---
+++ pkg/authentication/externalidp/awsiam/token_caching.go
@@ -0,0 +1,86 @@
+package awsiam
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/treeverse/lakefs/pkg/api/apigen"
+)
+
+const (
+	fileName = ".lakectl_token_cache.json"
+)
+
+type TokenCache struct {
+	Token          string `json:"token"`
+	ExpirationTime int64  `json:"expiration_time"`
+}
+
+type JWTCache struct {
+	filePath string
+}
+
+func NewJWTCache(cacheDir string) (*JWTCache, error) {
+	if cacheDir == "" {
+		var err error
+		cacheDir, err = os.UserHomeDir()
+		if err != nil {
+			return nil, err
+		}
+	}
+	jwtCache := &JWTCache{
+		filePath: filepath.Join(cacheDir, fileName),
+	}
+	return jwtCache, nil
+}
+
+func (c *JWTCache) SaveToken(token *apigen.AuthenticationToken) error {
+	if token == nil || token.Token == "" || token.TokenExpiration == nil {
+		return nil
+	}
+	cache := &TokenCache{
+		Token:          token.Token,
+		ExpirationTime: *token.TokenExpiration,
+	}
+
+	tmpFile := c.filePath + ".tmp"
+	file, err := os.OpenFile(tmpFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	err = json.NewEncoder(file).Encode(cache)
+	if err != nil {
+		return err
+	}
+	return os.Rename(tmpFile, c.filePath)
+}
+
+func (c *JWTCache) LoadToken() (*apigen.AuthenticationToken, error) {
+	file, err := os.OpenFile(c.filePath, os.O_RDONLY, 0)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var cache TokenCache
+
+	err = json.NewDecoder(file).Decode(&cache)
+	if err != nil {
+		return nil, err
+	}
+
+	if cache.ExpirationTime > 0 && time.Now().Unix() >= cache.ExpirationTime {
+		return nil, nil
+	}
+	token := &apigen.AuthenticationToken{
+		Token:           cache.Token,
+		TokenExpiration: &cache.ExpirationTime}
+	return token, nil
+
+}
+
+func (c *JWTCache) ClearCache() error {
+	return os.Remove(c.filePath)
+}
