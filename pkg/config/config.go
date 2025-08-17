e2c84445af7e469c75c12b3c9fcb9932e0023894
Annaseli <anna.seliverstov@treeverse.io>
2025-08-17 16:01:58 +0300

    Renamed the LoginUrlMethod to LoginURLMethod in config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -664,7 +664,7 @@ type Auth struct {
 	UIConfig          struct {
 		RBAC                 string   `mapstructure:"rbac"`
 		LoginURL             string   `mapstructure:"login_url"`
-		LoginUrlMethod       string   `mapstructure:"login_url_method"`
+		LoginURLMethod       string   `mapstructure:"login_url_method"`
 		LoginFailedMessage   string   `mapstructure:"login_failed_message"`
 		FallbackLoginURL     *string  `mapstructure:"fallback_login_url"`
 		FallbackLoginLabel   *string  `mapstructure:"fallback_login_label"`
