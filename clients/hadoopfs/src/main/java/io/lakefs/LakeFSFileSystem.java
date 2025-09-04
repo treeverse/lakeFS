136ebab9c462775a8956d9bddb3def04a204a3e3
Barak Amar <barak.amar@treeverse.io>
2025-09-04 16:31:28 +0300

    delete extra space

--- clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
+++ clients/hadoopfs/src/main/java/io/lakefs/LakeFSFileSystem.java
@@ -83,7 +83,7 @@ public class LakeFSFileSystem extends FileSystem {
     public static final Logger LOG = LoggerFactory.getLogger(LakeFSFileSystem.class);
     public static final Logger OPERATIONS_LOG = LoggerFactory.getLogger(LakeFSFileSystem.class + "[OPERATION]");
     public static final String LAKEFS_DELETE_BULK_SIZE = "fs.lakefs.delete.bulk_size";
-    public static final String LAKEFS_DELETE_OBJECT_NO_TOMBSTONE = " fs.lakefs.delete.object.no.tombstone"; // Experimental feature to delete object with no-tombstone if possible
+    public static final String LAKEFS_DELETE_OBJECT_NO_TOMBSTONE = "fs.lakefs.delete.object.no.tombstone"; // Experimental feature to delete object with no-tombstone if possible
 
     private Configuration conf;
     private URI uri;
