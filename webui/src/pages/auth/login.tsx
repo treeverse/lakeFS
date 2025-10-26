5cd834e260331709ad401afddd0ee3d7493869a1
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-10-26 14:51:09 +0200

    Refactor `login.tsx` to remove unused imports and redundant `useAuth` destructuring for cleaner code.

--- webui/src/pages/auth/login.tsx
+++ webui/src/pages/auth/login.tsx
@@ -8,7 +8,7 @@ import {useRouter} from "../../lib/hooks/router";
 import {useAPI} from "../../lib/hooks/api";
 import {usePluginManager} from "../../extendable/plugins/pluginsContext";
 import {AUTH_STATUS, useAuth} from "../../lib/auth/authContext";
-import {Navigate, useLocation} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 
 interface SetupResponse {
     state: string;
@@ -120,7 +120,6 @@ const LoginPage = () => {
     const router = useRouter();
     const location = useLocation();
     const pluginManager = usePluginManager();
-    const { status } = useAuth();
     const { response, error, loading } = useAPI(() => setup.getState());
 
     if (loading) {
