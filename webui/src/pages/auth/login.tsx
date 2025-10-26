a61eb2129a6e1633316171c0468c1127eb3a49d7
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-10-26 14:25:29 +0200

    Refactor `login.tsx` to remove unused `useEffect` import.

--- webui/src/pages/auth/login.tsx
+++ webui/src/pages/auth/login.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
