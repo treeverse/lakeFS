461d9af52f1c1bd58cb79d7ad24a05f1ebed8592
Barak Amar <barak.amar@treeverse.io>
2025-10-27 17:10:47 +0100

    packaging: Change top-level `lakefs` module exports to be PEP484 compliant (#9585)
    Reviewed and checked the that python wrapper test passed. Force as contribution PRs still not run all required workflows.

--- clients/python-wrapper/lakefs/__init__.py
+++ clients/python-wrapper/lakefs/__init__.py
@@ -1,22 +1,29 @@
 """
 Allow importing of models from package root
 """
-from lakefs.client import Client
-from lakefs.repository import Repository, repositories
-from lakefs.reference import Reference
-from lakefs.models import (
-    Commit,
-    Change,
-    ImportStatus,
-    ServerStorageConfiguration,
-    ObjectInfo,
-    CommonPrefix,
-    RepositoryProperties
+from lakefs.client import Client as Client
+from lakefs.repository import (
+    Repository as Repository,
+    repositories as repositories
 )
-from lakefs.tag import Tag
-from lakefs.branch import Branch
-from lakefs.object import StoredObject, WriteableObject, ObjectReader
-from lakefs.branch import LakeFSDeprecationWarning
+from lakefs.reference import Reference as Reference
+from lakefs.models import (
+    Commit as Commit,
+    Change as Change,
+    ImportStatus as ImportStatus,
+    ServerStorageConfiguration as ServerStorageConfiguration,
+    ObjectInfo as ObjectInfo,
+    CommonPrefix as CommonPrefix,
+    RepositoryProperties as RepositoryProperties
+)
+from lakefs.tag import Tag as Tag
+from lakefs.branch import Branch as Branch
+from lakefs.object import (
+    StoredObject as StoredObject,
+    WriteableObject as WriteableObject,
+    ObjectReader as ObjectReader
+)
+from lakefs.branch import LakeFSDeprecationWarning as LakeFSDeprecationWarning
 
 
 def repository(repository_id: str, *args, **kwargs) -> Repository:
