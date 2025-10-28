b6b4ee895a45108bb21e15a9d710b6da06b0ca47
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-10-28 16:32:22 +0200

    Update `RequiresAuth` to display loading state when user authentication is in progress

--- webui/src/lib/components/requiresAuth.tsx
+++ webui/src/lib/components/requiresAuth.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { AUTH_STATUS, useAuth } from "../auth/authContext";
+import useUser from "../hooks/user";
+import {Loading} from "./controls";
 
 const RequiresAuth: React.FC = () => {
     const { status } = useAuth();
+    const { loading } = useUser();
     const location = useLocation();
 
     if (status === AUTH_STATUS.UNAUTHENTICATED) {
         const next = location.pathname + (location.search || "") + (location.hash || "");
         return <Navigate to="/auth/login" replace state={{ next, redirected: true }} />;
+    }
+
+    if (status !== AUTH_STATUS.AUTHENTICATED || loading) {
+        return <Loading />;
     }
 
     return <Outlet />;
