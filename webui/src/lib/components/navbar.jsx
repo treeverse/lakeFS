ecfc0ec959f0ceed57980dcbcba43b7cddec6dc6
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-10-29 11:12:47 +0200

    Fix inconsistent formatting in `onClick` handler within `navbar.jsx`.

--- webui/src/lib/components/navbar.jsx
+++ webui/src/lib/components/navbar.jsx
@@ -37,7 +37,7 @@ const NavUserInfo = () => {
                     </>
             </NavDropdown.Item><NavDropdown.Divider/></>}
             <NavDropdown.Item
-                onClick={() => {
+                onClick={()=> {
                     auth.clearCurrentUser();
                     window.location.replace(logoutUrl);
                 }}>
