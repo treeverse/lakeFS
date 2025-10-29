3fbc0d9549edd5291685bf916633f3040abaee8d
Ben-El_Musayov <ben-el.musayov@treeverse.io>
2025-10-29 20:15:36 +0200

    Remove unused `useEffect` import from `requiresAuth.tsx`.

--- webui/src/lib/components/requiresAuth.tsx
+++ webui/src/lib/components/requiresAuth.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Loading } from "./controls";
 import useUser from "../hooks/user";
