9c8e4ad381ac91a7e572305f613528b732d0a08c
Barak Amar <barak.amar@treeverse.io>
2025-10-30 10:41:37 +0100

    packaging: Move to pyproject.toml setup (PEP517) (#9563)
    * packaging: Move to pyproject.toml setup (PEP517)
    
    This is a more future-proof way to package the `lakefs` wrapper project.
    
    No functional changes intended, except that instead of excluding the tests, we now
    explicitly include the `lakectl` and `lakefs` packages instead, which means that the
    docs folder not being pasted directly into the site-packages directory anymore.
    
    This also enables lakefs to use PEP517 build backends such as `build`. In fact, this
    change was tested with the `python -m build . -w` command, which gives the expected results.
    
    * fix pyproject layout as per review comments
    
    * Restrict setuptools to >=77 for PEP639 support
    
    * refactor(pyproject): rewrite extras as PEP735 dependency groups
    
    * revert all and aws-iam to be extras
    
    * pyproject: Make version a dynamic attribute
    
    This makes the version easier to parse without installing the lakefs package in CI
    and in the Makefile docs build invocation.
    
    * actions: Remove version extraction from wrapper docs publishing job
    
    * refactor: Put version as static artifact into pyproject.toml
    
    For obtaining it in the Makefile, add a small private utility script in the lakefs
    source folder.
    
    Adds `tomli` as a dependency for Pythons earlier than 3.11 due to pyproject.toml parsing.
    
    * refactor: Move version parsing script out of `lakefs` package
    
    Instead, move it into the source directory, since it is only used for development
    purposes.
    
    * fix(packaging): Delete unused dynamic versioning code
    
    Also, make tomli an optional dev extra by putting it into the `requirements.txt` file.
    Since we only need tomli during development, it should not be in the package requirements.
    Instead, put it into the requirements file, which is an implicit prerequisite for the
    wrapper docs generation job in the Makefile.

---
+++ clients/python-wrapper/_version.py
@@ -0,0 +1,21 @@
+"""A utility to parse the lakefs version for the Makefile."""
+
+import sys
+from pathlib import Path
+
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
+
+
+def parse() -> str:
+    """Returns the lakefs version as a string directly from pyproject.toml."""
+    pyproject_path = Path(__file__).parent / "pyproject.toml"
+    with open(pyproject_path, "rb") as f:
+        metadata = tomllib.load(f)
+        return metadata["project"]["version"]
+
+
+if __name__ == "__main__":
+    print(parse())
