e8bcdc060026c678907bc5f2a0b940c422123c7a
Barak Amar <barak.amar@treeverse.io>
2025-11-03 17:25:58 +0200

    add missing file

---
+++ pkg/api/params/customviewer.go
@@ -0,0 +1,10 @@
+package params
+
+// CustomViewer defines a custom viewer configuration
+type CustomViewer struct {
+	Name         string
+	URL          string
+	Extensions   []string
+	ContentTypes []string
+}
+
