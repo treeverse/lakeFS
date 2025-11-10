7bca8f9ef6ebeab9086cd752d53bcfe5bdbc852e
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2025-11-10 08:05:16 +0200

    golangci-lint

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -954,7 +954,7 @@ func (c *Controller) ReleaseTokenToMailbox(w http.ResponseWriter, r *http.Reques
 			WithField("accept", r.Header.Get("Accept")).
 			Debug("Failed to get user - redirect to login")
 		redirectURL := url.URL{
-			Path: fmt.Sprintf("/auth/login"),
+			Path: "/auth/login",
 			// TODO(ariels): Use a relative URI?
 			RawQuery: fmt.Sprintf("next=%s", url.QueryEscape(r.URL.String())),
 		}
