b05f8f8d56b745dc0bee19b3e347d31dca60834a
Bridget McErlean <bridget.mcerlean@treeverse.io>
2025-11-17 14:28:06 -0500

    Fix UI showing wrong field removed when deleting metadata field (#9669)
    Fixes bug where deleting a metadata key-value pair appeared to remove
    the last field in the list instead of the one that had been selected.
    The issue was caused by using array indices as React keys, which caused
    DOM element reuse when fields were removed.
    
    This change extracts the `MetadataFields` component into its own file
    and changes the component to be controlled rather than uncontrolled by
    setting `value` instead of `defaultValue` in the inputs.
    
    Also remove `useCallback` from `onChangeKey`, `onChangeValue`, and
    `onRemovePair` as they return new closures on each render, making
    memoization redundant.

---
+++ webui/src/lib/components/repository/metadata.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import {PlusIcon, XIcon} from "@primer/octicons-react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+/**
+ * MetadataFields is a component that allows the user to add/remove key-value pairs of metadata.
+ * @param {Array<{key: string, value: string}>} metadataFields - current metadata fields to display
+ * @param {Function} setMetadataFields - callback to update the metadata fields
+ * @param rest - any other props to pass to the component
+ */
+export const MetadataFields = ({ metadataFields, setMetadataFields, ...rest}) => {
+    const onChangeKey = (i) => {
+        return e => {
+            const newKey = e.currentTarget.value;
+            setMetadataFields(prev => [...prev.slice(0,i), {...prev[i], key: newKey}, ...prev.slice(i+1)]);
+        };
+    };
+
+    const onChangeValue = (i) => {
+        return e => {
+            const newValue = e.currentTarget.value;
+            setMetadataFields(prev => [...prev.slice(0,i),  {...prev[i], value: newValue}, ...prev.slice(i+1)]);
+        };
+    };
+
+    const onRemoveKeyValue = (i) => {
+        return () => setMetadataFields(prev => [...prev.slice(0, i), ...prev.slice(i + 1)]);
+    };
+
+    const onAddKeyValue = () => {
+        setMetadataFields(prev => [...prev, {key: "", value: ""}])
+    };
+
+    return (
+        <div className="mt-3 mb-3" {...rest}>
+            {metadataFields.map((f, i) => {
+                return (
+                    <Form.Group key={`commit-metadata-field-${i}`} className="mb-3">
+                        <Row>
+                            <Col md={{span: 5}}>
+                                <Form.Control type="text" placeholder="Key" value={f.key} onChange={onChangeKey(i)}/>
+                            </Col>
+                            <Col md={{span: 5}}>
+                                <Form.Control type="text" placeholder="Value" value={f.value}  onChange={onChangeValue(i)}/>
+                            </Col>
+                            <Col md={{span: 1}}>
+                                <Form.Text>
+                                    <Button size="sm" variant="secondary" onClick={onRemoveKeyValue(i)}>
+                                        <XIcon/>
+                                    </Button>
+                                </Form.Text>
+                            </Col>
+                        </Row>
+                    </Form.Group>
+                )
+            })}
+            <Button onClick={onAddKeyValue} size="sm" variant="secondary">
+                <PlusIcon/>{' '}
+                Add Metadata field
+            </Button>
+        </div>
+    )
+}
