2779e5d22d000ea2a825025594b3f62df79fe20a
Barak Amar <barak.amar@treeverse.io>
2025-12-21 17:40:35 +0200

    Reduce memory consumption of prepare gc active commits (#9779)
    * Store CommitMap values in arena.Map
    
    This reduces a third of memory - from ~9+ to ~6+ GiB in one case.
    
    * Use arena.BoundedKeyMap CommitMap in prepare GC commits
    
    This saves ~40% memory when processing a huge repository.
    
    * Commit IDs are 32 bytes not 16
    
    Discovered during manually running GC.
    
    * [CR] Improve some comments
    
    Flagged by copilot
    
    * Fix active_commits_test
    
    It needs to use 256-bit commit IDs - this is now required by active_commits.
    
    * [CR] Rename ToBinaryCommitID -> MustToBinaryCommitID
    
    It _does_ panic, but this _is_ enforced by CI.  (Golang types are too weak
    to enforce correct commit IDs in the code during compilation.)

--- pkg/graveler/retention/active_commits.go
+++ pkg/graveler/retention/active_commits.go
@@ -2,31 +2,67 @@ package retention
 
 import (
 	"context"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"time"
 
+	"github.com/treeverse/lakefs/pkg/arena"
 	"github.com/treeverse/lakefs/pkg/graveler"
 	"github.com/treeverse/lakefs/pkg/logging"
 )
 
 type CommitNode struct {
-	CreationDate time.Time
-	MainParent   graveler.CommitID
-	MetaRangeID  graveler.MetaRangeID
+	// CreationUsecs is in microseconds-since-epoch.  It takes 3x less space than time.Time.
+	CreationUsecs int64
+	MainParent    graveler.CommitID
+	MetaRangeID   graveler.MetaRangeID
 }
 
-// CommitsMap is an immutable cache of commits.  Each commit can be set
-// once, and will be read if needed.  It is *not* thread-safe.
+// CommitsMap is an immutable cache of commits.  Each commit can be set once, and will be read
+// if needed.  It is *not* thread-safe.
 type CommitsMap struct {
 	ctx          context.Context
 	Log          logging.Logger
 	NumMisses    int64
 	CommitGetter RepositoryCommitGetter
-	Map          map[graveler.CommitID]CommitNode
+	Map          arena.Map[BinaryCommitID, CommitNode]
+}
+
+// BinaryCommitID holds the bytes of a commit ID as a string.  It has exactly
+// arena.KeySizeBound bytes.
+type BinaryCommitID string
+
+var (
+	ErrBadCommitID = errors.New("bad format commit ID")
+)
+
+func ToBinaryCommitID(hexID graveler.CommitID) (BinaryCommitID, error) {
+	b, err := hex.DecodeString(string(hexID))
+	if err != nil {
+		// This check should happen at compile time but the Go type system is far too
+		// weak for that.  It is safe here because **all commit IDs are hex** - if not,
+		// this code will always fail integration testing.  So "it cannot happen".
+		return "", fmt.Errorf("%w: decode hex %s: %s", ErrBadCommitID, hexID, err)
+	}
+	if len(b) < arena.KeySizeBound {
+		return "", fmt.Errorf("%w: %d too short", ErrBadCommitID, len(b))
+	}
+	return BinaryCommitID(b[:arena.KeySizeBound]), nil
+}
+
+func MustToBinaryCommitID(hexID graveler.CommitID) BinaryCommitID {
+	ret, err := ToBinaryCommitID(hexID)
+	if err != nil {
+		// See ToBinaryCommitID - this cannot happen in the system, and is caught by
+		// testing.
+		panic(err)
+	}
+	return ret
 }
 
 func NewCommitsMap(ctx context.Context, commitGetter RepositoryCommitGetter) (CommitsMap, error) {
-	initialMap := make(map[graveler.CommitID]CommitNode)
+	initialMap := arena.NewBoundedKeyMap[BinaryCommitID, CommitNode]()
 	it, err := commitGetter.List(ctx)
 	if err != nil {
 		return CommitsMap{}, fmt.Errorf("list existing commits into map: %w", err)
@@ -34,8 +70,9 @@ func NewCommitsMap(ctx context.Context, commitGetter RepositoryCommitGetter) (Co
 	defer it.Close()
 	for it.Next() {
 		commit := it.Value()
-		initialMap[commit.CommitID] = nodeFromCommit(commit.Commit)
+		initialMap.Put(MustToBinaryCommitID(commit.CommitID), nodeFromCommit(commit.Commit))
 	}
+	initialMap.Optimize()
 	return CommitsMap{
 		ctx:          ctx,
 		Log:          logging.FromContext(ctx),
@@ -47,35 +84,35 @@ func NewCommitsMap(ctx context.Context, commitGetter RepositoryCommitGetter) (Co
 
 // Set sets a commit.  It will not be looked up again in CommitGetter.
 func (c *CommitsMap) Set(id graveler.CommitID, node CommitNode) {
-	c.Map[id] = node
+	c.Map.Put(MustToBinaryCommitID(id), node)
 }
 
 // Get gets a commit.  If the commit has not been Set it uses CommitGetter
 // to read it.
-func (c *CommitsMap) Get(id graveler.CommitID) (CommitNode, error) {
-	ret, ok := c.Map[id]
-	if ok {
+func (c *CommitsMap) Get(id graveler.CommitID) (*CommitNode, error) {
+	ret := c.Map.Get(MustToBinaryCommitID(id))
+	if ret != nil {
 		return ret, nil
 	}
 	// Unlikely: id raced with initial bunch of Sets.
 	commit, err := c.CommitGetter.Get(c.ctx, id)
 	if err != nil {
-		return CommitNode{}, fmt.Errorf("get missing commit ID %s: %w", id, err)
+		return nil, fmt.Errorf("get missing commit ID %s: %w", id, err)
 	}
-	ret = nodeFromCommit(commit)
-	c.Map[id] = ret
+	ret = c.Map.Put(MustToBinaryCommitID(id), nodeFromCommit(commit))
 	c.NumMisses++
+	createdAt := time.UnixMicro(ret.CreationUsecs)
 	c.Log.WithFields(logging.Fields{
 		"commit_id": id,
-		"created":   ret.CreationDate,
-		"age":       time.Since(ret.CreationDate),
+		"created":   createdAt,
+		"age":       time.Since(createdAt),
 	}).Warn("Loaded single commit, probably new")
 	return ret, nil
 }
 
 // GetMap returns the entire map of commits.  It is probably incorrect to
 // modify it.
-func (c *CommitsMap) GetMap() map[graveler.CommitID]CommitNode {
+func (c *CommitsMap) GetMap() arena.Map[BinaryCommitID, CommitNode] {
 	return c.Map
 }
 
@@ -90,9 +127,9 @@ func nodeFromCommit(commit *graveler.Commit) CommitNode {
 		}
 	}
 	return CommitNode{
-		CreationDate: commit.CreationDate,
-		MainParent:   mainParent,
-		MetaRangeID:  commit.MetaRangeID,
+		CreationUsecs: commit.CreationDate.UnixMicro(),
+		MainParent:    mainParent,
+		MetaRangeID:   commit.MetaRangeID,
 	}
 }
 
@@ -127,9 +164,9 @@ func GetGarbageCollectionCommits(ctx context.Context, startingPointIterator *GCS
 		}
 		if startingPoint.BranchID == "" {
 			// If the current commit is NOT a branch HEAD (a dangling commit) - add a hypothetical HEAD as its child
-			commitNode = CommitNode{
-				CreationDate: commitNode.CreationDate,
-				MainParent:   startingPoint.CommitID,
+			commitNode = &CommitNode{
+				CreationUsecs: commitNode.CreationUsecs,
+				MainParent:    startingPoint.CommitID,
 			}
 		} else {
 			// If the current commit IS a branch HEAD - fetch and retention rules for this branch and...
@@ -154,7 +191,8 @@ func GetGarbageCollectionCommits(ctx context.Context, startingPointIterator *GCS
 				// wins
 				break
 			}
-			if commitNode.CreationDate.After(branchExpirationThreshold) {
+			createdAt := time.UnixMicro(commitNode.CreationUsecs)
+			if createdAt.After(branchExpirationThreshold) {
 				// If the current commit creation time is after the threshold, then its parent is active because the
 				// definition for 'active' is either creation time is after the threshold, or the first beyond
 				// the threshold. In either way, the PARENT is active.
@@ -176,10 +214,10 @@ func GetGarbageCollectionCommits(ctx context.Context, startingPointIterator *GCS
 	return makeCommitMap(commitsMap.GetMap(), activeMap), nil
 }
 
-func makeCommitMap(commitNodes map[graveler.CommitID]CommitNode, commitSet map[graveler.CommitID]struct{}) map[graveler.CommitID]graveler.MetaRangeID {
+func makeCommitMap(commitNodes arena.Map[BinaryCommitID, CommitNode], commitSet map[graveler.CommitID]struct{}) map[graveler.CommitID]graveler.MetaRangeID {
 	res := make(map[graveler.CommitID]graveler.MetaRangeID)
 	for commitID := range commitSet {
-		res[commitID] = commitNodes[commitID].MetaRangeID
+		res[commitID] = commitNodes.Get(MustToBinaryCommitID(commitID)).MetaRangeID
 	}
 	return res
 }
