7800bbc22f7e7293193158d719ba453011480c8f
Udi Ben Amitai <udi.benamitai@treeverse.io>
2026-01-18 17:32:49 +0200

    Update cmd/lakectl/cmd/auth_users_aws_iam.go
    Co-authored-by: isan_rivkin <isanrivkin@gmail.com>

--- cmd/lakectl/cmd/auth_users_aws_iam.go
+++ cmd/lakectl/cmd/auth_users_aws_iam.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 var authUsersAWSIAMCmd = &cobra.Command{
 	Use:   "aws-iam",
-	Short: "Manage AWS IAM",
+	Short: "Manage AWS IAM Role for lakeFS Users (External Principals API)",
 }
 
 //nolint:gochecknoinits
