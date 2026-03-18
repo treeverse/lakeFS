2d9402e9f2ed2ce43a5f272839af567d84775c1f
Itai Gilo <itai.gilo@treeverse.io>
2026-03-18 08:41:52 +0000

    Fix lint

--- webui/src/extendable/plugins/impls/DefaultTablesBrowserPlugin.tsx
+++ webui/src/extendable/plugins/impls/DefaultTablesBrowserPlugin.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { PluginTablesBrowser } from '../pluginTablesBrowser';
 
-const TablesPlaceholder: React.FC = () => (
-    <p>Tables browsing is an Enterprise feature.</p>
-);
+const TablesPlaceholder: React.FC = () => <p>Tables browsing is an Enterprise feature.</p>;
 
 class DefaultTablesBrowserPlugin implements PluginTablesBrowser {
     getTablesBrowserComponent(): React.FC {
