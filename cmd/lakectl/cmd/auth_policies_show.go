ed9e836cb0af6ddc5e4a069e43c62e4afa009c56
David Boublil <davdsb@gmail.com>
2026-03-24 12:44:07 +0200

    pretty print resource as array instead of a string

--- cmd/lakectl/cmd/auth_policies_show.go
+++ cmd/lakectl/cmd/auth_policies_show.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/spf13/cobra"
@@ -9,6 +10,29 @@ import (
 
 type StatementDoc struct {
 	Statement []apigen.Statement `json:"statement"`
+}
+
+// printStatement mirrors apigen.Statement but uses json.RawMessage for Resource,
+// so that a multi-resource value stored as a JSON array string is printed as an array.
+type printStatement struct {
+	Action    []string                    `json:"action"`
+	Condition *apigen.Statement_Condition `json:"condition,omitempty"`
+	Effect    string                      `json:"effect"`
+	Resource  json.RawMessage             `json:"resource"`
+}
+
+func toPrintStatement(s apigen.Statement) printStatement {
+	var arr []string
+	if err := json.Unmarshal([]byte(s.Resource), &arr); err == nil {
+		raw, _ := json.Marshal(arr)
+		return printStatement{Action: s.Action, Condition: s.Condition, Effect: s.Effect, Resource: raw}
+	}
+	raw, _ := json.Marshal(s.Resource)
+	return printStatement{Action: s.Action, Condition: s.Condition, Effect: s.Effect, Resource: raw}
+}
+
+type showStatementDoc struct {
+	Statement []printStatement `json:"statement"`
 }
 
 const policyDetailsTemplate = `
@@ -33,14 +57,18 @@ var authPoliciesShow = &cobra.Command{
 		}
 
 		policy := *resp.JSON200
+		stmts := make([]printStatement, len(policy.Statement))
+		for i, s := range policy.Statement {
+			stmts[i] = toPrintStatement(s)
+		}
 		Write(policyDetailsTemplate, struct {
 			ID           string
 			CreationDate int64
-			StatementDoc StatementDoc
+			StatementDoc showStatementDoc
 		}{
 			ID:           policy.Id,
 			CreationDate: *policy.CreationDate,
-			StatementDoc: StatementDoc{Statement: policy.Statement},
+			StatementDoc: showStatementDoc{Statement: stmts},
 		})
 	},
 }
