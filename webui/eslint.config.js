11bbdd4e6949c30199207fc3729f79e2f5169b87
Barak Amar <barak.amar@treeverse.io>
2026-04-16 15:00:40 +0300

    webui: add filename case lint rule and fix violations (#10363)

--- webui/eslint.config.js
+++ webui/eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import compat from 'eslint-plugin-compat';
+import checkFile from 'eslint-plugin-check-file';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import { includeIgnoreFile } from '@eslint/compat';
 import { fileURLToPath } from 'node:url';
@@ -12,74 +13,92 @@ import { fileURLToPath } from 'node:url';
 const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
-  {
-    name: 'ignore/defaults',
-    ignores: ['node_modules', 'dist', 'pub'],
-  },
-  {
-    name: 'ignore/gitignore',
-    ...includeIgnoreFile(gitignorePath),
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    name: 'app/js-ts-jsx-tsx',
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      react,
-      'react-hooks': reactHooks,
-      compat,
+    {
+        name: 'ignore/defaults',
+        ignores: ['node_modules', 'dist', 'pub'],
     },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        ...globals.browser,
-        process: 'readonly',
-      },
+    {
+        name: 'ignore/gitignore',
+        ...includeIgnoreFile(gitignorePath),
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        name: 'app/js-ts-jsx-tsx',
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            react,
+            'react-hooks': reactHooks,
+            compat,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+                process: 'readonly',
+            },
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        rules: {
+            'no-console': 'error',
+            eqeqeq: 'warn',
+            'react/prop-types': 0,
+            'react/jsx-key': 0,
+            'react/display-name': 0,
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'none',
+                },
+            ],
+            '@typescript-eslint/no-unused-expressions': [
+                'error',
+                {
+                    allowShortCircuit: true,
+                    allowTernary: true,
+                },
+            ],
+            'react-hooks/exhaustive-deps': [
+                'error',
+                {
+                    additionalHooks: '(useAPI|useAPIWithPagination)',
+                },
+            ],
+        },
     },
-    rules: {
-      'no-console': 'error',
-      'eqeqeq': 'warn',
-      'react/prop-types': 0,
-      'react/jsx-key': 0,
-      'react/display-name': 0,
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          caughtErrors: 'none',
+    {
+        name: 'app/filename-convention',
+        files: ['src/**/*.{js,jsx,ts,tsx}'],
+        plugins: {
+            'check-file': checkFile,
         },
-      ],
-      '@typescript-eslint/no-unused-expressions': [
-        'error',
-        {
-          allowShortCircuit: true,
-          allowTernary: true,
+        rules: {
+            'check-file/filename-naming-convention': [
+                'error',
+                {
+                    '**/*.{js,jsx,ts,tsx}': 'CAMEL_CASE',
+                },
+                {
+                    ignoreMiddleExtensions: true,
+                },
+            ],
         },
-      ],
-      'react-hooks/exhaustive-deps': [
-        'error',
-        {
-          additionalHooks: '(useAPI|useAPIWithPagination)',
-        },
-      ],
     },
-  },
-  {
-    name: 'prettier/config',
-    ...eslintConfigPrettier,
-  },
+    {
+        name: 'prettier/config',
+        ...eslintConfigPrettier,
+    },
 ]);
